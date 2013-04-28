bb1372055c0375e3c409cdd6080043d73b8bcf7b
lifelonglearner127 <life.long.learner127@outlook.com>
2013-04-28 03:24:39 +0300

    fix

--- application/views/layouts/main.tpl.php
+++ application/views/layouts/main.tpl.php
@@ -8,7 +8,7 @@
 					<a href="<?php echo base_url();?>" class="logo_title">TrillionMonkeys</a>
 				</div>
 				<div class="pull-right">
-					<a href="/auth/logout" class="log_out">Log Out</a>
+					<a href="<?php echo base_url();?>auth/logout" class="log_out">Log Out</a>
 				</div>
 			</div>
 		</div>
