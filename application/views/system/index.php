38057c81e4fa2ff1f689d1603e1b1dacc9cb6270
lifelonglearner127 <life.long.learner127@outlook.com>
2013-04-30 18:02:01 +0300

    Fixed deleted styles and broken system tab form

--- application/views/system/index.php
+++ application/views/system/index.php
@@ -1,3 +1,4 @@
+					<?php echo form_open("system/save", array("class"=>"form-horizontal", "id"=>"product_description"));?>
 					<h3>Original Descriptions:</h3>
 					<div class="row-fluid">
 						<div class="span6 admin_system_content">
@@ -7,7 +8,7 @@
 						<div class="span6 admin_system_content">
 							<p class="mt_40">Database:</p>
 							<input type="text" id="database" class="mt_40"/>
-						</div>						
+						</div>
 					</div>
 					<h3>Generator Paths:</h3>
 					<div class="row-fluid">
@@ -24,39 +25,47 @@
 							<div class="clear-fix"></div>
 							<p>Java input:</p>
 							<input type="text" id="java_input"/>
-						</div>						
+						</div>
 					</div>
 					<div class="row-fluid">
 						<h3>New Account Defaults:</h3>
-					    <form class="form-horizontal" id="product_description">
-						    <div class="control-group">
-							    <label class="control-label" for="account_title">Title:</label>
-							    <div class="controls">
-									<ul class="span12 product_title_content">
-										<li><span>Channels</span><a hef="#">x</a></li>
-										<li><span>Product Name</span><a hef="#">x</a></li>
-										<li><span>Type</span><a hef="#">x</a></li>
-										<li><span>Item</span><a hef="#">x</a></li>
-										<li><span>Specs</span><a hef="#">x</a></li>
-									</ul>
-							    </div>
+					    <!-- form class="form-horizontal" id="product_description"-->
+					    <div class="control-group">
+						    <label class="control-label" for="account_title">Title:</label>
+						    <div class="controls">
+								<ul class="span12 product_title_content">
+									<li><span>Channels</span><a hef="#">x</a></li>
+									<li><span>Product Name</span><a hef="#">x</a></li>
+									<li><span>Type</span><a hef="#">x</a></li>
+									<li><span>Item</span><a hef="#">x</a></li>
+									<li><span>Specs</span><a hef="#">x</a></li>
+								</ul>
 						    </div>
-						    <div class="control-group">
-							    <label class="control-label" for="default_title">Default Title:</label>
-							    <div class="controls">
-								    <input type="text" id="default_title" class="span2">
-									<p class="title_max">characters max</p>
+					    </div>
+						<div class="span6 ml_0">
+							    <div class="control-group">
+								    <label class="control-label" for="default_title">Default Title:</label>
+								    <div class="controls">
+									    <input type="text" id="default_title" class="span6">
+										<p class="title_max">characters max</p>
+								    </div>
 							    </div>
-						    </div>
-						    <div class="control-group">
-							    <label class="control-label" for="description_length">Description length:</label>
-							    <div class="controls">
-								    <input type="text" id="description_length" class="span2">
-									<p class="title_max">words max</p>
+							    <div class="control-group">
+								    <label class="control-label" for="description_length">Description length:</label>
+								    <div class="controls">
+									    <input type="text" id="description_length" class="span6">
+										<p class="title_max">words max</p>
+								    </div>
 							    </div>
-						    </div>
+						</div>
+															        					<div class="title_item_content">
+								<textarea placeholder="Title Item List"></textarea>
+							</div>
+
+					</div>
+
+					<div class="row-fluid">
 						<h3>Description Generators:</h3>
-				    	<?php echo form_open("system/save", array("class"=>"form-horizontal", "id"=>"system_configure"));?>
 					    <?php foreach ($this->config->item('generators') as $key => $generator) {?>
 				    		<div class="control-group">
 								<label class="control-label" for="java_generator"><?php echo $generator[0];?></label>
@@ -71,8 +80,5 @@
 								    <button type="submit" class="btn ml_20">Restore Default</button>
 							    </div>
 						    </div>
-					    </form>	
-						<div class="title_item_content">
-							<textarea placeholder="Title Item List"></textarea>
-						</div>					
 					</div>
+				    <?php echo form_close();?>
