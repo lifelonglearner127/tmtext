5ea2d1d1c53f03bd409a5143ebdd7297b40d5850
lifelonglearner127 <life.long.learner127@outlook.com>
2013-05-03 12:27:14 +0300

    Textarea replacement, minor fixes

--- application/controllers/admin_editor.php
+++ application/controllers/admin_editor.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Customer extends MY_Controller {
+class Admin_Editor extends MY_Controller {
 
 	function __construct()
  	{
