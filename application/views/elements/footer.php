9a04e427404345fcadc79670da639bf9c43f3b57
lifelonglearner127 <life.long.learner127@outlook.com>
2013-05-13 19:30:41 +0300

    fix

--- application/views/elements/footer.php
+++ application/views/elements/footer.php
@@ -1,7 +1,7 @@
 <div class="container">
 		<div class="row-fluid">
 			<p class="pull-left mb_40">
-				<small>Copyright &copy; 2013 <?php echo isset($settings['site_name'])? $settings['company_name']:'' ?>. All Rights Reserved. </small>
+				<small>Copyright &copy; 2013 <?php echo isset($settings['company_name'])? $settings['company_name']:'' ?>. All Rights Reserved. </small>
 			</p>
 			<p class="pull-right mb_40">
 				<small>Page rendered in <strong>{elapsed_time}</strong> seconds</small>
