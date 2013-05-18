a733803037ba8799617b6b42a50ee53c3045697d
lifelonglearner127 <life.long.learner127@outlook.com>
2013-05-18 21:15:27 +0300

    add description and styles to measure mockup

--- application/views/measure/index.php
+++ application/views/measure/index.php
@@ -27,29 +27,23 @@ $(document).ready(function () {
             <div id="websites_third" class="dropdowns"></div>
         </div>
         <div class="row-fluid mt_10">
-            <div class="span9"><b>Item description</b></div>
+            <div class="span9 item_title"><b>Item description</b></div>
         </div>
 	<div class="row-fluid">            
-            <div class="span9 search_area uneditable-input cursor_default" style="height:350px; overflow : auto;" id="items">                
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-                       Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. 
-                       Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
-                       Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. 
-                       Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. 
-                       Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, 
-                       sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. 
-                       Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. 
-                       Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
-                        <dl>
-                            <dt>Definition list</dt>
-                            <dd>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-                         aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
-                         commodo consequat.</dd>
-                            <dt>Lorem ipsum dolor sit amet</dt>
-                            <dd>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-                         aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
-                         commodo consequat.</dd>
-                         </dl>                
+           <div class="span9 search_area uneditable-input cursor_default item_section">
+                    <div class="item_section_content" >
+                        <div class="item_body">
+                            <div>
+                                <span class="ql-details-short-desc">
+                                Enter a world of dazzling picture quality and unlimited entertainment. This elegantly slim LED TV boasts Full HD 1080p for incredible detail and Edge LED backlighting for boosted contrast. Internet connectivity is at your fingertips, so you can watch YouTube clips and access online HD movies, music and more.<br><br><b>Note:</b> You must have a source of HD programming in order to take full advantage of the Sony 55" HDTV. Contact your local cable or satellite TV provider for details on how to upgrade.
+                                </span>
+                            </div>
+                            <div>
+                                <br><b>Sony 55" Class LED 1080p 60Hz HDTV, KDL-55EX640:</b><ul><li>55" LCD panel<br>With a 1920 x 1080 Full HD resolution</li><li>True 16:9 aspect ratio<br>View your movies as the director intended</li><li>Wide 178-degree vertical and 178-degree horizontal angles<br>See a clear picture from anywhere in the room</li><li>Built-in digital tuner<br>Watch digital broadcasts, including HDTV programs where available</li><li>HDMI Inputs: 4<br>Enjoy a superior HD experience with HDMI one cable solution</li><li>54.6" screen measured diagonally from corner to corner</li><li>WiFi adaptor included<br>Connect your HDTV to the Internet and stream videos</li><li>BRAVIA Sync<br>Sync up all Sony devices</li><li>Wall mountable<br>VESA standard 300mm x 300mm</li></ul><br><b>ENERGY STAR<sup>&reg;</sup></b><br>Products that are ENERGY STAR-qualified prevent greenhouse gas emissions by meeting strict energy efficiency guidelines set by the U.S. Environmental Protection Agency and the U.S. Department of Energy. The ENERGY STAR name and marks are registered marks owned by the U.S. government, as part of their energy efficiency and environmental activities.
+                            </div>
+                        </div>
+                        <div class="ProdQuestions prtHid" id="hideProductQuesLink">Do you have questions about this product? <a onclick="s_objectID=&quot;http://www.walmart.com/ip/Sony-55-Class-LED-1080p-60Hz-HDTV-2-3-8-ultra-slim-KDL-55EX640/20593825_36&quot;;return this.s_oc?this.s_oc(e):true" href="#Q%26A+Exchange">Ask a question</a>.</div>
+                    </div>
             </div>
             <div class="span3" style="width:195px; margin-top: -80px;" id="attributes">
                     <h3>Metrics</h3>
