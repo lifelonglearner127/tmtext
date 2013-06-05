f16535b0db4183814b830f0d86b62b9f294c2198
lifelonglearner127 <life.long.learner127@outlook.com>
2013-06-05 16:21:11 +0300

    Research & Edit js in separate file

--- application/views/elements/header.php
+++ application/views/elements/header.php
@@ -37,6 +37,7 @@
 	<script src="<?php echo base_url();?>js/fileupload/jquery.fileupload.js"></script>
 	<script src="<?php echo base_url();?>js/search_highlight_plug.js"></script>
     <script src="<?php echo base_url();?>js/tag_editor.js"></script>
+    <script src="<?php echo base_url();?>js/research.js"></script>
 
 	<!-- script src="<?php echo base_url();?>js/main.js"></script-->
 <?php if (isset($js)){ ?>
