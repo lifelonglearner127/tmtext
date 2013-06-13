4899d6ef82b637091e66220baecf467697df31a6
lifelonglearner127 <life.long.learner127@outlook.com>
2013-06-13 23:05:44 +0400

    compare interface UI

--- application/views/system/system_compare.php
+++ application/views/system/system_compare.php
@@ -1,9 +1,3 @@
-<script type='text/javascript'>
-	// ----- COMPARE INTERFACE UI (START)
-
-	// ----- COMPARE INTERFACE UI (END)
-</script>
-
 <div class="tabbable">
   <ul class="nav nav-tabs jq-system-tabs">
 	<li class=""><a data-toggle="tab" href="<?php echo site_url('system');?>">General</a></li>
@@ -19,21 +13,48 @@
     	<div class='span5'>
     		<div class='well'>
     			<p class='centered'><span class="label">SELECT PRODUCTS BOX</span></p>
+    			<ul class='nav nav-pills nav-stacked'>
+    				<li class='active'><a href='javascript:void(0)'>Test 1</a></li>
+    				<li><a href='javascript:void(0)'>Test 2</a></li>
+    				<li><a href='javascript:void(0)'>Test 3</a></li>
+    				<li><a href='javascript:void(0)'>Test 4</a></li>
+				</ul>
 			</div>
 		</div>
 		<!-- INTERACTIONS CONTROL BOX -->
 		<div class='span2'>
     		<div class='well'>
     			<p class='centered'><span class="label label-info">CONTROLS</span></p>
+    			<button id='ibc_move_btn' type='button' class='btn btn-primary icb_systme_compare_btn margin_bottom'><i class="icon-chevron-right icon-white"></i>&nbsp;Move</button>
+    			<button id='ibc_clean_btn' type='button' class='btn btn-danger icb_systme_compare_btn'><i class="icon-off icon-white"></i>&nbsp;Clean</button>
 			</div>
 		</div>
 		<!-- SELECTED FOR COMPARE BOX -->
 		<div class='span5'>
     		<div class='well'>
     			<p class='centered'><span class="label label-success">PRODUCTS FOR COMPARE</span></p>
+    			<ul class='nav nav-pills nav-stacked'>
+    				<li><a href='javascript:void(0)'>Test 1</a></li>
+    				<li><a href='javascript:void(0)'>Test 2</a></li>
+    				<li><a href='javascript:void(0)'>Test 3</a></li>
+    				<li><a href='javascript:void(0)'>Test 4</a></li>
+				</ul>
 			</div>
 		</div>
 
     </div>
   </div>
 </div>
+
+<script type='text/javascript'>
+	// ----- COMPARE INTERFACE UI (START)
+	$("#ibc_move_btn").tooltip({
+		placement: 'right',
+		title: "move to compare list"
+	});
+	$("#ibc_clean_btn").tooltip({
+		placement: 'right',
+		title: "clean compare list"
+	});
+	// ----- COMPARE INTERFACE UI (END)
+</script>
