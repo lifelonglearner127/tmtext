f7c8a70e98990828ffbd400efcc65512beb89810
lifelonglearner127 <life.long.learner127@outlook.com>
2013-06-13 22:38:30 +0400

    compare interface

--- application/views/system/system_compare.php
+++ application/views/system/system_compare.php
@@ -1,6 +1,6 @@
 <script type='text/javascript'>
 	// ----- COMPARE INTERFACE UI (START)
-	
+
 	// ----- COMPARE INTERFACE UI (END)
 </script>
 
@@ -14,7 +14,26 @@
   </ul>
   <div class="tab-content">
     <div id="tab5" class="tab-pane active">
-    	compare box
+
+    	<!-- PRODUCTS SELECTION BOX -->
+    	<div class='span5'>
+    		<div class='well'>
+    			<p class='centered'><span class="label">SELECT PRODUCTS BOX</span></p>
+			</div>
+		</div>
+		<!-- INTERACTIONS CONTROL BOX -->
+		<div class='span2'>
+    		<div class='well'>
+    			<p class='centered'><span class="label label-info">CONTROLS</span></p>
+			</div>
+		</div>
+		<!-- SELECTED FOR COMPARE BOX -->
+		<div class='span5'>
+    		<div class='well'>
+    			<p class='centered'><span class="label label-success">PRODUCTS FOR COMPARE</span></p>
+			</div>
+		</div>
+
     </div>
   </div>
 </div>
