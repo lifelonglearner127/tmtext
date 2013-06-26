cd5eecae5d2f015656dd94785ef047ef5079fce8
lifelonglearner127 <life.long.learner127@outlook.com>
2013-06-26 16:22:26 +0300

    'Validate' - commented out

--- application/views/elements/left_nav.php
+++ application/views/elements/left_nav.php
@@ -11,7 +11,7 @@ $menu = array(
     array('controller' => 'measure', 'name' => 'Competitive Intelligence'),
     array('controller' => 'research', 'name' => 'Research & Edit'),
     array('controller' => 'editor', 'name' => 'Create'),
-    array('controller' => 'validate', 'name' => 'Validate'),
+    //array('controller' => 'validate', 'name' => 'Validate'),
 	array('controller' => 'customer', 'name' => 'Settings'),
 );
 
