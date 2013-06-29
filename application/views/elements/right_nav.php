228292cc8ed7e3bd35e79e22c6f645d2cbf22e43
lifelonglearner127 <life.long.learner127@outlook.com>
2013-06-29 18:09:21 +0300

    Comment out Editors tab

--- application/views/elements/right_nav.php
+++ application/views/elements/right_nav.php
@@ -2,7 +2,7 @@
 $menu = array(
 	array('controller' => 'system', 'name' => 'System'),
 	array('controller' => 'admin_customer', 'name' => 'Customers'),
-	array('controller' => 'admin_editor', 'name' => 'Editors'),
+	/*array('controller' => 'admin_editor', 'name' => 'Editors'),*/
     array('controller' => 'admin_tag_editor', 'name' => 'Tag Editor'),
     /*array('controller' => 'site_crawler', 'name' => 'Site Crawler'),*/
 );
