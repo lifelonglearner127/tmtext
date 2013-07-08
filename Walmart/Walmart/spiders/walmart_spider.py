5a45c70bdf8a2df5a78d00dff735fe68c5152654
lifelonglearner127 <life.long.learner127@outlook.com>
2013-07-09 01:50:46 +0300

    added parent extraction for walmart spider

--- Walmart/Walmart/spiders/walmart_spider.py
+++ Walmart/Walmart/spiders/walmart_spider.py
@@ -15,7 +15,7 @@ class WalmartSpider(BaseSpider):
     name = "walmart"
     allowed_domains = ["walmart.com"]
     start_urls = [
-        "http://www.amazon.com/gp/site-directory/ref=sa_menu_top_fullstore",
+        "http://www.walmart.com/cp/All-Departments/121828",
     ]
 
     def parse(self, response):
@@ -27,11 +27,19 @@ class WalmartSpider(BaseSpider):
             item = WalmartItem()
             # select the preceding siblings that are a category title (have a child that is an a tag with a certain class)
             #TODO: right now it selects them for parents node too, to be fixed
-            parent = link.select('parent::node()').select('preceding-sibling::node()').select('child::a[@class=\'NavXLBold\']')
-            # if we found such siblings, get the last one to be the parent
-            if parent:
-                item['parent_text'] = parent[-1].select('text()').extract()
-                item['parent_url'] = parent[-1].select('@href').extract()
+            # if it's a bottom level category search for its parent
+            parents = []
+
+            # check class of a tag and use it to determine if the category has a parent
+            cat_type = link.select('@class').extract()[0]
+            if cat_type.decode('utf-8') == 'NavM':
+                parents = link.select('parent::node()').select('preceding-sibling::node()').select('child::a[@class=\'NavXLBold\']')
+            
+            # if we found such siblings, get the last one to be the parents
+            if parents:
+                item['parent_text'] = parents[-1].select('text()').extract()
+                item['parent_url'] = parents[-1].select('@href').extract()
+
             item['text'] = link.select('text()').extract()
             item['url'] = link.select('@href').extract()
             items.append(item)
