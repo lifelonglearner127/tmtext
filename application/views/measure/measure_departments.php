1bb844746ee07b157fee62d4eb90f7b1af5c40d7
lifelonglearner127 <life.long.learner127@outlook.com>
2013-07-08 11:03:51 +0300

    issue 164 - in progress

--- application/views/measure/measure_departments.php
+++ application/views/measure/measure_departments.php
@@ -6,5 +6,25 @@
         <li class=""><a data-toggle="tab" href="<?php echo site_url('measure/measure_gridview'); ?>"><b>Grid View</b></a></li>
     </ul>
     <div class="tab-content">
+        <!--
+        <table class="tbl_week_paggination">
+            <tr>
+                <td>
+                    View Report for:
+                </td>
+                <td style="width: 100px;">
+                    <select class='slt_year'>
+                        <option value="2013">2013</option>
+                    </select>
+                </td>
+                <td>
+                    Week:
+                </td>
+                <td>
+                    << < 1 2 3 4 5 > >>
+                </td>
+            </tr>
+        </table>
+        -->
     </div>
 </div>
