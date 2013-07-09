ff2f23d1b28b720aa8df4efa2ba1d3778e527365
lifelonglearner127 <life.long.learner127@outlook.com>
2013-07-09 13:54:28 +0300

    added some comments for walmart spider and removed TODO

--- Walmart/Walmart/spiders/walmart_spider.py
+++ Walmart/Walmart/spiders/walmart_spider.py
@@ -25,17 +25,17 @@ class WalmartSpider(BaseSpider):
 
         for link in links:
             item = WalmartItem()
-            # select the preceding siblings that are a category title (have a child that is an a tag with a certain class)
-            #TODO: right now it selects them for parents node too, to be fixed
+
             # if it's a bottom level category search for its parent
             parents = []
 
             # check class of a tag and use it to determine if the category has a parent
             cat_type = link.select('@class').extract()[0]
             if cat_type.decode('utf-8') == 'NavM':
+            # select the preceding siblings that are a category title (have a child that is an a tag with a certain class)
                 parents = link.select('parent::node()').select('preceding-sibling::node()').select('child::a[@class=\'NavXLBold\']')
-            
-            # if we found such siblings, get the last one to be the parents
+
+            # if we found such siblings, get the last one to be the parent
             if parents:
                 item['parent_text'] = parents[-1].select('text()').extract()
                 item['parent_url'] = parents[-1].select('@href').extract()
