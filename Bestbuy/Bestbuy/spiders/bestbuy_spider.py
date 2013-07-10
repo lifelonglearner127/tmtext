e8cddf4146d0ef67eaa6c261fd21994e50ef4099
lifelonglearner127 <life.long.learner127@outlook.com>
2013-07-10 23:45:36 +0300

    added parent extraction for bestbuy spider

--- Bestbuy/Bestbuy/spiders/bestbuy_spider.py
+++ Bestbuy/Bestbuy/spiders/bestbuy_spider.py
@@ -19,15 +19,26 @@ class BestbuySpider(BaseSpider):
     ]
 
     def parse(self, response):
+        # extracting all links in the list (from all levels of nesting).
+        # adding parent_url and parent_text fields for the bottom level categories
+
+        # currently not extracting parents that are non-links (smaller parent categories like "resources" and "shops")
         hxs = HtmlXPathSelector(response)
         links = hxs.select("//div[@id='container']/div/header//nav/ul[@id='nav']//a")
         items = []
 
-        #TODO: implement parents
         for link in links:
+
+            # retrieve parent category for this link
+            parent = link.select("parent::node()/parent::node()/preceding-sibling::node()/a")
             item = BestbuyItem()
+
             item['text'] = link.select('text()').extract()
             item['url'] = link.select('@href').extract()
+
+            item['parent_text'] = parent.select('text()').extract()
+            item['parent_url'] = parent.select('@href').extract()
+
             items.append(item)
 
         return items
