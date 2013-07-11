804e2b05684a8c1f14743b8acd79a2c7f222f598
lifelonglearner127 <life.long.learner127@outlook.com>
2013-07-11 20:05:29 +0300

    added new fields (level) for walmart spider

--- Walmart/Walmart/items.py
+++ Walmart/Walmart/items.py
@@ -1,15 +1,11 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/topics/items.html
+# Definition of models for the scraped items
 
 from scrapy.item import Item, Field
 
 class WalmartItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    url = Field()
-    text = Field()
-    parent_text = Field()
-    parent_url = Field()
-    # parent
+    url = Field() # url of category
+    text = Field() # name of category
+    parent_text = Field() # name of parent category
+    parent_url = Field() # url of parent category
+    level = Field() # level of category in the nested list (from narrower to broader categories)
+    special = Field() # is it a special category? (1 or nothing)
--- Walmart/Walmart/spiders/walmart_spider.py
+++ Walmart/Walmart/spiders/walmart_spider.py
@@ -20,28 +20,39 @@ class WalmartSpider(BaseSpider):
 
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
-        links = hxs.select("//div[@class='MidContainer']/div[3]//a")
+        links = hxs.select("//div[@class='MidContainer']/div[3]//a[@class='NavM']")
+        parent_links = hxs.select("//div[@class='MidContainer']/div[3]//a[@class='NavXLBold']")
         items = []
 
         for link in links:
             item = WalmartItem()
 
-            # if it's a bottom level category search for its parent
+            # search for the category's parent
             parents = []
 
-            # check class of a tag and use it to determine if the category has a parent
-            cat_type = link.select('@class').extract()[0]
-            if cat_type.decode('utf-8') == 'NavM':
             # select the preceding siblings that are a category title (have a child that is an a tag with a certain class)
-                parents = link.select('parent::node()').select('preceding-sibling::node()').select('child::a[@class=\'NavXLBold\']')
+            parents = link.select('parent::node()').select('preceding-sibling::node()').select('child::a[@class=\'NavXLBold\']')
 
             # if we found such siblings, get the last one to be the parent
             if parents:
-                item['parent_text'] = parents[-1].select('text()').extract()
-                item['parent_url'] = parents[-1].select('@href').extract()
+                item['parent_text'] = parents[-1].select('text()').extract()[0]
+                item['parent_url'] = parents[-1].select('@href').extract()[0]
 
             item['text'] = link.select('text()').extract()
             item['url'] = link.select('@href').extract()
+
+            item['level'] = 0
+
+            items.append(item)
+
+        for link in parent_links:
+            item = WalmartItem()
+
+            item['text'] = link.select('text()').extract()
+            item['url'] = link.select('@href').extract()
+
+            item['level'] = 1
+
             items.append(item)
 
         return items
