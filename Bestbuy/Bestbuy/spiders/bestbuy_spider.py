7d71a9866d4a01b233250690902532daee6bff50
lifelonglearner127 <life.long.learner127@outlook.com>
2013-07-11 17:25:16 +0300

    added level and special field for bestbuy (still has a bug)

--- Bestbuy/Bestbuy/spiders/bestbuy_spider.py
+++ Bestbuy/Bestbuy/spiders/bestbuy_spider.py
@@ -22,21 +22,60 @@ class BestbuySpider(BaseSpider):
     def parse(self, response):
         # currently not extracting parents that are non-links (smaller parent categories like "resources" and "shops")
         hxs = HtmlXPathSelector(response)
-        links = hxs.select("//div[@id='container']/div/header//nav/ul[@id='nav']/a")
+
+        # select all categories (bottom level)
+        product_links = hxs.select("//div[@id='container']/div/header//nav/ul[@id='nav']/li[@class='nav-pro']/ul/li/ul/li/a")
+
+        # select other categories (special)
+        other_links = hxs.select("//div[@id='container']/div/header//nav/ul[@id='nav']/li[@class!='nav-pro']/ul/li/ul/li/a")
+
+        # select all parent categories
+        parent_links = hxs.select("//div[@id='container']/div/header//nav/ul[@id='nav']/li[@class='nav-pro']/ul/li/h4/a")
+
+        #TODO: add extraction of level 3 categories (broadest: products, services,...)
+        
         items = []
 
-        for link in links:
+        for link in product_links:
 
             # retrieve parent category for this link
-            #TODO: for links that don't have a parent this output should be omitted, to fix
             parent = link.select("parent::node()/parent::node()/preceding-sibling::node()/a")
             item = BestbuyItem()
 
-            item['text'] = link.select('text()').extract()
-            item['url'] = link.select('@href').extract()
+            item['text'] = link.select('text()').extract()[0]
+            item['url'] = link.select('@href').extract()[0]
 
-            item['parent_text'] = parent.select('text()').extract()
-            item['parent_url'] = parent.select('@href').extract()
+            item['parent_text'] = parent.select('text()').extract()[0]
+            item['parent_url'] = parent.select('@href').extract()[0]
+
+            # mark it as special if a certain condition is checked
+            if (link.select("parent::node()/parent::*[@class='nav-res']")):
+                item['special'] = 1
+            #TODO: add its direct parent if it's special (not among the categories). ex: shops, resources...
+
+            # get grandparent of the category, mark item as special if grandparent is special
+            #TODO: buggy
+            grandparent = parent.select("parent::node()/parent::node()/parent::node()/parent::node()")
+            if (grandparent.select('@class').extract() != 'nav-pro'):
+                item['special'] = 1
+
+            item['level'] = 0
+
+            items.append(item)
+
+        for link in parent_links:
+            item = BestbuyItem()
+
+            item['text'] = link.select('text()').extract()[0]
+            item['url'] = link.select('@href').extract()[0]
+
+            item['level'] = 1
+            parent = link.select("parent::node()/parent::node()/parent::node()/parent::node()")
+
+            # mark item as special if its parent is special
+            #TODO: buggy
+            if (parent.select('@class').extract() != 'nav-pro'):
+                item['special'] = 1
 
             items.append(item)
 
