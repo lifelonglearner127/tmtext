3ee7986e4db3bbf7ce02b7c10cf24e16dfe9aeb6
lifelonglearner127 <life.long.learner127@outlook.com>
2013-07-27 00:43:40 +0300

    added spider for bestsellers list for wayfair

--- Wayfair/Wayfair/items.py
+++ Wayfair/Wayfair/items.py
@@ -2,7 +2,7 @@
 
 from scrapy.item import Item, Field
 
-class WayfairItem(Item):
+class CategoryItem(Item):
     url = Field() # url of category
     text = Field() # name of category
     parent_text = Field() # name of parent category (if any)
@@ -11,3 +11,16 @@ class WayfairItem(Item):
     grandparent_url = Field() # name of grandparent url (if any)
     level = Field() # level of category in the nested list (from narrower to broader categories)
     special = Field() # is it a special category? (1 or nothing)
+
+class ProductItem(Item):
+	url = Field() # url of product page
+	list_name = Field() # name of product - from bestsellers list
+	product_name = Field() # name of product - from product page
+	brand = Field() # brand of product, from product name
+	page_title = Field() # title (title tag text) of product page
+	department = Field() # department of product - its name
+	price = Field() # price of product - a string like "29.5$"
+	listprice = Field() # "list price" of product - a string like "29.5$"
+	rank = Field() # rank of the product in the bestsellers list
+	SKU = Field() # SKU code of product (where available)
+	UPC = Field() # UPC code of product (where available)
--- Wayfair/Wayfair/spiders/wayfair_spider.py
+++ Wayfair/Wayfair/spiders/wayfair_spider.py
@@ -1,7 +1,10 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
-from Wayfair.items import WayfairItem
+from scrapy.http import Request
+from Wayfair.items import CategoryItem
+from Wayfair.items import ProductItem
 import sys
+import re
 
 ################################
 # Run with 
@@ -10,7 +13,7 @@ import sys
 #
 ################################
 
-
+# scrape sitemap and extract categories
 class WayfairSpider(BaseSpider):
     name = "wayfair"
     allowed_domains = ["wayfair.com"]
@@ -37,7 +40,7 @@ class WayfairSpider(BaseSpider):
             # extracting grandparents
             grandparent = parent.select('parent::node()/parent::node()/parent::node()/a')
             
-            item = WayfairItem()
+            item = CategoryItem()
             item['text'] = link.select('text()').extract()[0]
             item['url'] = link.select('@href').extract()[0]
 
@@ -60,7 +63,7 @@ class WayfairSpider(BaseSpider):
             # extracting parents
             parent = link.select('parent::node()/parent::node()/parent::node()/a')
             
-            item = WayfairItem()
+            item = CategoryItem()
             item['text'] = link.select('text()').extract()[0]
             item['url'] = link.select('@href').extract()[0]
 
@@ -78,7 +81,7 @@ class WayfairSpider(BaseSpider):
 
         # last 2 categories need to be marked as special
         for link in grandparent_links[:-2]:
-            item = WayfairItem()
+            item = CategoryItem()
             item['text'] = link.select('text()').extract()[0]
             item['url'] = link.select('@href').extract()[0]
 
@@ -87,7 +90,7 @@ class WayfairSpider(BaseSpider):
             items.append(item)
 
         for link in grandparent_links[-2:]:
-            item = WayfairItem()
+            item = CategoryItem()
             item['text'] = link.select('text()').extract()[0]
             item['url'] = link.select('@href').extract()[0]
 
@@ -101,7 +104,7 @@ class WayfairSpider(BaseSpider):
             # extracting parents
             parent = link.select('parent::node()/parent::node()/parent::node()/a')
 
-            item = WayfairItem()
+            item = CategoryItem()
             item['text'] = link.select('text()').extract()[0]
             item['url'] = link.select('@href').extract()[0]
 
@@ -119,7 +122,7 @@ class WayfairSpider(BaseSpider):
             items.append(item)
 
         for link in special_parent_links:
-            item = WayfairItem()
+            item = CategoryItem()
             item['text'] = link.select('text()').extract()[0]
             item['url'] = link.select('@href').extract()[0]
 
@@ -129,3 +132,100 @@ class WayfairSpider(BaseSpider):
             items.append(item)
 
         return items
+
+
+# scrape bestsellers list and extract products
+class BestsellerSpider(BaseSpider):
+    name = "bestseller"
+    allowed_domains = ["wayfair.com"]
+    start_urls = [
+        "http://www.wayfair.com/Best-Sellers-l951.html?keyword=&dept=0&sortby=1&itemsperpage=40",
+    ]
+
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+
+        # extract page links and pass them to parsePage to parse each results page, also pass the current one
+        request = Request(response.url, callback = self.parsePage)
+
+        # send page number to parsePage
+        request.meta['page'] = 1
+        yield request
+
+        pages = hxs.select("//span[@class='pagenumbers']/a")
+
+        for page_i in range(len(pages)):
+            url = pages[page_i].select("@href").extract()[0]
+            request = Request(url, callback = self.parsePage)
+
+            # send page number to parsePage (starts from page 2)
+            request.meta['page'] = page_i + 2
+            yield request
+
+    def parsePage(self, response):
+        #TODO: add department?
+
+        hxs = HtmlXPathSelector(response)
+        products = hxs.select("//li[@class='productbox']")
+
+        products_per_page = 40
+        page_nr = response.meta['page']
+
+        # counter to keep track of product rank
+        rank = 0
+
+        for product in products:
+            item = ProductItem()
+
+            rank += 1
+
+            product_link = product.select(".//a[@class='toplink']")
+            url = product_link.select("@href").extract()
+            if url:
+                item['url'] = url[0]
+            else:
+                continue
+
+            # compute global item rank using rank on current page and page number
+            item['rank'] = str((page_nr - 1) * products_per_page + rank)
+
+            product_name = product_link.select("div[@class='prodname']/text()").extract()
+            brand_name = product_link.select("div[@class='prodname']/div[@class='prodbrandname emphasis']/text()").extract()
+
+            if product_name:
+                item['list_name'] = product_name[0].strip()
+            if brand_name:
+                item['brand'] = brand_name[0].strip()
+
+            #TODO: also "Reg price", extract that as well?
+            listprice = product.select(".//div[@class='wasprice']/span/text()").extract()
+            if listprice:
+                item['listprice'] = listprice[0]
+
+            price = product.select(".//div[@class='price secondarytext midtitle']/text() | .//div[@class='price noticetext midtitle']/text()").extract()
+            if price:
+                item['price'] = price[0]
+
+            # pass item to parseProduct method
+            request = Request(item['url'], callback = self.parseProduct)
+            request.meta['item'] = item
+            yield request
+
+    def parseProduct(self, response):
+        hxs = HtmlXPathSelector(response)
+
+        item = response.meta['item']
+
+        # this is only the name, without the brand
+        product_name = hxs.select("//h1/text()").extract()[0].strip()
+        page_title = hxs.select("//title/text()").extract()[0]
+
+        # remove "Wayfair.com" suffix from page name
+        m = re.match("(.*) \| Wayfair", page_title, re.UNICODE)
+        if m:
+            page_title = m.group(1).strip()
+
+        item['page_title'] = page_title
+        item['product_name'] = product_name
+
+        yield item
