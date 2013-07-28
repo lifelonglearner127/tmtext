52fb93c54fce2ff785adae7201e77ddd52438c46
lifelonglearner127 <life.long.learner127@outlook.com>
2013-07-29 02:38:52 +0300

    added to search product spider

--- search/search/spiders/search_spider.py
+++ search/search/spiders/search_spider.py
@@ -2,13 +2,18 @@ from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from scrapy.http import Request
 from search.items import SearchItem
+import re
 
 class SearchSpider(BaseSpider):
 
 	name = "search"
 
-	def __init__(self, product_name):
-		search_query = "+".join(product_name.split())
+	# pass product as argument to constructor - either product name or product url
+	def __init__(self, product_name = None, product_url = None):
+		if product_name:
+			self.search_query = "+".join(product_name.split())
+		if product_url:
+			self.search_query = self.get_name(product_url)
 		self.start_urls = ["http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=" + search_query]
 
 	def parse(self, response):
@@ -18,5 +23,14 @@ class SearchSpider(BaseSpider):
 		item['name'] = product
 
 		yield item
+
+
+	# get the name of a product from the URL of its page
+	# the URL can be from various sites
+	def get_name(product_url):
+
+		# extract domain
+		#TODO: under construction
+
 
 
