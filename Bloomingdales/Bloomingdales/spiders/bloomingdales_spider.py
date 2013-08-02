ce3f7000ca2abb7b2e5bb8b96c4e75885d9fabb9
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-02 19:27:34 +0300

    fixed bugs in bloomingdales bestsellers spider (still has some problems)

--- Bloomingdales/Bloomingdales/spiders/bloomingdales_spider.py
+++ Bloomingdales/Bloomingdales/spiders/bloomingdales_spider.py
@@ -3,7 +3,10 @@ from scrapy.selector import HtmlXPathSelector
 from Bloomingdales.items import CategoryItem
 from Bloomingdales.items import ProductItem
 from scrapy.http import Request
+from scrapy.http import TextResponse
+from selenium import webdriver
 import re
+import time
 import sys
 
 ################################
@@ -59,19 +62,21 @@ class BloomingdalesSpider(BaseSpider):
 
         #TODO: can't parse page 2 pf shoes
         #TODO: they're not ordered by bestseller
+        #TODO: add date
         pages = [
                     # handbags
-                "http://www1.bloomingdales.com/shop/handbags/best-sellers?id=23173#fn=sortBy%3DBEST_SELLERS%26productsPerPage%3D96",\
+                "http://www1.bloomingdales.com/shop/handbags/best-sellers?id=23173",\
                     # shoes
-                "http://www1.bloomingdales.com/shop/shoes/best-sellers?id=23268#fn=sortBy%3DBEST_SELLERS%26productsPerPage%3D96", \
-                    # shoes page 2
-                "http://www1.bloomingdales.com/shop/shoes/best-sellers?id=23268#fn=pageIndex%3D2%26sortBy%3DBEST_SELLERS%26productsPerPage%3D96"
+                "http://www1.bloomingdales.com/shop/shoes/best-sellers?id=23268", \
+                #     # shoes page 2
+                # #"http://www1.bloomingdales.com/shop/shoes/best-sellers?id=23268#fn=pageIndex%3D2%26sortBy%3DBEST_SELLERS%26productsPerPage%3D96"
+                # "http://www1.bloomingdales.com/shop/shoes/best-sellers?id=23268&pageIndex=2"
                  
         ]
 
         # call parsePage for each of these pages
         for page_i in range(len(pages)):
-            request = Request(pages[page_i], callback = self.parsePage)
+            request = Request(pages[page_i], callback = self.parseDept)
             if page_i == 0:
                 department = "Handbags"
             else:
@@ -80,14 +85,72 @@ class BloomingdalesSpider(BaseSpider):
             request.meta['department'] = department
             yield request
 
-    def parsePage(self, response):
+    def parseDept(self, response):
+        department = response.meta['department']
+        hxs = HtmlXPathSelector(response)
+        items = []
+
+        # # use selenium to select the sorting option
+        driver = webdriver.Firefox()
+        driver.get(response.url)
+
+        # use selenium to select USD currency
+       
+        link = driver.find_element_by_xpath("//li[@id='bl_nav_account_flag']//a")
+        link.click()
+        time.sleep(5)
+        button = driver.find_element_by_id("iShip_shipToUS")
+        button.click()
+        time.sleep(10)
+
+        dropdown = driver.find_element_by_id("sortBy")
+        for option in dropdown.find_elements_by_tag_name("option"):
+            if option.text == 'Best Sellers':
+                option.click()
+
+        time.sleep(5)
+
+        # convert html to "nice format"
+        text_html = driver.page_source.encode('utf-8')
+        html_str = str(text_html)
+
+        # this is a hack that initiates a "TextResponse" object (taken from the Scrapy module)
+        resp_for_scrapy = TextResponse('none',200,{},html_str,[],None)
+       
+        # pass first page to parsePage function to extract products
+        items += self.parsePage(resp_for_scrapy, department)
+
+
+        next_page_url = hxs.select("//li[@class='nextArrow']/div/a")
+        while next_page_url:
+            # use selenium to click on next page arrow and retrieve the resulted page if any
+            next = driver.find_element_by_xpath("//li[@class='nextArrow']/div/a")
+            next.click()
+
+            time.sleep(5)
+
+            # convert html to "nice format"
+            text_html = driver.page_source.encode('utf-8')
+            html_str = str(text_html)
+
+            # this is a hack that initiates a "TextResponse" object (taken from the Scrapy module)
+            resp_for_scrapy = TextResponse('none',200,{},html_str,[],None)
+
+            # pass the page to parsePage function to extract products
+            items += self.parsePage(resp_for_scrapy, department)
+
+            hxs = HtmlXPathSelector(resp_for_scrapy)
+            next_page_url = hxs.select("//li[@class='nextArrow']/div/a")
+            if next_page_url:
+                print "NEXT_PAGE_URL", next_page_url
+
+        driver.close()
+
+        return items
+
+    def parsePage(self, response, department):
         hxs = HtmlXPathSelector(response)
         products = hxs.select("//div[@class='productThumbnail showQuickView']")
-
-
-
-
-        #sys.stderr.write("---------------------------------------------------" + response.url)
 
         if not products:
             return
@@ -102,7 +165,7 @@ class BloomingdalesSpider(BaseSpider):
             item['rank'] = str(rank)
 
             # get item department from response's meta
-            item['department'] = response.meta['department']
+            item['department'] = department
 
             # extract name and url from bestsellers list
             product_link = product.select("div[@class='shortDescription']/a")
