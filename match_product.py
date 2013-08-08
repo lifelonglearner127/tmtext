c6fb92b3044ec0c909434c1394a5f123908b5f29
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-08 20:07:35 +0300

    updated product matching script

--- match_product.py
+++ match_product.py
@@ -1,23 +1,55 @@
 #!/usr/bin/python
 import codecs
 import re
+import json
+import sys
 from pprint import pprint
+import nltk
+from nltk.corpus import stopwords
 
-def match(filename1, filename2, nrproduct):
-	file1 = open(filename1, "r")
-	file2 = open(filename2, "r")
+def get_products(filename, category):
+	output_all = codecs.open(filename, "r", "utf-8")
 
+	products = []
+
+	for line in output_all:
+		# print line
+		if line.strip():
+			item = json.loads(line.strip())
+			if 'department' in item:
+				if item['department'] == category:
+					products.append(item)
+			if 'category' in item:
+				if item['category'] == category:
+					products.append(item)
+
+	# close all opened files
+	output_all.close()
+	return products
+
+def normalize(text):
+	text = re.sub("([^\w\"])|(u')", " ", text)
+	stopset = set(stopwords.words('english'))#["and", "the", "&", "for", "of", "on", "as", "to", "in"]
+	stemmer = nltk.PorterStemmer()
+	tokens = nltk.WordPunctTokenizer().tokenize(text)
+	clean = [token.lower() for token in tokens if token.lower() not in stopset and len(token) > 0]
+
+	return clean
+
+
+def match(products1, products2, nrproduct):
 	product = ''
 
-	for i in range(nrproduct):
-		product = file1.readline().strip()
+	product = products1[nrproduct]
 	products_found = []
 
-	for line in file2:
-		product = re.sub("([^\w\"])|(u')", " ", product)
-		product2 = re.sub("([^\w\"])|(u')", " ", line.strip())
-		words1 = set(filter(None, re.split("\s", product2)))
-		words2 = set(filter(None, re.split("\s", product)))
+	for product2 in products2:
+		product_name = product['product_name']
+		product_name2 = product2['product_name']
+		# words1 = set(filter(None, re.split("\s", product_name2)))
+		# words2 = set(filter(None, re.split("\s", product_name)))
+		words1 = set(normalize(product_name))
+		words2 = set(normalize(product_name2))
 		common_words = words1.intersection(words2)
 		threshold = 0.5*(len(words1) + len(words2))/2
 		if len(common_words) >= threshold:
@@ -29,19 +61,27 @@ def match(filename1, filename2, nrproduct):
 			products_found.append(product2)
 			#break
 
-	file1.close()
-	file2.close()
-
 	return product, products_found
 
 results = 0
+site1 = sys.argv[1]
+category1 = sys.argv[2]
+site2 = sys.argv[3]
+category2 = sys.argv[4]
 
-for nrprod in range(50):
-	(prod, res) = match("walmart_apparel.txt", "overstock_clothing.txt", nrprod)
-	print prod
-	pprint(res)
+products1 = get_products("sample_output/" + site1 + "_bestsellers_dept.jl", category1)
+products2 = get_products("sample_output/" + site2 + "_bestsellers_dept.jl", category2)
+
+for nrprod in range(len(products1)):
+	(prod, res) = match(products1, products2, nrprod)
+	print prod['product_name']
+	print 'res:'
+	for product in res:
+		print '-', product['product_name']
 	print ''
 	if res:
+		for product in res:
+			print product
 		results += 1
 
 print "results: ", results
