1b55f00bef03495ec74a3ca9df5b0da3cb08970c
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-10 04:48:42 +0300

    #337

--- application/controllers/research.php
+++ application/controllers/research.php
@@ -505,8 +505,13 @@ class Research extends MY_Controller {
                 $items_short_products_content++;
             }
 
-            if (($result_row->short_description_wc <= 20 && ($build_assess_params->long_less == -1 || $build_assess_params->long_less == false)) ||
-                ($result_row->long_description_wc <= 100 && ($build_assess_params->short_less == -1 || $build_assess_params->short_less == false)){
+            if (($result_row->short_description_wc <= 20 && $build_assess_params->long_less == false ) ||
+                ($result_row->long_description_wc <= 100 && $build_assess_params->short_less == false)){
+                $items_short_products_content++;
+            }
+
+            if (($result_row->short_description_wc <= 20 && $build_assess_params->long_less == -1 ) ||
+                ($result_row->long_description_wc <= 100 && $build_assess_params->short_less == -1)){
                 $items_short_products_content++;
             }
 
