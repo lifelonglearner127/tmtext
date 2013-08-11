0c8d94273317f421b06167468f074daab496f54c
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-11 16:54:05 +0300

    #345

--- application/views/batches/index.php
+++ application/views/batches/index.php
@@ -73,7 +73,7 @@
                                 $('#csv_import_create_batch').trigger('click');
                                 setTimeout(function(){
                                     $('#progress .bar').css({'background':'none'});
-                                }, 10);
+                                }, 100);
                             },
                             progressall: function (e, data) {
                                 var progress = parseInt(data.loaded / data.total * 100, 10);
