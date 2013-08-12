9a29a7dbaefcc73e9b43fa4b7c48482a631d3daf
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-13 00:41:26 +0300

    cron test

--- application/controllers/crons.php
+++ application/controllers/crons.php
@@ -160,34 +160,35 @@ class Crons extends MY_Controller {
     }
 
     public function do_stats(){
-        $this->load->model('batches_model');
-        $this->load->model('research_data_model');
-        $this->load->model('statistics_model');
-        $this->statistics_model->truncate();
-        $batches = $this->batches_model->getAll();
-        $file = 'debug_cli.txt';
-        // Open the file to get existing content
-        $current = file_get_contents($file);
-        foreach($batches as $batch){
-            $data = $this->research_data_model->do_stats($batch->title);
-            if(count($data) > 0){
-                foreach($data as $obj){
-                    $this->statistics_model->insert($obj->rid, $obj->imported_data_id, $obj->batch_name,
-                        $obj->product_name, $obj->url, $obj->short_description, $obj->long_description,
-                        $obj->short_description_wc, $obj->long_description_wc,
-                        $obj->short_seo_phrases, $obj->long_seo_phrases);
+        if ( file_exists($tmp_dir.".locked") )
+        { exit;}
+        touch($tmp_dir.".locked");
+        try {
+            $this->load->model('batches_model');
+            $this->load->model('research_data_model');
+            $this->load->model('statistics_model');
+            $this->statistics_model->truncate();
+            $batches = $this->batches_model->getAll();
+            foreach($batches as $batch){
+                $data = $this->research_data_model->do_stats($batch->title);
+                if(count($data) > 0){
+                    foreach($data as $obj){
+                        $this->statistics_model->insert($obj->rid, $obj->imported_data_id, $obj->batch_name,
+                            $obj->product_name, $obj->url, $obj->short_description, $obj->long_description,
+                            $obj->short_description_wc, $obj->long_description_wc,
+                            $obj->short_seo_phrases, $obj->long_seo_phrases);
+                    }
+                } else {
+                   echo $batch->title." - batch doesn't have statistic data\n";
                 }
-            } else {
-                $current .= $batch->title." - batch doesn't have statistic data\n";
             }
+            echo "Cron Job Finished";
+        } catch (Exception $e) {
+            echo 'Ошибка',  $e->getMessage(), "\n";
+        } finally {
+            unlink($tmp_dir.".locked");
         }
-        file_put_contents($file, $current);
-        echo "Cron Job Finished";
     }
 
-    public function hello(){
-        print_r('aaa');
-        echo 'Hello world!';
-    }
 
 }
