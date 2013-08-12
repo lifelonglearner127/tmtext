3e9c97c4cd0cbd0de6dece56089a0601a8d83db8
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-12 17:39:43 +0300

    Issue #353 - fixed

--- application/controllers/research.php
+++ application/controllers/research.php
@@ -828,7 +828,8 @@ class Research extends MY_Controller {
         $customer_logo = $img_path.$customer->image_url;
 
         $this->load->model('reports_model');
-        $report_cover = $this->reports_model->get_by_name('Assess')[0]->body;
+        $report = $this->reports_model->get_by_name('Assess');
+        $report_cover = $report[0]->body;
         $report_cover = str_replace('#date#', date('F j, Y'), $report_cover);
         $report_cover = str_replace('#customer name#', $customer->name, $report_cover);
 
