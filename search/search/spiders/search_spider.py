8c95d4f28261e7530c6c54515846f360cb844c4b
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-14 11:44:03 +0300

    updated search spider: no more selenium for staples

--- search/search/spiders/search_spider.py
+++ search/search/spiders/search_spider.py
@@ -1,18 +1,20 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from scrapy.http import Request
+from scrapy.http import TextResponse
 from search.items import SearchItem
 import urllib
 import re
 
 from selenium import webdriver
+import time
 
 ################################
 # Run with 
 #
-# scrapy crawl search -a product_name="<name>"
+# scrapy crawl search -a product_name="<name>" -a target_site="<site>"
 #      -- or --
-# scrapy crawl search -a product_url="<url>"
+# scrapy crawl search -a product_url="<url>" -a target_site="<site>"
 #
 ################################
 
@@ -22,7 +24,7 @@ class SearchSpider(BaseSpider):
 
 	name = "search"
 	allowed_domains = ["amazon.com", "walmart.com", "bloomingdales.com", "overstock.com", "wayfair.com", "bestbuy.com", "toysrus.com",\
-					   "bjs.com", "sears.com"]
+					   "bjs.com", "sears.com", "staples.com"]
 
 	# pass product as argument to constructor - either product name or product url
 	def __init__(self, product_name = None, product_url = None, target_site = None):
@@ -32,9 +34,9 @@ class SearchSpider(BaseSpider):
 		# bloomingales scraper only works with this in the start_urls list
 		self.start_urls = ["http://www.amazon.com", "http://www.walmart.com", "http://www1.bloomingdales.com",\
 						   "http://www.overstock.com", "http://www.wayfair.com", "http://www.bestbuy.com", \
-						   "http://www.toysrus.com", "http://www.bjs.com", "http://www.sears.com"]
+						   "http://www.toysrus.com", "http://www.bjs.com", "http://www.sears.com", "http://www.staples.com"]
 
-	def build_search_pages(search_query):
+	def build_search_pages(self, search_query):
 		# build list of urls = search pages for each site
 		search_pages = {
 						"amazon" : "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=" + search_query, \
@@ -53,7 +55,7 @@ class SearchSpider(BaseSpider):
 
 		return search_pages
 
-	def build_search_query(product_name):
+	def build_search_query(self, product_name):
 		# put + instead of spaces, lowercase all words
 		search_query = "+".join([name.lower() for name in product_name.split()])
 		return search_query
@@ -61,60 +63,65 @@ class SearchSpider(BaseSpider):
 	def parse(self, response):
 
 		# if we have product names, pass them to parseResults
+		if self.product_name:
+			search_query = self.build_search_query(product_name)
+			search_pages = build_search_pages(search_query)
+
+			request = Request(search_pages[site], callback = self.parseResults)
+			request.meta['site'] = self.target_site
+			yield request
+		
 		# if we have product URLs, pass them to parseURL to extract product names (which will pass them to parseResults)
-		for site in [target_site]:#search_pages:
-			search_query = ""
-
-			if self.product_name:
-				# put + instead of spaces, lowercase all words
-				search_query = "+".join([name.lower() for name in product_name.split()])
-				request = Request(search_pages[site], callback = self.parseResults)
-				request.meta['site'] = site
-				yield request
-
-			if self.product_url:
-				# extract site domain
-				m = re.match("http://www1?\.([^\.]+)\.com.*", self.product_url)
-				origin_site = ""
-				if m:
-					origin_site = m.group(1)
-				else:
-					sys.stderr.write('Can\'t extract domain from URL.\n')
-				request = Request(search_pages[site], callback = self.parseURL)
-				request.meta['site'] = origin_site
-				yield request
+		if self.product_url:
+			# extract site domain
+			m = re.match("http://www1?\.([^\.]+)\.com.*", self.product_url)
+			origin_site = ""
+			if m:
+				origin_site = m.group(1)
+			else:
+				sys.stderr.write('Can\'t extract domain from URL.\n')
+			request = Request(self.product_url, callback = self.parseURL)
+			request.meta['site'] = origin_site
+			if origin_site == 'staples':
+				zipcode = "12345"
+				request.cookies = {"zipcode": zipcode}
+			yield request
 
 	# parse a product page (given its URL) and extract product's name
 	def parseURL(self, response):
-		if site == 'staples'
-			# use selenium to complete the zipcode form and get the product page
-			driver = webdriver.Firefox()
-			driver.get(response.url)
+		site = response.meta['site']
+		if site == 'staples':
+			# # use selenium to complete the zipcode form and get the product page
+			# driver = webdriver.Firefox()
+			# driver.get(response.url)
 
-			# set a hardcoded value for zipcode
-			zipcode = "12345"
+			# # set a hardcoded value for zipcode
+			# zipcode = "12345"
 
-			textbox = driver.find_element_by_name("zipCode")
-			textbox.send_keys(zipcode)
+			# textbox = driver.find_element_by_name("zipCode")
+			# textbox.send_keys(zipcode)
 
-			button = driver.find_element_by_id("submitLink")
-			button.click()
+			# button = driver.find_element_by_id("submitLink")
+			# button.click()
 
-			cookie = {"zipcode": zipcode}
-			driver.add_cookie(cookie)
+			# cookie = {"zipcode": zipcode}
+			# driver.add_cookie(cookie)
 
-			time.sleep(5)
+			# time.sleep(5)
 
-			# convert html to "nice format"
-			text_html = driver.page_source.encode('utf-8')
-			#print "TEXT_HTML", text_html
-			html_str = str(text_html)
+			# # convert html to "nice format"
+			# text_html = driver.page_source.encode('utf-8')
+			# #print "TEXT_HTML", text_html
+			# html_str = str(text_html)
 
-			# this is a hack that initiates a "TextResponse" object (taken from the Scrapy module)
-			resp_for_scrapy = TextResponse('none',200,{},html_str,[],None)
+			# # this is a hack that initiates a "TextResponse" object (taken from the Scrapy module)
+			# resp_for_scrapy = TextResponse('none',200,{},html_str,[],None)
 
-			# extract product name
-			hxs = HtmlXPathSelector(resp_for_scrapy)
+			# driver.close()
+
+			# # extract product name
+			# hxs = HtmlXPathSelector(resp_for_scrapy)
+			hxs = HtmlXPathSelector(response)
 
 			product_name = hxs.select("//h1/text()").extract()[0]
 
@@ -124,9 +131,13 @@ class SearchSpider(BaseSpider):
 			if m:
 				product_model = m.group(1)
 
-			query = build_search_query(product_name)
-			#UNDER CONSTRUCITON
-			request = Request(url, callback, method, headers, body, cookies, meta, encoding, priority, dont_filter, errback)
+			query = self.build_search_query(product_name)
+			search_pages = self.build_search_pages(query)
+			page = search_pages[self.target_site]
+			print "SEARCH_PAGE", page
+			request = Request(page, callback = self.parseResults)
+			request.meta['site'] = self.target_site
+			yield request
 
 
 	# parse results page, handle each site separately
@@ -144,6 +155,8 @@ class SearchSpider(BaseSpider):
 
 		# amazon
 		if (site == 'amazon'):
+			# amazon returns partial results as well so we can just search for the entire product name and select from there
+			items = []
 
 			#product = hxs.select("//div[@id='result_0']/h3/a/span/text()").extract()[0]
 			#TODO: refine this. get divs with id of the form result_<number>. not all of them have h3's
@@ -237,13 +250,8 @@ class SearchSpider(BaseSpider):
 
 		# sears
 
-
-	# get the name of a product from the URL of its page
-	# the URL can be from various sites
-	#def get_name(product_url):
-
-		# extract domain
-		#TODO: under construction
+		# # staples
+		# if (site == 'staples')
 
 
 
