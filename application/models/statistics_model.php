9757bec9fa651085189c0a7cb2f6204cd4109bc3
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-14 23:32:21 +0300

    test

--- application/models/statistics_model.php
+++ application/models/statistics_model.php
@@ -63,8 +63,7 @@ class Statistics_model extends CI_Model {
             ->select('s.*, rd.include_in_assess_report')
             ->from($this->tables['statistics'].' as s')
             ->join($this->tables['research_data'].' as rd', 'rd.id = s.research_data_id', 'inner')
-            ->like('s.product_name', $txt_filter)
-            ->where('s.batch_name', $batch_name)
+            ->where('s.batch_name2', $batch_name)->like('s.product_name', $txt_filter)
             ->get();
         $result =  $query->result();
         return $result;
