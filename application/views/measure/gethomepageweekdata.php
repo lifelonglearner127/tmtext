759afd181833918f1f54e42eccf742e76c3773eb
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-15 20:54:22 +0400

    385

--- application/views/measure/gethomepageweekdata.php
+++ application/views/measure/gethomepageweekdata.php
@@ -86,7 +86,7 @@
 				$("#art_img_" + item_id).html("<a href='#screen_lightbox_" + item_id  + "' data-toggle='lightbox'><img style='cursor: pointer; height: 100%' src='" + tmp_thumb + "'></a>");
 			} else {
 				$("#screen_lightbox_img_" + item_id).attr('src', data['img']);
-				$("#art_img_" + item_id).html("<a href='#screen_lightbox_" + item_id  + "' data-toggle='lightbox' style='background-image: url(" + data['thumb'] + "); background-position: top center; background-repeat: no-repeat;'></a>");
+				$("#art_img_" + item_id).html("<a href='#screen_lightbox_" + item_id  + "' data-toggle='lightbox' style='background-image: url(" + data['thumb'] + "); background-position: top left; background-repeat: no-repeat;'></a>");
 			}
 			var t = moment(data['stamp']).format('MMMM Do, YYYY');
 			$("#crawl_date_" + item_id).text(t);
@@ -110,7 +110,7 @@
 					$("#art_img_" + item_id).html("<a href='#screen_lightbox_" + item_id  + "' data-toggle='lightbox'><img style='cursor: pointer; height: 100%' src='" + tmp_thumb + "'></a>");
     			} else {
     				$("#screen_lightbox_img_" + item_id).attr('src', data[i]['cell']['img']);
-					$("#art_img_" + item_id).html("<a href='#screen_lightbox_" + item_id  + "' data-toggle='lightbox' style='background-image: url(" + data[i]['cell']['thumb'] + "); background-position: top center; background-repeat: no-repeat;'></a>");
+					$("#art_img_" + item_id).html("<a href='#screen_lightbox_" + item_id  + "' data-toggle='lightbox' style='background-image: url(" + data[i]['cell']['thumb'] + "); background-position: top left; background-repeat: no-repeat;'></a>");
     			}
     			
 				var t = moment(data[i]['cell']['screen_stamp']).format('MMMM Do, YYYY');
