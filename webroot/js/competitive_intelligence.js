a1668caa52f623ac0842c16f4b3204d92aaead9e
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-16 01:22:33 +0300

    fast price display fix

--- webroot/js/competitive_intelligence.js
+++ webroot/js/competitive_intelligence.js
@@ -2,7 +2,7 @@
 
 $( function() {
     /* Pricing */
-    readGridData(); // for pricing
+    readGridData1(); // for pricing
 });
 
 // for pricing
@@ -51,4 +51,41 @@ function readGridData() {
             $( '#ajaxLoadAni' ).fadeOut( 'slow' );
         }
     });
+}
+
+function readGridData1() {
+    //display ajax loader animation
+//    $( '#ajaxLoadAni' ).fadeIn( 'slow' );
+
+    $( '#records > tbody' ).html( '' );
+
+    //append new rows
+//    $( '#readTemplate' ).render( response ).appendTo( "#records > tbody" );
+
+    //apply dataTable to #records table and save its object in dataTable variable
+    dataTable = $( '#records' ).dataTable({
+        "bJQueryUI": true,
+        "bDestroy": true,
+        "sPaginationType": "full_numbers",
+        "bProcessing": true,
+        "bServerSide": true,
+        "sAjaxSource": readUrl,
+        "oLanguage": {
+            "sInfo": "Showing _START_ to _END_ of _TOTAL_ records",
+            "sInfoEmpty": "Showing 0 to 0 of 0 records",
+            "sInfoFiltered": ""
+        },
+        "aoColumns": [
+            {"sName" : "created"},
+            {"sName" : "url"},
+            {"sName" : "model"},
+            {"sName" : "product_name"},
+            {"sName" : "price"}
+        ]
+
+    });
+    dataTable.fnSort([[0, 'desc']]);
+
+    //hide ajax loader animation here...
+  //  $( '#ajaxLoadAni' ).fadeOut( 'slow' );
 }
