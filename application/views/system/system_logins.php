2e7ea395981f95209106066912190f36046b088c
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-21 17:28:02 +0300

    update logins

--- application/views/system/system_logins.php
+++ application/views/system/system_logins.php
@@ -9,45 +9,42 @@
         <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_reports');?>">Reports</a></li>
         <li class="active"><a data-toggle="tab" href="<?php echo site_url('system/system_logins');?>">Logins</a></li>
     </ul>
-    
+        
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>css/styles.css" />
     <div class="tab-content">
         <div id="tab9" class="tab-pane active">
-			<div>
-				<table id="records" style="width: 893px;" class="dataTable" aria-describedby="records_info">
-                    <thead>
-						<tr role="row">
-							<th class="ui-state-default" tabindex="0" rowspan="1" colspan="1" aria-label="Product Name: activate to sort column ascending" style="width: 308px;">
-								<div class="DataTables_sort_wrapper">First Name</div>
-							</th>
-							<th class="ui-state-default" tabindex="0" rowspan="1" colspan="1" aria-label="URL: activate to sort column ascending" style="width: 532px; height: 30px;">
-								<div class="DataTables_sort_wrapper">Email</div>
-							</th>
-						</tr>
-					</thead>                     
-                        <tbody class="logins_list" role="alert" aria-live="polite" aria-relevant="all">
-						</tbody>
-				</table>
-			</div>
+			<table id="records" style="width: 893px;" class="dataTable" aria-describedby="records_info">
+				<thead>
+					<tr role="row">
+						<th class="ui-state-default" tabindex="0" rowspan="1" colspan="1" aria-label="Product Name: activate to sort column ascending" style="width: 308px;">
+							<div class="DataTables_sort_wrapper">First Name</div>
+						</th>
+						<th class="ui-state-default" tabindex="0" rowspan="1" colspan="1" aria-label="URL: activate to sort column ascending" style="width: 532px; height: 30px;">
+							<div class="DataTables_sort_wrapper">Email</div>
+						</th>
+					</tr>
+				</thead>                     
+					<tbody class="logins_list" role="alert" aria-live="polite" aria-relevant="all">
+					</tbody>
+			</table>
 		</div>
 	</div>
-    
-    <script type="text/javascript">
-		$(function() {
-			function last_logins() {
-				$.get(base_url + 'index.php/system/system_last_logins', {}, function(data){
-					var tr_class = 'even';
-					$.each(data.data, function(index, value){
-						if( tr_class == 'odd' )
-							tr_class = 'even';
-						else
-							tr_class = 'odd';
-						var logins = '<tr class="'+tr_class+'"><td>'+value.first_name+'</td><td>'+value.email+'</td></tr>';
-						$('.logins_list').append(logins);
-					});
-				});
-			}
-			last_logins();
-		});
-    </script>
-    
 </div>
+
+<script type="text/javascript">
+	$(function() {
+		function last_logins() {
+			$.get(base_url + 'index.php/system/system_last_logins', {}, function(data){
+				var tr_class = 'even';
+				$.each(data.data, function(index, value){
+					if( tr_class == 'odd' ) tr_class = 'even';
+					else	tr_class = 'odd';
+					
+					var logins = '<tr class="'+tr_class+'"><td>'+value.first_name+'</td><td>'+value.email+'</td></tr>';
+					$('.logins_list').append(logins);
+				});
+			});
+		}
+		last_logins();
+	});
+</script>
