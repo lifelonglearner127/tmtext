74a51d1255084abe008dc6e1a3075f7c45e3d842
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-22 19:49:50 +0300

    #457

--- application/views/batches/index.php
+++ application/views/batches/index.php
@@ -39,6 +39,8 @@
                             }else{
                                 $('#fileupload').trigger('click');
                             }
+                        }else{
+                            $('#fileupload').trigger('click');
                         }
                         return false;
                     }
