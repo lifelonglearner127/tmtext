3c29c70dad66715b3cb25cd18e2a99a10fe92298
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-25 02:10:49 +0300

    fixed test for product matching

--- search/search/tests/test_processtext.py
+++ search/search/tests/test_processtext.py
@@ -1,13 +1,16 @@
 from nose.tools import assert_true, assert_false, assert_not_equal, assert_equal
-from ..spiders import search_spider.ProcessText
+import unittest
+#from ..spiders.search_spider import ProcessText
+from ..spiders.search_spider import ProcessText
 
 class ProcessText_test(unittest.TestCase):
 
 	def runTest(self):
 		#text = "Sony BRAVIA KDL55HX750 55-Inch 240Hz 1080p 3D LED Internet TV, Black"
+		#
 		text = "27in"
 		res = self.p.normalize(text)
-		expected_res = ""
+		expected_res = ["27\""]
 		assert_equal(res, expected_res)
 
 	def setUp(self):
