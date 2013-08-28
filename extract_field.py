8e682ffe31aed774d262d2a2d6643afb3da0f66e
lifelonglearner127 <life.long.learner127@outlook.com>
2013-08-28 17:32:47 +0300

    added script to extract field from spider output - for debugging

---
+++ extract_field.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# extract specific field from each item from spider output
+# usage: python extract_field <output_filename> <field>
+
+import json
+import sys
+import codecs
+from pprint import pprint
+
+filename = sys.argv[1]
+field = sys.argv[2]
+
+f = codecs.open(filename, "rb", "utf-8")
+fields = []
+
+for line in f:
+	item = json.loads(line.strip())
+
+	if field in item:
+		fields.append(item[field])
+
+pprint(set(fields))
+
+f.close()
