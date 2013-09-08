c6e976b792e7cceddf162d3dd000dabbf87c608b
lifelonglearner127 <life.long.learner127@outlook.com>
2013-09-08 18:43:31 +0300

    (Issue #13) updated twitter crawler - complete except for output format and applying to all input sites

--- MostSocialCompanies/twitter_crawler.py
+++ MostSocialCompanies/twitter_crawler.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python
 
 import twitter
+from datetime import datetime
+from pprint import pprint
 
-#api = twitter.Api()
+class CrawlTweets():
 
-api = twitter.Api(consumer_key='r5w2kkRf41hqVLhkDyZOQ', consumer_secret='ikmZYINui6evYZZkkyqHbVQa3LKJIaaSy6LJm2cVUk', access_token_key='1786251122-Nmr09nFArsRKRKUdvKyyyA3RCtVTvNz0eLdYLLc', access_token_secret='0AQ3rA19GQBMdmxTasRBxOTtuYPgNB7KtnGextKQ')
+	def __init__(self):
+		self.api = twitter.Api(consumer_key='r5w2kkRf41hqVLhkDyZOQ', consumer_secret='ikmZYINui6evYZZkkyqHbVQa3LKJIaaSy6LJm2cVUk', \
+			access_token_key='1786251122-Nmr09nFArsRKRKUdvKyyyA3RCtVTvNz0eLdYLLc', access_token_secret='0AQ3rA19GQBMdmxTasRBxOTtuYPgNB7KtnGextKQ')
+		self.api.VerifyCredentials()
 
-api.VerifyCredentials()
 
-# doesn't work:
-username = "@Amazon"
-statuses = api.GetUserTimeline(username)
-print [s.text for s in statuses]
+	# convert unicode date (as returned by twitter api) to datetime object
+	@staticmethod
+	def toDate(unicode_date):
+		return datetime.strptime(unicode_date, "%a %b %d %H:%M:%S +0000 %Y")
+
+	def getTweets(self, username, min_date, max_date):
+		statuses = self.api.GetUserTimeline(screen_name=username, count=100)
+
+		while CrawlTweets.toDate(statuses[-1].created_at) > min_date:
+			statuses += self.api.GetUserTimeline(screen_name=username, count=100)
+		return filter(lambda x : (CrawlTweets.toDate(x.created_at) <= max_date) \
+			and (CrawlTweets.toDate(x.created_at) >= min_date), statuses)
+
+
+ct = CrawlTweets()
+tweets = ct.getTweets("@amazon", datetime(2013, 04, 15), datetime(2013, 05, 15))
+pprint([(tweet.text, tweet.created_at) for tweet in tweets])
