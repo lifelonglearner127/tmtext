9617d5373664175e80c3cfc3b86d46e8e70f43aa
lifelonglearner127 <life.long.learner127@outlook.com>
2013-09-11 05:12:07 +0300

    (Issue #15) updated caturls spider for overstock to be more general (for any category)

--- Caturls/Caturls/spiders/caturls_spider.py
+++ Caturls/Caturls/spiders/caturls_spider.py
@@ -293,14 +293,16 @@ class CaturlsSpider(BaseSpider):
 
 			return Request(url = self.cat_page, callback = self.parsePage_sonoma)
 
-		#TODO: improve the way you build the url - currently more or less hardcoded
+		#TODO: is the list of product numbers ok for all pages? got if from laptops category request, seems to work for others as well even though it's not the same
 		if site == 'overstock':
-			# get category, and if it's laptops treat it specially using the hardcoded url
-			m = re.match("http://www.overstock.com/[^/]+/([^/]+)/.*", self.cat_page)
-			if m and m.group(1) == "Laptops":
-				return Request(url = self.cat_page + "&index=1&count=25&products=7516115,6519070,7516111,7646312,7382330,7626684,8086492,8233094,7646360,8135172,6691004,8022278&infinite=true", callback = self.parsePage_overstock, \
-					headers = {"Referer": self.cat_page + "&page=2", "X-Requested-With": "XMLHttpRequest"}, \
-					meta = {"index" : 1})
+			# # get category, and if it's laptops treat it specially using the hardcoded url
+			# m = re.match("http://www.overstock.com/[^/]+/([^/]+)/.*", self.cat_page)
+			# if m and m.group(1) == "Laptops":
+			return Request(url = self.cat_page + "&index=1&count=25&products=7516115,6519070,7516111,7646312,7382330,7626684,8086492,8233094,7646360,8135172,6691004,8022278&infinite=true", callback = self.parsePage_overstock, \
+				headers = {"Referer": self.cat_page + "&page=2", "X-Requested-With": "XMLHttpRequest"}, \
+				meta = {"index" : 1})
+			# else:
+			# 	return Request(url = self.cat_page, callback = self.parsePage_overstock)
 
 
 	# parse macy's category
@@ -507,27 +509,23 @@ class CaturlsSpider(BaseSpider):
 			url = product_link.extract()
 
 			# remove irrelevant last part of url
-			m = re.match("(.*product\.html)\?refccid.*", url)
+			m = re.match("(.*product\.html)\?re.*", url)
 			if m:
 				url = m.group(1)
 			item['product_url'] = url
 			yield item
-			# if item not in items:
-			# 	items.append(item)
-			# 	yield item
-
-		# parse next pages as well
-		index = int(response.meta['index']) + 25
 
 		# get next pages, stop when you find no more product urls
 		# url = http://www.overstock.com/Electronics/Laptops/133/subcat.html?index=101&sort=Top+Sellers&TID=SORT:Top+Sellers&count=25&products=7516115,6519070,7516111,7646312,7382330,7626684,8086492,8233094,7646360,8135172,6691004,8022278&infinite=true
 		if product_links:
-			# get category, and if it's laptops treat it specially using the hardcoded url
-			m = re.match("http://www.overstock.com/[^/]+/([^/]+)/.*", self.cat_page)
-			if m and m.group(1) == "Laptops":
-				yield Request(url = self.cat_page + "&index=" + str(index) + "&count=25&products=7516115,6519070,7516111,7646312,7382330,7626684,8086492,8233094,7646360,8135172,6691004,8022278&infinite=true", callback = self.parsePage_overstock, \
-						headers = {"Referer": self.cat_page + "&page=2", "X-Requested-With": "XMLHttpRequest"}, \
-						meta = {"index" : index})
+			# # get category, and if it's laptops treat it specially using the hardcoded url
+			# m = re.match("http://www.overstock.com/[^/]+/([^/]+)/.*", self.cat_page)
+			# if m and m.group(1) == "Laptops":
+			# parse next pages as well
+			index = int(response.meta['index']) + 25
+			yield Request(url = self.cat_page + "&index=" + str(index) + "&count=25&products=7516115,6519070,7516111,7646312,7382330,7626684,8086492,8233094,7646360,8135172,6691004,8022278&infinite=true", callback = self.parsePage_overstock, \
+					headers = {"Referer": self.cat_page + "&page=2", "X-Requested-With": "XMLHttpRequest"}, \
+					meta = {"index" : index})
 				
 
 				#TODO: same thing for other categories?
