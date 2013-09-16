2f75f94d6658662789be3b953caf7722bb0abac3
lifelonglearner127 <life.long.learner127@outlook.com>
2013-09-17 00:18:07 +0400

    wget 'playground'

--- application/controllers/measure.php
+++ application/controllers/measure.php
@@ -42,10 +42,11 @@ class Measure extends MY_Controller {
         $opt = $this->input->post('opt');
         $id = $this->input->post('id');
         $path_to_cron = base_url()."index.php/crons/site_crawler_screens?ids=$id";
+        $cmd = "";
         if($opt == 1) {
             $cmd = "wget -q $path_to_cron > /dev/null 2>&1";
-        } else {
-            $cmd = "";
+        } else if($opt == 2) {
+            $cmd = "wget -q $path_to_cron 2>&1";
         }
         shell_exec($cmd);
         $this->output->set_content_type('application/json')->set_output(json_encode($cmd));
