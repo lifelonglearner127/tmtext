4762380aaa84657175c307d7559d49d5253532c1
lifelonglearner127 <life.long.learner127@outlook.com>
2013-09-22 01:16:21 +0300

    (Issue #19) added extraction of subcategories to newegg spider

--- Newegg/Newegg/spiders/newegg_spider.py
+++ Newegg/Newegg/spiders/newegg_spider.py
@@ -25,8 +25,11 @@ class NeweggSpider(BaseSpider):
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
 
-        items = []
         category_links = hxs.select("//div[@class='itmNav']//a")
+
+        # unique ids for departments
+        department_id = 0
+
         for category_link in category_links:
 
             item = CategoryItem()
@@ -34,6 +37,49 @@ class NeweggSpider(BaseSpider):
             item['url'] = category_link.select("@href").extract()[0]
             # mark as department
             item['level'] = 1
-            items.append(item)
 
-        return items
+            # mark it as its own department, will be passed on to its subcategories
+            item['department_text'] = item['text']
+            item['department_url'] = item['url']
+            item['department_id'] = department_id
+
+            #items.append(item)
+            yield Request(url = item['url'], callback = self.parseCategory, meta = {"item" : item, \
+                'department_text' : item['department_text'], 'department_url' : item['department_url'], 'department_id' : item['department_id']})
+
+        #return items
+
+    def parseCategory(self, response):
+        hxs = HtmlXPathSelector(response)
+
+        item = response.meta['item']
+
+        #TODO
+        # extract number of products if available
+
+
+        #TODO
+        # extract description if available
+
+        yield item
+
+        parent = item
+
+        #TODO
+        # extract and parse subcategories
+        subcats = hxs.select("//dl[@class='categoryList primaryNav']/dd/a")
+        for subcat in subcats:
+            item = CategoryItem()
+            
+            item['text'] = subcat.select("text()").extract()[0].strip()
+            item['url'] = subcat.select("@href").extract()[0]
+
+            item['parent_text'] = parent['text']
+            item['parent_url'] = parent['url']
+            item['level'] = parent['level'] - 1
+            item['department_text'] = response.meta['department_text']
+            item['department_url'] = response.meta['department_url']
+            item['department_id'] = response.meta['department_id']
+
+            yield Request(url = item['url'], callback = self.parseCategory, meta = {"item" : item, \
+                "department_text" : response.meta['department_text'], "department_url" : response.meta['department_url'], "department_id" : response.meta['department_id']})
