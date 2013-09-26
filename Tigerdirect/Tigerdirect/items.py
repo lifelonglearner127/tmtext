ecb0cfe2225a8fe425fe8e259c4d350f6d80e240
lifelonglearner127 <life.long.learner127@outlook.com>
2013-09-26 21:53:23 +0300

    (Issue #19) updated tigerdirect spider - added description info

--- Tigerdirect/Tigerdirect/items.py
+++ Tigerdirect/Tigerdirect/items.py
@@ -6,9 +6,20 @@
 from scrapy.item import Item, Field
 
 class TigerdirectItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
     url = Field()
     text = Field()
-    #parent_text = Field()
-    #parent_url = Field()
+    parent_text = Field() # name of parent category
+    parent_url = Field() # url of parent category
+    grandparent_text = Field() # name of grandparent category
+    grandparent_url = Field() # url of grandparent category
+    level = Field() # level of category in the nested list (from narrower to broader categories)
+    special = Field() # is it a special category? (1 or nothing)
+    description_text = Field() # text of category description (if any)
+    description_title = Field() # title of category description (if any)
+    description_wc = Field() # number of words in description text, 0 if no description
+    keyword_count = Field()
+    keyword_density = Field()
+    nr_products = Field() # number of items in the category
+    department_text = Field() # name of the department it belongs to
+    department_url = Field() # url of the department it belongs to
+    department_id = Field() # unique id of the department it belongs to
--- Tigerdirect/Tigerdirect/spiders/tigerdirect_spider.py
+++ Tigerdirect/Tigerdirect/spiders/tigerdirect_spider.py
@@ -3,6 +3,8 @@ from scrapy.selector import HtmlXPathSelector
 from Tigerdirect.items import TigerdirectItem
 from scrapy.http import Request
 import sys
+import re
+from spiders_utils import Utils
 
 ################################
 # Run with 
@@ -27,6 +29,7 @@ class TigerdirectSpider(BaseSpider):
             item = TigerdirectItem()
             item['text'] = link.select('text()').extract()[0]
             item['url'] = link.select('@href').extract()[0]
+            item['level'] = 1
             yield Request(url = item['url'], callback = self.parseCategory, meta = {'item' : item})
 
     # receive one category url, add aditional info and return it; then extract its subcategories and parse them as well
@@ -41,7 +44,25 @@ class TigerdirectSpider(BaseSpider):
             item['nr_products'] = int(nrproducts_holder[0])
 
         # extract description if available
-        desc_holder = hxs.select("//div[@class='textBlock']")
+        description_holders = hxs.select("//div[@class='textBlock']")
+        # if the list is not empty and contains at least one non-whitespace item
+        if description_holders:
+            description_texts = description_holders.select(".//text()[not(ancestor::h2)]").extract()
+
+            # replace all whitespace with one space, strip, and remove empty texts; then join them
+            item['description_text'] = " ".join([re.sub("\s+"," ", description_text.strip()) for description_text in description_texts if description_text.strip()])
+
+            tokenized = Utils.normalize_text(item['description_text'])
+            item['description_wc'] = len(tokenized)
+
+            description_title = description_holders.select(".//h2/text()").extract()
+            if description_title:
+                item['description_title'] = description_title[0].strip()
+
+                (item['keyword_count'], item['keyword_density']) = Utils.phrases_freq(item['description_title'], item['description_text'])
+        else:
+            item['description_wc'] = 0
+
 
 
         yield item
