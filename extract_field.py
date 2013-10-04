d7eb7daf2ec12700d56b0934e0f523a7d78f32c5
lifelonglearner127 <life.long.learner127@outlook.com>
2013-10-04 19:09:09 +0300

    improved extract fields script, with refined option parsing

--- extract_field.py
+++ extract_field.py
@@ -8,28 +8,45 @@ import sys
 import codecs
 from pprint import pprint
 
-filename = sys.argv[1]
-field = sys.argv[2]
+from optparse import OptionParser
 
-if len(sys.argv) > 3:
-	filter_field = sys.argv[3]
-	value = sys.argv[4]
+parser = OptionParser()
+parser.add_option("--file", dest="filename",
+                  help="extract from file")
+parser.add_option("--field", dest="field",
+                  help="field to extract")
 
-f = codecs.open(filename, "rb", "utf-8")
+parser.add_option("--field2", dest="field2",
+                  help="second field to extract", default=None)
+
+parser.add_option("--filter_field", dest="filter_field",
+                  help="field to filter by", default=None)
+parser.add_option("--filter_value", dest="filter_value",
+                  help="field value to filter by", default=None)
+
+(options, args) = parser.parse_args()
+
+f = codecs.open(options.filename, "rb", "utf-8")
 fields = []
 
 for line in f:
 	item = json.loads(line.strip())
 
-	if field in item:
-		if len(sys.argv) > 3:
-			if filter_field in item and item[filter_field] == value:
-				fields.append(item[field])
+	if options.field in item:
+		if options.filter_field and options.filter_value:
+			if options.filter_field in item and item[options.filter_field] == options.filter_value:
+				fields.append(item[options.field])
 		else:
-			fields.append(item[field])
+			if options.field2 and options.field2 in item:
+				fields.append((item[options.field], item[options.field2]))
+			else:
+				fields.append(item[options.field])
 
 for el in sorted(fields):
-	print el.encode("utf-8")
+	if type(el) is tuple:
+		print map(lambda x: x.encode("utf-8"), el)
+	else:
+		print el.encode("utf-8")
 
 print len(fields), len(set(fields))
 
