094ec9a9b3fd6d159d22f910a50d7f5d875e79d4
lifelonglearner127 <life.long.learner127@outlook.com>
2013-10-04 20:06:50 +0300

    changed walmart spider pipelines - removed some redundant rules

--- Walmart/Walmart/pipelines.py
+++ Walmart/Walmart/pipelines.py
@@ -9,14 +9,7 @@ class WalmartPipeline(object):
 	def __init__(self):
 		self.file = open('walmart_categories.jl', 'wb')
 
-		# keep crawled items represented by (url, parent_url) pairs
-		# to eliminate duplicates
-		self.crawled = []
-	
 	def process_item(self, item, spider):
-		if 'parent_url' not in item or (item['url'], item['parent_url']) not in self.crawled:
-			line = json.dumps(dict(item)) + "\n"
-			self.file.write(line)
-			if 'parent_url' in item:
-				self.crawled.append((item['url'], item['parent_url']))
-			return item
+		line = json.dumps(dict(item)) + "\n"
+		self.file.write(line)
+		return item
