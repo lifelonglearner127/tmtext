c094f4dbd639504fe8db312ad3027c1a05db276e
lifelonglearner127 <life.long.learner127@outlook.com>
2013-10-08 20:43:17 +0300

    updated walmart spider to correct output format

--- Walmart/Walmart/pipelines.py
+++ Walmart/Walmart/pipelines.py
@@ -9,7 +9,18 @@ class WalmartPipeline(object):
 	def __init__(self):
 		self.file = open('walmart_categories.jl', 'wb')
 
+		# flag indicating if the first item has been written to output
+		self.started = False
+
 	def process_item(self, item, spider):
-		line = json.dumps(dict(item)) + ",\n"
-		self.file.write(line)
+		line = json.dumps(dict(item))
+		if self.started:
+			self.file.write(",\n" + line)
+			self.started = True
+		else:
+			self.file.write(line)
 		return item
+
+	def close_spider(self):
+		self.file.close()
+
