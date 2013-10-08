3babf4c61eccf5f494672364f3fb96cf08a0dd8e
lifelonglearner127 <life.long.learner127@outlook.com>
2013-10-08 22:20:18 +0400

    804

--- application/models/department_members_model.php
+++ application/models/department_members_model.php
@@ -219,7 +219,7 @@ class Department_members_model extends CI_Model {
     }
 
     function getDataByDepMemberId($dm_id) {
-        $sql = "SELECT `d`.`id`, `d`.`text`, `d`.`description_text`, `d`.`description_words`, `d`.`title_keyword_description_density`,
+        $sql = "SELECT `d`.`id`, `d`.`text`, `d`.`description_text`, `d`.`description_words`, `d`.`title_keyword_description_density`, `d`.`title_keyword_description_count`,
         `d`.`url`, `s`.`snap_path` FROM `department_members` as `d`
         left join `site_departments_snaps` as `s` on `d`.`id` = `s`.`dep_id`
             WHERE `d`.`id` = '".$dm_id."' and `d`.`flag`='ready' and `s`.`dep_id` is not null";
--- application/views/measure/get_board_view_item_data.php
+++ application/views/measure/get_board_view_item_data.php
@@ -1,14 +1,50 @@
 <div class="modal-body" style='overflow: hidden'>
     <div class='snap_holder'>
-        <div class='snap_area'><a target='_blank' href="<?php echo $data->snap_path ?>"><img src="<?php echo $data->snap_path ?>"></a></div>
+        <div class='snap_area'><a target='_blank' href="<?php echo $data->url ?>"><img src="<?php echo $data->snap_path ?>"></a></div>
         <div class='info_area'>
-            <b>Description word count: <?php echo $data->description_words; ?></b><br><br>
-            <?php if($data->description_words != 0) { ?>
-            <b>Category description: <?php echo $data->description_text; ?></b><br>
-            <?php } else { ?>
-            <b>Category description: N/A</b>
+            <p style='font-weight: bold; font-size: 12px;'>Description word count: <?php echo $data->description_words; ?></p>
+            <?php 
+                $k_words = array();
+                $tkdc_decoded = json_decode($data->title_keyword_description_count);
+                if(count($tkdc_decoded) > 0) {
+                    foreach($tkdc_decoded as $ki => $vi) {
+                        $mid = array(
+                            'w' => $ki,
+                            'c' => $vi,
+                            'd' => 0
+                        );
+                        $k_words[] = $mid;
+                    }
+                }
+                if(count($k_words) > 0) { 
+                    $tkdd_decoded = json_decode($data->title_keyword_description_density);
+                    if(count($tkdd_decoded) > 0) {
+                        foreach($tkdd_decoded as $kd => $vd) { // === search for density
+                            foreach ($k_words as $ks => $vs) {
+                                if($vs['w'] == $kd) {
+                                    $k_words[$ks]['d'] = $vd;
+                                }
+                            }
+                        }
+                    }
+                }
+            ?>
+            <?php if(count($k_words) > 0) { ?>
+                <p style='font-weight: bold; font-size: 12px;'>Keywords (frequency, density):</p>
+                <ul style='margin-left: 0px;'>
+                <?php foreach($k_words as $kw => $vw) { ?>
+                    <li style='font-size: 12px; list-style: none; list-style-position: inside;'><span><?php echo $vw['w']; ?> : </span><span><?php echo $vw['c']; ?></span> - <span><?php echo $vw['d'] ?>%</span></li>
+                <?php } ?>
+                </ul>
             <?php } ?>
-            <b></b>
+            <?php if($data->description_words != 0) { ?>
+            <div>
+                <p style='font-weight: bold; font-size: 12px;'>Category description:</p>
+                <div><?php echo $data->description_text; ?></div>
+            </div>
+            <?php } else { ?>
+            <p>Category description: N/A</p>
+            <?php } ?>
         </div>
     </div>
 </div>
