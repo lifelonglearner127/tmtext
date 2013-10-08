dbd5ecee1869b46d992f65c08237894040003796
lifelonglearner127 <life.long.learner127@outlook.com>
2013-10-08 16:50:31 +0300

    testing

--- application/controllers/system.php
+++ application/controllers/system.php
@@ -628,9 +628,12 @@ class System extends MY_Controller {
         $file = $this->config->item('csv_upload_dir').$this->input->post('choosen_file');
         $_rows = array();
         if (($handle = fopen($file, "r")) !== FALSE) {
-            while (($data = utf8_encode(fgetcsv($handle, 15000, "\n"))) !== FALSE) {
+            while (($data = fgetcsv($handle, 15000, "\n")) !== FALSE) {
                 var_dump($data[0]);
-                var_dump(json_decode(str_replace ('\"','"', $data[0])));
+                var_dump(utf8_encode($data[0]));
+                var_dump(json_decode(str_replace ('"','\"', utf8_encode($data[0]))));
+
+
                 if(!is_null($data[0]) && $data[0]!=''){
                     $_rows[] = json_decode($data[0]);
                 }
