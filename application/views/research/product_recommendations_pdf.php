f58c3f3d19cbc782466626248e68d80162637abd
lifelonglearner127 <life.long.learner127@outlook.com>
2013-10-11 12:53:18 +0400

    #812

--- application/views/research/product_recommendations_pdf.php
+++ application/views/research/product_recommendations_pdf.php
@@ -9,9 +9,9 @@
     </thead>
     <tbody>
         <?php foreach ($report_details as $row) { ?>
-            <?php $data = json_decode($row[10]); ?>
+            <?php $data = json_decode($row[11]); ?>
             <tr>
-                <td class="name"><?php echo $row[1]; ?></td>
+                <td class="name"><?php echo $row[2]; ?></td>
                 <td class="url"><a href="<?php echo $data->url; ?>" target="_blank"><?php echo implode('<br/>', str_split($data->url, 120)); ?></a></td>
                 <td class="recommendations">
                     <table border="0" cellspacing="0" cellpadding="0" style="border-collapse: collapse;border-spacing: 0;">
