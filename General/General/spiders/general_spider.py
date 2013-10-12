277911cf28d970f9b3f1c882cb1c53d5ab0aa71c
lifelonglearner127 <life.long.learner127@outlook.com>
2013-10-12 04:33:15 +0300

    (Issue #35) improvements for product price extraction on product page

--- General/General/spiders/general_spider.py
+++ General/General/spiders/general_spider.py
@@ -24,7 +24,7 @@ class ProductsSpider(BaseSpider):
     "amazon.com", "staples.com", "williams-sonoma.com"] # to be added - check bitbucket
     start_urls = [
         # random url
-        "http://www.staples.com",
+        "http://www.walmart.com",
     ]
 
     # store the cateory page url given as an argument into a field and add it to the start_urls list
@@ -40,6 +40,7 @@ class ProductsSpider(BaseSpider):
     def parse(self, response):
         for url in self.prod_urls:
             domain =  Utils.extract_domain(url)
+            #TODO: pass some cookie with country value for sites where price for example is displayed in local currency
             if domain != 'staples':
                 yield Request(url, callback = self.parseProdpage)
             # for staples we need extra cookies
@@ -100,10 +101,21 @@ class ProductsSpider(BaseSpider):
                         else:
                             print 'Error: no product name: ', response.url
 
+
+        #TODO: needs improvement
         # extract price
         price_holder = hxs.select("//text()[normalize-space()='$' or normalize-space()='USD' or normalize-space()='usd']/parent::*/parent::*")
         # look for number regular expressions
-        price = price_holder.select("//text")
+        price = price_holder.select(".//text()").re("[0-9\s]+")
+        # assume first value is dollars and second is cents (if they are different)
+        if len(price) > 1 and price[0] != price[1]:
+            #TODO: errors for tigerdirect when price is > 1000 => 1.999
+            price_string = "$" + price[0] + "." + price[1]
+        else:
+            price_string = price[0]
+        if price:
+            item['price'] = price_string
+            #TODO: what if there are more prices on the page, like for other products? see tigerdirect
 
 
         # # bestbuy
