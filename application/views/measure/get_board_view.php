66b895d2adc1410d57727f3e8d1d3a88da25a506
lifelonglearner127 <life.long.learner127@outlook.com>
2013-10-14 20:58:24 +0400

    814 (addition)

--- application/views/measure/get_board_view.php
+++ application/views/measure/get_board_view.php
@@ -25,34 +25,74 @@
 		// === re-order stack of items (end)
 	?>
 
-	<?php 
-		$item_per_row = 4;
-		$render_rows = ceil(count($board_list)/$item_per_row);
-	?>
+	<?php $item_per_row = 4; ?>
 
-	<?php for($i = 1; $i <= $render_rows; $i++) { ?>
-		<?php $position = $item_per_row*($i-1); ?>
-		<?php $board_row = array_slice($board_list, $position, $item_per_row); ?>
-		<div class='board_item_row'>
-			<?php foreach($board_row as $k => $v) { ?>
-			  <?php 
-			  	$red_border_cl = "";
-			  	$item_lm = "";
-			  	if($v['description_words'] == 0) {
-			  		$red_border_cl = " red_border_cl";
-			  	}
-			  	if($k != 0) $item_lm = " board_item_ml";
-			  ?>
-				<div class="board_item<?php echo $red_border_cl.$item_lm; ?>">
-					<input type='hidden' name='dm_id' value="<?php echo $v['id'] ?>">
-					<p><?php echo $v['text']; ?></p>
-					<img src="<?php echo $v['snap'] ?>">
-					<div class='prod_description_new'>
-						<p style='font-size: 11px; margin-top: 15px; margin-bottom: 0px; color: #949494'>Description: <?php echo $v['description_words'] ?> words</p>
+	<!-- Render red boxea (start) -->
+	<?php if(count($board_list_empty) > 0) { ?>
+		<?php 
+			$render_red_rows = ceil(count($board_list_empty)/$item_per_row);
+		?>
+		<?php for($i = 1; $i <= $render_red_rows; $i++) { ?>
+			<?php $position = $item_per_row*($i-1); ?>
+			<?php $board_row = array_slice($board_list_empty, $position, $item_per_row); ?>
+			<div class='board_item_row'>
+				<?php foreach($board_row as $k => $v) { ?>
+				  <?php 
+				  	$item_lm = "";
+				  	if($k != 0) $item_lm = " board_item_ml";
+				  ?>
+					<div class="board_item red_border_cl<?php echo $item_lm; ?>">
+						<input type='hidden' name='dm_id' value="<?php echo $v['id'] ?>">
+						<p class='board_item_title st'><?php echo $v['text']; ?></p>
+						<img src="<?php echo $v['snap'] ?>">
+						<div class='prod_description_new'>
+							<p style='font-size: 11px; margin-top: 15px; margin-bottom: 0px; color: #949494'>Description: <?php echo $v['description_words'] ?> words</p>
+						</div>
 					</div>
-				</div>
-			<?php } ?>
-		</div>
+				<?php } ?>
+			</div>
+		<?php } ?>
 	<?php } ?>
+	<!-- Render red boxea (end) -->
+
+	<!-- Render gray boxea (start) -->
+	<?php if(count($board_list_full) > 0) { ?>
+		<?php 
+			$render_gray_rows = ceil(count($board_list_full)/$item_per_row);
+		?>
+		<?php for($i = 1; $i <= $render_gray_rows; $i++) { ?>
+			<?php $position = $item_per_row*($i-1); ?>
+			<?php $board_row = array_slice($board_list_full, $position, $item_per_row); ?>
+			<div class='board_item_row'>
+				<?php foreach($board_row as $k => $v) { ?>
+				  <?php 
+				  	$item_lm = "";
+				  	if($k != 0) $item_lm = " board_item_ml";
+				  ?>
+					<div class="board_item <?php echo $item_lm; ?>">
+						<input type='hidden' name='dm_id' value="<?php echo $v['id'] ?>">
+						<p class='board_item_title st'><?php echo $v['text']; ?></p>
+						<img src="<?php echo $v['snap'] ?>">
+						<div class='prod_description_new'>
+							<p style='font-size: 11px; margin-top: 15px; margin-bottom: 0px; color: #949494'>Description: <?php echo $v['description_words'] ?> words</p>
+						</div>
+					</div>
+				<?php } ?>
+			</div>
+		<?php } ?>
+	<?php } ?>
+	<!-- Render gray boxea (end) -->
 
 <?php } ?>
+
+<script type='text/javascript'>
+	$(document).ready(function() {
+		$(".board_item .board_item_title").bind('mouseover', function(e) {
+			$(e.target).addClass('full');
+		});
+
+		$(".board_item .board_item_title").bind('mouseleave', function(e) {
+			$(e.target).removeClass('full');
+		});
+	});
+</script>
