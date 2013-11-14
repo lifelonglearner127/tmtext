7c37a820422c7f97792dea826848514507efffc1
lifelonglearner127 <life.long.learner127@outlook.com>
2013-11-14 22:33:41 +0400

    Debugging Issue #972

--- application/models/imported_data_parsed_model.php
+++ application/models/imported_data_parsed_model.php
@@ -2408,7 +2408,7 @@ class Imported_data_parsed_model extends CI_Model {
         $i = 0;
         foreach ($results as $result) {
             ++$i;
-            if($i==84000){
+            if($i==82000){
                 var_dump($result);echo '<br>';
                 //$i=0;
             }
