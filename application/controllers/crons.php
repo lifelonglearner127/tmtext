c3c5990c519838bddbdb362097b5bc2a7896830e
lifelonglearner127 <life.long.learner127@outlook.com>
2013-11-15 12:14:24 +0400

    do_stats_for_updated (get only products that have product name)

--- application/controllers/crons.php
+++ application/controllers/crons.php
@@ -1031,6 +1031,9 @@ class Crons extends MY_Controller {
                             $same_pr = $this->imported_data_parsed_model->get_by_custom_model($model, $im_data_id);
                         } else {
                             echo "geting custom model";
+                            if(!$data_import['product_name']){
+                                continue;
+                            }
                             $same_pr = $this->imported_data_parsed_model->getByProductNameNew($im_data_id, $data_import['product_name'], '', 0);
                             echo "custom model is ready";
                         }
