71307da10d66885fccfabc8a01adc2fc82e59006
lifelonglearner127 <life.long.learner127@outlook.com>
2013-11-16 10:52:52 +0400

    do_stats_for_updated (get only products that have product name) optimazed query

--- webroot/index.php
+++ webroot/index.php
@@ -26,7 +26,7 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
-
+error_reporting(E_ALL);
 if (defined('ENVIRONMENT'))
 {
 	switch (ENVIRONMENT)
