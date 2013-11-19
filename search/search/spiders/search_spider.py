d8f04de970edfcf81bd8ff272ce9f550d5cc6e09
lifelonglearner127 <life.long.learner127@outlook.com>
2013-11-19 19:04:22 +0200

    (Issue #3) improved splitting by 'x'

--- search/search/spiders/search_spider.py
+++ search/search/spiders/search_spider.py
@@ -798,7 +798,7 @@ class ProcessText():
 		#TODO also do something to match 30x30 with 30"x30"?
 		# replace x between numbers (or ") with space (usualy a dimension e.g. 11"x30")
 		#TODO: what if it's part of a model number? (maybe not because model numbers are all in caps?)
-		text = re.sub("(?<=[0-9\"])x(?=[0-9])", " ", text)
+		text = re.sub("(?<= |^[0-9\"])x(?=[0-9])", " ", text)
 
 		#! including ' as an exception keeps things like women's a single word. also doesn't find it as a word in wordnet -> too high a priority
 		# excluding it leads to women's->women (s is a stopword)
