ecb00a78b463327f762c7577f5629bd8f0e48b2d
lifelonglearner127 <life.long.learner127@outlook.com>
2013-11-26 03:02:35 +0200

    (Issue #43) extended ebay search spider with product page info

--- search/search/spiders/ebay_spider.py
+++ search/search/spiders/ebay_spider.py
@@ -38,27 +38,105 @@ class EbaySpider(SearchSpider):
 		else:
 			items = set()
 
+		# add product URLs to be parsed to this list
+		if 'search_results' not in response.meta:
+			product_urls = set()
+		else:
+			product_urls = response.meta['search_results']
+
 
 
 		results = hxs.select("//div[@id='ResultSetItems']//h4/a | //div[@id='PaginationAndExpansionsContainer']//h4/a")
 		for result in results:
-			item = SearchItem()
-			item['site'] = site
-			item['product_name'] = result.select("text()").extract()[0]
-			item['product_url'] = result.select("@href").extract()[0]
+			product_url = result.select("@href").extract()[0]
 
-			if 'origin_url' in response.meta:
-				item['origin_url'] = response.meta['origin_url']
+			product_urls.add(product_url)
 
-			if 'origin_id' in response.meta:
-				request.meta['origin_id'] = response.meta['origin_id']
-				assert self.by_id
-			else:
-				assert not self.by_id
+		# extract product info from product pages (send request to parse first URL in list)
+		# add as meta all that was received as meta, will pass it on to reduceResults function in the end
+		# also send as meta the entire results list (the product pages URLs), will receive callback when they have all been parsed
+
+		# send the request further to parse product pages only if we gathered all the product URLs from all the queries 
+		# (there are no more pending requests)
+		# otherwise send them back to parseResults and wait for the next query, save all product URLs in search_results
+		# this way we avoid duplicates
+		if product_urls and ('pending_requests' not in response.meta or not response.meta['pending_requests']):
+			request = Request(product_urls.pop(), callback = self.parse_product_ebay, meta = response.meta)
+			request.meta['items'] = items
+
+			# this will be the new product_urls list with the first item popped
+			request.meta['search_results'] = product_urls
+
+			return request
+
+		# if there were no results, the request will never get back to reduceResults
+		# so send it from here so it can parse the next queries
+		# add to the response the URLs of the products to crawl we have so far, items (handles case when it was not created yet)
+		# and field 'parsed' to indicate that the call was received from this method (was not the initial one)
+		else:
+			response.meta['items'] = items
+			response.meta['parsed'] = True
+			response.meta['search_results'] = product_urls
+			# only send the response we have as an argument, no need to make a new request
+			return self.reduceResults(response)
 
 
+
+	def parse_product_ebay(self, response):
+		hxs = HtmlXPathSelector(response)
+
+		items = response.meta['items']
+
+		site = response.meta['site']
+		origin_url = response.meta['origin_url']
+
+		item = SearchItem()
+		item['product_url'] = response.url
+		item['site'] = site
+		item['origin_url'] = origin_url
+
+		if 'origin_id' in response.meta:
+			item['origin_id'] = response.meta['origin_id']
+			assert self.by_id
+		else:
+			assert not self.by_id
+
+		# extract product name
+		product_name = hxs.select("//h1[@id='itemTitle']/text()").extract()
+		if not product_name:
+			self.log("Error: No product name: " + str(response.url), level=log.INFO)
+
+		else:
+
+			item['product_name'] = product_name[0]
+
+			# extract product brand
+			product_brand_holder = hxs.select("//td[@class='attrLabels'][contains(normalize-space(),'Brand')]" + \
+				"/following-sibling::node()[normalize-space()!=''][1]//text()[normalize-space()!='']").extract()
+			if product_brand_holder:
+				item['product_brand'] = product_brand_holder[0]
+
+			# extract product model
+			product_model_holder = hxs.select("//td[@class='attrLabels'][contains(normalize-space(),'Model')]" + \
+				"/following-sibling::node()[normalize-space()!=''][1]//text()[normalize-space()!='']").extract()
+
+			# add result to items
 			items.add(item)
 
-		response.meta['items'] = items
-		response.meta['parsed'] = items
-		return self.reduceResults(response)
+		if product_urls:
+			request = Request(product_urls.pop(), callback = self.parse_product_ebay, meta = response.meta)
+			request.meta['items'] = items
+			# eliminate next product from pending list (this will be the new list with the first item popped)
+			request.meta['search_results'] = product_urls
+
+			return request
+		else:
+			# otherwise, we are done, send a the response back to reduceResults (no need to make a new request)
+			# add as meta newly added items
+			# also add 'parsed' field to indicate that the parsing of all products was completed and they cand be further used
+			# (actually that the call was made from this method and was not the initial one, so it has to move on to the next request)
+
+			response.meta['parsed'] = True
+			response.meta['items'] = items
+
+			return self.reduceResults(response)
