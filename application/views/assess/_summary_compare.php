c3986427fa531060b54bc5aff34fa73e14ceaf45
lifelonglearner127 <life.long.learner127@outlook.com>
2013-11-27 03:47:12 +0200

    fixed images

--- application/views/assess/_summary_compare.php
+++ application/views/assess/_summary_compare.php
@@ -32,7 +32,7 @@
 				
 <div class="mt_10 ml_15">
 	<div class="mr_10">
-		<img src="<?php echo base_url(); ?>img/assess_report_seo.png" />
+		<img src="<?php echo base_url(); ?>img/assess_report_seo_red.png" />
 		SKUs that have product content with zero optimized keywords:
 		<span class="skus_zero_optimized_keywords mr_10" ></span>
 	</div>
@@ -40,7 +40,7 @@
 
 <div class="mt_10 ml_15">
 	<div class="mr_10">
-		<img src="<?php echo base_url(); ?>img/assess_report_seo.png" />
+		<img src="<?php echo base_url(); ?>img/assess_report_seo_red.png" />
 		SKUs that have product content with one optimized keyword:
 		<span class="skus_one_optimized_keywords mr_10" ></span>
 	</div>
@@ -48,7 +48,7 @@
 
 <div class="mt_10 ml_15">
 	<div class="mr_10">
-		<img src="<?php echo base_url(); ?>img/assess_report_seo.png" />
+		<img src="<?php echo base_url(); ?>img/assess_report_seo_yellow.png" />
 		SKUs that have product content with two optimized keywords:
 		<span class="skus_two_optimized_keywords mr_10" ></span>
 	</div>
@@ -64,7 +64,7 @@
 
 <div class="mt_10 ml_15">
 	<div class="mr_10">
-		<img src="<?php echo base_url(); ?>img/assess_report_seo.png" />
+		<img src="<?php echo base_url(); ?>img/assess_report_seo_red.png" />
 		SKUs that have fewer reviews than competitor:
 		<span class="skus_fewer_reviews_than_competitor mr_10" ></span>
 	</div>
@@ -80,7 +80,7 @@
 
 <div class="mt_10 ml_15">
 	<div class="mr_10">
-		<img src="<?php echo base_url(); ?>img/assess_report_seo.png" />
+		<img src="<?php echo base_url(); ?>img/assess_report_seo_red.png" />
 		SKUs that have 75% or more duplicate content:
 		<span class="skus_75_duplicate_content mr_10" ></span>
 	</div>
@@ -88,7 +88,7 @@
 
 <div class="mt_10 ml_15">
 	<div class="mr_10">
-		<img src="<?php echo base_url(); ?>img/assess_report_seo.png" />
+		<img src="<?php echo base_url(); ?>img/assess_report_seo_red.png" />
 		SKUs that have 50% or more duplicate content:
 		<span class="skus_50_duplicate_content mr_10" ></span>
 	</div>
@@ -96,7 +96,7 @@
 
 <div class="mt_10 ml_15">
 	<div class="mr_10">
-		<img src="<?php echo base_url(); ?>img/assess_report_seo.png" />
+		<img src="<?php echo base_url(); ?>img/assess_report_seo_yellow.png" />
 		SKUs that have 25% or more duplicate content:
 		<span class="skus_25_duplicate_content mr_10" ></span>
 	</div>
