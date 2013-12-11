9ae7972369ce3cb938abcc1a120b689be84a6b18
lifelonglearner127 <life.long.learner127@outlook.com>
2013-12-11 17:12:46 +0400

    title_keywords improvment

--- application/controllers/crons.php
+++ application/controllers/crons.php
@@ -2636,8 +2636,8 @@ class Crons extends MY_Controller {
         if (!empty($title_seo_prases)) {
             foreach($title_seo_prases as $ar_key => $seo_pr){
                 foreach($title_seo_prases as $ar_key1=>$seo_pr1){
-                    if(($ar_key!=$ar_key1 && $this->compare_str($seo_pr['ph'],$seo_pr1['ph']) 
-                            && ($seo_pr['frq']==$seo_pr1['frq'])) || substr_count($seo_pr['ph'], $seo_pr1['ph'])>0 ){
+                    if($ar_key!=$ar_key1 && $this->compare_str($seo_pr['ph'],$seo_pr1['ph']) 
+                            && ($seo_pr['frq']==$seo_pr1['frq'])){
                         unset($title_seo_prases[$ar_key1]);
                     }
                 }
