f341d096975da8d81e0933a0bd699d7161c048f4
lifelonglearner127 <life.long.learner127@outlook.com>
2013-12-13 00:10:58 +0200

    table headers for external links

--- application/views/assess/compare_results.php
+++ application/views/assess/compare_results.php
@@ -687,7 +687,7 @@
                 $('select[id="research_assess_compare_batches_batch"]').val(cmp_selected).change()
                  $('#research_assess_update').click();
                 },2000)
-				var scrollScore = 0; $(window).scroll(function(){ if(scrollScore < 10 && $( "table[id^=tblAsses] th" ).length > 0){scrollScore++;} if(scrollScore == 9){$('table#tblAssess').floatThead('reflow');} console.log(scrollScore);});
+				var scrollScore = 0; $(window).scroll(function(){ if(scrollScore < 10 && $( "table[id^=tblAsses] th" ).length > 0){scrollScore++;} if(scrollScore == 9){$('table#tblAssess').floatThead('reflow');}});
 				
             /*
 			
