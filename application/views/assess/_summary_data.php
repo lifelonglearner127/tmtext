39633c5ef483bb104f7f7cceb47fe2981bda18d7
lifelonglearner127 <life.long.learner127@outlook.com>
2013-12-16 17:13:02 +0200

    fixed issue #1134

--- application/views/assess/_summary_data.php
+++ application/views/assess/_summary_data.php
@@ -8,7 +8,7 @@
 	<?php echo render_filter_item($batch_set . 'assess_report_total_items', 'assess_report_number.png', 'Total SKUs Analyzed:', 'batch1_filter_item', $is_extended_partial, array('explanation' => 'The total number of SKUs in the batch that were analyzed.')) ?>	
 	
 	<?php if ($is_extended_partial): ?>				
-		<?php echo render_filter_item($batch_set . 'assess_report_competitor_matches_number', 'assess_report_number.png', 'Total number of corresponding SKUs found on competitor site:', 'batch2_filter_item', $is_extended_partial, array('explanation' => 'The total number of SKUs from the batch for which we found corresponding products on the competitor’s site.')) ?>			
+		<?php echo render_filter_item($batch_set . 'assess_report_competitor_matches_number', 'assess_report_number.png', 'Total number of corresponding SKUs on competitor site:', 'batch2_filter_item', $is_extended_partial, array('explanation' => 'The total number of SKUs from the batch for which we found corresponding products on the competitor’s site.')) ?>			
 	<?php endif ?>
 	
 	<?php echo render_filter_item($batch_set . 'assess_report_items_priced_higher_than_competitors', 'assess_report_dollar.png', 'SKUs priced higher than competitor:', '', $is_extended_partial, array('explanation' => 'The number of your SKUs with matching competitor SKUs that are priced higher than your competitor.')) ?>				
