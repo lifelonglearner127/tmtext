4740c2e112a7db95dd5c43261919027c1b4f19c9
lifelonglearner127 <life.long.learner127@outlook.com>
2013-12-20 18:45:46 +0400

    #1177

--- application/views/system/batch_review.php
+++ application/views/system/batch_review.php
@@ -1,22 +1,23 @@
 <div class="tabbable">
        <ul class="nav nav-tabs jq-system-tabs">
-            <li class=""><a data-toggle="tab" href="<?php echo site_url('system');?>">General</a></li>
-            <li class=""><a data-toggle="tab" href="<?php echo site_url('system/sites_view');?>">Sites</a></li>
-            <li class=""><a data-toggle="tab" href="<?php echo site_url('site_crawler');?>">Site Crawler</a></li>
-            <li class=""><a data-toggle="tab" href="<?php echo site_url('brand/import');?>">Brands</a></li>
-            <li class="active"><a data-toggle="tab" href="<?php echo site_url('system/batch_review');?>">Batch Review</a></li>
-            <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_compare');?>">Product Compare</a></li>
-            <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_productsmatch');?>">Product Match</a></li>
-           <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_reports');?>">Reports</a></li>
-           <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_logins');?>">Logins</a></li>
-           <li class=""><a data-toggle="tab" href="<?php echo site_url('system/keywords');?>">Keywords</a></li>
-           <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_rankings');?>">Rankings</a></li>
-           <li class=""><a data-toggle="tab" href="<?php echo site_url('measure/measure_pricing');?>">Pricing </a></li>
-           <li class=""><a data-toggle="tab" href="<?php echo site_url('measure/product_models');?>">Product models </a></li>
-           <li class=""><a data-toggle="tab" href="<?php echo site_url('system/snapshot_queue');?>">Snapshot Queue</a></li>
-           <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_uploadmatchurls');?>">Upload Match URLs</a></li>
-           <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_dostatsmonitor');?>">Do_stats Monitor</a></li>
-           <li class=""><a data-toggle="tab" href="<?php echo site_url('system/sync_keyword_status');?>">Sync Keyword Status</a></li>
+        <li class="active"><a data-toggle="tab" href="<?php echo site_url('system/batch_review');?>">Batch Review</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('brand/import');?>">Brands</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('site_crawler/instances_list');?>">Crawler Instances</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_dostatsmonitor');?>">Do_stats Monitor</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system');?>">General</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/keywords');?>">Keywords</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_logins');?>">Logins</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('measure/measure_pricing');?>">Pricing </a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_compare');?>">Product Compare</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_productsmatch');?>">Product Match</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('measure/product_models');?>">Product models </a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_rankings');?>">Rankings</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_reports');?>">Reports</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('site_crawler');?>">Site Crawler</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/sites_view');?>">Sites</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/snapshot_queue');?>">Snapshot Queue</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/sync_keyword_status');?>">Sync Keyword Status</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_uploadmatchurls');?>">Upload Match URLs</a></li>  
         </ul>
     <div class="tab-content">
         <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>css/smoothness/jquery-ui-1.8.2.custom.css" />
--- application/views/system/sync_keyword_status.php
+++ application/views/system/sync_keyword_status.php
@@ -1,22 +1,23 @@
 <div class="tabbable">
     <ul class="nav nav-tabs jq-system-tabs">
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('system'); ?>">General</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/sites_view'); ?>">Sites</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('site_crawler'); ?>">Site Crawler</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('brand/import'); ?>">Brands</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/batch_review'); ?>">Batch Review</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_compare'); ?>">Product Compare</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_productsmatch'); ?>">Product Match</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_reports'); ?>">Reports</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_logins'); ?>">Logins</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/keywords'); ?>">Keywords</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_rankings'); ?>">Rankings</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('measure/measure_pricing'); ?>">Pricing </a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('measure/product_models'); ?>">Product models </a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/snapshot_queue'); ?>">Snapshot Queue</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_uploadmatchurls'); ?>">Upload Match URLs</a></li>
-        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_dostatsmonitor'); ?>">Do_stats Monitor</a></li>
-        <li class="active"><a data-toggle="tab" href="<?php echo site_url('system/sync_keyword_status'); ?>">Sync Keyword Status</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/batch_review');?>">Batch Review</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('brand/import');?>">Brands</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('site_crawler/instances_list');?>">Crawler Instances</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_dostatsmonitor');?>">Do_stats Monitor</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system');?>">General</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/keywords');?>">Keywords</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_logins');?>">Logins</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('measure/measure_pricing');?>">Pricing </a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_compare');?>">Product Compare</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_productsmatch');?>">Product Match</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('measure/product_models');?>">Product models </a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_rankings');?>">Rankings</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_reports');?>">Reports</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('site_crawler');?>">Site Crawler</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/sites_view');?>">Sites</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/snapshot_queue');?>">Snapshot Queue</a></li>
+        <li class="active"><a data-toggle="tab" href="<?php echo site_url('system/sync_keyword_status');?>">Sync Keyword Status</a></li>
+        <li class=""><a data-toggle="tab" href="<?php echo site_url('system/system_uploadmatchurls');?>">Upload Match URLs</a></li>
     </ul>
     <div class="tab-content">
             <?php echo form_dropdown('batches_list', $batches_list, array(), ' class="sk_batches_list" id="sk_batches_list" style="width: 207px;float:left;margin-right: 20px;margin-top: 10px;"'); ?>
