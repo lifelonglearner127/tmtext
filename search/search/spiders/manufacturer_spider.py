e47434676b471c1c0bd558b7d4de048a8b031a04
lifelonglearner127 <life.long.learner127@outlook.com>
2013-12-21 09:33:14 +0200

    (Issue #44) cleaned manufacturer spider code

--- search/search/spiders/manufacturer_spider.py
+++ search/search/spiders/manufacturer_spider.py
@@ -28,14 +28,6 @@ class ManufacturerSpider(SearchSpider):
 		self.threshold = 0.5
 		self.fast = 0
 
-
-
-	# # parse results page, extract info for all products returned by search (keep them in "meta")
-	# def parseResults(self, response):
-	# 	if site == 'sony':
-	# 		yield self.parseResults_sony(response)
-
-
 	# parse results page, extract info for all products returned by search (keep them in "meta")
 	def parseResults(self, response):
 		hxs = HtmlXPathSelector(response)
@@ -45,12 +37,6 @@ class ManufacturerSpider(SearchSpider):
 		else:
 			items = set()
 
-		# # add product URLs to be parsed to this list
-		# if 'search_results' not in response.meta:
-		# 	product_urls = set()
-		# else:
-		# 	product_urls = response.meta['search_results']
-
 		results = hxs.select("//h2[@class='ws-product-title fn']//text()")
 
 		# if we were redirected to a product page, it means it's an exact match so stop search here
@@ -58,7 +44,6 @@ class ManufacturerSpider(SearchSpider):
 			response.meta['pending_requests'] = []
 			for result in results:
 				# we are already on the product page
-				#product_urls.add(product_url)
 				item = SearchItem()
 				item['product_url'] = response.url
 				item['product_name'] = result.extract()
@@ -79,93 +64,8 @@ class ManufacturerSpider(SearchSpider):
 				items.add(item)
 
 
-		# extract product info from product pages (send request to parse first URL in list)
-		# add as meta all that was received as meta, will pass it on to reduceResults function in the end
-		# also send as meta the entire results list (the product pages URLs), will receive callback when they have all been parsed
-
-		# send the request further to parse product pages only if we gathered all the product URLs from all the queries 
-		# (there are no more pending requests)
-		# otherwise send them back to parseResults and wait for the next query, save all product URLs in search_results
-		# this way we avoid duplicates
-		# if product_urls and ('pending_requests' not in response.meta or not response.meta['pending_requests']):
-		# 	request = Request(product_urls.pop(), callback = self.parse_product_sony, meta = response.meta)
-		# 	request.meta['items'] = items
-
-		# 	# this will be the new product_urls list with the first item popped
-		# 	request.meta['search_results'] = product_urls
-
-		# 	return request
-
-		# # if there were no results, the request will never get back to reduceResults
-		# # so send it from here so it can parse the next queries
-		# # add to the response the URLs of the products to crawl we have so far, items (handles case when it was not created yet)
-		# # and field 'parsed' to indicate that the call was received from this method (was not the initial one)
-		# else:
 		response.meta['items'] = items
 		response.meta['parsed'] = True
-		#response.meta['search_results'] = product_urls
-		# only send the response we have as an argument, no need to make a new request
+		
 		return self.reduceResults(response)
 
-	# # extract product info from a product page for sony
-	# # keep product pages left to parse in 'search_results' meta key, send back to parseResults_new when done with all
-	# def parse_product_sony(self, response):
-
-	# 	hxs = HtmlXPathSelector(response)
-
-	# 	items = response.meta['items']
-
-	# 	site = response.meta['site']
-	# 	origin_url = response.meta['origin_url']
-
-	# 	item = SearchItem()
-	# 	item['product_url'] = response.url
-	# 	item['site'] = site
-	# 	item['origin_url'] = origin_url
-
-	# 	if 'origin_id' in response.meta:
-	# 		item['origin_id'] = response.meta['origin_id']
-	# 		assert self.by_id
-	# 	else:
-	# 		assert not self.by_id
-
-
-		
-	# 	product_name = hxs.select("//h2[@class='ws-product-title fn//text()")
-	# 	if not product_name:
-	# 		self.log("Error: No product name: " + str(response.url), level=log.INFO)
-
-	# 	else:
-	# 		item['product_name'] = product_name[0].strip()
-
-	# 	# 	# extract product model number
-	# 	# 	model_number_holder = 
-	# 	# 	if model_number_holder:
-	# 	# 		item['product_model'] = model_number_holder[0]
-
-	# 	# hardcode brand as sony, no need to extract it
-	# 	item['product_brand'] = 'sony'
-
-	# 	# add result to items
-	# 	items.add(item)
-
-	# 	# if there are any more results to be parsed, send a request back to this method with the next product to be parsed
-	# 	product_urls = response.meta['search_results']
-
-	# 	if product_urls:
-	# 		request = Request(product_urls.pop(), callback = self.parse_product_amazon, meta = response.meta)
-	# 		request.meta['items'] = items
-	# 		# eliminate next product from pending list (this will be the new list with the first item popped)
-	# 		request.meta['search_results'] = product_urls
-
-	# 		return request
-	# 	else:
-	# 		# otherwise, we are done, send a the response back to reduceResults (no need to make a new request)
-	# 		# add as meta newly added items
-	# 		# also add 'parsed' field to indicate that the parsing of all products was completed and they cand be further used
-	# 		# (actually that the call was made from this method and was not the initial one, so it has to move on to the next request)
-
-	# 		response.meta['parsed'] = True
-	# 		response.meta['items'] = items
-
-	# 		return self.reduceResults(response)
