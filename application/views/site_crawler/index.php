1df37399b268fcee1976b95e23bdbe9a740b0c8f
lifelonglearner127 <life.long.learner127@outlook.com>
2013-12-23 23:25:20 +0200

    Small fix. Don't allow load system tab using ajax

--- application/views/site_crawler/index.php
+++ application/views/site_crawler/index.php
@@ -17,7 +17,7 @@
             <li class=""><a data-toggle="tab" href="<?php echo site_url('measure/measure_pricing');?>">Pricing </a></li>
             <li class=""><a data-toggle="tab" href="<?php echo site_url('measure/product_models');?>">Product models </a></li>
             <li class=""><a data-toggle="tab" href="<?php echo site_url('system/snapshot_queue');?>">Snapshot Queue</a></li>
-            <li class=""><a data-toggle="tab" href="<?php echo site_url('system/sync_keyword_status');?>">Sync Keyword Status</a></li>       
+            <li class=""><a data-toggle="tab" href="<?php echo site_url('system/sync_keyword_status');?>">Sync Keyword Status</a></li>
     </ul>
 	  <div class="tab-content">
 
