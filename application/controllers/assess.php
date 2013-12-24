59bd839e5d8449e36a99e79dbeec6148d8435ca8
lifelonglearner127 <life.long.learner127@outlook.com>
2013-12-25 00:12:22 +0200

    fixed 1216

--- application/controllers/assess.php
+++ application/controllers/assess.php
@@ -4510,7 +4510,7 @@ class Assess extends MY_Controller {
             // echo "<br />";
             // echo "<br />";
 
-            if ($c < ($display_start + $display_length - 1) && $this->checkSuccessFilterEntries($success_filter_entries, $build_assess_params->summaryFilterData)) {
+            if ($c >= $display_start && $c < ($display_start + $display_length - 1) && $this->checkSuccessFilterEntries($success_filter_entries, $build_assess_params->summaryFilterData)) {
                 $result_table[] = $result_row;
             }
 			//we can't do this verification bc filters should be calculated for all items in the batch
