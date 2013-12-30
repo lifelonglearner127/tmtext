3ed850209b651fb57999597c5acef81c72120e9c
lifelonglearner127 <life.long.learner127@outlook.com>
2013-12-30 20:21:45 +0200

    Revert "init"
    This reverts commit df93b3035512cf68d33402b01782255fd3e3644a.

--- webroot/js/initdata.js
+++ webroot/js/initdata.js
@@ -27,6 +27,11 @@ var long_wc_total_not_0 = 0;
 var items_short_products_content_short = 0;
 var items_long_products_content_short = 0;
 var columns = [
+        // { // I.L.
+        //     "sTitle": "Own Price",
+        //     "sName": "price",            
+        //     "sClass": "own_price_text"
+        // },
         {
             "sTitle": "Snapshot",
             "sName": "snap",            
@@ -105,11 +110,6 @@ var columns = [
             "sTitle": "Title",
             "sName": "title_pa",            
             "sClass": "title_pa"
-        },
-        {
-            "sTitle": "Links",
-            "sName": "links_count",            
-            "sClass": "links_count"
         },
         {
             "sTitle": "<span class='subtitle_desc_long' >Long </span>Description",
@@ -254,6 +254,26 @@ var columns = [
             "sName": "Meta_Keywords1",            
             "sClass": "Meta_Keywords1"
         },
+		{
+            "sTitle": "Title Keywords",
+            "sName": "title_seo_phrases1",            
+            "sClass": "title_seo_phrases1"
+        },
+		 {
+            "sTitle": "Images",
+            "sName": "images_cmp1",            
+            "sClass": "images_cmp1"
+        },
+        {
+            "sTitle": "Videos",
+            "sName": "video_count1",            
+            "sClass": "video_count1"
+        },
+        {
+            "sTitle": "Title",
+            "sName": "title_pa1",            
+            "sClass": "title_pa1"
+        },
         {
             "sTitle": "<span class='subtitle_desc_long1' >Long </span>Description",
             "sName": "Long_Description1",            
@@ -275,11 +295,7 @@ var columns = [
             "sName" : "Meta_Description_Count1",             
             "sClass" : "Meta_Description_Count1"            
         },
-        {
-            "sTitle": "Third Party Content",
-            "sName": "column_external_content1",            
-            "sClass" :  "column_external_content1"
-        },
+        
         {
             "sTitle" : "H1 Tags", 
             "sName":"H1_Tags1",             
@@ -299,6 +315,11 @@ var columns = [
             "sTitle" : "Chars", 
             "sName":"H2_Tags_Count1",             
             "sClass" :  "HTags1 CharsHTags1"
+        }, 
+		{
+            "sTitle": "Third Party Content",
+            "sName": "column_external_content1",            
+            "sClass" :  "column_external_content1"
         },
         {
             "sTitle": "Reviews",
@@ -316,39 +337,14 @@ var columns = [
             "sClass" :  "column_features1"
         },
         {
-            "sTitle": "Title Keywords",
-            "sName": "title_seo_phrases1",            
-            "sClass": "title_seo_phrases1"
-        },
-        {
-            "sTitle": "Images",
-            "sName": "images_cmp1",            
-            "sClass": "images_cmp1"
-        },
-        {
-            "sTitle": "Videos",
-            "sName": "video_count1",            
-            "sClass": "video_count1"
-        },
-        {
-            "sTitle": "Title",
-            "sName": "title_pa1",            
-            "sClass": "title_pa1"
-        },
-        {
-            "sTitle": "Links",
-            "sName": "links_count1",            
-            "sClass": "links_count1"
-        },
-        {
             "sTitle": "Gap Analysis",
             "sName": "gap",            
-            "sClass" :  ""
+            "sClass" :  "gap"
         },
         {
             "sTitle": "Duplicate Content",
             "sName": "Duplicate_Content",            
-            "sClass" :  ""
+            "sClass" :  "Duplicate_Content"
         },
 
     ];
@@ -412,14 +408,8 @@ var summaryFieldNames = [
 		'skus_with_more_than_one_product_image',
 		'skus_with_no_product_images_competitor',
 		'skus_with_one_product_image_competitor',
+
 		'skus_with_more_than_one_product_image_competitor',
-		
-		'skus_with_zero_product_description_links',
-		'skus_with_zero_product_description_links_competitor',
-		'skus_with_more_than_one_product_description_links',
-		'skus_with_more_than_one_product_description_links_competitor',
-		
-		
 		'assess_report_items_priced_higher_than_competitors'
 	];
 	
@@ -438,6 +428,7 @@ var summaryFieldNames = [
 	
     var tableCase = {
         details: [
+            // "price", // I.L.
             "snap",
             "created",
             "imp_data_id",
@@ -471,8 +462,7 @@ var summaryFieldNames = [
             "average_review",
             "column_features",
             "price_diff",
-            "product_selection",
-            "links_count"
+            "product_selection"
 
         ],
          details_compare: [
@@ -529,8 +519,7 @@ var summaryFieldNames = [
             "video_count1",
             "title_pa1",
             "gap",
-            "Duplicate_Content",
-            "links_count"
+            "Duplicate_Content"
             
         ],
         recommendations: [
