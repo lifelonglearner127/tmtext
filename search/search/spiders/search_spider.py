8c262b7a441d1907f71f319aa97de401103df0a4
lifelonglearner127 <life.long.learner127@outlook.com>
2013-12-31 14:49:30 +0200

    in search spider: restored default threshold to lower value

--- search/search/spiders/search_spider.py
+++ search/search/spiders/search_spider.py
@@ -51,7 +51,7 @@ class SearchSpider(BaseSpider):
 	#				output - integer(1/2) option indicating output type (either result URL (1), or result URL and source product URL (2))
 	#				threshold - parameter for selecting results (the lower the value the more permissive the selection)
 	def __init__(self, product_name = None, product_url = None, product_urls_file = None, walmart_ids_file = None, \
-		output = 1, threshold = 1.2, outfile = "search_results.txt", outfile2 = "not_matched.txt", fast = 0, use_proxy = False, by_id = False):
+		output = 1, threshold = 1.0, outfile = "search_results.txt", outfile2 = "not_matched.txt", fast = 0, use_proxy = False, by_id = False):
 
 		# call specific init for each derived class
 		self.init_sub()
