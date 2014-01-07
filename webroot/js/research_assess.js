b12ef484741c22e6c91a1f1e75d31c639e6c0e0b
lifelonglearner127 <life.long.learner127@outlook.com>
2014-01-07 21:34:59 +0200

    fixed columns issue

--- webroot/js/research_assess.js
+++ webroot/js/research_assess.js
@@ -417,7 +417,10 @@ $(function() {
 				hideColumns(); 									
 				setBorderSeparator();					
 				loadSetTK();
-																			 
+				
+				// bad hack, Nikita, please check
+				$('#tblAssess thead th').css('width', '100%');	
+				
 				resizeImpDown();										
 			},
 			fnRowCallback : function(nRow, aData, iDisplayIndex) {					
@@ -426,7 +429,7 @@ $(function() {
 			},
 			fnDrawCallback : function(oSettings) {
 				console.log('local draw callback');					
-				tblAssess.fnAdjustColumnSizing(false);				
+				tblAssess.fnAdjustColumnSizing(false);						
 			},
 			fnPreDrawCallback : function( oSettings ) {										
 				buildReport(json_data);
