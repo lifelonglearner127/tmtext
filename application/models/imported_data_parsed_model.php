8b57bdc2c05a3b21e2f36c5d53ce8b8319bb8026
lifelonglearner127 <life.long.learner127@outlook.com>
2014-01-11 14:17:52 +0400

    product models change

--- application/models/imported_data_parsed_model.php
+++ application/models/imported_data_parsed_model.php
@@ -2571,9 +2571,13 @@ echo "j  = ".$j;
     }
 
     function give_model($im_id, $model) {
+        $model = trim($model);
         $update_object = array(
             'model' => $model,
         );
+        $this->db->delete('statistics_new', array('imported_data_id' => $im_id));
+        $this->db->like('similar_products_competitors', $im_id);
+        $this->db->delete('statistics_new'); 
         $this->db->where('imported_data_id', $im_id);
         $this->db->update($this->tables['imported_data_parsed'], $update_object);
     }
@@ -2584,6 +2588,9 @@ echo "j  = ".$j;
         $update_object = array(
             'model' => NULL,
         );
+        $this->db->delete('statistics_new', array('imported_data_id' => $im_id));
+        $this->db->like('similar_products_competitors', $im_id);
+        $this->db->delete('statistics_new');
         $this->db->where('imported_data_id', $im_id);
         $this->db->update($this->tables['imported_data_parsed'], $update_object);
     }
