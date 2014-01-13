d086c1adde3398ddb8f0998116266ea947709ee0
lifelonglearner127 <life.long.learner127@outlook.com>
2014-01-14 00:58:53 +0200

    Issue #1297: testing

--- application/controllers/assess.php
+++ application/controllers/assess.php
@@ -4208,18 +4208,18 @@ class Assess extends MY_Controller {
 					$skus_three_optimized_keywords_competitor++;			
 					$this->filterBySummaryCriteria('skus_three_optimized_keywords_competitor', $build_assess_params->summaryFilterData, $success_filter_entries, $stored_filter_items, $row_iterator);
 				}
-				//if (isset($data_row->mvid)) {
+				if (isset($data_row->mvid)) {
 							$skus_with_manufacturer_videos++;
 							$this->filterBySummaryCriteria('skus_with_manufacturer_videos_available', $build_assess_params->summaryFilterData, $success_filter_entries, $stored_filter_items, $row_iterator);
-				//}
-			        //if (isset($data_row->mimg)) {
+				}
+			        if (isset($data_row->mimg)) {
 							$skus_with_manufacturer_images++;
 							$this->filterBySummaryCriteria('skus_with_manufacturer_images_available', $build_assess_params->summaryFilterData, $success_filter_entries, $stored_filter_items, $row_iterator);
-				//} 
-				//if (isset($data_row->murl)) { 
+				} 
+				if (isset($data_row->murl)) { 
 							$skus_with_manufacturer_pages++;
 							$this->filterBySummaryCriteria('skus_with_manufacturer_pages', $build_assess_params->summaryFilterData, $success_filter_entries, $stored_filter_items, $row_iterator);
-				//}				
+				}				
             }
 					
 			if ($row_iterator < $iterator_limit)
