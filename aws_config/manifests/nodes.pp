d73e608b4b4fbb94c96a8d9495549ddb5c33f5bb
lifelonglearner127 <life.long.learner127@outlook.com>
2014-01-26 02:13:17 +0200

    in aws config - deleted clone_repo script (not needed anymore), fixed clone command in manifest

--- aws_config/manifests/nodes.pp
+++ aws_config/manifests/nodes.pp
@@ -82,7 +82,7 @@ node 'node1', 'node2', 'node3', 'node4', 'node5', 'node6', 'node7', 'node8', 'no
 		cwd => "/home/ubuntu",
 		user => "ubuntu",
 		provider => "shell",
-		command => "git clone -q git@bitbucket.org:dfeinleib/tmtext.git",
+		command => "/usr/bin/git clone -q git@bitbucket.org:dfeinleib/tmtext.git",
 		require => package['git'],
 	}
 
