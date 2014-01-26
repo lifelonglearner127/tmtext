a296a23b32711f7194c7a68cf2b31594b700162f
lifelonglearner127 <life.long.learner127@outlook.com>
2014-01-26 02:31:27 +0200

    fix in clone command in puppet manifest

--- aws_config/manifests/nodes.pp
+++ aws_config/manifests/nodes.pp
@@ -81,7 +81,6 @@ node 'node1', 'node2', 'node3', 'node4', 'node5', 'node6', 'node7', 'node8', 'no
 		creates => "/home/ubuntu/tmtext",
 		cwd => "/home/ubuntu",
 		user => "ubuntu",
-		provider => "shell",
 		command => "/usr/bin/git clone -q git@bitbucket.org:dfeinleib/tmtext.git",
 		require => package['git'],
 	}
