121f9f06044f2b81bd3883b27d2c57e56075eea2
lifelonglearner127 <life.long.learner127@outlook.com>
2014-01-27 10:23:18 +0200

    change preloader

--- application/views/system/sites_view.php
+++ application/views/system/sites_view.php
@@ -619,7 +619,7 @@
                                 url: url,
                                 dataType: 'json',
                                 //add preloader
-                                change: function(){$("#second_department").parent().prepend("<img id='preloader-dc' src='"+base_url+"/img/287'>");},
+                                change: function(){$("#second_department").parent().prepend("<img id='preloader-dc' src='"+base_url+"/img/loader_scr.gif'>");},
                                 done: function (e, data) {
           
                                     $('input[name="choosen_file"]').val(data.result.files[0].name);
