075960239ce6465741ba4b6e19dfc05305dd09cd
lifelonglearner127 <life.long.learner127@outlook.com>
2014-01-29 05:17:24 +0200

    fixed migration

--- application/migrations/015_filters_items_update.php
+++ application/migrations/015_filters_items_update.php
@@ -8,14 +8,14 @@ class Migration_Filters_items_update extends CI_Migration
 	public function up()
 	{
 		$this->db->query("
-			ALTER TABLE  `filters_items` CHANGE  `filters_values_ids`  `filter_id` INT( 11 ) NOT NULL;
+			ALTER TABLE  `filters_items` CHANGE  `filters_values_id`  `filter_id` INT( 11 ) NOT NULL;
 		");
 	}
 
 	public function down()
 	{
 		$this->db->query("
-			ALTER TABLE  `filters_items` CHANGE  `filter_id`  `filters_values_ids` INT( 11 ) NOT NULL;
+			ALTER TABLE  `filters_items` CHANGE  `filter_id`  `filters_values_id` INT( 11 ) NOT NULL;
 		");
 	}
 
