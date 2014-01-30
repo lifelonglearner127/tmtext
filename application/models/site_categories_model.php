4e41a10f9cbae96c9695d5769a9558fce490eae6
lifelonglearner127 <life.long.learner127@outlook.com>
2014-01-30 15:38:07 +0200

    repeat sub cat

--- application/models/site_categories_model.php
+++ application/models/site_categories_model.php
@@ -219,16 +219,15 @@ class Site_categories_model extends CI_Model {
 
     function getDescriptionData($site_id)
     {
-        $sql = $this->db->query("SELECT count(SELECT COUNT( * ) AS duplicate_count
-                                        FROM (
-
-                                        SELECT TEXT
-                                        FROM  `site_categories` 
-                                        WHERE site_id =".$site_id."
-                                        AND  `flag` =  'ready'
-                                        GROUP BY TEXT
-                                        HAVING COUNT( TEXT ) >0
-                                        ) AS t) as total,
+        $sql = $this->db->query("SELECT c(SELECT COUNT( * ) AS duplicate_count
+                            FROM (
+                            SELECT TEXT
+                            FROM  `site_categories` 
+                            WHERE site_id =".$site_id." 
+                            AND  `flag` =  'ready'
+                            GROUP BY TEXT
+                            HAVING COUNT( TEXT ) >0
+                            ) AS t) as total, 
                 (SELECT round(avg(`description_words`)) AS c  FROM `site_categories` WHERE `site_id`=".$site_id." and `description_words`>0 GROUP BY `site_id`) as res_avg,
                 count(if((`description_words`>0 and `description_words`<250), id, null)) as more,
                 count(if(`description_words`>0, id, null)) as more_than_0
