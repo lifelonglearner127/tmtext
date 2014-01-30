4af26c1ec0e3d1c21d5844b6a46d6e7734a945d0
lifelonglearner127 <life.long.learner127@outlook.com>
2014-01-30 14:41:19 +0200

    in aws_config - changed run_crawler script to use fast option, halt with sudo

--- aws_config/run_crawler.sh
+++ aws_config/run_crawler.sh
@@ -2,12 +2,13 @@
 cd /home/ubuntu/tmtext/search;
 BATCH=$1
 OUT=/home/ubuntu/shared_sshfs/walmart_amazon_40000_"$BATCH"_matches.csv
-time scrapy crawl amazon -a product_urls_file=/home/ubuntu/shared_sshfs/walmart_urls_40000_$BATCH.csv -s LOG_ENABLED=1 -s LOG_LEVEL="INFO" -s HTTPCACHE_ENABLED=0 \
+time scrapy crawl amazon -a product_urls_file=/home/ubuntu/shared_sshfs/walmart_urls_40000_$BATCH.csv \
+-s LOG_ENABLED=1 -s LOG_LEVEL="INFO" -s HTTPCACHE_ENABLED=0 -a fast=1 \
 -a outfile=$OUT 2>/home/ubuntu/shared_sshfs/search_log_walmart_amazon_40000_$BATCH.txt;
 sleep 10;
 
 # only halt if this is not batch 1 (node 1 hosts shared folder)
 if [ "$BATCH" -ne 1 ]
 	then
-	halt;
+	sudo halt;
 fi
