32771c5a0d7c115d3f6323a6caec6746fd6d8fc6
lifelonglearner127 <life.long.learner127@outlook.com>
2014-01-31 00:20:39 +0200

    in run_crawler - not halting machines after they're finished

--- aws_config/run_crawler.sh
+++ aws_config/run_crawler.sh
@@ -7,10 +7,10 @@ time scrapy crawl amazon -a product_urls_file=/home/ubuntu/shared_sshfs/walmart_
 -a outfile=$OUT 2>/home/ubuntu/shared_sshfs/search_log_walmart_amazon_40000_unmatched_$BATCH.txt;
 sleep 10;
 
-# only halt if this is not batch 1 (node 1 hosts shared folder)
-#if [ "$BATCH" -ne 1 ]
-HOST=`hostname`; 
-if [ "$HOST" != "node1" ]
-	then
-	sudo halt;
-fi
+# # only halt if this is not batch 1 (node 1 hosts shared folder)
+# #if [ "$BATCH" -ne 1 ]
+# HOST=`hostname`; 
+# if [ "$HOST" != "node1" ]
+# 	then
+# 	sudo halt;
+# fi
