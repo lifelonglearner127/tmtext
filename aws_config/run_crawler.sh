76084ce3a54bdb449a175a6dc35bee5b28f2b9a6
lifelonglearner127 <life.long.learner127@outlook.com>
2014-01-31 05:10:09 +0200

    disabled fast option in run_crawler

--- aws_config/run_crawler.sh
+++ aws_config/run_crawler.sh
@@ -3,7 +3,7 @@ cd /home/ubuntu/tmtext/search;
 BATCH=$1
 OUT=/home/ubuntu/shared_sshfs/walmart_amazon_40000_unmatched_"$BATCH"_matches.csv
 time scrapy crawl amazon -a product_urls_file=/home/ubuntu/shared_sshfs/walmart_urls_40000_unmatched_$BATCH.csv \
--s LOG_ENABLED=1 -s LOG_LEVEL="INFO" -s HTTPCACHE_ENABLED=0 -a fast=1 \
+-s LOG_ENABLED=1 -s LOG_LEVEL="INFO" -s HTTPCACHE_ENABLED=0 -a fast=0 \
 -a outfile=$OUT 2>/home/ubuntu/shared_sshfs/search_log_walmart_amazon_40000_unmatched_$BATCH.txt;
 sleep 10;
 
