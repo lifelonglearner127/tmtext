48851befdaa87113c2732a11d507184e3769d858
lifelonglearner127 <life.long.learner127@outlook.com>
2014-02-01 02:21:34 +0200

    minor change in run_crawler - changed input names

--- aws_config/run_crawler.sh
+++ aws_config/run_crawler.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /home/ubuntu/tmtext/search;
 BATCH=$1
-OUT=/home/ubuntu/shared_sshfs/walmart_amazon_40000_unmatched3_"$BATCH"_matches.csv
+OUT=/home/ubuntu/shared_sshfs/walmart_amazon_40000_unmatched4_"$BATCH"_matches.csv
 COOKIES=/home/ubuntu/shared_sshfs/cookies_node"$BATCH".jl
-time scrapy crawl amazon -a product_urls_file=/home/ubuntu/shared_sshfs/walmart_urls_40000_unmatched3_$BATCH.csv \
+time scrapy crawl amazon -a product_urls_file=/home/ubuntu/shared_sshfs/walmart_urls_40000_unmatched4_$BATCH.csv \
 -s LOG_ENABLED=1 -s LOG_LEVEL="INFO" -s HTTPCACHE_ENABLED=0 -a fast=1 \
--a outfile=$OUT -a cookies_file=$COOKIES 2>/home/ubuntu/shared_sshfs/search_log_walmart_amazon_40000_unmatched3_$BATCH.txt;
+-a outfile=$OUT -a cookies_file=$COOKIES 2>/home/ubuntu/shared_sshfs/search_log_walmart_amazon_40000_unmatched4_$BATCH.txt;
 sleep 10;
 
 # only halt if this is not batch 1 (node 1 hosts shared folder)
--- aws_config/setup_sshfs.sh
+++ aws_config/setup_sshfs.sh
@@ -5,9 +5,9 @@
 #IP="10.170.60.95"
 
 # usually for node 2..10
+./get_private_ips.sh 1
 for node in "$@"
 do
-	#./get_private_ips.sh
 	IP=`cat hosts | python -c "import sys; print sys.stdin.read().split()[1]"`
 	#vagrant ssh node$node -c 'rm /home/ubuntu/shared_sshfs/*; /usr/bin/sshfs -d -o StrictHostKeyChecking=no ubuntu@10.91.176.34:/home/ubuntu/shared_sshfs /home/ubuntu/shared_sshfs 2> sshfslog; sleep 5; ls /home/ubuntu/shared_sshfs';
 	#vagrant ssh node$node -c 'screen -dm /usr/bin/nohup /usr/bin/sshfs -d -o ssh_command="ssh -i /home/ubuntu/.ssh/id_rsa" -o StrictHostKeyChecking=no ubuntu@10.91.176.34:/home/ubuntu/shared_sshfs /home/ubuntu/shared_sshfs &';
