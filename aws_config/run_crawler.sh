a9efab730f4a815ae3f2614dc950e2245704880d
lifelonglearner127 <life.long.learner127@outlook.com>
2014-02-20 11:58:21 +0200

    in run_crawler script, changed output type to 3 (including confidence)

--- aws_config/run_crawler.sh
+++ aws_config/run_crawler.sh
@@ -4,7 +4,7 @@ BATCH=$1
 OUT=/home/ubuntu/shared_sshfs/walmart_amazon_40000_"$BATCH"_matches.csv
 COOKIES=/home/ubuntu/shared_sshfs/cookies_node"$BATCH".jl
 time scrapy crawl amazon -a product_urls_file=/home/ubuntu/shared_sshfs/walmart_urls_40000_"$BATCH".csv \
--s LOG_ENABLED=1 -s LOG_LEVEL="INFO" -s HTTPCACHE_ENABLED=0 -a fast=0 \
+-s LOG_ENABLED=1 -s LOG_LEVEL="INFO" -s HTTPCACHE_ENABLED=0 -a fast=0 -a output=3\
 -a outfile=$OUT 2>/home/ubuntu/shared_sshfs/search_log_walmart_amazon_40000_"$BATCH".txt;
 sleep 10;
 
