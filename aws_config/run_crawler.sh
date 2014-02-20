7ebbac07354ed4ace8097cedbb5bbbe11b6af033
lifelonglearner127 <life.long.learner127@outlook.com>
2014-02-20 12:19:09 +0200

    fixed typo in run_crawler script

--- aws_config/run_crawler.sh
+++ aws_config/run_crawler.sh
@@ -4,11 +4,11 @@ BATCH=$1
 OUT=/home/ubuntu/shared_sshfs/walmart_amazon_40000_"$BATCH"_matches.csv
 COOKIES=/home/ubuntu/shared_sshfs/cookies_node"$BATCH".jl
 time scrapy crawl amazon -a product_urls_file=/home/ubuntu/shared_sshfs/walmart_urls_40000_"$BATCH".csv \
--s LOG_ENABLED=1 -s LOG_LEVEL="INFO" -s HTTPCACHE_ENABLED=0 -a fast=0 -a output=3\
+-s LOG_ENABLED=1 -s LOG_LEVEL="INFO" -s HTTPCACHE_ENABLED=0 -a fast=0 -a output=3 \
 -a outfile=$OUT 2>/home/ubuntu/shared_sshfs/search_log_walmart_amazon_40000_"$BATCH".txt;
 sleep 10;
 
-only halt if this is not batch 1 (node 1 hosts shared folder)
+# only halt if this is not batch 1 (node 1 hosts shared folder)
 if [ "$BATCH" -ne 1 ]
 HOST=`hostname`; 
 if [ "$HOST" != "node1" ]
