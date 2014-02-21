56c11b26fc5fe7a89773ca062c59832d42cdd88e
lifelonglearner127 <life.long.learner127@outlook.com>
2014-02-21 17:45:49 +0200

    parametrized run_crawler script with input file and target site

--- aws_config/run_crawler.sh
+++ aws_config/run_crawler.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 cd /home/ubuntu/tmtext/search;
+# batch number
 BATCH=$1
-OUT=/home/ubuntu/shared_sshfs/walmart_amazon_40000_"$BATCH"_matches.csv
+# input file base name (wihout extension)
+INPUT=$2
+# target site
+SITE=$3
+
+IN=/home/ubuntu/shared_sshfs/"$INPUT"_"$BATCH".csv
+OUT=/home/ubuntu/shared_sshfs/"$INPUT"_"$SITE"_"$BATCH"_matches.csv
 COOKIES=/home/ubuntu/shared_sshfs/cookies_node"$BATCH".jl
-time scrapy crawl amazon -a product_urls_file=/home/ubuntu/shared_sshfs/walmart_urls_40000_"$BATCH".csv \
+time scrapy crawl $SITE -a product_urls_file=$IN \
 -s LOG_ENABLED=1 -s LOG_LEVEL="INFO" -s HTTPCACHE_ENABLED=0 -a fast=0 -a output=3 \
--a outfile=$OUT 2>/home/ubuntu/shared_sshfs/search_log_walmart_amazon_40000_"$BATCH".txt;
+-a outfile=$OUT 2>/home/ubuntu/shared_sshfs/search_log_"$INPUT"_"$SITE"_"$BATCH".txt;
 sleep 10;
 
 # only halt if this is not batch 1 (node 1 hosts shared folder)
