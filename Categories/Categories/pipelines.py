a5d1318a3a9a8e3507b3e4b09a7b1e46564bb965
lifelonglearner127 <life.long.learner127@outlook.com>
2014-02-24 20:09:55 +0200

    (Issue #51) implemented building of categories tree in pipelines (process_item_fortree method)

--- Categories/Categories/pipelines.py
+++ Categories/Categories/pipelines.py
@@ -19,13 +19,16 @@ class LinesPipeline(object):
 # write each JSON object on one line, lines separated by commas
 class CommaSeparatedLinesPipeline(object):
 
-    # categories tree - tree of entire sitemap structured as dictionary with keys being URLs of categories, and values being a dictionary containing a list of its subcategories URLs
-    # if has_tree flag is on in spider, this tree will be built and used to aggregate number of products for each category where it's not explicitly specified on the site
-    categories_tree = {}
+	# categories tree - tree of entire sitemap structured as dictionary with keys being URLs of categories, and values being dictionaries containing:
+	# - item represented by that URL
+	# - list of subcategories of that item (just their URLs)
+	# if has_tree flag is on in spider, this tree will be built and used to aggregate number of products for each category where it's not explicitly specified on the site
+	categories_tree = {}
 
 	def __init__(self):
 		# flag indicating if the first item has been written to output
 		self.started = False
+
 
 	def open_spider(self, spider):
 		filename = spider.name + "_categories.jl"
@@ -33,7 +36,19 @@ class CommaSeparatedLinesPipeline(object):
 
 	# process item in spider for which we build the sitemap tree
 	def process_item_fortree(self, item):
-		pass
+		# add key-value pair where key is current's item URL
+		# and value is dict containing item and subcategories list (for now empty)
+		self.categories_tree[item['url']]['item'] = item
+		# create subcategories list if it doesn't exist (may have been created by previous items)
+		if 'subcategories' not in self.categories_tree[item['url']]:
+			self.categories_tree[item['url']]['subcategories'] = []
+		
+		# add item URL to subcategories list of its parent's element in the tree
+		# create parent item in categories tree if it doesn't exist
+		if item['parent_url'] not in categories_tree:
+			self.categories_tree[item['parent_url']] = {'subcategories': []}
+		# append url to the parent's subcategories list
+		self.categories_tree[item['parent_url']]['subcategories'].append(item['url'])
 
 	def process_item(self, item, spider):
 		if spider.has_tree:
