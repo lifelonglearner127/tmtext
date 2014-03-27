a7e3b62c4462748db67f51d3de565201cf0fb629
lifelonglearner127 <life.long.learner127@outlook.com>
2014-03-27 19:24:34 -0300

    Reorganized logging.

--- page_fetcher/page_fetcher/pipelines.py
+++ page_fetcher/page_fetcher/pipelines.py
@@ -3,7 +3,8 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import scrapy.log
+from scrapy.log import INFO, ERROR
+from scrapy.log import msg
 import requests
 
 from page_fetcher.items import (PageItem, RequestErrorItem)
@@ -15,6 +16,9 @@ class PageFetcherPipeline(object):
     URL_SAVED_PARSED_FROM_TEXT = 'save_parsed_from_text/'
     URL_URL_LOAD_FAILED = 'url_load_failed/'
 
+    def __init__(self):
+        self.log = msg
+
     def process_item(self, item, spider):
         if isinstance(item, PageItem):
             self.save_page(item)
@@ -24,8 +28,7 @@ class PageFetcherPipeline(object):
         return item
 
     def save_page(self, page_item):
-        scrapy.log.msg("Saving page %s." % page_item.get('url'),
-                       level=scrapy.log.INFO)
+        self.log("Saving page %s." % page_item.get('url'), level=INFO)
         r = requests.post(self.URL_BASE + self.URL_SAVED_PARSED_FROM_TEXT, {
             'url': page_item['url'],
             'id': page_item['id'],
@@ -37,19 +40,18 @@ class PageFetcherPipeline(object):
 
         if r.status_code < 400:
             msg = "Page saved (%d)." % r.status_code
-            level = scrapy.log.INFO
+            level = INFO
         elif r.status_code < 500:
             msg = "Page save failed because of us (%d)!" % r.status_code
-            level = scrapy.log.ERROR
+            level = ERROR
         else:
             msg = "Page save failed because of server (%d)!" % r.status_code
-            level = scrapy.log.ERROR
-        scrapy.log.msg(msg, level=level)
+            level = ERROR
+        self.log(msg, level=level)
 
     def save_error(self, error_item):
-        scrapy.log.msg("Saving load failure for URL id %d."
-                       % error_item.get('id'),
-                       level=scrapy.log.INFO)
+        self.log("Saving load failure for URL id %d." % error_item.get('id'),
+                 level=INFO)
         r = requests.post(self.URL_BASE + self.URL_URL_LOAD_FAILED, {
             'id': error_item['id'],
             'http_code': error_item['http_code'],
@@ -58,13 +60,13 @@ class PageFetcherPipeline(object):
 
         if r.status_code < 400:
             msg = "Load failure saved (%d)." % r.status_code
-            level = scrapy.log.INFO
+            level = INFO
         elif r.status_code < 500:
             msg = "Load failure not saved because of us (%d)!" \
                 % r.status_code
-            level = scrapy.log.ERROR
+            level = ERROR
         else:
             msg = "Load failure not saved because of server (%d)!" \
                 % r.status_code
-            level = scrapy.log.ERROR
-        scrapy.log.msg(msg, level=level)
+            level = ERROR
+        self.log(msg, level=level)
