351810d18df423619e0722e2411adcdb7d30f81a
lifelonglearner127 <life.long.learner127@outlook.com>
2014-05-08 21:24:43 +0300

    (Issue #71) added extraction of pdf for walmart products; changed output format to json

--- extract_walmart_video.py
+++ extract_walmart_video.py
@@ -3,8 +3,12 @@
 import urllib
 import re
 import sys
+import json
 
-BASE_URL = "http://json.webcollage.net/apps/json/walmart?callback=jsonCallback&environment-id=live&cpi="
+# base URL for request containing video URL
+BASE_URL_VIDEOREQ = "http://json.webcollage.net/apps/json/walmart?callback=jsonCallback&environment-id=live&cpi="
+# base URL for request containing pdf URL
+BASE_URL_PDFREQ = "http://content.webcollage.net/walmart/smart-button?ignore-jsp=true&ird=true&channel-product-id="
 
 def _extract_product_id(product_page_url):
 	#TODO: throw exception if not expected format
@@ -12,7 +16,7 @@ def _extract_product_id(product_page_url):
 	return product_id
 
 def video_url(product_page_url):
-	request_url = BASE_URL + _extract_product_id(product_page_url)
+	request_url = BASE_URL_VIDEOREQ + _extract_product_id(product_page_url)
 
 	#TODO: handle errors
 	response_text = urllib.urlopen(request_url).read()
@@ -30,8 +34,26 @@ def video_url(product_page_url):
 
 		return None
 
+def pdf_url(product_page_url):
+	request_url = BASE_URL_PDFREQ + _extract_product_id(product_page_url)
+
+	response_text = urllib.urlopen(request_url).read().decode('string-escape')
+
+	pdf_url_candidates = re.findall('"http[^"]*media\.webcollage\.net[^"]*[^"]+\.pdf"', response_text)
+	print pdf_url_candidates
+	if pdf_url_candidates:
+		# remove escapes
+		pdf_url = re.sub('\\\\', "", pdf_url_candidates[0])
+
+		return pdf_url
+
+	else:
+		return None
 
 if __name__=="__main__":
 	product_page_url = sys.argv[1]
 
-	print video_url(product_page_url)
+	# create json object with video and pdf urls
+	results = {'video_url' : video_url(product_page_url), \
+				'pdf_url' : pdf_url(product_page_url)}
+	print json.dumps(results)
