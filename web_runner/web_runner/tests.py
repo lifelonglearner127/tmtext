c170c7eb783f77cf2eff4a3e85c27967b8c62055
lifelonglearner127 <life.long.learner127@outlook.com>
2014-05-15 22:52:53 -0300

    Working REST interface to run spiders and get result.

--- web_runner/web_runner/tests.py
+++ web_runner/web_runner/tests.py
@@ -1,17 +1,26 @@
 import unittest
 
 from pyramid import testing
+import pyramid.exceptions as exc
 
 
-class ViewTests(unittest.TestCase):
+class SpiderViewTests(unittest.TestCase):
+
     def setUp(self):
-        self.config = testing.setUp()
+        self.settings = {
+            'spider._names': 'spider_cfg',
+            'spider._scrapyd.base_url': 'http://localhost:6800/',
+            'spider._result.base_url': 'http://localhost:8000/',
+            'spider.spider_cfg.resource': 'spider_resource',
+            'spider.spider_cfg.spider_name': 'spider_name',
+            'spider.spider_cfg.project_name': 'spider_project_name',
+        }
+        self.config = testing.setUp(settings=self.settings)
 
     def tearDown(self):
         testing.tearDown()
 
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'web_runner')
+    def test_unknown_resource(self):
+        from .views import spider_view
+        request = testing.DummyRequest(path="/unexistant/path/")
+        self.assertRaises(exc.HTTPNotFound, spider_view, request)
