f08cf449f34fe2bf57cc7b0d20888a5de24a717b
lifelonglearner127 <life.long.learner127@outlook.com>
2014-05-28 22:52:10 -0300

    Handle SiteProductItems without a title and unicode titles.

--- product-ranking/product_ranking/pipelines.py
+++ product-ranking/product_ranking/pipelines.py
@@ -14,39 +14,47 @@ class AddCalculatedFields(object):
 
     @staticmethod
     def _normalize(s):
-        return str(s).translate(
-            AddCalculatedFields._TRANSLATE_TABLE,
-            string.punctuation
-        ).lower()
+        try:
+            return str(s).translate(
+                AddCalculatedFields._TRANSLATE_TABLE,
+                string.punctuation
+            ).lower()
+        except UnicodeEncodeError:
+            # Less efficient version for truly unicode strings.
+            for c in string.punctuation:
+                s = s.replace(c, '')
+            return s
 
     @staticmethod
     def process_item(product, spider):
-        title = AddCalculatedFields._normalize(product['title'])
-        search_term = AddCalculatedFields._normalize(product['search_term'])
-        search_term_words = search_term.split()
+        try:
+            title = AddCalculatedFields._normalize(product['title'])
+            search_term = AddCalculatedFields._normalize(product['search_term'])
+            search_term_words = search_term.split()
 
-        product['search_term_in_title_exactly'] = search_term in title
+            product['search_term_in_title_exactly'] = search_term in title
 
-        product['search_term_in_title_partial'] = st_partial = any(
-            word in title for word in search_term_words)
+            product['search_term_in_title_partial'] = st_partial = any(
+                word in title for word in search_term_words)
 
-        product['search_term_in_title_interleaved'] = False
-        if st_partial:
-            title_words = title.split()
-            offset = 0
-            for st_word in search_term_words:
-                for i, title_word in enumerate(title_words[offset:]):
-                    if st_word == title_word:
-                        offset += i + 1
-                        break  # Found one!
+            product['search_term_in_title_interleaved'] = False
+            if st_partial:
+                title_words = title.split()
+                offset = 0
+                for st_word in search_term_words:
+                    for i, title_word in enumerate(title_words[offset:]):
+                        if st_word == title_word:
+                            offset += i + 1
+                            break  # Found one!
+                    else:
+                        break  # A search term was not in the title.
                 else:
-                    break  # A search term was not in the title.
-            else:
-                # The whole search term was traversed so it's interleaved.
-                product['search_term_in_title_interleaved'] = True
+                    # The whole search term was traversed so it's interleaved.
+                    product['search_term_in_title_interleaved'] = True
+        except KeyError:
+            pass
 
         return product
-
 
 class AddCalculatedFieldsTest(unittest.TestCase):
 
