45174fb8b4a02606325e9097704082c5a9550ce1
lifelonglearner127 <life.long.learner127@outlook.com>
2014-06-06 11:49:13 -0300

    Changed semantics of search_term_in_title_* fields
    Now only the most restrictive that's true is True.

--- product-ranking/product_ranking/pipelines.py
+++ product-ranking/product_ranking/pipelines.py
@@ -27,34 +27,47 @@ class AddCalculatedFields(object):
 
     @staticmethod
     def process_item(product, spider):
+        product['search_term_in_title_exactly'] = False
+        product['search_term_in_title_partial'] = False
+        product['search_term_in_title_interleaved'] = False
+
         try:
             title = AddCalculatedFields._normalize(product['title'])
             search_term = AddCalculatedFields._normalize(product['search_term'])
             search_term_words = search_term.split()
 
-            product['search_term_in_title_exactly'] = search_term in title
-
-            product['search_term_in_title_partial'] = st_partial = any(
-                word in title for word in search_term_words)
-
-            product['search_term_in_title_interleaved'] = False
-            if st_partial:
-                title_words = title.split()
-                offset = 0
-                for st_word in search_term_words:
-                    for i, title_word in enumerate(title_words[offset:]):
-                        if st_word == title_word:
-                            offset += i + 1
-                            break  # Found one!
-                    else:
-                        break  # A search term was not in the title.
-                else:
-                    # The whole search term was traversed so it's interleaved.
-                    product['search_term_in_title_interleaved'] = True
+            if search_term in title:
+                product['search_term_in_title_exactly'] = True
+            elif AddCalculatedFields._is_title_interleaved(
+                    title, search_term_words):
+                product['search_term_in_title_interleaved'] = True
+            else:
+                product['search_term_in_title_partial'] = any(
+                    word in title for word in search_term_words)
         except KeyError:
             pass
 
         return product
+
+    @staticmethod
+    def _is_title_interleaved(title, search_term_words):
+        result = False
+
+        title_words = title.split()
+        offset = 0
+        for st_word in search_term_words:
+            for i, title_word in enumerate(title_words[offset:]):
+                if st_word == title_word:
+                    offset += i + 1
+                    break  # Found one!
+            else:
+                break  # A search term was not in the title.
+        else:
+            # The whole search term was traversed so it's interleaved.
+            result = True
+
+        return result
+
 
 class AddCalculatedFieldsTest(unittest.TestCase):
 
@@ -65,6 +78,8 @@ class AddCalculatedFieldsTest(unittest.TestCase):
         result = AddCalculatedFields.process_item(item, None)
 
         assert result['search_term_in_title_interleaved']
+        assert not result['search_term_in_title_partial']
+        assert not result['search_term_in_title_exactly']
 
 
 if __name__ == '__main__':
