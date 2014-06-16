be8232896caddbc8197ef551862d39863d7f57f4
lifelonglearner127 <life.long.learner127@outlook.com>
2014-06-16 18:05:58 -0300

    Fixed URL of command-pending.

--- web_runner/web_runner/__init__.py
+++ web_runner/web_runner/__init__.py
@@ -53,7 +53,7 @@ def main(global_config, **settings):
                      '/result/project/{project}/spider/{spider}/job/{jobid}/')
 
     config.add_route("command pending jobs", '/command/{name}/pending/{jobid}/')
-    config.add_route("command job results", '/command/{name}/result/{jobid}')
+    config.add_route("command job results", '/command/{name}/result/{jobid}/')
 
     add_routes(settings, config)
 
