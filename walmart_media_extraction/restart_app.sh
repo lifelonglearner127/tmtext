8da0cafb10a3e5a034f82478c38998101a2bfd58
lifelonglearner127 <life.long.learner127@outlook.com>
2014-07-10 18:22:58 +0300

    added scripts to start walmart media service and log errors (to be used for cron job)

---
+++ walmart_media_extraction/restart_app.sh
@@ -0,0 +1,1 @@
+/usr/bin/pkill walmart_media; /usr/bin/screen -dm /bin/bash /home/ubuntu/tmtext/walmart_media_extraction/start_app.sh 2>>/home/ubuntu/tmtext/walmart_media_extraction/logs/startapp_log.txt
---
+++ walmart_media_extraction/start_app.sh
@@ -0,0 +1,2 @@
+time=`date +"%F"`
+python /home/ubuntu/tmtext/walmart_media_extraction/walmart_media_app.py 2>>/home/ubuntu/tmtext/walmart_media_extraction/logs/log_"$time".txt
