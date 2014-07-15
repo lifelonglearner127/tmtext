e159c69788b87579469aad8e9562db7a86d628a6
lifelonglearner127 <life.long.learner127@outlook.com>
2014-07-15 09:28:43 -0300

    adding production and development settings

---
+++ web_runner_web/web_runner_web/settings.production.py
@@ -0,0 +1,10 @@
+try:
+    from web_runner_web.shared_settings import *
+except ImportError as e:
+    pass
+
+# WEB_RUNNER_WEB settings
+WEB_RUNNER_LOG_FILES = (
+    '/home/javier/virtual-environments/web-runner/pyramid.log',
+    '/home/javier/virtual-environments/web-runner/access.log',
+)
