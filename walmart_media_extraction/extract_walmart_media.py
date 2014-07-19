ede9e6554bc7362296d3a198008feeea2fadbcda
lifelonglearner127 <life.long.learner127@outlook.com>
2014-07-19 23:38:01 +0300

    (Issue #84) minor improvement to price extraction

--- walmart_media_extraction/extract_walmart_media.py
+++ walmart_media_extraction/extract_walmart_media.py
@@ -206,7 +206,10 @@ def _long_description_from_tree(tree_html):
 # TODO:
 #      - test this for many products. xpath might not be general enough
 def _price_from_tree(tree_html):
-	return "".join(tree_html.xpath("//*[contains(@class, 'camelPrice')]//text()")).strip()
+	price = "".join(tree_html.xpath("//*[contains(@class, 'camelPrice')]//text()")).strip()
+	# don't return the empty string
+	if not price:
+		return None
 
 # extract product price from its product product page tree
 # ! may throw exception if not found
