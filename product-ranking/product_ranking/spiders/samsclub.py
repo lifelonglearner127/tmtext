cbc87f4a16e0e01e17b9872b92b60e7a24031a8c
lifelonglearner127 <life.long.learner127@outlook.com>
2014-08-11 03:42:49 +0300

    Initial commit of samsclub.com spider

---
+++ product-ranking/product_ranking/spiders/samsclub.py
@@ -0,0 +1,89 @@
+from __future__ import division, absolute_import, unicode_literals
+from future_builtins import *
+
+from scrapy.log import ERROR
+
+from product_ranking.items import SiteProductItem
+from product_ranking.spiders import BaseProductsSpider
+
+
+class SamsclubProductsSpider(BaseProductsSpider):
+    name = 'samsclub_products'
+    allowed_domains = ["samsclub.com"]
+    start_urls = []
+    SEARCH_URL = "http://www.samsclub.com/sams/search/searchResults.jsp?searchCategoryId=all&searchTerm={search_term}&fromHome=no&_requestid=29417"
+
+    def __init__(self, *args, **kwargs):
+        super(SamsclubProductsSpider, self).__init__(
+            #url_formatter=FormatterWithDefaults(pagenum=1, prods_per_page=32),
+            *args, **kwargs)
+
+    def parse_product(self, response):
+        product = response.meta['product']
+        brand = response.xpath("//div[contains(@class,'prodTitlePlus')]/span[@itemprop='brand']/text()")
+        brand = brand.extract()[0]
+        product['brand'] = brand
+
+        title = response.xpath("//div[contains(@class,'prodTitle')]/h1/span[@itemprop='name']/text()")
+        title = title.extract()[0]
+        product['title'] = title
+
+        image_url = response.xpath("//div[@id='plImageHolder']/img/@src")
+        image_url = image_url.extract()[0]
+        product['image_url'] = image_url
+
+        price = response.xpath("//div[@class='moneyBoxBtn']/a/span[contains(@class,'onlinePrice')]/text()")
+        if len(price) > 0:
+            price = price.extract()[0]
+        else:
+            price = ''
+        product['price'] = price
+
+        j = response.xpath("//div[@itemprop='description']/descendant::*[text()]/text()")
+        info = " ".join(j.extract())
+        product['description'] = info
+
+        productid = response.xpath("//span[@itemprop='productID']/text()")
+        productid = productid.extract()[0].strip().replace('#:', '', 1)
+        product['upc'] = int(productid)
+
+        model = response.xpath("//span[@itemprop='model']/text()")
+        if model:
+            model = model.extract()[0]
+        else:
+            model = ""
+        product['model'] = model
+
+        product['locale'] = "en-US"
+        product['related_products'] = {}
+        return product
+
+    def _scrape_total_matches(self, response):
+        if not response.url.find('ajaxSearch') > 0:
+            total = response.xpath("//div[contains(@class,'shelfSearchRelMsg2')]/span/span[@class='gray3']/text()").extract()[0]
+            return int(total)
+        total = response.xpath("//body/li[contains(@class,'item')]")
+        return len(total)
+
+    def _scrape_product_links(self, response):
+        if response.url.find('ajaxSearch') > 0:
+            links = response.xpath("//body/li/a/@href").extract()
+        else:
+            links = response.xpath("//ul[contains(@class,'shelfItems')]/li[contains(@class,'item')]/a/@href").extract()
+
+        if not links:
+            self.log("Found no product links.", ERROR)
+
+        for no, link in enumerate(links):
+            yield link, SiteProductItem()
+
+    def _scrape_next_results_page_link(self, response):
+        link = "http://www.samsclub.com/sams/shop/common/ajaxSearchPageLazyLoad.jsp" \
+            "?sortKey=relevance&searchCategoryId=all&searchTerm={0}&noOfRecordsPerPage={1}" \
+            "&sortOrder=0&offset=0&rootDimension=0&tireSearch=&selectedFilter=null" \
+            "&pageView=list&servDesc=null&_=1407437029456"
+
+        max_item = int(self._scrape_total_matches(response))
+        search_term = response.meta['search_term']
+        link = link.format(search_term, max_item + 11)
+        return link
