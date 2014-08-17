b1c9c428b2e195fb920584d6b3874341546f61a5
lifelonglearner127 <life.long.learner127@outlook.com>
2014-08-17 20:18:29 +0300

    added extension to search crawler for logging 503 errors

--- search/search/settings.py
+++ search/search/settings.py
@@ -41,5 +41,9 @@ DOWNLOADER_MIDDLEWARES = {
     'search.middlewares.ProxyMiddleware': 100,
 }
 
+EXTENSIONS = {
+	'search.Handle503_extension.SpiderLog503' : 500
+}
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36"
