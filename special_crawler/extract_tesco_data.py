a05b224d6a464bd753ff8e74b9d15bce01783d26
lifelonglearner127 <life.long.learner127@outlook.com>
2014-08-25 15:17:37 -0600

    fixed reviews and brand scraping

--- special_crawler/extract_tesco_data.py
+++ special_crawler/extract_tesco_data.py
@@ -7,95 +7,48 @@ import json
 
 from lxml import html
 import time
-
+import requests
 from extract_data import Scraper
 
 class TescoScraper(Scraper):
-    #TODO
+    
     """
         DATA_TYPES = { \
         # Info extracted from product page
         "name" :         done
-        "keywords" :     *Tesco does not seem to have meta name="Keywords"
-        "brand" :        *Tesco does not seem to have meta name="Brand"
+        "keywords" :     returns None, *Tesco does not seem to have meta name="Keywords"
+        "brand" :        done
         "short_desc" :   done
-        "long_desc" :    done, using Product Details, and NOT Product Specifications
-        "price" :        done, passes through currency symbol
+        "long_desc" :    done
+        "price" :        done
         "anchors" :      done, *I couldn't find examples on Tesco where product descriptions had links, but if one does, the updated code should catch them
         "htags" :        done
-        "model" :        *Tesco does not seem to include model numbers, I searched maybe 2 dozen different products, and they only have a Tesco ID #
+        "model" :        returns None, *Tesco does not seem to include model numbers, I searched maybe 2 dozen different products, and they only have a Tesco ID #
         "features" :     done
         "title" :        done
         "seller":        done
-        "reviews":       *ratings container is coming up empty, help?
-
+        "reviews":       done
         "load_time":     done
         }
     """
+    
+    bazaarvoice = None
     
     # TODO:
     #      better way of extracting id now that URL format is more permissive
     #      though this method still seems to work...
     def _extract_product_id(self):
         product_id = self.product_page_url.split('/')[-1]
+        product_id = product_id.split('.')[0]
         return product_id
-
-    def _video_url(self):
-        request_url = self.BASE_URL_VIDEOREQ + self._extract_product_id()
-
-        #TODO: handle errors
-        response_text = urllib.urlopen(request_url).read()
-
-        # get first "src" value in response
-        video_url_candidates = re.findall("src: \"([^\"]+)\"", response_text)
-        if video_url_candidates:
-            # remove escapes
-            #TODO: better way to do this?
-            video_url_candidate = re.sub('\\\\', "", video_url_candidates[0])
-
-            # if it ends in flv, it's a video, ok
-            if video_url_candidate.endswith(".flv"):
-                return video_url_candidate
-
-            # if it doesn't, it may be a url to make another request to, to get customer reviews video
-            new_response = urllib.urlopen(video_url_candidate).read()
-            video_id_candidates = re.findall("param name=\"video\" value=\"(.*)\"", new_response)
-
-            if video_id_candidates:
-                video_id = video_id_candidates[0]
-
-                video_url_req = "http://client.expotv.com/vurl/%s?output=mp4" % video_id
-                video_url = urllib.urlopen(video_url_req).url
-
-                return video_url
-
-        return None
 
     # return dictionary with one element containing the video url
     def video_for_url(self):
-        video_url = self._video_url()
-        results = {'video_url' : video_url}
-        return results
-
-    def _pdf_url(self):
-        request_url = self.BASE_URL_PDFREQ + self._extract_product_id()
-
-        response_text = urllib.urlopen(request_url).read().decode('string-escape')
-
-        pdf_url_candidates = re.findall('(?<=")http[^"]*media\.webcollage\.net[^"]*[^"]+\.pdf(?=")', response_text)
-        if pdf_url_candidates:
-            # remove escapes
-            pdf_url = re.sub('\\\\', "", pdf_url_candidates[0])
-
-            return pdf_url
-
-        else:
-            return None
+        return None
 
     # return dictionary with one element containing the PDF
     def pdf_for_url(self):
-        results = {"pdf_url" : self._pdf_url()}
-        return results
+        return None
 
     # Deprecated
     def media_for_url(self):
@@ -104,52 +57,65 @@ class TescoScraper(Scraper):
                     'pdf_url' : _pdf_url(self.product_page_url)}
         return results
 
+    def load_bazaarvoice(self):
+        url = "http://api.bazaarvoice.com/data/batch.json?passkey=asiwwvlu4jk00qyffn49sr7tb&apiversion=5.4&displaycode=1235-en_gb&resource.q0=products&filter.q0=id%3Aeq%3A" \
+        + self._extract_product_id() + \
+        "&stats.q0=reviews&filteredstats.q0=reviews&filter_reviews.q0=contentlocale%3Aeq%3Aen_AU%2Cen_CA%2Cen_DE%2Cen_GB%2Cen_IE%2Cen_NZ%2Cen_US"
+        
+        self.bazaarvoice = requests.get(url).json()
+        
     def reviews_for_url(self):
-        request_url = self.BASE_URL_REVIEWSREQ.format(self._extract_product_id())
-        content = urllib.urlopen(request_url).read()
-        try:
-            reviews_count = re.findall(r"BVRRNonZeroCount\\\"><span class=\\\"BVRRNumber\\\">([0-9,]+)<", content)[0]
-            average_review = re.findall(r"class=\\\"BVRRRatingNormalOutOf\\\"> <span class=\\\"BVRRNumber BVRRRatingNumber\\\">([0-9\.]+)<", content)[0]
-        except Exception, e:
-            return {"reviews" : {"total_reviews": None, "average_review": None}}
-        return {"reviews" : {"total_reviews": reviews_count, "average_review": average_review}}
+        '''
+        file = open("newfile222.txt", "w")
+        file.write(json.dumps(self.javascript_json, sort_keys=True, indent=4, separators=(',', ': ')))
+        file.close()
+        #'''
+
+        if not self.bazaarvoice:
+            self.load_bazaarvoice()
+        average_review = self.bazaarvoice['BatchedResults']['q0']['Results'][0]['FilteredReviewStatistics']['AverageOverallRating']
+        nr_reviews = self.bazaarvoice['BatchedResults']['q0']['Results'][0]['FilteredReviewStatistics']['TotalReviewCount']
+    
+        return {'average_review' : average_review, 'nr_reviews' : nr_reviews}
 
 
     # extract product name from its product page tree
     # ! may throw exception if not found
-    def _product_name_from_tree(self, tree_html):
-        return tree_html.xpath("//h1")[0].text
+    def _product_name_from_tree(self):
+        return self.tree_html.xpath("//h1")[0].text
 
     # extract meta "keywords" tag for a product from its product page tree
     # ! may throw exception if not found
-    def _meta_keywords_from_tree(self, tree_html):
-        return tree_html.xpath("//meta[@name='Keywords']/@content")[0]
+    def _meta_keywords_from_tree(self):
+        return None
 
     # extract meta "brand" tag for a product from its product page tree
     # ! may throw exception if not found
-    def _meta_brand_from_tree(self, tree_html):
-        return tree_html.xpath("//meta[@itemprop='brand']/@content")[0]
+    def _meta_brand_from_tree(self):
+        if not self.bazaarvoice:
+            self.load_bazaarvoice()
+        return self.bazaarvoice['BatchedResults']['q0']['Results'][0]['Brand']['Name']
 
 
     # extract product short description from its product page tree
     # ! may throw exception if not found
-    def _short_description_from_tree(self, tree_html):
-        short_description = " ".join(tree_html.xpath("//ul[@class='features']/li//text()")).strip()
+    def _short_description_from_tree(self):
+        short_description = " ".join(self.tree_html.xpath("//ul[@class='features']/li//text()")).strip()
         return short_description
 
     # extract product long description from its product product page tree
     # ! may throw exception if not found
     # TODO:
     #      - keep line endings maybe? (it sometimes looks sort of like a table and removing them makes things confusing)
-    def _long_description_from_tree(self, tree_html):
-        full_description = " ".join(tree_html.xpath("//section[@id='product-details-link']/section[@class='detailWrapper']//text()")).strip()
+    def _long_description_from_tree(self):
+        full_description = " ".join(self.tree_html.xpath("//section[@id='product-details-link']/section[@class='detailWrapper']//text()")).strip()
         # TODO: return None if no description
         return full_description
 
 
     # extract product price from its product product page tree
-    def _price_from_tree(self, tree_html):
-        meta_price = tree_html.xpath("//p[@class='current-price']//text()")
+    def _price_from_tree(self):
+        meta_price = self.tree_html.xpath("//p[@class='current-price']//text()")
         if meta_price:
             return meta_price[0].strip()
         else:
@@ -160,9 +126,9 @@ class TescoScraper(Scraper):
     # TODO:
     #      - test
     #      - is format ok?
-    def _anchors_from_tree(self, tree_html):
+    def _anchors_from_tree(self):
         # get all links found in the description text
-        description_node = tree_html.xpath("//section[@class='detailWrapper']")[0]
+        description_node = self.tree_html.xpath("//section[@class='detailWrapper']")[0]
         links = description_node.xpath(".//a")
         nr_links = len(links)
 
@@ -180,25 +146,27 @@ class TescoScraper(Scraper):
 
 
     # extract htags (h1, h2) from its product product page tree
-    def _htags_from_tree(self, tree_html):
+    def _htags_from_tree(self):
         htags_dict = {}
 
         # add h1 tags text to the list corresponding to the "h1" key in the dict
-        htags_dict["h1"] = map(lambda t: self._clean_text(t), tree_html.xpath("//h1//text()[normalize-space()!='']"))
+        htags_dict["h1"] = map(lambda t: self._clean_text(t), self.tree_html.xpath("//h1//text()[normalize-space()!='']"))
         # add h2 tags text to the list corresponding to the "h2" key in the dict
-        htags_dict["h2"] = map(lambda t: self._clean_text(t), tree_html.xpath("//h2//text()[normalize-space()!='']"))
+        htags_dict["h2"] = map(lambda t: self._clean_text(t), self.tree_html.xpath("//h2//text()[normalize-space()!='']"))
 
         return htags_dict
 
     # extract product model from its product product page tree
     # ! may throw exception if not found
-    def _model_from_tree(self, tree_html):
-        return tree_html.xpath("//table[@class='SpecTable']//td[contains(text(),'Model')]/following-sibling::*/text()")[0]
+    def _model_from_tree(self):
+        if not self.bazaarvoice:
+            self.load_bazaarvoice()
+        return self.bazaarvoice['BatchedResults']['q0']['Results'][0]['ModelNumbers']
 
     # extract product features list from its product product page tree, return as string
-    def _features_from_tree(self, tree_html):
+    def _features_from_tree(self):
         # join all text in spec table; separate rows by newlines and eliminate spaces between cells
-        rows = tree_html.xpath("//section[@class='detailWrapper']//tr")
+        rows = self.tree_html.xpath("//section[@class='detailWrapper']//tr")
         # list of lists of cells (by rows)
         cells = map(lambda row: row.xpath(".//*//text()"), rows)
         # list of text in each row
@@ -211,30 +179,30 @@ class TescoScraper(Scraper):
         all_features_text = "\n".join(rows_text)
 
         # return dict with all features info
-        return {"features_list": all_features_text, "nr_features": self._nr_features_from_tree(tree_html)}
+        return {"features_list": all_features_text, "nr_features": self._nr_features_from_tree()}
 
     # extract number of features from tree
     # ! may throw exception if not found
-    def _nr_features_from_tree(self, tree_html):
+    def _nr_features_from_tree(self):
         # select table rows with more than 2 cells (the others are just headers), count them
-        return len(filter(lambda row: len(row.xpath(".//td"))>0, tree_html.xpath("//section[@class='detailWrapper']//tr")))
+        return len(filter(lambda row: len(row.xpath(".//td"))>0, self.tree_html.xpath("//section[@class='detailWrapper']//tr")))
 
     # extract page title from its product product page tree
     # ! may throw exception if not found
-    def _title_from_tree(self, tree_html):
-        return tree_html.xpath("//title//text()")[0].strip()
+    def _title_from_tree(self):
+        return self.tree_html.xpath("//title//text()")[0].strip()
 
     # extract product seller meta keyword from its product product page tree
     # ! may throw exception if not found
-    def _seller_meta_from_tree(self, tree_html):
-        return tree_html.xpath("//meta[@itemprop='brand']/@content")[0]
+    def _seller_meta_from_tree(self):
+        return self.tree_html.xpath("//meta[@itemprop='brand']/@content")[0]
 
     # extract product seller information from its product product page tree (using h2 visible tags)
     # TODO:
     #      test this in conjuction with _seller_meta_from_tree; also test at least one of the values is 1
-    def _seller_from_tree(self, tree_html):
+    def _seller_from_tree(self):
         seller_info = {}
-        h2_tags = map(lambda text: self._clean_text(text), tree_html.xpath("//h2//text()"))
+        h2_tags = map(lambda text: self._clean_text(text), self.tree_html.xpath("//h2//text()"))
         seller_info['owned'] = 1 if "Buy on Tesco Direct from:" in h2_tags else 0
         seller_info['marketplace'] = 1 if "more buying option(s) from:" in h2_tags else 0
 
@@ -243,15 +211,13 @@ class TescoScraper(Scraper):
     # extract product reviews information from its product page
     # ! may throw exception if not found
     #TODO: For Tesco, the BVRRSummaryContainer is coming up empty for some reason.
-    def _reviews_from_tree(self, tree_html):
-        reviews_info_node = tree_html.xpath("//div[@id='BVRRSummaryContainer']")[0]
+    def _reviews_from_tree(self):
+        reviews_info_node = self.tree_html.xpath("//div[@id='BVRRSummaryContainer']")[0]
         
-        '''
         print "--------"
-        print html.tostring(tree_html.xpath("//div[@class='details-container']")[0])
+        print html.tostring(self.tree_html.xpath("//div[@class='details-container']")[0])
         #print html.tostring(reviews_info_node)
         print "--------"
-        '''
         
         average_review = (reviews_info_node.xpath("//span[@itemprop='ratingValue']/text()")[0])
         nr_reviews = (reviews_info_node.xpath("//span[@itemprop='reviewCount']/text()")[0])
@@ -296,27 +262,26 @@ class TescoScraper(Scraper):
         # Info extracted from product page
         "name" : _product_name_from_tree, \
         "keywords" : _meta_keywords_from_tree, \
-        "brand" : _meta_brand_from_tree, \
         "short_desc" : _short_description_from_tree, \
         "long_desc" : _long_description_from_tree, \
         "price" : _price_from_tree, \
         "anchors" : _anchors_from_tree, \
         "htags" : _htags_from_tree, \
-        "model" : _model_from_tree, \
         "features" : _features_from_tree, \
         "title" : _title_from_tree, \
         "seller": _seller_from_tree, \
-        "reviews": _reviews_from_tree, \
-
+        "product_id" : _extract_product_id, \
         "load_time": None \
         }
 
     # special data that can't be extracted from the product page
     # associated methods return already built dictionary containing the data
     DATA_TYPES_SPECIAL = { \
+        "brand" : _meta_brand_from_tree, \
+        "model" : _model_from_tree, \
         "video_url" : video_for_url, \
         "pdf_url" : pdf_for_url, \
-    #    "reviews" : reviews_for_url \
+        "reviews" : reviews_for_url \
     }
 
 
