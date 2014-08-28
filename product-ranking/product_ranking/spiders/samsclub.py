cba4c4f5d9c1cf2286fd8855fc2671d7042f1293
lifelonglearner127 <life.long.learner127@outlook.com>
2014-08-28 18:34:57 -0300

    Improved _scrape_next_results_page_link & _scrape_total_matches
    Handle when there are no matches.
    The server is requested less items, if possible.
    Total matches is calculated only once.
    Don't set related_products if no related products are added.
    Some products were being requested twice, this is improved but not completely fixed.

--- product-ranking/product_ranking/spiders/samsclub.py
+++ product-ranking/product_ranking/spiders/samsclub.py
@@ -11,12 +11,16 @@ class SamsclubProductsSpider(BaseProductsSpider):
     name = 'samsclub_products'
     allowed_domains = ["samsclub.com"]
     start_urls = []
-    SEARCH_URL = "http://www.samsclub.com/sams/search/searchResults.jsp?searchCategoryId=all&searchTerm={search_term}&fromHome=no&_requestid=29417"
 
-    def __init__(self, *args, **kwargs):
-        super(SamsclubProductsSpider, self).__init__(
-            #url_formatter=FormatterWithDefaults(pagenum=1, prods_per_page=32),
-            *args, **kwargs)
+    SEARCH_URL = "http://www.samsclub.com/sams/search/searchResults.jsp" \
+        "?searchCategoryId=all&searchTerm={search_term}&fromHome=no" \
+        "&_requestid=29417"
+
+    _NEXT_PAGE_URL = "http://www.samsclub.com/sams/shop/common" \
+        "/ajaxSearchPageLazyLoad.jsp?sortKey=relevance&searchCategoryId=all" \
+        "&searchTerm={search_term}&noOfRecordsPerPage={prods_per_page}" \
+        "&sortOrder=0&offset={offset}&rootDimension=0&tireSearch=" \
+        "&selectedFilter=null&pageView=list&servDesc=null&_=1407437029456"
 
     def parse_product(self, response):
         product = response.meta['product']
@@ -45,17 +49,25 @@ class SamsclubProductsSpider(BaseProductsSpider):
             "//span[@itemprop='model']/text()").extract())
 
         product['locale'] = "en-US"
-        product['related_products'] = {}
+
         return product
 
     def _scrape_total_matches(self, response):
         if response.url.find('ajaxSearch') > 0:
-            total = response.xpath("//body/li[contains(@class,'item')]")
-            return len(total)
-        total = response.xpath("//div[contains(@class,'shelfSearchRelMsg2')]/span/span[@class='gray3']/text()").extract()
-        if total:
-            return int(total[0])
-        return None
+            items = response.xpath("//body/li[contains(@class,'item')]")
+            return len(items)
+
+        totals = response.xpath(
+            "//div[contains(@class,'shelfSearchRelMsg2')]"
+            "/span/span[@class='gray3']/text()"
+        ).extract()
+        if totals:
+            total = int(totals[0])
+        elif response.css('.nullSearchShelfZeroResults'):
+            total = 0
+        else:
+            total = None
+        return total
 
     def _scrape_product_links(self, response):
         if response.url.find('ajaxSearch') > 0:
@@ -70,12 +82,12 @@ class SamsclubProductsSpider(BaseProductsSpider):
             yield link, SiteProductItem()
 
     def _scrape_next_results_page_link(self, response):
-        link = "http://www.samsclub.com/sams/shop/common/ajaxSearchPageLazyLoad.jsp" \
-            "?sortKey=relevance&searchCategoryId=all&searchTerm={0}&noOfRecordsPerPage={1}" \
-            "&sortOrder=0&offset=0&rootDimension=0&tireSearch=&selectedFilter=null" \
-            "&pageView=list&servDesc=null&_=1407437029456"
-
-        max_item = int(self._scrape_total_matches(response))
-        search_term = response.meta['search_term']
-        link = link.format(search_term, max_item + 11)
-        return link
+        # If the total number of matches cannot be scrapped it will not be set.
+        num_items = min(response.meta.get('total_matches', 0), self.quantity)
+        if num_items:
+            return SamsclubProductsSpider._NEXT_PAGE_URL.format(
+                search_term=response.meta['search_term'],
+                offset=response.meta['products_per_page'],
+                prods_per_page=num_items,
+            )
+        return None
