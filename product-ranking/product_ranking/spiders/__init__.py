9071a0cdc1c0039a40fb306ff7ab2082b980898b
lifelonglearner127 <life.long.learner127@outlook.com>
2014-09-09 01:03:00 -0300

    Allows cond_set to receive None as a value and discards it.

--- product-ranking/product_ranking/spiders/__init__.py
+++ product-ranking/product_ranking/spiders/__init__.py
@@ -33,12 +33,13 @@ def cond_set(item, key, values, conv=identity):
     """Conditionally sets the first element of the given iterable to the given
     dict.
 
-    The condition is that the key is not set, or the value in the dict None.
+    The condition is that the key is not set in the item or its value is None.
     Also, the value to be set must not be None.
     """
     try:
-        value = next(iter(values))
-        cond_set_value(item, key, value, conv)
+        if values:
+            value = next(iter(values))
+            cond_set_value(item, key, value, conv)
     except StopIteration:
         pass
 
@@ -46,7 +47,7 @@ def cond_set(item, key, values, conv=identity):
 def cond_set_value(item, key, value, conv=identity):
     """Conditionally sets the given value to the given dict.
 
-    The condition is that the key is not set, or the value in the dict None.
+    The condition is that the key is not set in the item or its value is None.
     Also, the value to be set must not be None.
     """
     if item.get(key) is None and value is not None and conv(value) is not None:
