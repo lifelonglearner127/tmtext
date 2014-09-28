d86febc92fe7d00c68c4e8c052049ff5c1daf856
lifelonglearner127 <life.long.learner127@outlook.com>
2014-09-28 14:46:16 +0300

    Sainsburys.co.uk spider is DONE

--- product-ranking/product_ranking/spiders/sainsburys_uk.py
+++ product-ranking/product_ranking/spiders/sainsburys_uk.py
@@ -12,33 +12,6 @@ from product_ranking.items import SiteProductItem, RelatedProduct
 from product_ranking.spiders import BaseProductsSpider, FormatterWithDefaults, \
      cond_set, cond_set_value, _extract_open_graph_metadata
 
-class Category:
-    sort_orders = None
-    def __init__(self, url, sort_orders = None):
-        self.url = url
-        if sort_orders:
-            self.sort_orders = sort_orders
-
-class Grocery(Category):
-    sort_orders = {
-        'default' : 'RELEVANCE',
-        'low_price': 'PRICE_ASC',
-        'high_price': 'PRICE_DESC',
-        'name_asc' : 'NAME_ASC',
-        'name_desc':'NAME_DESC',
-        'best_sellers':'TOP_SELLERS',
-        'rating': 'RATINGS_DESC',
-    }        
-class Products(Category):
-    sort_orders = {
-        'default' : 'default',
-        'newest': 'newest_first',
-        'rating': 'avg_rating',
-        'name_asc' : ' price_asc',
-        'high_price': 'price_desc',
-        'offers':'great_offers'
-    } 
-
 def clear_text(l):
     """
     usefull for  clearing sel.xpath('.//text()').explode() expressions
@@ -55,32 +28,137 @@ class SainsburysUkProductsSpider(BaseProductsSpider):
         "bmForm=global_search&GLOBAL_DATA._search_term1={search_term}&" \
         "GLOBAL_DATA._searchType=0"
         
+    class GroceryCategory:
+        url = ("http://www.sainsburys.co.uk/"
+                "webapp/wcs/stores/servlet/SearchDisplayView?" 
+                "langId=44&storeId=10151&catalogId=10122&pageSize=30&"
+                "orderBy={sort_order}&searchTerm={search_term}&beginIndex=0")
+                
+        sort_orders = {
+            'default' : 'RELEVANCE',
+            'low_price': 'PRICE_ASC',
+            'high_price': 'PRICE_DESC',
+            'name_asc' : 'NAME_ASC',
+            'name_desc':'NAME_DESC',
+            'best_sellers':'TOP_SELLERS',
+            'rating': 'RATINGS_DESC',
+        }        
+        def _scrape_product_links(self, response):
+            return response.css(
+                '#productLister ul.productLister '
+                '.productNameAndPromotions h3 a::attr(href)'
+            ).extract()
+            
+        def _scrape_next_results_page_link(self, response): 
+            return response.css(
+                '#productLister .pagination li.next a::attr(href)'
+            ).extract()
+        
+        def parse_product(self, response):
+            prod = response.meta['product']
+            
+            cond_set(prod, 'title', response.xpath(
+                    '//div[@class="productSummary"]/h1/text()'
+                ).extract(), string.strip) 
+            
+            cond_set(prod, 'price', response.xpath(
+                    '//p[@class="pricePerUnit"]/text()'
+                ).extract(), string.strip) 
+            
+            img_url = response.xpath(
+                    '//div[@class="productSummary"]/img/@src'
+                ).extract()
+            if img_url:
+                img_url = urlparse.urljoin(response.url, img_url[0])    
+                cond_set_value(prod, 'image_url', img_url)
+            
+            desc = response.xpath(
+                    '//div[@id="information"]'
+                    '/h2[@class="access"]/following-sibling::*'
+                ).extract()
+            if desc:
+                cond_set_value(prod, 'description', clear_text(desc))
+               
+            return prod
+            
+    class ProductsCategory:
+        url = None
+        url_template = ("http://www.sainsburys.co.uk/"
+                "sol/shop/{slug}/list.html?search={{search_term}}"
+                "&sort={{sort_order}}")
+        sort_orders = {
+            'default' : 'default',
+            'newest': 'newest_first',
+            'rating': 'avg_rating',
+            'name_asc' : ' price_asc',
+            'high_price': 'price_desc',
+            'offers':'great_offers'
+        } 
+        
+        def __init__(self, slug):
+            self.url = self.url_template.format(slug=slug) 
+               
+        def _scrape_product_links(self, response):      
+            return response.xpath(
+                '(//ul[@class="itemList"])[1]'
+                '//h3[@class="listItemDesc"]//a/@href'
+            ).extract() 
+            
+        def _scrape_next_results_page_link(self, response): 
+            return response.css(
+                '.paging li.next a::attr(href)'
+            ).extract()  
+        
+        def parse_product(self, response):
+            prod = response.meta['product']
+            cond_set(prod, 'title', response.xpath(
+                    '//div[@class="productHeaderTextInner"]/h3/text()'
+                ).extract(), string.strip) 
+            cond_set(prod, 'price', response.css(
+                    'div.productContent').xpath(
+                    'string(.//div[@class="priceNow"])'
+                ).extract(), string.strip)
+            cond_set(prod, 'image_url', response.xpath(
+                    '//img[@id="bigImage"]/@src'
+                ).extract())
+            desc = response.xpath(
+                    '//div[@class="productOverviewPanel"]/node()'
+                ).extract()
+                
+            if desc:     
+                cond_set_value(prod, 'description', clear_text(desc))
+            
+            in_stock = response.xpath('//span[@id="stockStatus"]/text()').extract()             
+            if in_stock and in_stock[0] != 'In stock':
+                cond_set_value(prod, 'is_out_of_stock', True)  
+            
+            cond_set(prod, 'brand', response.xpath(
+                    '//li/label[@for="Brand1"]/text()'
+                ).extract())      
+            
+            consider = response.xpath('//div[@class="consideredPanelInner"]//a')
+            recommend = response.xpath(
+                '//div[@class="recommendationPanel clearFloat"]//a')
+            rel = []    
+            for link in consider+recommend:
+                href = link.xpath('@href').extract()
+                title = link.xpath('text()').extract()
+                if title and href:
+                    url = urlparse.urljoin(response.url, href[0])
+                    title = title[0].strip()
+                    rel.append(RelatedProduct(title=title, url=url))
+            cond_set_value(prod, 'related_products', {'recommended':rel} )     
+            return prod     
+                
     CATEGORIES = {
-        'Groceries' : Grocery("http://www.sainsburys.co.uk/"
-            "webapp/wcs/stores/servlet/SearchDisplayView?" 
-            "langId=44&storeId=10151&catalogId=10122&pageSize=30&"
-            "orderBy={sort_order}&searchTerm={search_term}&beginIndex=0"),
-        'Home & garden' : Products("http://www.sainsburys.co.uk/"
-            "sol/shop/home_and_garden/list.html?search={search_term}"
-            "&sort={sort_order}"),
-        'Appliances' : Products("http://www.sainsburys.co.uk/"
-            "sol/shop/appliances/list.html?search={search_term}"
-            "&sort={sort_order}"),
-        'Technology' : Products("http://www.sainsburys.co.uk/"
-            "sol/shop/technology/list.html?search={search_term}"
-            "&sort={sort_order}"), 
-        'Toys' : Products("http://www.sainsburys.co.uk/"
-            "sol/shop/toys_and_nursery/list.html?search={search_term}"
-            "&sort={sort_order}"), 
-        'Sport & leisure' : Products("http://www.sainsburys.co.uk/"
-            "sol/shop/sport_and_leisure/list.html?search={search_term}"
-            "&sort={sort_order}"),
-        'Baby' : Products("http://www.sainsburys.co.uk/"
-            "sol/shop/baby/list.html?search={search_term}"
-            "&sort={sort_order}"),
-        'Events' : Products("http://www.sainsburys.co.uk/"
-            "sol/shop/events/list.html?search={search_term}"
-            "&sort={sort_order}"),            
+        'Groceries' : GroceryCategory(),
+        'Home & garden' : ProductsCategory("home_and_garden"),
+        'Appliances' : ProductsCategory("appliances"),
+        'Technology' : ProductsCategory("technology"), 
+        'Toys' : ProductsCategory("toys_and_nursery"),
+        'Sport & leisure' : ProductsCategory("sport_and_leisure"),
+        'Baby' : ProductsCategory("baby"),
+        'Events' : ProductsCategory("events"),            
     }    
     
     def __init__(self, search_sort='default', *args, **kwargs):
@@ -117,14 +195,19 @@ class SainsburysUkProductsSpider(BaseProductsSpider):
                     categories.append(product)
                     
             if categories:
-                response.meta['categories'] = categories
-                return self._request_category(0, response)
+                response.meta['categories'] = iter(categories)
+                return self._next_category(response)
         else:
             self.log("No products found.", INFO)
-    def _request_category(self, cat, response):
-        st = response.meta['search_term']
-        cat_obj = self.CATEGORIES[response.meta['categories'][cat]]
+            
+    def _next_category(self, response):
         
+        try:
+            cat = next(response.meta['categories'])
+        except StopIteration:    
+            return
+        cat_obj = self.CATEGORIES[cat]
+        st = response.meta['search_term']
         sort = cat_obj.sort_orders.get(
             self.search_sort, 
             cat_obj.sort_orders['default'])
@@ -134,82 +217,54 @@ class SainsburysUkProductsSpider(BaseProductsSpider):
             sort_order=sort    
             )
         new_meta = response.meta.copy()
-        new_meta['cat'] = cat    
+        new_meta['cat'] = cat
         new_meta['products_per_page'] = None
         return Request(
             url,
             meta=new_meta)
-                
-                 
+
     def parse_product(self, response):
-        product = response.meta['product']
-    
-
+        cat = self.CATEGORIES[response.meta['cat']]
+        cat.parse_product(response)
+        product = cat.parse_product(response)
+        cond_set_value(product, 'locale', 'en-GB')
         return product
-            
-
+          
     def _scrape_total_matches(self, response):
         total = None
-        
         totals = response.css('.panelContentHeader strong::text').extract()
-        
         if totals:
-            total = int(totals[0].strip())
-                      
+            total = int(totals[0].strip())           
         else:
             self.log(
                 "Failed to find 'total matches' for %s" % response.url,
-                ERROR
-            )
-    
+                ERROR)
         return total    
         
     def _scrape_product_links(self, response):
-        cat = response.meta['categories'][response.meta['cat']]
-        if cat == 'Groceries':
-        
-            items = response.css(
-                '#productLister ul.productLister .productNameAndPromotions h3 a'
-            )
-        else:
-            items = response.xpath(
-                '(//ul[@class="itemList"])[1]'
-                '//h3[@class="listItemDesc"]//a'
-            )
+        cat = self.CATEGORIES[response.meta['cat']]
+        items = cat._scrape_product_links(response)
                 
-        if not items:
-            self.log("Found no product links.", DEBUG)
         self.log("Found {} links.".format(len(items)), DEBUG)
-        for item in items:
-            title = item.xpath('text()').extract()
-            link = item.xpath('@href').extract()
-            
-            if link and title:
-                title = title[0].strip()
-                yield None, SiteProductItem(
-                    title=title,
-                    url=link[0])
+        
+        for link in items:
+            prod_item = SiteProductItem()
+            url = urlparse.urljoin(response.url, link)
+            yield Request(
+                url,
+                callback=self.parse_product,
+                meta={'product': prod_item, 'cat':response.meta['cat']},
+            ), prod_item
+
             
     def _scrape_next_results_page_link(self, response):
-        cat_index = response.meta['cat']
-        cat = response.meta['categories'][cat_index]
-        if cat == 'Groceries':
+        cat = self.CATEGORIES[response.meta['cat']]
         
-            next = response.css(
-                '#productLister .pagination li.next a::attr(href)'
-            ).extract()
-            
-        else:
-            next = response.css(
-                '.paging li.next a::attr(href)'
-            ).extract()
+        next = cat._scrape_next_results_page_link(response)
         link = None
         if next:
             link = urlparse.urljoin(response.url, next[0])    
         else:
-            cat_index += 1
-            
-            if cat_index < len(response.meta['categories']):
-                link = self._request_category(cat_index, response)
+            link = self._next_category(response)
         return link
                               
