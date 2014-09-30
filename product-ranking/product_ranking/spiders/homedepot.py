d7b1e88595551b1a8666a2ae86a55179027cb2aa
lifelonglearner127 <life.long.learner127@outlook.com>
2014-09-30 11:53:59 -0300

    Pulls 2 spiders from products_ranking_wip branch.

---
+++ product-ranking/product_ranking/spiders/homedepot.py
@@ -0,0 +1,264 @@
+from __future__ import division, absolute_import, unicode_literals
+from future_builtins import *
+
+import json
+import re
+import string
+import urllib
+import urlparse
+
+from scrapy import Request, Selector
+from scrapy.log import DEBUG
+
+from product_ranking.items import SiteProductItem, RelatedProduct
+from product_ranking.spiders import BaseProductsSpider, cond_set
+
+
+def is_num(s):
+    try:
+        int(s.strip())
+        return True
+    except ValueError:
+        return False
+
+
+class HomedepotProductsSpider(BaseProductsSpider):
+    name = 'homedepot_products'
+    allowed_domains = ["homedepot.com", "www.res-x.com"]
+    start_urls = []
+
+    SEARCH_URL = "http://www.homedepot.com/s/{search_term}?NCNI-5"
+    SCRIPT_URL = "http://www.res-x.com/ws/r2/Resonance.aspx"
+    DETAILS_URL = "http://www.homedepot.com/p/getSkuDetails?itemId=%s"
+
+    def __init__(self, *args, **kwargs):
+        # All this is to set the site_name since we have several
+        # allowed_domains.
+        super(HomedepotProductsSpider, self).__init__(
+            site_name=self.allowed_domains[0],
+            *args,
+            **kwargs)
+
+    def parse_product(self, response):
+        product = response.meta['product']
+
+        cond_set(
+            product,
+            'title',
+            response.xpath("//h1[@class='product_title']/text()").extract())
+
+        cond_set(
+            product,
+            'brand',
+            response.xpath("//h2[@class='brandName']/span/text()").extract(),
+            conv=string.strip)
+
+        cond_set(
+            product,
+            'image_url',
+            response.xpath(
+                "//div[@class='product_mainimg']/img/@src").extract())
+
+        cond_set(
+            product,
+            'price',
+            response.xpath(
+                "//div[@class='pricingReg']"
+                "/span[@itemprop='price']/text()").extract())
+
+        cond_set(
+            product,
+            'upc',
+            response.xpath(
+                "//h2[@class='internetNo']"
+                "/span[@itemprop='productID']/text()").extract(),
+            conv=int
+        )
+
+        desc = response.xpath(
+            "//div[@id='product_description']"
+            "/div[contains(@class,'main_description')]"
+            "/descendant::*[text()]/text()").extract()
+        desc = " ".join(l.strip() for l in desc if len(l.strip()) > 0)
+        product['description'] = desc
+
+        product['locale'] = "en-US"
+
+        metadata = response.xpath(
+            "//script[contains(text(),'PRODUCT_METADATA_JSON')]"
+            "/text()").re('var PRODUCT_METADATA_JSON = (.*);')
+        skus = []
+        if metadata:
+            metadata = metadata[0]
+            jsmeta = json.loads(metadata)
+            try:
+                skus = jsmeta['attributeDefinition']['attributeLookup']
+                response.meta['skus'] = skus
+                metaname = jsmeta['attributeDefinition']['attributeListing'][0][
+                    'label']
+                response.meta['attributelabel'] = metaname
+            except (KeyError, IndexError):
+                self.log("Incomplete data from Javascript.", DEBUG)
+
+        certona_url = self._gen_certona_url(response)
+        if certona_url:
+            new_meta = response.meta.copy()
+            new_meta['product'] = product
+            new_meta['handle_httpstatus_list'] = [404]
+            return Request(
+                certona_url,
+                self._parse_certona,
+                meta=new_meta,
+                priority=1000,
+            )
+        return self._gen_variants_requests(response, product, skus)
+
+    def _gen_variants_requests(self, response, product, skus):
+        for _, sku in skus:
+            new_product = product.copy()
+            new_product['upc'] = sku
+
+            new_meta = response.meta.copy()
+            new_meta['product'] = new_product
+            new_meta['handle_httpstatus_list'] = [404]
+            url = self.DETAILS_URL % sku
+            yield Request(
+                url,
+                self._parse_skudetails,
+                meta=new_meta,
+                priority=1000,
+            )
+
+    def _gen_certona_url(self, response):
+        appid = response.xpath("//input[@id='certona_appId']/@value").extract()
+        if not appid:
+            return
+
+        critemid = response.xpath(
+            "//input[@id='certona_critemId']/@value").extract()
+        if not critemid:
+            return
+
+        payload = {
+            "appid": appid[0],
+            "tk": "62903038691729",
+            "ss": "181357350200414",
+            "sg": "1",
+            "pg": "210528030293062",
+            "vr": "4.2x",
+            "bx": "true",
+            "sc": "PIPHorizontal1_rr",
+            "ev": "product",
+            "ei": critemid[0],
+            "storenum": "121",
+            "cb": "",
+        }
+        return urlparse.urljoin(
+            self.SCRIPT_URL, "?" + urllib.urlencode(payload))
+
+    def _parse_certona(self, response):
+        product = response.meta['product']
+
+        if response.status == 404:
+            # No further pages were found.
+            return product
+
+        # FIXME If it doesn't match, "js" is undefined.
+        m = re.match(r'None\((.*)\);', response.body)
+        if m:
+            js = m.group(1)
+        jsdata = json.loads(js)
+
+        try:
+            html = jsdata['Resonance']['Response'][0]['output']
+        except (KeyError, IndexError):
+            html = None
+
+        if html:
+            sel = Selector(text=html)
+
+            el = sel.xpath(
+                "//div[contains(@class,'pod')]/div/div"
+                "/a[@class='item_description']")
+            prods = []
+            for e in el:
+                href = e.xpath("@href").extract()
+                if href:
+                    href = href[0]
+                name = e.xpath("text()").extract()
+                if name:
+                    name = name[0].strip()
+                prods.append(RelatedProduct(
+                    name, urlparse.urljoin(product['url'], href)))
+
+            if prods:
+                product['related_products'] = {"recommended": prods}
+
+        skus = response.meta.get('skus', None)
+        if not skus:
+            return product
+        return self._gen_variants_requests(response, product, skus)
+
+    def _parse_skudetails(self, response):
+        product = response.meta['product']
+        if response.status == 404:
+            # No further pages were found.
+            return product
+
+        jsdata = json.loads(response.body_as_unicode())
+        try:
+            storeskus = jsdata['storeSkus']
+            price = storeskus['storeSku']['pricing']['originalPrice']
+            product['price'] = price
+
+            desc = jsdata['info']['description']
+            product['description'] = desc
+
+            url = jsdata['canonicalURL']
+            url = urlparse.urljoin(product['url'], url)
+            product['url'] = url
+
+            image = jsdata['inlinePlayerJSON']['IMAGE'][1]['mediaUrl']
+            product['image_url'] = image
+
+            attrname = response.meta.get('attributelabel', 'Color/Finish')
+            colornames = jsdata['attributeGroups']['group'][0]['entries'][
+                'attribute']
+            colornames = [el['value'] for el in colornames
+                          if el['name'] == attrname]
+            if colornames:
+                product['model'] = colornames[0]
+        except (KeyError, IndexError):
+            self.log("Failed to parse SKU details.", DEBUG)
+
+        return product
+
+    def _scrape_total_matches(self, response):
+        totals = response.xpath(
+            "//a[@id='all_products']/label"
+            "/text()").re(r'All Products \(([\d,]+)\)')
+        if totals:
+            totals = totals[0]
+            totals = totals.replace(",", "")
+            if is_num(totals):
+                return int(totals)
+        return None
+
+    def _scrape_product_links(self, response):
+        links = response.xpath(
+            "//div[contains(@class,'product') "
+            "and contains(@class,'plp-grid')]"
+            "/descendant::a[@class='item_description']/@href").extract()
+
+        if not links:
+            self.log("Found no product links.", DEBUG)
+
+        for link in links:
+            yield link, SiteProductItem()
+
+    def _scrape_next_results_page_link(self, response):
+        next = response.xpath(
+            "//div[@class='pagination-wrapper']/ul/li/span"
+            "/a[@title='Next']/@href").extract()
+        if next:
+            return urlparse.urljoin(response.url, next[0])
