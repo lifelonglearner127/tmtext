24eab0ea6eb82ab39605f71549e6dd0af96e7533
lifelonglearner127 <life.long.learner127@outlook.com>
2014-10-08 01:12:55 +0300

    adapted matching monitoring scripts to be more flexible (and allow single file inputs)

--- aws_config/count_exceptions.sh
+++ aws_config/count_exceptions.sh
@@ -5,12 +5,31 @@ echo "--------------"
 
 FILE=$1
 
-for NR in {1..10}
-do
-	LOGFILE=shared_sshfs/search_log_"$FILE"_"$NR".txt
+# if no second argument was given, just analyze one file
+# the one given as first argument
+if [ -z "$2" ]
+then
+	LOGFILE=$FILE
 	if [ -f "$LOGFILE" ]
-		then
-		printf "$NR "; ack "<exception" $LOGFILE |  wc -l
+	then
+		cat $LOGFILE | grep "<exception" | wc -l
 	fi
-done
+
+else
+
+# if second argument was given, representing range of subbatches for this input
+# to run the stats on - then run on given number of batches
+# (input will be part of the filenames to be monitored)
+
+	RANGE=$2
+	for NR in $RANGE
+	do
+		LOGFILE=shared_sshfs/search_log_"$FILE"_"$NR".txt
+		if [ -f "$LOGFILE" ]
+			then
+			printf "$NR "; ack "<exception" $LOGFILE |  wc -l
+		fi
+	done
+
+fi
 
