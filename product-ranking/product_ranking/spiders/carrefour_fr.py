c6a1d386ef634f5b93f421be6f8e88877aa5b034
lifelonglearner127 <life.long.learner127@outlook.com>
2014-10-17 18:55:43 -0300

    Adds spider from products_ranking_wip.

---
+++ product-ranking/product_ranking/spiders/carrefour_fr.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, absolute_import, unicode_literals
+from future_builtins import *
+
+import string
+
+from scrapy.log import ERROR, DEBUG
+
+from product_ranking.items import SiteProductItem, RelatedProduct
+from product_ranking.spiders import BaseProductsSpider, cond_set, cond_set_value
+
+
+class CarrefourProductsSpider(BaseProductsSpider):
+    name = 'carrefour_products'
+    allowed_domains = ["carrefour.fr"]
+    start_urls = []
+
+    SEARCH_URL = 'http://www.carrefour.fr/search/site/--{search_term}/100'
+
+    def _scrape_product_links(self, response):
+        products = response.xpath('//ol[contains(@class, "search-results")]'
+                                  '//div[contains(@class, "sc_result_list")]')
+        if not products:
+            self.log("Found no product links.", ERROR)
+
+        for product in products:
+            prod_links = product.xpath(
+                './/div[contains(@class, "sc_result_title")]//a/@href'
+            ).extract()
+            if not prod_links:
+                self.log(
+                    "Failed to extract product link for item: %r"
+                    % (product.extract(),),
+                    ERROR
+                )
+                continue
+            prod_link = prod_links[0]
+
+            item = SiteProductItem()
+
+            cond_set(
+                item,
+                'title',
+                product.css('div.sc_result_title a::text').extract(),
+                conv=string.strip,
+            )
+
+            cond_set(
+                item,
+                'price',
+                product.css('div.sc_result_price::text').re('(\d.+)'),
+            )
+
+            cond_set(
+                item,
+                'locale',
+                response.xpath('//html/@lang').extract(),
+                conv=string.strip,
+            )
+            cond_set_value(item, 'locale', 'fr-FR')  # Default.
+
+            yield prod_link, item
+
+    def _scrape_next_results_page_link(self, response):
+        link = response.xpath(
+            '//ul[@class="pager"]'
+            '//li[@class="pager-current"]/following-sibling::li[1]//a/@href'
+        ).extract()
+        if not link:
+            self.log("Next page link not found.", DEBUG)
+            return
+        return link[0]
+
+    def _scrape_total_matches(self, response):
+        totals = response.css('h2.search-results-message::text').re('(\d+)')
+        if totals:
+            total = int(totals[0])
+        else:
+            self.log(
+                "'total matches' string not found at %s" % response.url,
+                ERROR
+            )
+            return
+        return total
+
+    def parse_product(self, response):
+        product = response.meta['product']
+
+        _big_img = response.xpath('//*[contains(@class, "superbox-gallery")]'
+                                  '//a[contains(@class, "cloud-zoom")]/@href')
+        _big_img = _big_img.extract()
+        if _big_img:
+            product['image_url'] = _big_img[0].strip()
+
+        cond_set(product, 'brand',
+                 response.xpath('//*[@itemprop="brand"]/text()').extract())
+
+        cond_set_value(
+            product,
+            'is_out_of_stock',
+            not response.css('.prd-available'),
+        )
+
+        related = response.xpath(
+            u'//h2[contains(text(), "également consulté ces articles")]/..'
+            u'//p/a[contains(@href, "carrefour.fr")]'
+            u'[contains(@onclick, "Proposition_accessoire")]'
+        )
+        recommended_prods = []
+        for rel in related:
+            try:
+                title = rel.xpath('text()').extract()[0].strip()
+                href = rel.xpath('@href').extract()[0].strip()
+                recommended_prods.append(RelatedProduct(title, href))
+            except IndexError:
+                pass
+        if recommended_prods:
+            product['related_products'] = {"recommended": recommended_prods}
+
+        cond_set(
+            product, 'description', response.css('.box-prd-desc').extract())
+        if product.get('description'):
+            product['description'] = product['description'].replace(
+                u'<a class="action-link see-tech-specs" href="#box-tech-specs">'
+                u'Voir la fiche technique complète du produit</a>', ''
+            )
+
+        return product
