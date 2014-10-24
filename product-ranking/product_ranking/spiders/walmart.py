df1ecd7beeda4bb3565eb5bef1170e05df46ee86
lifelonglearner127 <life.long.learner127@outlook.com>
2014-10-24 12:17:29 -0300

    Rewrites Walmart.com's spider since the site changed.

--- product-ranking/product_ranking/spiders/walmart.py
+++ product-ranking/product_ranking/spiders/walmart.py
@@ -2,18 +2,15 @@ from __future__ import division, absolute_import, unicode_literals
 from future_builtins import *
 
 import json
+import pprint
 import re
-import string
-import urllib
 import urlparse
 
-from scrapy.http import Request
 from scrapy.log import ERROR, WARNING, INFO
-from scrapy.selector import Selector
 
 from product_ranking.items import SiteProductItem, RelatedProduct
 from product_ranking.spiders import BaseProductsSpider, FormatterWithDefaults, \
-    compose, cond_set, cond_set_value, populate_from_open_graph
+    cond_set, cond_set_value
 
 
 class WalmartProductsSpider(BaseProductsSpider):
@@ -34,13 +31,7 @@ class WalmartProductsSpider(BaseProductsSpider):
         "&_refineresult=true&ic=16_0&search_constraint=0" \
         "&search_query={search_term}&sort={search_sort}"
 
-    RELATED_PRODUCTS_URL = \
-        "http://irsws.walmart.com/irs-ws/irs/2.0?" \
-        "callback=irs_process_response&modules_bit_field=0000000" \
-        "&api_key=01&visitor_id=78348875473&category={category_id}" \
-        "&client_guid={client_guid}&config_id=0&parent_item_id={item_id}"
-
-    SEARCH_SORT = {
+    _SEARCH_SORT = {
         'best_match': 0,
         'high_price': 'price_high',
         'low_price': 'price_low',
@@ -49,294 +40,146 @@ class WalmartProductsSpider(BaseProductsSpider):
         'rating': 'rating_high',
     }
 
+    _JS_DATA_RE = re.compile(
+        r'define\(\s*"product/data\"\s*,\s*(\{.+?\})\s*\)\s*;', re.DOTALL)
+
     def __init__(self, search_sort='best_match', *args, **kwargs):
         super(WalmartProductsSpider, self).__init__(
             url_formatter=FormatterWithDefaults(
-                search_sort=self.SEARCH_SORT[search_sort]
+                search_sort=self._SEARCH_SORT[search_sort]
             ),
             *args, **kwargs)
 
-    def start_requests(self):
-        for request in super(WalmartProductsSpider, self).start_requests():
-            request.meta['dont_redirect'] = True
-            request.meta['handle_httpstatus_list'] = [302]
-            yield request
-
-    def _create_from_redirect(self, response):
-        # Create comparable URL tuples.
-        redirect_url = response.headers['Location']
-        redirect_url_split = urlparse.urlsplit(redirect_url)
-        redirect_url_split = redirect_url_split._replace(
-            query=urlparse.parse_qs(redirect_url_split.query))
-        original_url_split = urlparse.urlsplit(response.request.url)
-        original_url_split = original_url_split._replace(
-            query=urlparse.parse_qs(original_url_split.query))
-
-        if redirect_url_split == original_url_split:
-            self.log("Found identical redirect!", INFO)
-            request = response.request.replace(dont_filter=True)
-        else:
-            self.log("Found legit redirect!", INFO)
-            request = response.request.replace(url=redirect_url)
-        request.meta['dont_redirect'] = True
-        request.meta['handle_httpstatus_list'] = [302]
-
-        return request
-
-    def parse(self, response):
-        if response.status == 302:
-            yield self._create_from_redirect(response)
-        else:
-            for request in super(WalmartProductsSpider, self).parse(response):
-                request.meta['dont_redirect'] = True
-                request.meta['handle_httpstatus_list'] = [302]
-                yield request
-
-    def _get_products(self, response):
-        if response.status == 302:
-            yield self._create_from_redirect(response)
-        else:
-            for request_or_item in super(
-                    WalmartProductsSpider, self)._get_products(response):
-                if isinstance(request_or_item, Request):
-                    request_or_item.meta['dont_redirect'] = True
-                    request_or_item.meta['handle_httpstatus_list'] = [302]
-                yield request_or_item
-
     def parse_product(self, response):
-        if response.status == 302:
-            return self._create_from_redirect(response)
-
         if self._search_page_error(response):
             self.log(
-                "Got 404 when coming from %s." % response.request.url, ERROR)
+                "Got 404 when coming from %r." % response.request.url, ERROR)
             return
-
-        p = response.meta['product']
-
-        self._populate_from_open_graph(response, p)
-
-        item_id, category_id = self._populate_from_js(response, p)
-
-        self._populate_from_html(response, p)
-
-        cond_set_value(p, 'locale', 'en-US')  # Default locale.
-
-        if item_id is None or category_id is None:
-            result = p
-        else:
-            meta = response.meta.copy()
-            meta['dont_redirect'] = True
-            meta['handle_httpstatus_list'] = [302]
-
-            result = Request(
-                self.RELATED_PRODUCTS_URL.format(
-                    item_id=item_id,
-                    category_id=urllib.quote(category_id),
-                    # It's unknown how the following value is generated but it
-                    # looks like a UUID. Also, it changes on every request.
-                    client_guid="f3f03064-bede-45f2-b2f1-7b35cdb5e5ed",
-                ),
-                self.parse_related_products,
-                meta=meta,
-                priority=100,
-            )
-        return result
-
-    def parse_related_products(self, response):
-        if response.status == 302:
-            return self._create_from_redirect(response)
 
         product = response.meta['product']
 
-        m = re.match(r'.*?\((.+)\)', response.body_as_unicode())
-        if not m:
-            self.log("Failed to parse related products.", WARNING)
-        else:
-            data = json.loads(m.group(1))
-            module_list = data.get('result', {}).get('moduleList', [])
-            for module in module_list:
-                if module['moduleTitle'] \
-                        == "Other items purchased by customers who viewed this item":
-                    product['related_products'] = {
-                        "buyers_also_bought": [
-                            RelatedProduct(
-                                self._generate_title_from_url(url),
-                                urlparse.urljoin(response.url, url)
-                            )
-                            for url in Selector(text=module['html']).css(
-                                'a.irs-title ::attr(href)').extract()
-                        ],
-                    }
-                    break
-        return product
+        self._populate_from_js(response, product)
 
-    @staticmethod
-    def _generate_title_from_url(url):
-        slug = url.rsplit('/', 2)[-2]
-        return ' '.join(map(string.capitalize, slug.split('-')))
+        self._populate_from_html(response, product)
+
+        cond_set_value(product, 'locale', 'en-US')  # Default locale.
+
+        return product
 
     def _search_page_error(self, response):
         path = urlparse.urlsplit(response.url)[2]
         return path == '/FileNotFound.aspx'
 
+    def _build_related_products(self, url, related_product_nodes):
+        also_considered = []
+        for node in related_product_nodes:
+            link = urlparse.urljoin(url, node.xpath('../@href').extract()[0])
+            title = node.xpath('text()').extract()[0]
+            also_considered.append(RelatedProduct(title, link))
+        return also_considered
+
     def _populate_from_html(self, response, product):
-        # Since different chunks of invalid HTML keep appearing in this
-        # element, I'll just dump whatever is in there.
-        cond_set(product, 'title',
-                 response.xpath('//meta[@name="title"]/@content').extract())
-        # TODO This source for the description is not reliable.
         cond_set(
             product,
             'description',
-            response.xpath('//*[@class="ql-details-short-desc"]/*').extract(),
-            conv=compose(string.strip, ''.join)
-        )
-        # Smaller but good description as fallback.
-        cond_set(
-            product,
-            'description',
-            response.xpath(
-                '//meta[@name="twitter:description"]/@content').extract(),
-        )
-        # Lower quality description as last resort.
-        cond_set(
-            product,
-            'description',
-            response.xpath('//meta[@name="Description"]/@content').extract(),
+            response.css('.about-product-section').extract(),
+            conv=''.join
         )
 
-        prices = response.xpath('//*[@id="SWFromPrice"]//text()').extract()
-        if prices:
-            price = "".join([p.strip() for p in prices])
-            price = price.replace('$', '')
-            if price != "0.0":
-                product['price'] = price
+        also_considered = self._build_related_products(
+            response.url,
+            response.css('.top-product-recommendations .tile-heading'),
+        )
+        if also_considered:
+            product.setdefault(
+                'related_products', {})["buyers_also_bought"] = also_considered
+
+        recommended = self._build_related_products(
+            response.url,
+            response.xpath(
+                "//p[contains(text(), 'Check out these related products')]/.."
+                "//*[contains(@class, 'tile-heading')]"
+            ),
+        )
+        if recommended:
+            product.setdefault(
+                'related_products', {})['recommended'] = recommended
 
     def _populate_from_js(self, response, product):
-        # This fails with movies.
-        scripts = response.xpath(
-            "//script[contains(text(), 'var DefaultItem =')]")
+        scripts = response.xpath("//script").re(
+            WalmartProductsSpider._JS_DATA_RE)
         if not scripts:
-            self.log("No JS matched in %s" % response.url, WARNING)
-            return None, None
-
-        if not scripts.re("currentItemPrice:\s*([-+]?\d*\.\d+|\d+),"):
-            scripts = response.xpath(
-                "//script[contains(text(), 'var DefaultItemWidget =')]")
-
+            self.log("No JS matched in %r." % response.url, ERROR)
+            return
         if len(scripts) > 1:
             self.log(
-                "Matched multiple script blocks in %s" % response.url,
-                WARNING
+                "Matched multiple script blocks in %r." % response.url,
+                ERROR
             )
 
-        cond_set(product, 'upc', scripts.re("upc:\s*'(\d+)',"))
-        cond_set(product, 'brand',
-                 filter(None, scripts.re("brand:\s*'(.+)',")))
-        cond_set(product, 'model', scripts.re("model:\s*'(.+)',"))
-        cond_set(product, 'title', scripts.re("friendlyName:\s*'(.+)',"))
-        cond_set(
-            product, 'price',
-            map(float, scripts.re("currentItemPrice:\s*([-+]?\d*\.\d+|\d+),"))
+        data = json.loads(scripts[0])
+        cond_set_value(product, 'title', data['productName'])
+        available = data['buyingOptions']['available']
+        cond_set_value(
+            product,
+            'is_out_of_stock',
+            not available,
         )
-        cond_set(product, 'rating',
-                 map(float, scripts.re("currentRating:\s*'(.+)',")))
-
-        is_on_wwws = scripts.re("isBuyableOnWWW:\s*(.+),")
-        is_on_www = "true"
-        if is_on_wwws:
-            is_on_www = is_on_wwws[0].lower().strip()
-
-        is_in_stores = scripts.re("isBuyableInStore:\s*(.+),")
-        is_in_store = "true"
-        if is_in_stores:
-            is_in_store = is_in_stores[0].lower().strip()
-
-        in_store_only = False
-        if is_on_www == "false":
-            if is_in_store == "true":
-                in_store_only = True
-
-        product["is_in_store_only"] = in_store_only
-
-        in_stocks = scripts.re("isInStock:\s*(.+),")
-        out_stock = True
-        if in_stocks:
-            in_stock = in_stocks[0].strip()
-            if in_stock == "false":
-                out_stock = True
-            else:
-                out_stock = False
-        product['is_out_of_stock'] = out_stock
-
-        item_id_list = scripts.re("itemId:\s*(\d+),")
-        category_id_list = scripts.re("primaryCategoryPath:\s*'([\d:]+)',")
-        return (
-            item_id_list[0] if item_id_list else None,
-            category_id_list[0] if category_id_list else None,
+        cond_set_value(
+            product,
+            'is_in_store_only',
+            data['buyingOptions']['storeOnlyItem'],
         )
+        if available:
+            try:
+                cond_set_value(
+                    product,
+                    'price',
+                    data['buyingOptions']['price']['displayPrice'],
+                )
+            except KeyError:
+                # Packs of products have different buyingOptions.
+                try:
+                    cond_set_value(
+                        product,
+                        'price',
+                        data['buyingOptions']['maxPrice']['displayPrice'],
+                    )
+                except KeyError:
+                    self.log(
+                        "Product with unknown buyingOptions structure: %s\n%s"
+                        % (response.url, pprint.pformat(data)),
+                        ERROR
+                    )
 
-    def _populate_from_open_graph(self, response, product):
-        """Uses the generic function but removes some fields which are not good.
-        """
-        populate_from_open_graph(response, product)
-
-        # The title is excluded as it contains a "Walmart: " prefix.
         try:
-            del product['title']
-        except KeyError:
-            pass
+            cond_set_value(
+                product, 'upc', data['analyticsData']['upc'], conv=int)
+        except ValueError:
+            pass  # Not really a UPC.
+        cond_set_value(product, 'image_url', data['primaryImageUrl'])
+        cond_set_value(product, 'brand', data['analyticsData']['brand'])
 
     def _scrape_total_matches(self, response):
         if response.css('.no-results'):
             return 0
 
-        num_results = None
-
-        # We get two different types of pages.
-        multiple_matches = [
-            response.css(".numResults ::text").extract(),
-            response.css('.result-summary-container ::text').re(
-                'Showing \d+ of (\d+) results'),
-        ]
-        for i, matches in enumerate(multiple_matches):
-            if matches:
-                num_results = int(matches[0].split()[0])
-                break
-            else:
-                self.log(
-                    "Failed to extract total matches using method %d from %s"
-                    % (i, response.url),
-                    INFO
-                )
+        matches = response.css('.result-summary-container ::text').re(
+            'Showing \d+ of (\d+) results')
+        if matches:
+            num_results = int(matches[0])
         else:
+            num_results = None
             self.log(
-                "Failed to extract total matches from: %s" % response.url,
+                "Failed to extract total matches from %r." % response.url,
                 ERROR
             )
         return num_results
 
     def _scrape_product_links(self, response):
-        links = []
-
-        # We get two different types of pages.
-        css_expressions = [
-            'a.prodLink.ListItemLink ::attr(href)',
-            'a.js-product-title ::attr(href)',
-        ]
-        for i, expr in enumerate(css_expressions):
-            links = response.css(expr).extract()
-            if links:
-                break
-            else:
-                self.log(
-                    "Found no product links using method %d in %s."
-                    % (i, response.url),
-                    INFO
-                )
-        else:
-            self.log("Found no product links %s." % response.url, WARNING)
+        links = response.css('a.js-product-title ::attr(href)').extract()
+        if not links:
+            self.log("Found no product links in %r." % response.url, INFO)
 
         for link in links:
             yield link, SiteProductItem()
@@ -344,33 +187,19 @@ class WalmartProductsSpider(BaseProductsSpider):
     def _scrape_next_results_page_link(self, response):
         next_page = None
 
-        multiple_links = [
-            response.css('li.btn-nextResults > a ::attr(href)'),
-            response.xpath(
-                # [1] is to get just the next sibling.
-                "//a[@class='active']/../following-sibling::li[1]/a/@href"
-            ),
-        ]
-        for i, next_page_links in enumerate(multiple_links):
-            if len(next_page_links) == 1:
-                next_page = next_page_links.extract()[0]
-                break
-            elif len(next_page_links) > 1:
-                self.log(
-                    "Found more than one 'next page' link using method %d"
-                    " in %s." % (i, response.url),
-                    WARNING
-                )
-            else:
-                self.log(
-                    "Found no 'next page' link using method %d in %s"
-                    " (which could be OK)." % (i, response.url),
-                    INFO
-                )
+        next_page_links = response.css(".paginator-btn-next ::attr(href)")
+        if len(next_page_links) == 1:
+            next_page = next_page_links.extract()[0]
+        elif len(next_page_links) > 1:
+            self.log(
+                "Found more than one 'next page' link in %r." % response.url,
+                ERROR
+            )
         else:
             self.log(
-                "Found no 'next page' link in %s (which could be OK)."
+                "Found no 'next page' link in %r (which could be OK)."
                 % response.url,
                 INFO
             )
+
         return next_page
