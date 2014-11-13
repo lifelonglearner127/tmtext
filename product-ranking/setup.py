e1d74175bdfd80f334d9246ee0f8eaee45aac55c
lifelonglearner127 <life.long.learner127@outlook.com>
2014-11-12 21:19:45 -0300

    Merged product_ranking_wip 9b6b40963f
    google.py with guess_brand.py changes

--- product-ranking/setup.py
+++ product-ranking/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 import os
+import itertools
 
 setup(
     name='product-ranking',
@@ -9,7 +10,8 @@ setup(
     zip_safe=False,  # Because of Captcha Breaker.
     include_package_data=True,
     data_files=[(root, [os.path.join(root, f) for f in files])
-         for root, _, files in os.walk('train_captchas_data')],
+         for root, _, files in itertools.chain(os.walk('train_captchas_data'),
+                                               os.walk('data'))],
     install_requires=[
         "Scrapy>=0.22",
     ],
