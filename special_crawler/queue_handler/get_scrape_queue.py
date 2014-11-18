74068b2609924bd20dce67383ff09b7796b33387
lifelonglearner127 <life.long.learner127@outlook.com>
2014-11-18 15:18:43 -0700

    small changes

--- special_crawler/queue_handler/get_scrape_queue.py
+++ special_crawler/queue_handler/get_scrape_queue.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 from sqs_connect import SQS_Queue
 import logging
 import time
--- special_crawler/queue_handler/queue_counts.py
+++ special_crawler/queue_handler/queue_counts.py
@@ -11,13 +11,14 @@ from sqs_connect import SQS_Queue
 # The queues you would like to check
 queues = ['test_scrape', 'test_process', 'test2_process']
 for q in queues:
-    #sqs_scrape.clear()
-    sqs_scrape = SQS_Queue(q)
-    print(q, "count : ", sqs_scrape.count())
+	print('processing: ', q),
+
+	sqs_scrape = SQS_Queue(q)
+	#sqs_scrape.clear()
+
+	print('... done')
+	print(q, "count : ", sqs_scrape.count())
 
 
 
 
-# 51 - 53
-# 49 - 55
-# 27 - 72
