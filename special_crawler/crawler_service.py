15ad81758cb16a0eaf0178b5eb749b7be0f09852
lifelonglearner127 <life.long.learner127@outlook.com>
2014-12-04 16:45:53 -0700

    adapted to support crawlera api calls

--- special_crawler/crawler_service.py
+++ special_crawler/crawler_service.py
@@ -176,9 +176,15 @@ def get_data():
         bot = request_arguments['bot'][0]
     else:
         bot = None
+    
+    if 'crawlera' in request_arguments:
+        crawlera = request_arguments['crawlera'][0]
+    else:
+        crawlera = None
+
 
     # create scraper class for requested site
-    site_scraper = SUPPORTED_SITES[site](url=url, bot=bot)
+    site_scraper = SUPPORTED_SITES[site](url=url, bot=bot, crawlera=crawlera)
 
     # validate parameter values
     # url
