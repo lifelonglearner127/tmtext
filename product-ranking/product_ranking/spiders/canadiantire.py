390b38ca5acfd045fd14944f380af2cdfa35b913
lifelonglearner127 <life.long.learner127@outlook.com>
2014-12-08 18:21:34 -0300

    Adding new price to more spiders

--- product-ranking/product_ranking/spiders/canadiantire.py
+++ product-ranking/product_ranking/spiders/canadiantire.py
@@ -1,33 +1,38 @@
+# -*- coding: utf-8 -*-#
 from __future__ import division, absolute_import, unicode_literals
 from future_builtins import *
 
 import json
+import re
 import string
 import urllib
 import urlparse
-import re
 
 from product_ranking.items import SiteProductItem, RelatedProduct
-from product_ranking.spiders import BaseProductsSpider, cond_set, cond_set_value
+from product_ranking.spiders import BaseProductsSpider
+from product_ranking.spiders import cond_set, cond_set_value
 from scrapy import Request
 from scrapy.log import DEBUG, ERROR
 
 
 class CanadiantireProductsSpider(BaseProductsSpider):
     name = 'canadiantire_products'
-    allowed_domains = ["canadiantire.ca"]
+    allowed_domains = ["canadiantire.ca", "cantire.guided.ss-omtrdc.net"]
     start_urls = []
-
-    SEARCH_URL = "http://www.canadiantire.ca/en/search-results.html" \
-        "?searchByTerm=true&q={search_term}"
-
-    SEARCH_URL2 = "http://tires.canadiantire.ca/en/search/?searchterm={search_term}"
+    SEARCH_URL = (
+        "http://cantire.guided.ss-omtrdc.net/?site=ct&format=json&"
+        "callback=SRPInitialLoad&count=15&searchByTerm=true&q={search_term}")
+    SEARCH_URL2 = (
+        "http://tires.canadiantire.ca/en/search/"
+        "?searchterm={search_term}")
 
     _PROD_DATA_RELATIVE_URL = "{load_url}?productCodes={product_code}" \
         "&locale={locale}&storeId=&showAdSkus={show_ad}"
 
     def __init__(self, *args, **kwargs):
-        super(CanadiantireProductsSpider, self).__init__(*args, **kwargs)
+        super(CanadiantireProductsSpider, self).__init__(
+            site_name=self.allowed_domains[0],
+            *args, **kwargs)
         self.force_tires = False
 
     def parse(self, response):
@@ -44,9 +49,10 @@ class CanadiantireProductsSpider(BaseProductsSpider):
             rlist = []
             for st in self.searchterms:
                 rlist.append(Request(
-                    self.url_formatter.format(self.SEARCH_URL2,
+                    self.url_formatter.format(
+                        self.SEARCH_URL2,
                         search_term=urllib.quote_plus(st)),
-                        meta={'search_term': st, 'remaining': self.quantity}
+                    meta={'search_term': st, 'remaining': self.quantity}
                 ))
             return rlist
         else:
@@ -62,7 +68,8 @@ class CanadiantireProductsSpider(BaseProductsSpider):
             "//div[contains(@class,'product-title')]/div/h1/text()").extract())
 
         cond_set(product, 'brand', response.xpath(
-            "//div[contains(@class,'product-title')]/img/@title").extract())
+            "//div[contains(@class,'pd-logo-entry')]/img/@alt").extract())
+        cond_set_value(product, 'brand', 'NO BRAND')
 
         cond_set(product, 'image_url', response.xpath(
             "//link[@rel='image_src']/@href").extract())
@@ -74,22 +81,27 @@ class CanadiantireProductsSpider(BaseProductsSpider):
             ).replace('-', '')
             product['upc'] = int(productid)
 
-        info = response.xpath("//div[@class='features_wrap']/descendant::*[text()]/text()")
-        cond_set_value(product, 'description', ''.join(info.extract()), conv=''.join)
+        cond_set(
+            product, 'description', response.xpath(
+                "//div[@class='features_wrap']").extract())
 
         related = response.xpath(
-            "//div[@class='sr-cross-sell__wrapper']/*/*/*/a")
+            "//div[@class='sr-cross-sell__wrapper']/ul/li"
+            "/descendant::div[contains(@class,'product_result')]"
+            "/div[@class='sri-name-cnt']/h2/a")
+
         lrelated = []
         for sel in related:
             link = sel.xpath('@href')
             link = link.extract()[0]
             link = urlparse.urljoin(response.url, link)
 
-            ltitle = sel.xpath('img/@title')
+            ltitle = sel.xpath('text()')
             ltitle = ltitle.extract()[0]
 
             lrelated.append(RelatedProduct(ltitle, link))
-        product['related_products'] = {"recommended": lrelated}
+        if lrelated:
+            product['related_products'] = {"recommended": lrelated}
 
         price = response.xpath(
             "//div[contains(@class,'product-price')]/@data-load-params")
@@ -119,12 +131,15 @@ class CanadiantireProductsSpider(BaseProductsSpider):
         product = response.meta['product']
 
         cond_set(product, 'title', map(string.strip, response.xpath(
-            "//div[@class='productContent']/h1/div[@id='productName']/text()").extract()))
+            "//div[@class='productContent']/h1"
+            "/div[@id='productName']/text()").extract()))
 
         cond_set(product, 'brand', response.xpath(
-            "//script[contains(text(),'dim7')]/text()").re(r'.*"dim7":"([^"]*)"}.*'))
+            "//script[contains(text(),'dim7')]"
+            "/text()").re(r'.*"dim7":"([^"]*)"}.*'))
 
-        productid = response.xpath("//p[@id='prodNo']/span[@id='metaProductID']/text()")
+        productid = response.xpath(
+            "//p[@id='prodNo']/span[@id='metaProductID']/text()")
         if productid:
             productid = productid.extract()[0].strip().replace('P', '')
             try:
@@ -134,10 +149,12 @@ class CanadiantireProductsSpider(BaseProductsSpider):
                     "Failed to parse upc number : %r" % productid, ERROR)
 
         cond_set(product, 'image_url', response.xpath(
-            "//div[@class='bigImage']/img[@id='mainProductImage']/@src").extract())
+            "//div[@class='bigImage']/img[@id='mainProductImage']"
+            "/@src").extract())
 
         price = response.xpath(
-            "//div[contains(@class,'bigPrice')]/div[@class='price']/descendant::*[text()]/text()")
+            "//div[contains(@class,'bigPrice')]/div[@class='price']"
+            "/descendant::*[text()]/text()")
         price = [x.strip() for x in price.extract()]
         price = "".join(price)
         m = re.match(r'\$(.*)\*.*', price)
@@ -145,7 +162,9 @@ class CanadiantireProductsSpider(BaseProductsSpider):
             price = m.group(1)
         cond_set_value(product, 'price', price)
 
-        info = response.xpath("//div[@id='features']/div[@class='tabContent']/descendant::*[text()]/text()")
+        info = response.xpath(
+            "//div[@id='features']/div[@class='tabContent']"
+            "/descendant::*[text()]/text()")
         if info:
             cond_set_value(product, 'description', " ".join(info.extract()))
 
@@ -157,25 +176,40 @@ class CanadiantireProductsSpider(BaseProductsSpider):
 
         data = json.loads(response.body_as_unicode())
         prod_data = data[0]
+        upc = prod_data.get('skus')[0].get('productNumber')
 
-        cond_set_value(product, 'price', prod_data.get('regularPrice'))
-        cond_set_value(product, 'title', prod_data.get('name'))
+        prices = set(x.get('currentPrice') for x in prod_data.get('skus'))
+        if len(prices) < 2:
+            product['upc'] = upc
+            cond_set_value(product, 'price', prod_data.get('currentPrice'))
+            cond_set_value(product, 'title', prod_data.get('name'))
+            return product
 
-        return product
+        self.log("Product %s PRICES= %s " % (upc, prices), DEBUG)
+        products = []
+        for skudata in prod_data.get('skus'):
+            new_product = product.copy()
+            new_product['upc'] = skudata.get('productNumber')
+            new_product['price'] = skudata.get('currentPrice')
+            new_product['title'] = prod_data.get('name')
+            new_product['model'] = "size:" + skudata.get('product_size')
+
+            products.append(new_product)
+        return products
 
     def _scrape_total_matches(self, response):
         if self.force_tires:
             return self._tires_scrape_total_matches(response)
-        total = response.xpath(
-            "//span[@class='search_results_filter__hilite-count']/text()")
-        if total:
-            return int(total.extract()[0])
-        else:
+        total = self.jsdata["query"]["total-results"]
+        try:
+            return int(total)
+        except ValueError:
             return 0
 
     def _tires_scrape_total_matches(self, response):
         total = response.xpath(
-            "//div[@id='productListing']/strong/h2/text()").re(r'.* returned (\d+) results:')
+            "//div[@id='productListing']/strong/h2"
+            "/text()").re(r'.* returned (\d+) results:')
         if total:
             return int(total[0])
         else:
@@ -187,10 +221,25 @@ class CanadiantireProductsSpider(BaseProductsSpider):
                 "//ul[@id='productList']/li/div[@class='productImage']/a/@href"
             ).extract()
         else:
-            links = response.xpath(
-                "//div[@id='search_results']/*/*/*"
-                "/div[contains(@class,'product_result')]/a/@href"
-            ).extract()
+            text = response.body_as_unicode()
+            m = re.match(r'^SRPInitialLoad\((.*)\)', text, re.DOTALL)
+            links = None
+            if m:
+                jstext = m.group(1)
+                try:
+                    jsdata = json.loads(jstext)
+                except ValueError:
+                    jsdata = {}
+
+                self.jsdata = jsdata
+                links = self.jsdata.get('results')
+
+                def full_ct_url(url):
+                    return urlparse.urljoin('http://www.canadiantire.ca/', url)
+
+                links = [full_ct_url(
+                    x['field']['short-pdp-url'] + '.html') for x in links]
+
         if not links:
             self.log("Found no product links.", ERROR)
 
@@ -200,11 +249,18 @@ class CanadiantireProductsSpider(BaseProductsSpider):
     def _scrape_next_results_page_link(self, response):
         if self.force_tires:
             next_page_links = response.xpath(
-                "//div[@class='pageNumbering']/a[contains(@class,'pageNext')]/@href")
+                "//div[@class='pageNumbering']"
+                "/a[contains(@class,'pageNext')]/@href")
         else:
-            next_page_links = response.xpath(
-                "//li[@class='pagination']/ul/li/a[contains(text(),'Next')]/@href")
+            next_page_link = self.jsdata['pagination']['next']
 
+            def full_url(url):
+                return urlparse.urljoin(response.url, url)
+
+            if not next_page_link or next_page_link == '':
+                return None
+            else:
+                return full_url(next_page_link)
         if next_page_links:
             next_page_link = urlparse.urljoin(
                 response.url, next_page_links.extract()[0])
