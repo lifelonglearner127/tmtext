32b80bf08010d9a063d266465e946794471f8cbe
lifelonglearner127 <life.long.learner127@outlook.com>
2015-02-12 12:19:19 +0200

    amazonfresh spider: search_sort was added, buyer_reviews were populated
    Conflicts:
    	product-ranking/product_ranking/settings.py

--- product-ranking/product_ranking/spiders/amazonfresh.py
+++ product-ranking/product_ranking/spiders/amazonfresh.py
@@ -2,35 +2,58 @@ from __future__ import division, absolute_import, unicode_literals
 from future_builtins import *
 
 import urlparse
+import re
 
 from scrapy.http import Request
 from scrapy import FormRequest
 from scrapy.log import ERROR
 from scrapy.selector import Selector
-from scrapy.utils.project import get_project_settings
 
-from product_ranking.items import SiteProductItem, Price
+from product_ranking.items import SiteProductItem, Price, BuyerReviews
 from product_ranking.spiders import BaseProductsSpider, cond_set, \
     FormatterWithDefaults
 
 
 class AmazonFreshProductsSpider(BaseProductsSpider):
+    """Spider for fresh.amazon.com site.
+
+    Allowed search sort:
+    'relevance'
+    'bestselling'
+    'price_lh'
+    'price_hl'
+
+    to run:
+    scrapy crawl -a searchterms_str=banana [-a zip_code=12345]
+    [-a order=relevance]
+
+    related_products, upc, is_in_stock_only fields don't populated
+    Note: if item marked as 'out_of_stock' price can not be parsed.
+    """
     name = "amazonfresh_products"
     allowed_domains = ["fresh.amazon.com"]
     start_urls = []
 
     SEARCH_URL = "https://fresh.amazon.com/Search?" \
         "resultsPerPage=50&predictiveSearchFlag=true&recipeSearchFlag=false" \
-        "&comNow=&input={search_term}"
+        "&comNow=&input={search_term}&sort={search_sort}"
 
     WELCOME_URL = "https://fresh.amazon.com/welcome"
 
     ZIP_URL = "https://fresh.amazon.com/zipEntrySubmit"
 
-    def __init__(self, zip_code='94117', *args, **kwargs):
+    search_sort = {
+        'relevance': 'relevance',  # default
+        'bestselling': 'bestselling',
+        'price_lh': 'price_low_to_high',
+        'price_hl': 'price_high_to_low',
+    }
+
+    def __init__(self, zip_code='94117', order='relevance', *args, **kwargs):
+        search_sort = self.search_sort.get(order, 'relevance')
         self.zip_code = zip_code
         super(AmazonFreshProductsSpider, self).__init__(
-            url_formatter=FormatterWithDefaults(),
+            url_formatter=FormatterWithDefaults(search_sort=search_sort),
             *args,
             **kwargs
         )
@@ -83,6 +106,20 @@ class AmazonFreshProductsSpider(BaseProductsSpider):
             prod['is_out_of_stock'] = True
         else:
             prod['is_out_of_stock'] = False
+        rating_quantity = response.xpath(
+            '//div[@class="ratingMinimal"]/node()[normalize-space()]'
+        ).extract()
+        rating_pict = response.xpath(
+            '//div[@class="ratingMinimal"]/img/@src'
+        ).extract()
+        if rating_quantity and rating_pict:
+            rating_pict = rating_pict[0]
+            rating_quantity = rating_quantity[0]
+            num_of_reviews = int(re.findall(r'(\d+)', rating_quantity)[0])
+            if num_of_reviews != 0:
+                avg = float(re.findall(r'search/(.*)-star', rating_pict)[0])
+                br = BuyerReviews(num_of_reviews, avg, {})
+                prod['buyer_reviews'] = br
         return prod
 
     def _search_page_error(self, response):
