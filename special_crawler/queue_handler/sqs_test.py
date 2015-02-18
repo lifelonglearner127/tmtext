5020ff27c52416c97800c96ee86c4f0fe4366a05
lifelonglearner127 <life.long.learner127@outlook.com>
2015-02-17 16:43:04 -0800

    Updated message and argument acceptance

--- special_crawler/queue_handler/sqs_test.py
+++ special_crawler/queue_handler/sqs_test.py
@@ -43,16 +43,12 @@ def runTests(scrape_queue_name, process_queue_name, url):
 if (__name__ == '__main__'):
     if len(sys.argv) > 1:
         url = sys.argv[1] # 'localhost/get_data?url=http://www.ozon.ru/context/detail/id/28659614/'
-        url_front = re.findall(r'^(localhost\/get_data\?url=)(.*)', url)[0][0]
-        url_end = re.findall(r'^(localhost\/get_data\?url=)(.*)', url)[0][1]
-        url = url_front + urllib.quote(url_end)
         print runTests('unit_test_scrape', 'unit_test_process', url)
 
     else:
         print "######################################################################################################"
-        print "This is Curl wrapper to support unicode string.(Author: Marek Glica)"
-        print "Please input correct argument.\nfor ex: python curl_wrapper.py 'localhost/get_data?url=http://www.ozon.ru/context/detail/id/28659614/'"
-        print '(Before, we used $ curl "localhost/get_data?url=http://ww.ozon.ru/context/detail/id/28659614/")'
+        print "This script sends a message to an SQS Queue for processing and prints the response.(Author: Quinn Stearns)"
+        print "Please input correct argument.\nfor ex: python sqs_test.py 'http://www.ozon.ru/context/detail/id/28659614/'"
         print "######################################################################################################"
 
     
