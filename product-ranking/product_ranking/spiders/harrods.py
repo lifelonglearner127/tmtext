fd0a543c7ea63e81fd8e8c4da1f6c8a2ca8e4f76
lifelonglearner127 <life.long.learner127@outlook.com>
2015-02-22 08:08:23 +0200

    harrods.com: add brandshop lookup.

--- product-ranking/product_ranking/spiders/harrods.py
+++ product-ranking/product_ranking/spiders/harrods.py
@@ -34,6 +34,7 @@ class HarrodsProductsSpider(BaseProductsSpider):
             self.SORTING = self.SORT_MODES[sort_mode]
 
         self.pageno = 1
+        self.brandvisited = []
         super(HarrodsProductsSpider, self).__init__(
             None,
             site_name=self.allowed_domains[0],
@@ -41,56 +42,104 @@ class HarrodsProductsSpider(BaseProductsSpider):
             **kwargs)
 
     def parse(self, response):
-    #     def full_url(url):
-    #         return urlparse.urljoin(response.url, url)
-        print "PARSE", response.url
+        def full_url(url):
+            return urlparse.urljoin(response.url, url)
+        # Sorting
         if '/search?' in response.url and not response.meta.get('set_sorting'):
-            print " SEARCH DETECTED"
             sorting = self.SORTING
-
             slink = response.xpath(
                 "//div[@class='sli_sort']/ul/li/a[text()='{sorting}']"
                 "/@href".format(sorting=sorting)).extract()
             if slink:
                 slink = slink[0]
-                print "SLINK=", slink
+                # print "SLINK=", slink
                 new_meta = response.meta.copy()
                 new_meta['set_sorting'] = True
                 yield Request(url=slink, meta=new_meta)
+        # Brand shop
+        if '/brand/' in response.url:
+            path = urlparse.urlparse(response.url).path
+            self.brandvisited.append(path)
+            links = response.xpath("//div/h2/a/@href").extract()
+            if links:
+                if response.meta.get('plinks'):
+                    links.extend(response.meta.get('plinks'))
+                    print "extend.links=", links
+                    links = [x for x in links if x not in self.brandvisited]
+                url = links[0]
+                new_meta = response.meta.copy()
+                new_meta['plinks'] = links[1:]
+                new_meta['links'] = []
+                new_meta['pageno'] = 1
+                self.brandvisited.append(url)
+                yield Request(
+                    url=full_url(url), meta=new_meta,
+                    callback=self.parse_links)
+                return
 
-    #     if '/brand/' in response.url:
-    #         print "-" * 20
-    #         print "PARSE BRAND", response.url
-    #         links = response.xpath("//div/h2/a/@href").extract()
-    #         print "add plinks=", links
-    #         if links:
-    #             if response.meta.get('plinks'):
-    #                 links.extend(response.meta.get('plinks'))
-    #                 print "extend.links=", links
-    #             link = links[0]
-    #             new_meta = response.meta.copy()
-    #             new_meta['plinks'] = links[1:]
-    #             print "REQUEST",link
-    #             print "new_meta=",new_meta
-    #             yield Request(url=full_url(link), meta=new_meta, callback=self.parse)
-    #     print "PARSE NO-BRAND"
         for l in super(HarrodsProductsSpider, self).parse(response):
             yield l
-    #         # print "REMAINING=", response.meta.get('remaining')
-    #     print "parse end-FOR",response.meta.get('plinks')
-    #     # print "META=", response.meta
 
-    # def _parse_brand(self, response):
-    #     print "_PARSE_BRAND", response.url
-    #     print "_parse_brand FOR"
-    #     for l in super(HarrodsProductsSpider, self).parse(response):
-    #         print "l2=", type(l), l
-    #         yield l
-    #     print "_parse_brand END-FOR"
+    def parse_links(self, response):
+        def full_url(url):
+            return urlparse.urljoin(response.url, url)
+
+        links = response.xpath(
+            "//ul[contains(@class,'products_row')]/li[@data-type]"
+            "/h3/a/@href").extract()
+        if links:
+            mlinks = response.meta['links']
+            mlinks.extend(links)
+            response.meta['links'] = mlinks[:]
+        plinks = response.xpath("//div/h2/a/@href").extract()
+        if plinks:
+            plinks.extend(response.meta['plinks'])
+        else:
+            plinks = response.meta['plinks']
+        plinks = [x for x in plinks if x not in self.brandvisited]
+        response.meta['plinks'] = plinks
+        if links:
+            pageno = response.meta['pageno']
+            pageno += 1
+            response.meta['pageno'] = pageno
+            cpageno = str(pageno)
+            pages = response.xpath(
+                "//div[@class='pages']/ul[contains(@class,'paging')]"
+                "/li/a[text()='{pageno}']"
+                "/@href".format(pageno=cpageno)).extract()
+            if pages:
+                url = pages[0]
+                new_meta = response.meta.copy()
+                return Request(
+                    url=full_url(url), meta=new_meta,
+                    callback=self.parse_links)
+        if plinks:
+            url = plinks[0]
+            if url in self.brandvisited:
+                raise ValueError("Duplicate %s" % url)
+            self.brandvisited.append(url)
+
+            new_meta = response.meta.copy()
+            new_meta['plinks'] = plinks[1:]
+            new_meta['pageno'] = 1
+            return Request(
+                url=full_url(url), meta=new_meta,
+                callback=self.parse_links)
+        paths = set()
+        l = response.meta['links']
+        ln = []
+        for url in l:
+            path = urlparse.urlparse(url).path
+            if path not in paths:
+                ln.append(url)
+                paths.add(path)
+        # print "LISTS full/filtered", len(l), len(ln)
+        response.meta['links'] = ln
+
+        l = list(super(HarrodsProductsSpider, self).parse(response))
+        return l
 
     def parse_product(self, response):
-        # with open("/tmp/harrods-item.html", "w") as f:
-        #     f.write(response.body_as_unicode().encode('utf-8'))
         def full_url(url):
             return urlparse.urljoin(response.url, url)
 
@@ -98,11 +147,13 @@ class HarrodsProductsSpider(BaseProductsSpider):
         product['url'] = unicode(response.url)
 
         cond_set(product, 'title', response.xpath(
-            "//h1[contains(@class,'product-title')]/span[@itemprop='name']/text()").extract(),
+            "//h1[contains(@class,'product-title')]"
+            "/span[@itemprop='name']/text()").extract(),
             conv=string.strip)
 
         cond_set(product, 'brand', response.xpath(
-            "//h1[contains(@class,'product-title')]/span[@itemprop='brand']/span[@itemprop='name']/text()").extract(),
+            "//h1[contains(@class,'product-title')]/span[@itemprop='brand']"
+            "/span[@itemprop='name']/text()").extract(),
             conv=string.strip)
         price = response.xpath(
             "//span[contains(@class,'price')]/span[@itemprop='offers']"
@@ -114,17 +165,20 @@ class HarrodsProductsSpider(BaseProductsSpider):
                 price=price, priceCurrency='GBP')
 
         cond_set(product, 'upc', response.xpath(
-            "//span[@class='product_code' and @data-prodid]/@data-prodid").extract())
+            "//span[@class='product_code' and @data-prodid]"
+            "/@data-prodid").extract())
 
         cond_set(product, 'image_url', response.xpath(
             "//div[@class='f_left']/a[@id='product_img']/img/@src").extract())
 
-        desc = response.xpath("//div[contains(@class,'product_right_box')]/dl").extract()
+        desc = response.xpath(
+            "//div[contains(@class,'product_right_box')]/dl").extract()
         cond_set_value(product, 'description', "".join(desc))
         cond_set_value(product, 'locale', "en-GB")
 
         rel = []
-        crst = response.xpath("//div[@id='CrossSellTabs']/div/div/div/ul/li/h3")
+        crst = response.xpath(
+            "//div[@id='CrossSellTabs']/div/div/div/ul/li/h3")
         for icrst in crst:
             name = icrst.xpath("text()").extract()
             if name:
@@ -144,13 +198,16 @@ class HarrodsProductsSpider(BaseProductsSpider):
         return alert
 
     def _scrape_total_matches(self, response):
+        if 'links' in response.meta:
+            links = response.meta['links']
+            return len(links)
+
         alert = self._check_alert(response)
         if alert:
             return 0
         total = response.xpath(
             "//a[@class='page_view_all']/span"
             "/text()").re("(\d+)")
-        print "TOTAL=", total
         if total:
             try:
                 return int(total[0])
@@ -161,24 +218,19 @@ class HarrodsProductsSpider(BaseProductsSpider):
     def _scrape_product_links(self, response):
         def full_url(url):
             return urlparse.urljoin(response.url, url)
-        print "_SCRAPE_PRODUCT_LINKS", response.url
-        if "http://www.harrods.com/brand/gucci/shoes" == response.url:
-
-            with open("/tmp/harrods-links-men.html", "w") as f:
-                f.write(response.body_as_unicode().encode('utf-8'))
-            exit(1)                
+        if 'links' in response.meta:
+            links = response.meta['links']
+            for link in links:
+                yield link, SiteProductItem()
+            return
         if self._check_alert(response):
             return
         links = response.xpath(
             "//ul[contains(@class,'products_row')]/li[@data-type]"
             "/h3/a/@href").extract()
-        print "LINKS=", len(links), links
-        #exit(1)
-
         if not links:
             self.log("Found no product links.", DEBUG)
         for link in links:
-            #yield None, SiteProductItem(url=full_url(link))
             yield link, SiteProductItem()
 
     def _scrape_next_results_page_link(self, response):
@@ -186,28 +238,13 @@ class HarrodsProductsSpider(BaseProductsSpider):
             return urlparse.urljoin(response.url, url)
         if self._check_alert(response):
             return
-        # Stop this path    
-        if 0 and "/brand/" in response.url:
-            self.pageno += 1
-            pages = response.xpath("//ul[contains(@class,'paging')]/li/a[text()='{pageno}']/@href".format(pageno=self.pageno)).extract()
-            if pages:
-                return pages[0]
-
-            self.pageno = 1
-            plinks = response.meta.get('plinks')
-            if not plinks:
-                return
-            link = plinks[0]
-            new_meta = response.meta.copy()
-            new_meta['plinks'] = plinks[1:]
-            # print "mew_meta=", new_meta
-            del new_meta['total_matches']
-            print "NEXT BAND",link
-            return Request(url=full_url(link), meta=new_meta)
-
+        if 'links' in response.meta:
+            return
         self.pageno += 1
         cpageno = str(self.pageno)
-        pages = response.xpath("//span[@class='pageselectortext']/a[text()='{pageno}']/@href".format(pageno=cpageno)).extract()
+        pages = response.xpath(
+            "//span[@class='pageselectortext']/a[text()='{pageno}']"
+            "/@href".format(pageno=cpageno)).extract()
         if pages:
             return pages[0]
         else:
