d36f9f795a659813e18b1052cc3b423cf332f05c
lifelonglearner127 <life.long.learner127@outlook.com>
2015-02-26 16:16:58 +0700

    Update the way the queues work

--- deploy/sqs_ranking_spiders/remote_instance_starter.py
+++ deploy/sqs_ranking_spiders/remote_instance_starter.py
@@ -11,16 +11,21 @@ import string
 import json
 import datetime
 import zipfile
+import time
+import hashlib
 
 import boto
 from boto.s3.key import Key
 
+from sqs_connect import SQS_Queue
+
 
 REPO_BASE_PATH = '~/repo/'
 REPO_URL = 'git@bitbucket.org:dfeinleib/tmtext.git'
-TASK_QUEUE_NAME = 'TEST_sqs_ranking_spiders_tasks'  # incoming queue for tasks
-DATA_QUEUE_NAME = 'TEST_sqs_ranking_spiders_data'  # output data
-LOGS_QUEUE_NAME = 'TEST_sqs_ranking_spiders_logs'  # output logs
+TASK_QUEUE_NAME = 'sqs_ranking_spiders_tasks'  # incoming queue for tasks
+DATA_QUEUE_NAME = 'sqs_ranking_spiders_data'  # output data
+LOGS_QUEUE_NAME = 'sqs_ranking_spiders_logs'  # output logs
+PROGRESS_QUEUE_NAME = 'sqs_ranking_spiders_progress'  # progress reports
 JOB_OUTPUT_PATH = '~/job_output'  # local dir
 AMAZON_BUCKET_NAME = 'spyder-bucket'  # Amazon S3 bucket name
 AMAZON_ACCESS_KEY = 'AKIAIKTYYIQIZF3RWNRA'
@@ -29,6 +34,7 @@ CWD = os.path.dirname(os.path.abspath(__file__))
 
 # TODO:
 # * pull message from sqs
+# * separate the initial starter script and the daemon (coz we may want to update the daemon in the future)
 # * push data to sqs (split into parts!)
 # * push logs into sqs (split into parts!)
 # * push compressed data & log files to Amazon S3 (for future processing)
@@ -72,14 +78,40 @@ def read_file_by_parts(fname, part_size=1024*128):
         yield part
 
 
-def read_msg_from_sqs(queue_name):
-    raise NotImplementedError
+def read_msg_from_sqs(queue_name_or_instance):
+    if isinstance(queue_name_or_instance, (str, unicode)):
+        sqs_queue = SQS_Queue(queue_name_or_instance)
+    else:
+        sqs_queue = queue_name_or_instance
+    if sqs_queue.count() == 0:
+        return  # the queue is empty
+    try:
+        # Get message from SQS
+        message = sqs_queue.get()
+    except IndexError as e:
+        # This exception will most likely be triggered because you were
+        #  grabbing off an empty queue
+        return
+    except Exception as e:
+        # Catch all other exceptions to prevent the whole thing from crashing
+        # TODO : Consider testing that sqs_scrape is still live, and restart
+        #  it if needed
+        return
+    try:
+        message = json.loads(message)
+    except Exception, e:
+        return
+    return message, sqs_queue  # we will need sqs_queue later
 
 
-def write_msg_to_sqs(queue_name, msg):
+def write_msg_to_sqs(queue_name_or_instance, msg):
     if not isinstance(msg, (str, unicode)):
         msg = json.dumps(msg)
-    raise NotImplementedError
+    if isinstance(queue_name_or_instance, (str, unicode)):
+        sqs_queue = SQS_Queue(queue_name_or_instance)
+    else:
+        sqs_queue = queue_name_or_instance
+    sqs_queue.put(msg)
 
 
 def put_file_into_sqs(fname, queue_name, metadata):
@@ -140,13 +172,51 @@ def put_file_into_s3(bucket_name, fname,
         print "Check file path..."
 
 
+def report_progress_and_wait(data_file, log_file, metadata,
+                             initial_sleep_time=15, sleep_time=3):
+    time.sleep(initial_sleep_time)
+    # if the data file does not exist - try to wait a bit longer
+    _max_initial_attempts = 10
+    for i in xrange(_max_initial_attempts):
+        if i >= _max_initial_attempts - 2:
+            return  # error - the data file still does not exist
+        if os.path.exists(data_file) and os.path.exists(log_file):
+            break  # the files exist, that means the spider has been started
+        time.sleep(initial_sleep_time)
+    write_msg_to_sqs(
+        metadata['server_name']+PROGRESS_QUEUE_NAME,
+        {
+            '_msg_id': metadata.get('task_id', metadata.get('task', None)),
+            'utc_datetime': datetime.datetime.utcnow(),
+            'progress': 0
+        }
+    )
+    while 1:  # main loop
+        raise NotImplementedError
+
+
 def execute_task_from_sqs():
     #TODO: code for pulling the SQS message from the queue
-    spider_name = 'walmart_products'
-    quantity = 100
-    searchterms_str = 'guitar'
-    metadata = read_msg_from_sqs(TASK_QUEUE_NAME)
+    while 1:  # try to read from the queue until a new message arrives
+        msg = read_msg_from_sqs(TASK_QUEUE_NAME)
+        if msg is None:
+            time.sleep(3)
+            continue
+        metadata, task_queue = msg  # store task_queue to re-use this instance
+                                    #  later
 
+    task_id = metadata.get('task_id', metadata.get('task', None))
+    searchterms_str = metadata.get('searchterms_str', None)
+    url = metadata.get('url', None)
+    site = metadata['site']
+    server_name = metadata['server_name']
+    cmd_line_args = metadata.get('cmd_args', {})  # dict of extra command-line
+                                                  # args, such as ordering
+
+    # TODO: remove me! it's only for tests
+    spider_name = 'walmart_products'
+    searchterms_str = 'guitar'
+    cmd_line_args = {'quantity': 100}
 
     # make sure the job output dir exists
     if not os.path.exists(os.path.expanduser(JOB_OUTPUT_PATH)):
@@ -154,20 +224,26 @@ def execute_task_from_sqs():
 
     local_job_id = get_random_hash()
     output_path = '%s/%s' % (os.path.expanduser(JOB_OUTPUT_PATH), local_job_id)
-    cmd = ('cd %s/tmtext/product-ranking/product_ranking '
-           '&& scrapy crawl %s -a searchterms_str="%s" %s -s LOG_FILE=%s -o %s')
+    cmd = ('cd %s/tmtext/product-ranking/product_ranking'
+           ' && scrapy crawl %s -a searchterms_str="%s" %s'
+           ' -s LOG_FILE=%s -o %s &')
+    # prepare command-line arguments
     options = ' '
-    if 'quantity' in locals():
-        options += '-a quantity=%s' % quantity
+    for key, value in cmd_line_args:
+        options += ' -a %s=%s' % (key, value)
     os.system(
         cmd % (
             REPO_BASE_PATH, spider_name, searchterms_str,
             options, output_path+'.log', output_path+'.jl')
     )
-    put_file_into_sqs(output_path+'.jl', DATA_QUEUE_NAME, metadata)
-    put_file_into_sqs(output_path+'.log', LOGS_QUEUE_NAME, metadata)
-    put_file_into_s3(output_path+'.jl')
-    put_file_into_s3(output_path+'.log')
+    # report progress and wait until the task is done
+    report_progress_and_wait(output_path+'.jl', output_path+'.log', metadata)
+    # upload the files to SQS and S3
+    put_file_into_sqs(output_path+'.jl', server_name+DATA_QUEUE_NAME, metadata)
+    put_file_into_sqs(output_path+'.log', server_name+LOGS_QUEUE_NAME,
+                      metadata)
+    put_file_into_s3(AMAZON_BUCKET_NAME, output_path+'.jl')
+    put_file_into_s3(AMAZON_BUCKET_NAME, output_path+'.log')
 
 
 if __name__ == '__main__':
