874c44d82cd99b8c14d1a1d5c299ea7598229f9a
lifelonglearner127 <life.long.learner127@outlook.com>
2015-03-04 09:02:15 +0000

    fix product name issue for special case

--- special_crawler/extract_data.py
+++ special_crawler/extract_data.py
@@ -187,7 +187,7 @@ class Scraper():
 
     #load phantom driver
     driver = webdriver.PhantomJS()
-    driver.set_window_size(1120, 550)
+    driver.set_window_size(100, 100)
 
     def load_image_hashes():
         '''Read file with image hashes list
