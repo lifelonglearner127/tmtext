bb007ee0479d0611f72d627cbec9c50d1cfebf56
lifelonglearner127 <life.long.learner127@outlook.com>
2015-03-05 21:00:55 +0100

    to debug in mobile_image_same()

--- special_crawler/compare_images.py
+++ special_crawler/compare_images.py
@@ -19,19 +19,23 @@ def compare_images(img1, img2):
         path += ".jpg"
 
         is_local = os.path.isfile(image)
+        return 1112
         if bool(re.findall("^[a-zA-Z]+://", image)):
             path = "1000.jpg"
             urllib.urlretrieve(image, path)
-
+            return 1113
+        return 1114
         if ext not in (".jpg", ".jpeg", ".png"):
             Image.open(path).convert('RGB').save(path)
-
+            return 1115
+        return 1116
         img = cv2.imread(path)
         hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
         hist.append(get_calcHist(hsv))
+        return 1117
         if not is_local:
             os.remove(path)
-
+        return 1118
     # Test all 4 comparison methods? iterate over methods in cycle
     #for i in range(0, 4):
     #    comp = cv2.compareHist(hist[0], hist[1], i)
