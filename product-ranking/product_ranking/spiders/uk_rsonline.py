aaab8121257beea4cefee3e3c5a97279f6807efb
lifelonglearner127 <life.long.learner127@outlook.com>
2015-03-30 15:16:59 +0400

    Fix: uk_rsonline_products - minimized probability of errors (download delay, rotating user-agents etc)

--- product-ranking/product_ranking/spiders/uk_rsonline.py
+++ product-ranking/product_ranking/spiders/uk_rsonline.py
@@ -7,7 +7,8 @@ import time
 
 from scrapy import Request
 
-from product_ranking.items import RelatedProduct, BuyerReviews, Price
+from product_ranking.items import RelatedProduct, BuyerReviews, Price, \
+    SiteProductItem
 from product_ranking.spiders import cond_set, cond_set_value, \
     cond_replace_value
 from product_ranking.spiders.contrib.product_spider import ProductsSpider
@@ -19,6 +20,21 @@ def _itemprop(response, prop, extract=True):
     else:
         return response.css('[itemprop="%s"]' % prop)
 
+
+USER_AGENT_LIST = [
+    'Mozilla/5.0 (Windows NT 6.1; rv:27.3) Gecko/20130101 Firefox/27.3',
+    'Mozilla/5.0 (X11; OpenBSD amd64; rv:28.0) Gecko/20100101 Firefox/28.0',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/29.0',
+    'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0',
+    'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
+    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36',
+    'Mozilla/5.0 (X11; OpenBSD i386) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36'
+]
 
 class UkRsOnlineProductsSpider(ProductsSpider):
     """ uk.rs-online.com product ranking spider
@@ -40,6 +56,14 @@ class UkRsOnlineProductsSpider(ProductsSpider):
 
     user_agent = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:35.0) Gecko'
                   '/20100101 Firefox/35.0')
+
+    download_delay = 5
+    enable_cookies = False
+
+    ERR_URL = 'http://uk.rs-online.com/web/app/error'
+    ERR_MSG = 'Your request could not be completed at this time'
+    ERR_RETRY_DELAY = 2
+    ERR_RETRIES = 10
 
     def _is_product_page(self, response):
         return response.url.startswith('http://uk.rs-online.com/web/p')
@@ -63,20 +87,45 @@ class UkRsOnlineProductsSpider(ProductsSpider):
             return [response]
         return response.css('.resultRow')
 
+    def _scrape_product_links(self, response):
+        for box in self._fetch_product_boxes(response):
+            url = urlparse.urljoin(response.url, self._link_from_box(box))
+            product = SiteProductItem()
+            self._populate_from_box(response, box, product)
+            meta = response.meta.copy()
+            meta['product'] = product
+            user_agent = USER_AGENT_LIST.pop(0)
+            USER_AGENT_LIST.append(user_agent)
+            request = Request(url, callback=self.parse_product, meta=meta)
+            request.headers.setdefault('User-Agent', user_agent)
+            yield request, product
+
     def _link_from_box(self, box):
         return box.css('.srDescDiv a::attr(href), '
                        '.tnProdDesc::attr(href)')[0].extract()
 
     def parse_product(self, response):
-        if response.url.startswith('http://uk.rs-online.com/web/app/error'):
-            self.log('Error detected, 1 second delay')
-            time.sleep(1)
-            return [Request(response.meta['product']['url'],
-                            callback=self.parse_product,
-                            meta=response.meta)]
-        else:
-            return list(
-                super(UkRsOnlineProductsSpider, self).parse_product(response))
+        is_err_page = response.url.startswith(self.ERR_URL)
+        if is_err_page or response.css('.red').re(self.ERR_MSG):
+            retry_num = response.meta.get("ERR_RETRY_NUM", 1)
+            if retry_num < self.ERR_RETRIES:
+                self.log(
+                    'Error detected, %i seconds delay (%i/%i retries) %s' %
+                    (self.ERR_RETRY_DELAY, retry_num, self.ERR_RETRIES,
+                     response.meta['product']['url']))
+                time.sleep(self.ERR_RETRY_DELAY)
+                response.meta['ERR_RETRY_NUM'] = retry_num + 1
+                user_agent = USER_AGENT_LIST.pop(0)
+                USER_AGENT_LIST.append(user_agent)
+                request = Request(response.meta['product']['url'],
+                                  callback=self.parse_product,
+                                  meta=response.meta, dont_filter=True)
+                request.headers.setdefault('User-Agent', user_agent)
+                return [request]
+            else:
+                self.log('Product page contains an error')
+        return list(
+            super(UkRsOnlineProductsSpider, self).parse_product(response))
 
     def _populate_from_box(self, response, box, product):
         if self._is_product_page(response):
@@ -111,8 +160,7 @@ class UkRsOnlineProductsSpider(ProductsSpider):
                        bool)
         details = response.css('.prodDetailsContainer').xpath(
             'node()[normalize-space()]')
-        details = [d.extract() for d in details if
-                   not d.css('.heading , form')]
+        details = [d.extract() for d in details if not d.css('form')]
         if details:
             cond_set_value(product, 'description', details, ''.join)
         self._populate_related_products(response, product)
