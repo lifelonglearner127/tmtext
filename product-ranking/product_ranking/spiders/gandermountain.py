e71f7ca3544acd58cbc3c730ffd4ed74caff1c5b
lifelonglearner127 <life.long.learner127@outlook.com>
2015-03-31 17:03:00 +0400

    gandermountain_products - commit 1

--- product-ranking/product_ranking/spiders/gandermountain.py
+++ product-ranking/product_ranking/spiders/gandermountain.py
@@ -1,111 +1,116 @@
 from __future__ import division, absolute_import, unicode_literals
-from future_builtins import *
+import json
+import re
 
-import urllib
-import urlparse
+from scrapy.http import HtmlResponse
 
-from scrapy.log import ERROR
-from scrapy.selector import Selector
-
-from scrapy.http import Request
-from product_ranking.items import SiteProductItem, Price
-from product_ranking.spiders import BaseProductsSpider, cond_set, \
-    FormatterWithDefaults
+from product_ranking.items import Price, BuyerReviews
+from product_ranking.spiders import cond_set, \
+    cond_set_value
 
 # scrapy crawl amazoncouk_products -a searchterms_str="iPhone"
 
-class GandermountainProductsSpider(BaseProductsSpider):
+
+from product_ranking.spiders.contrib.product_spider import ProductsSpider
+
+
+# TODO: related_products
+
+
+class GandermountainProductsSpider(ProductsSpider):
     name = "gandermountain_products"
     allowed_domains = ["www.gandermountain.com"]
     start_urls = []
 
-    
-    SEARCH_URL = "http://www.gandermountain.com/search/{search_term}"
+    SEARCH_URL = "http://www.gandermountain.com/modperl/wbsrvcs" \
+                 "/adobeSearch.cgi?do=json&q={search_term}&sort={sort_mode}" \
+                 "&page={page}"
 
-    
+    SORT_MODES = {
+        'default': ''
+    }
 
     def __init__(self, *args, **kwargs):
-        # locations = settings.get('AMAZONFRESH_LOCATION')
-        # loc = locations.get(location, '')
         super(GandermountainProductsSpider, self).__init__(*args, **kwargs)
+        self.url_formatter.defaults['page'] = 1
 
-    def parse_product(self, response):
-        prod = response.meta['product']
+    def parse(self, response):
+        json_data = json.loads(response.body_as_unicode())
+        resp = HtmlResponse(response.url, response.status, response.headers,
+                            ''.join([chunk for chunk in json_data.itervalues()
+                                     if isinstance(chunk, unicode)]),
+                            response.flags, response.request,
+                            encoding=response.encoding)
 
-        title = response.xpath('//div[@class="product-title"]/h1/text()').extract()
-        cond_set(prod, 'title', title)
-
-        price = response.xpath('//span[@class="regPrice"]/text()').re('([\d.]+)')
-        
-        if not price:
-            price = response.xpath('//span[@id="sale-range"]/text()').re('([\d.]+)')
-        if not price:
-            price = response.xpath('//div[@class="saleprice"][position()=1]/span/text()').re('([\d.]+)')
-            
-        prod['price'] = Price(price=price[0], priceCurrency='USD')
-
-        cond_set(prod, 'price', price)
-
-        des = response.xpath('//td[@class="detailsText"]').extract()
-        cond_set(prod, 'description', des)
-
-        img_url = response.xpath('//div[@class="zoomPad"]/img/@src').extract()
-        cond_set(prod, 'image_url', img_url)
-
-        cond_set(prod, 'locale', ['en-US'])
-
-        prod['url'] = response.url
-        return prod
-
-    def _search_page_error(self, response):
-        sel = Selector(response)
-
-        try:
-            found1 = sel.xpath('//section[@class="search-no-message"]/h2/text()').extract()[0]
-            found2 = sel.xpath(
-                '//div[@class="warning"]/p/strong/text()'
-            ).extract()[0]
-            found = found1 + " " + found2
-            if 'No Results Found' in found:
-                self.log(found, ERROR)
-                return True
-            return False
-        except IndexError:
-            return False
-
-    def _scrape_total_matches(self, response):
-        
-        
-        if 'did not match any products.' in response.body_as_unicode():
-            total_matches = 0
-        else:
-            count_matches = response.xpath(
-                '//p[@class="page-numbers"][position()=1]/strong[position()=1]/text()').re('([\d,]+)')
-
-            if count_matches and count_matches[-1]:
-                total_matches = int(count_matches[-1])
-            else:
-                total_matches = None
-        return total_matches
-
-    def _scrape_product_links(self, response):
-        links = response.xpath(
-            '//a[@class="browse-pdp-link"]/@href').extract()
-        for link in links:
-            yield link, SiteProductItem()
+        return list(super(GandermountainProductsSpider, self).parse(resp))
 
     def _scrape_next_results_page_link(self, response):
+        next_link = response.css('[alt=arrow-r-blue]')
+        if not next_link:
+            return
+        page = int(re.findall('page=(\d+)', response.url)[0]) + 1
+        search_term = response.meta['search_term']
+        return self.url_formatter.format(self.SEARCH_URL, page=page,
+                                         search_term=search_term)
 
-        page = response.xpath(
-            '//p[@class="page-numbers"]/a[position()=last()]/@onclick'
-        ).re('page=(\d+)')
-        
-        next_link = self.SEARCH_URL.replace('{search_term}', self.searchterms[0]) + "#filters?do=json&i=1&page=" +page[0] +"&q="+self.searchterms[0]+"&matched_cat=&ckey=i1page"+page[0]+"q"+self.searchterms[0]+"&cat_depth=0&matched_cat=0&is_refined=1"
-        
+    def _total_matches_from_html(self, response):
+        matches = response.xpath(
+            '//p[@class="page-numbers"]/strong/text()').re('\d+')
+        return int(matches[0]) if matches else 0
 
-        if next_link:
-            return next_link
-        return None
+    def _fetch_product_boxes(self, response):
+        return response.css('[id*=bItem]')
 
+    def _link_from_box(self, box):
+        return box.css('a[title]::attr(href)')[0].extract()
 
+    def _populate_from_box(self, response, box, product):
+        cond_set(product, 'title',
+                 box.css('a[data-item-number]::attr(title)').extract())
+        cond_set(product, 'price',
+                 box.css('.red-message.price-point::text').re('\$([\d ,.]+)'))
+        cond_set(product, 'price',
+                 box.css('.price-point::text').re('\$([\d ,.]+)'))
+
+    def _populate_from_html(self, response, product):
+        cond_set(product, 'title', response.css('.product-title h1::text'),
+                 unicode.strip)
+        cond_set(product, 'price',
+                 response.css('.saleprice span::text').re('\$([\d ,.]+)'))
+        cond_set(product, 'price',
+                 response.css('.regprice span::text').re('\$([\d ,.]+)'))
+        cond_set(product, 'image_url',
+                 response.css('.jqzoom img::attr(src)').extract())
+        cond_set_value(product, 'is_out_of_stock', not (response.css(
+            '.stockstatus .info::text').re('In Stock|Low Stock')))
+        cond_set(product, 'brand',
+                 response.css('.alignBrandImageSpec::attr(alt)').extract(),
+                 lambda brand: brand.replace('_', ' '))
+        xpath = '//td[@class="detailsText"]/node()[normalize-space()]'
+        cond_set_value(product, 'description',
+                       response.xpath(xpath).extract(), u''.join)
+        price = product.get('price', None)
+        if price == 0:
+            del product['price']
+        elif price:
+            product['price'] = Price(priceCurrency='USD',
+                                     price=re.sub('[ ,]', '', price))
+        self._populate_buyer_reviews(response, product)
+
+    def _populate_buyer_reviews(self, response, product):
+        total = response.css(
+            '.pr-snapshot-average-based-on-text .count::text').re('[\d ,]+')
+        if not total:
+            return
+        total = int(re.sub('[ ,]', '', total[0]))
+        avg = response.css('.pr-rating.pr-rounded.average::text')[0].extract()
+        avg = float(avg)
+        by_star = response.css('.pr-histogram-count span::text')
+        by_star = by_star.re('\(([\d, ]+)\)')
+        by_star = {i + 1: int(re.sub('[ ,]', '', c))
+                   for i, c in enumerate(reversed(by_star))}
+        cond_set_value(product, 'buyer_reviews',
+                       BuyerReviews(num_of_reviews=total,
+                                    average_rating=avg,
+                                    rating_by_star=by_star))
 
