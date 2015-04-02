f61311893d619181201263c566cfb640194152aa
lifelonglearner127 <life.long.learner127@outlook.com>
2015-04-02 13:20:30 +0300

    post_starters scripts for sqs were added to master branch

---
+++ deploy/sqs_ranking_spiders/post_starter_root.py
@@ -0,0 +1,37 @@
+# Put anything here you want to be executed BY THE SUPERUSER right after
+#  the instance spins up
+
+import os
+import sys
+
+
+main_folder = '/home/spiders/repo/'
+
+
+def can_run():
+    if os.path.exists(os.path.join(main_folder,
+                                   'remote_instance_starter.py.marker')):
+        # this script wasn't executed yet
+        if not os.path.exists(os.path.join(main_folder, __file__+'.marker')):
+            return True
+
+
+def mark_as_finished():
+    """ Mark this machine as the one that has already executed this script """
+    with open(os.path.join(main_folder, __file__+'.marker'), 'w') as fh:
+        fh.write('1')
+
+
+def main():
+    f = open('/tmp/check_file_post_starter_root', 'w')
+    f.write('1')
+    f.close()
+    # put anything you want here...
+    pass
+
+
+if __name__ == '__main__':
+    if not can_run():
+        sys.exit()
+    main()
+    mark_as_finished()
