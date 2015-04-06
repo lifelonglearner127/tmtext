d7b296a3caf1f18979f961dfc9eaeec6273d88ea
lifelonglearner127 <life.long.learner127@outlook.com>
2015-04-06 16:45:16 +0700

    Queue names should be dict to avoid putting something into production queue

--- deploy/sqs_ranking_spiders/add_task_to_sqs.py
+++ deploy/sqs_ranking_spiders/add_task_to_sqs.py
@@ -1,21 +1,21 @@
-import os
 import sys
 import json
+import random
 
 from boto.sqs.message import Message
 import boto.sqs
 
 
-TASK_QUEUES_LIST = [
-    'sqs_ranking_spiders_tasks',  # production one
-    'sqs_ranking_spiders_tasks_dev',  # development one
-    'sqs_ranking_spiders_tasks_tests',  # test one
-]
+TASK_QUEUES_LIST = {
+    'production': 'sqs_ranking_spiders_tasks',
+    'dev': 'sqs_ranking_spiders_tasks_dev',
+    'test': 'sqs_ranking_spiders_tasks_tests'
+}
 
 ### Script can be run with 'manual' argument to provide manual edditing
 # of task message
 
-def put_msg_to_sqs(message, queue_name=TASK_QUEUES_LIST[0]):
+def put_msg_to_sqs(message, queue_name=TASK_QUEUES_LIST['test']):
     conn = boto.sqs.connect_to_region("us-east-1")
     q = conn.get_queue(queue_name)
     m = Message()
@@ -26,11 +26,7 @@ def put_msg_to_sqs(message, queue_name=TASK_QUEUES_LIST[0]):
 
 
 def edit_message(msg):
-    keys = ['task_id',
-            'site',
-            'searchterms_str',
-            'server_name'
-            ]
+    keys = ['task_id', 'site', 'searchterms_str', 'server_name']
     for key in keys:
         data = raw_input("Provide '%s' for task message.\n"
                          "If you left field blunk default value will be used.\n"
@@ -52,10 +48,10 @@ if __name__ == '__main__':
     msg = {
             'task_id': 4444,
             'site': 'amazon',
-            'searchterms_str': 'water',
-            'server_name': 'test_server_name_urls',
-            # "url": "http://www.amazon.com/sdfasfasdfasdfasdfas",
-            'cmd_args': {'quantity': 400}
+            'searchterms_str': random.choice(['water', 'cola', 'wine']),
+            'server_name': 'test_server_name',
+            # "url": "http://www.walmart.com/ip/42211446?productRedirect=true",
+            'cmd_args': {'quantity': 30}
         }
     if 'manual' in [a.lower().strip() for a in sys.argv]:
         edit_message(msg)
