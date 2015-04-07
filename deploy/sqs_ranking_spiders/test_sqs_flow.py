896fa1082a4418d2571c69575438f7bab0b46578
lifelonglearner127 <life.long.learner127@outlook.com>
2015-04-07 19:03:29 +0700

    sqs tests: working, but not finished version

--- deploy/sqs_ranking_spiders/test_sqs_flow.py
+++ deploy/sqs_ranking_spiders/test_sqs_flow.py
@@ -12,6 +12,7 @@ import json
 
 import boto
 import boto.sqs
+from boto.s3.connection import S3Connection
 
 
 CWD = os.path.dirname(os.path.abspath(__file__))
@@ -33,15 +34,26 @@ except ImportError:
 MAX_WORKING_TIME = 30  # 30 mins
 LOCAL_DUMP_PATH = '/tmp/_test_sqs_messages'
 REGION = "us-east-1"
+DELETE_MESSAGES = True  # set False to avoid deleting messages in output queues
 
 
-def _create_sqs_queue(queue_name, visib_timeout=30):
+def _get_conn():
     conf = read_access_and_secret_keys()
     conn = boto.sqs.connect_to_region(
         REGION,
         aws_access_key_id=conf[0],
         aws_secret_access_key=conf[1]
     )
+    return conn
+
+
+def _get_sqs_queue(queue_name):
+    conn = _get_conn()
+    return conn.get_queue(queue_name)
+
+
+def _create_sqs_queue(queue_name, visib_timeout=30):
+    conn = _get_conn()
     try:
         return conn.create_queue(queue_name, visib_timeout)
     except Exception, e:
@@ -50,13 +62,7 @@ def _create_sqs_queue(queue_name, visib_timeout=30):
 
 def _read_queue(queue_name):
     """ Returns all the messages in the queue """
-    conf = read_access_and_secret_keys()
-    conn = boto.sqs.connect_to_region(
-        REGION,
-        aws_access_key_id=conf[0],
-        aws_secret_access_key=conf[1]
-    )
-    queue = conn.get_queue(queue_name)
+    queue = _get_sqs_queue(queue_name)
     if queue is None:
         return []  # the queue has not been created yet (?)
     return queue.get_messages(
@@ -71,13 +77,17 @@ def get_sqs_messages(queue_name):
             os.makedirs(LOCAL_DUMP_PATH)
         fname = str(random.randint(1000000, 2000000))
         if not isinstance(message, (str, unicode)):
-            message = json.dumps(message)
-        with open(os.path.join(LOCAL_DUMP_PATH, fname), 'w') as fh:
+            message = json.dumps(message.get_body())
+        fname = os.path.join(LOCAL_DUMP_PATH, fname)
+        with open(fname, 'w') as fh:
             fh.write(message)
         return fname
 
+    queue = _get_sqs_queue(queue_name)
     for message in _read_queue(queue_name):
         fname = dump_message(message)
+        if DELETE_MESSAGES:
+            queue.delete_message(message)
         yield fname
 
 
@@ -87,15 +97,54 @@ def read_msgs_until_task_id_appears(task_id, queue_name, max_wait_time=30*60):
         for fname in list(get_sqs_messages(queue_name)):
             with open(fname, 'r') as fh:
                 msg_data = json.loads(fh.read())
+            # may still be string due to double json encoding on dumps()
+            if isinstance(msg_data, (str, unicode)):
+                msg_data = json.loads(msg_data)
             task_id_sqs = msg_data['_msg_id']
             if str(task_id) == str(task_id_sqs):
                 return fname, msg_data
 
 
+def download_s3_file(bucket, s3_path, local_path):
+    aws_connection = S3Connection(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY)
+    bucket = aws_connection.get_bucket(bucket)
+    key = bucket.get_key(s3_path)
+    key.get_contents_to_filename(local_path)
+
+
+def validate_data_file(fname, search_term):
+    """ Checks the local data file (downloaded from S3) """
+    all_lines_are_jsons = True
+    with open(fname, 'r') as fh:
+        for line_no, line in enumerate(fh):
+            line = line.strip()
+            if not line:
+                continue
+            try:
+                line = json.loads(line)
+            except Exception, e:
+                print 'LINE %i is not JSON!' % line_no
+                all_lines_are_jsons = False
+                continue
+            if line.get('search_term', '') != search_term:
+                print 'SEARCH TERMS DO NOT MARCH AT LINE %i' % line_no
+                return False
+    return all_lines_are_jsons
+
+
+def is_plain_jsonlist_file(fname):
+    with open(fname, 'r') as fh:
+        cont = fh.read(1024)
+    if not cont:
+        return
+    return cont[0] == '{'
+
+
 if __name__ == '__main__':
     test_server_name = 'test_server'
     test_queue_name = 'sqs_ranking_spiders_tasks_tests'
-    search_term = 'snowboard'
+    search_term = 'asus'
+    local_data_file = '/tmp/_s3_data_file.zip'
 
     # create output 'queues' for this server
     _create_sqs_queue(test_server_name+'sqs_ranking_spiders_progress',
@@ -122,6 +171,7 @@ if __name__ == '__main__':
         sys.exit()
     fname, msg_data = progress_result
     print 'Progress queue first message:', msg_data
+    assert 'progress' in msg_data
 
     # 3. Read the output queue and get the S3 bucket filename
     output_result = read_msgs_until_task_id_appears(
@@ -131,6 +181,15 @@ if __name__ == '__main__':
         sys.exit()
     fname, msg_data = output_result
     print 'Data queue first message:', msg_data
+    assert 's3_key_data' in msg_data
 
     # 4. Read the S3 bucket file and validate its content
-    # TODO: implement
+    download_s3_file(
+        AMAZON_BUCKET_NAME, msg_data['s3_key_data'], local_data_file)
+    if not is_plain_jsonlist_file(local_data_file):
+        # TODO: unpack files if needed!
+        assert False, 'zip extraction is not implemented yet - do it!'
+    if validate_data_file(local_data_file, search_term):
+        print 'EVERYTHING IS OK'
+    else:
+        print 'DATA FILE CHECK FAILED'
