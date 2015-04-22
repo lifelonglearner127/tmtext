2e60d137b650e7b93859aeb04d0d2e7192673447
lifelonglearner127 <life.long.learner127@outlook.com>
2015-04-22 11:18:06 +0300

    staplesadvantage_products: single_url supported now

--- product-ranking/product_ranking/spiders/staplesadvantage.py
+++ product-ranking/product_ranking/spiders/staplesadvantage.py
@@ -189,6 +189,10 @@ class StaplesadvantageProductsSpider(ProductsSpider):
                  unicode.strip)
 
     def _populate_from_html(self, response, product):
+        title = response.xpath('//p[contains(@class, "search-prod-desc")]'
+            '/@title'
+            ).extract()
+        cond_set(product, 'title', title)
         xpath = '//div[@id="dotcombrand"]/../preceding-sibling::li[1]/text()'
         brand = response.xpath(xpath).extract()
         if not brand:
@@ -263,3 +267,6 @@ class StaplesadvantageProductsSpider(ProductsSpider):
         total = data['review_count']
         cond_set_value(response.meta['product'], 'buyer_reviews',
                        BuyerReviews(total, avg, ratings))
+
+    def _parse_single_product(self, response):
+        return self.parse_product(response)
