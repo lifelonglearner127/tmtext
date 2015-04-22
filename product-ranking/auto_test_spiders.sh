24b90ba1a9add17765361daa6530e94fa39f0259
lifelonglearner127 <life.long.learner127@outlook.com>
2015-04-22 02:58:46 +0200

    updated: auto test shell script

--- product-ranking/auto_test_spiders.sh
+++ product-ranking/auto_test_spiders.sh
@@ -4,4 +4,5 @@
 # PATH=$PATH:/usr/local/bin
 # export PATH
 source /home/web_runner/virtual-environments/scrapyd/bin/activate
+cd /home/web_runner/repos/tmtext/product-ranking
 scrapy crawl amazon_products -a searchterms_str="MSIA 02"  -a validate=1
