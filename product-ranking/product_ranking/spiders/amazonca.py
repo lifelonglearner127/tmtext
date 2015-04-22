d2edd9e738ed289a8daea3eb396fb656961fbc40
lifelonglearner127 <life.long.learner127@outlook.com>
2015-04-22 14:05:15 +0300

    Fix: remove calculate_buyer_reviews.

--- product-ranking/product_ranking/spiders/amazonca.py
+++ product-ranking/product_ranking/spiders/amazonca.py
@@ -113,7 +113,7 @@ class AmazonProductsSpider(BaseProductsSpider):
 
     def _populate_from_html(self, response, product):
         cond_set(product, 'brand', response.css('#brand ::text').extract())
-
+        cond_set(product, 'brand', ['NO BRAND'])
         price = response.xpath(
             '//span[@id="priceblock_saleprice"]/text()'
         ).extract()
@@ -391,8 +391,8 @@ class AmazonProductsSpider(BaseProductsSpider):
         average = response.css('#avgRating span::text').extract()
         average = re.search('\d[\d ,.]*', average[0] if average else '')
         average = float(re.sub('[ ,]+', '',
-                               average.group())) if average else None
-        ratings = {}
+                               average.group())) if average else 0.0
+        ratings = {1: 0, 2: 0, 3: 0, 4: 0,5: 0}
         for row in response.css('.a-histogram-row .a-span10 ~ td a'):
             title = row.css('::attr(title)').extract()
             text = row.css('::text').extract()
@@ -402,46 +402,9 @@ class AmazonProductsSpider(BaseProductsSpider):
                 stars = int(re.sub('[ ,]+', '', stars.group(1)))
                 ratings[stars] = int(text[0])
         if not total:
-            total = sum(ratings.itervalues()) if ratings else 0
-        if not average:
-            average = sum(k * v for k, v in
-                          ratings.iteritems()) / total if ratings else 0
-
-        if not ratings:
-            table = response.xpath(
-                '//table[@id="histogramTable"]'
-                '/tr[@class="a-histogram-row"]')
-            ratings \
-                = self._calculate_buyer_reviews_from_percents(
-                    total, table)
+            total = sum(ratings.itervalues())
 
         buyer_reviews = BuyerReviews(num_of_reviews=total,
                                      average_rating=average,
                                      rating_by_star=ratings)
         cond_set_value(product, 'buyer_reviews', buyer_reviews)
-
-    def _calculate_buyer_reviews_from_percents(self, total_reviews, table):
-        rating_by_star = {}
-        for title in table.xpath('.//a/@title'):
-            title = title.extract()
-            _match = re.search('(\d+)% of reviews have (\d+) star', title)
-            if _match:
-                _percent, _star = _match.group(1), _match.group(2)
-                if not _star.isdigit() or not _percent.isdigit():
-                    continue
-                rating_by_star[_star] = int(_percent)
-            else:
-                continue
-        # check if some stars are missing (that means, percent is 0)
-        for _star in range(1, 5):
-            if _star not in rating_by_star and str(_star) not in rating_by_star:
-                rating_by_star[str(_star)] = 0
-        # turn percents into numbers
-        for _star, _percent in rating_by_star.items():
-            if int(total_reviews) == 0:  # avoid division by zero
-                rating_by_star[_star] = 0
-            else:
-                rating_by_star[_star] \
-                    = float(int(total_reviews)) * (float(_percent) / 100)
-                rating_by_star[_star] = int(round(rating_by_star[_star]))
-        return rating_by_star
