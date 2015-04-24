707fd54b1a1db431d8934ff75c39237c722fc104
lifelonglearner127 <life.long.learner127@outlook.com>
2015-04-24 16:04:15 +0300

    adjsust tests

--- deploy/cache_layer/test_cache_flow.py
+++ deploy/cache_layer/test_cache_flow.py
@@ -99,114 +99,114 @@ class TestSQSCache(unittest.TestCase):
         if cached:
             self.assertEqual(message['cached'], cached)
 
-    # def test_non_existed_response_and_request_and_send_same_task_again(self):
-    #     print(
-    #         "\nThree cases at one time:"
-    #         "\n\t1. Response not exist, request not exist."
-    #         "\n\t2. Response not exist, request exist and fresh enough."
-    #         "\n\t3. Response exist at database and fresh enough."
-    #     )
-    #     # response not exist, request not exist
-    #     print("Send non existing task to cache")
-    #     msg = self.generate_message()
-    #     print(msg["task_id"])
-    #     self.provide_msg_to_queue(msg, self.cache_task_queue)
-    #     queue_name = msg['server_name'] + self.cache_progress_queue
-    #     status_list = [
-    #         'Ok. Task was received.',
-    #         'Redirect request to spiders sqs.',
-    #     ]
-    #     status = self.check_status(queue_name)
-    #     print ("Rcvd status: '%s'" % status)
-    #     self.assertTrue(status in status_list)
-    #     status2 = self.check_status(queue_name)
-    #     print ("Rcvd status: '%s'" % status2)
-    #     self.assertTrue(status2 in status_list)
+    def test_non_existed_response_and_request_and_send_same_task_again(self):
+        print(
+            "\nThree cases at one time:"
+            "\n\t1. Response not exist, request not exist."
+            "\n\t2. Response not exist, request exist and fresh enough."
+            "\n\t3. Response exist at database and fresh enough."
+        )
+        # response not exist, request not exist
+        print("Send non existing task to cache")
+        msg = self.generate_message()
+        print(msg["task_id"])
+        self.provide_msg_to_queue(msg, self.cache_task_queue)
+        queue_name = msg['server_name'] + self.cache_progress_queue
+        status_list = [
+            'Ok. Task was received.',
+            'Redirect request to spiders sqs.',
+        ]
+        status = self.check_status(queue_name)
+        print ("Rcvd status: '%s'" % status)
+        self.assertTrue(status in status_list)
+        status2 = self.check_status(queue_name)
+        print ("Rcvd status: '%s'" % status2)
+        self.assertTrue(status2 in status_list)
 
-    #     # response not exist, request exist and fresh enough
-    #     print("Send the same task again(but change server_name)."
-    #           "Response not exist but request already was generated")
-    #     another_server_msg = copy.deepcopy(msg)
-    #     second_server = 'cache_test_server_name2'
-    #     another_server_msg['server_name'] = second_server
-    #     self.provide_msg_to_queue(another_server_msg, self.cache_task_queue)
-    #     second_server_q = second_server + self.cache_progress_queue
-    #     status_list_for_second_server = [
-    #         'Ok. Task was received.',
-    #         'Wait for request from other instance.',
-    #     ]
-    #     status = self.check_status(second_server_q)
-    #     print ("Rcvd status: '%s'" % status)
-    #     self.assertTrue(status in status_list_for_second_server)
-    #     status2 = self.check_status(second_server_q)
-    #     print ("Rcvd status: '%s'" % status2)
-    #     self.assertTrue(status2 in status_list_for_second_server)
+        # response not exist, request exist and fresh enough
+        print("Send the same task again(but change server_name)."
+              "Response not exist but request already was generated")
+        another_server_msg = copy.deepcopy(msg)
+        second_server = 'cache_test_server_name2'
+        another_server_msg['server_name'] = second_server
+        self.provide_msg_to_queue(another_server_msg, self.cache_task_queue)
+        second_server_q = second_server + self.cache_progress_queue
+        status_list_for_second_server = [
+            'Ok. Task was received.',
+            'Wait for request from other instance.',
+        ]
+        status = self.check_status(second_server_q)
+        print ("Rcvd status: '%s'" % status)
+        self.assertTrue(status in status_list_for_second_server)
+        status2 = self.check_status(second_server_q)
+        print ("Rcvd status: '%s'" % status2)
+        self.assertTrue(status2 in status_list_for_second_server)
 
-    #     print("Now wait until scrapy_daemon/cache will finish their work")
-    #     print("It may take about 20-30 minutes")
-    #     while True:
-    #         status = self.check_status(queue_name)
-    #         if status:
-    #             self.assertEqual(status, 'Finished.')
-    #             break
-    #     self.check_cache_results(msg)
-    #     self.check_cache_results(another_server_msg)
+        print("Now wait until scrapy_daemon/cache will finish their work")
+        print("It may take about 20-30 minutes")
+        while True:
+            status = self.check_status(queue_name)
+            if status:
+                self.assertEqual(status, 'Finished.')
+                break
+        self.check_cache_results(msg)
+        self.check_cache_results(another_server_msg)
 
-    #     # response exist at database and fresh enough(request not required)
-    #     print("Send again the same task to cache. Response should exists")
-    #     msg['task_id'] = random.randint(100000, 200000)
-    #     print(msg["task_id"])
-    #     self.provide_msg_to_queue(msg, self.cache_task_queue)
-    #     status_list = [
-    #         'Ok. Task was received.',
-    #         'Finished.',
-    #     ]
-    #     status = self.check_status(queue_name)
-    #     print ("Rcvd status: '%s'" % status)
-    #     self.assertTrue(status in status_list)
-    #     status2 = self.check_status(queue_name)
-    #     print ("Rcvd status: '%s'" % status2)
-    #     self.assertTrue(status2 in status_list)
+        # response exist at database and fresh enough(request not required)
+        print("Send again the same task to cache. Response should exists")
+        msg['task_id'] = random.randint(100000, 200000)
+        print(msg["task_id"])
+        self.provide_msg_to_queue(msg, self.cache_task_queue)
+        status_list = [
+            'Ok. Task was received.',
+            'Finished.',
+        ]
+        status = self.check_status(queue_name)
+        print ("Rcvd status: '%s'" % status)
+        self.assertTrue(status in status_list)
+        status2 = self.check_status(queue_name)
+        print ("Rcvd status: '%s'" % status2)
+        self.assertTrue(status2 in status_list)
 
-    #     self.check_cache_results(msg, cached=True)
-    #     task_stamp = sqs_cache.generate_task_stamp(msg)
-    #     print("Delete collected responses for this task")
-    #     self.db.hdel('responses', task_stamp)
-    #     self.db.hdel('last_request', task_stamp)
+        self.check_cache_results(msg, cached=True)
+        task_stamp = sqs_cache.generate_task_stamp(msg)
+        print("Delete collected responses for this task")
+        self.db.hdel('responses', task_stamp)
+        self.db.hdel('last_request', task_stamp)
 
-    # def test_not_fresh_request(self):
-    #     print("\nCase: Response not exist,"
-    #           " request exist but not fresh enough")
-    #     msg = self.generate_message()
-    #     print("First simulate that request for the same task was already"
-    #           " sent but more than 1 hour ago")
-    #     print(msg["task_id"])
-    #     task_stamp = sqs_cache.generate_task_stamp(msg)
-    #     request_item = (time.time() + 3601, msg['server_name'])
-    #     self.db.hset('last_request', task_stamp, time.time()-3601)
-    #     print("Now send task to cache.")
-    #     self.provide_msg_to_queue(msg, self.cache_task_queue)
-    #     queue_name = msg['server_name'] + self.cache_progress_queue
-    #     status_list = [
-    #         'Ok. Task was received.',
-    #         'Request for this task was found but '\
-    #         'it was sent more than 1 hour ago.',
-    #         'Redirect request to spiders sqs.',
-    #     ]
-    #     for i in range(3):
-    #         status = self.check_status(queue_name)
-    #         print ("Rcvd status: '%s'" % status)
-    #         self.assertTrue(status in status_list)
-    #     print("Now wait until scrapy_daemon/cache will finish their work")
-    #     print("It may take about 20-30 minutes")
-    #     while True:
-    #         status = self.check_status(queue_name)
-    #         if status:
-    #             self.assertEqual(status, 'Finished.')
-    #             break
-    #     self.check_cache_results(msg)
-    #     self.db.hdel('responses', task_stamp)
-    #     self.db.hdel('last_request', task_stamp)
+    def test_not_fresh_request(self):
+        print("\nCase: Response not exist,"
+              " request exist but not fresh enough")
+        msg = self.generate_message()
+        print("First simulate that request for the same task was already"
+              " sent but more than 1 hour ago")
+        print(msg["task_id"])
+        task_stamp = sqs_cache.generate_task_stamp(msg)
+        request_item = (time.time() + 3601, msg['server_name'])
+        self.db.hset('last_request', task_stamp, time.time()-3601)
+        print("Now send task to cache.")
+        self.provide_msg_to_queue(msg, self.cache_task_queue)
+        queue_name = msg['server_name'] + self.cache_progress_queue
+        status_list = [
+            'Ok. Task was received.',
+            'Request for this task was found but '\
+            'it was sent more than 1 hour ago.',
+            'Redirect request to spiders sqs.',
+        ]
+        for i in range(3):
+            status = self.check_status(queue_name)
+            print ("Rcvd status: '%s'" % status)
+            self.assertTrue(status in status_list)
+        print("Now wait until scrapy_daemon/cache will finish their work")
+        print("It may take about 20-30 minutes")
+        while True:
+            status = self.check_status(queue_name)
+            if status:
+                self.assertEqual(status, 'Finished.')
+                break
+        self.check_cache_results(msg)
+        self.db.hdel('responses', task_stamp)
+        self.db.hdel('last_request', task_stamp)
 
     def test_response_not_satisfy_freshness(self):
         print("\nCase: Response exist but not satisfy freshness")
