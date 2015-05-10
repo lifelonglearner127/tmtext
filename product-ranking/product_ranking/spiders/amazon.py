cba99453cd93a09d84fd2b20866a1ee7eb859930
lifelonglearner127 <life.long.learner127@outlook.com>
2015-05-11 00:59:20 +0700

    Update optional fields

--- product-ranking/product_ranking/spiders/amazon.py
+++ product-ranking/product_ranking/spiders/amazon.py
@@ -35,10 +35,10 @@ except ImportError as e:
 
 
 class AmazonValidatorSettings(object):  # do NOT set BaseValidatorSettings as parent
-    optional_fields = ['model', 'brand', 'description', 'price']
+    optional_fields = ['model', 'brand', 'price']
     ignore_fields = [
         'is_in_store_only', 'is_out_of_stock', 'related_products', 'upc',
-        'buyer_reviews', 'google_source_site'
+        'buyer_reviews', 'google_source_site', 'description'
     ]
     ignore_log_errors = False  # don't check logs for errors?
     ignore_log_duplications = False  # ... duplicated requests?
