c31612db19d849c1ecef74ff4eaa3395b91222d5
lifelonglearner127 <life.long.learner127@outlook.com>
2015-05-20 10:05:52 +0300

    sqs metrics were updated

--- deploy/cache_layer/additional_sqs_metrics.py
+++ deploy/cache_layer/additional_sqs_metrics.py
@@ -12,6 +12,25 @@ def check_instance_quantity():
     return len(group.instances)
 
 
+def get_key_value(key_name, bucket, purge=False, next_value=False):
+    k = Key(bucket)
+    k.key = key_name
+    try:
+        result = int(k.get_contents_as_string())
+    except:
+        k.set_contents_from_string('0')
+        result = 0
+    # in case get_contets_as_string return blank string ''
+    if not result:
+        result = 0
+    if purge:
+        k.set_contents_from_string('0')
+    if next_value:
+        if not isinstance(next_value, str):
+            next_value = str(next_value)
+        k.set_contents_from_string(next_value)
+    return result
+
 def get_sqs_metrics(purge=False):
     """
     This function will count sqs metrics and return them in json format
@@ -21,24 +40,18 @@ def get_sqs_metrics(purge=False):
     AMAZON_BUCKET_NAME = 'spyder-bucket'  # Amazon S3 bucket name
     conn = boto.connect_s3()
     bucket = conn.get_bucket(AMAZON_BUCKET_NAME, validate=False)
-    k = Key(bucket)
     result = {}
     keys_names = [
         'daily_sqs_instance_counter',
-        'daily_sqs_executed_tasks'
+        'daily_sqs_executed_tasks',
+        'previous_day_tasks_at_sqs'
     ]
 
     # get daily_sqs_instance_counter
-    k.key = keys_names[0]
-    result[keys_names[0]] = int(k.get_contents_as_string())
-    if purge:
-        k.set_contents_from_string('0')
+    result[keys_names[0]] = get_key_value(keys_names[0], bucket, purge=purge)
 
     # get daily_sqs_executed_tasks
-    k.key = keys_names[1]
-    executed_task = k.get_contents_as_string()
-    if purge:
-        k.set_contents_from_string('0')
+    executed_task = get_key_value(keys_names[1], bucket, purge=purge)
     waiting_task = 0
     sqs_conn = boto.sqs.connect_to_region('us-east-1')
     # TODO: import this from sqs_ranking_spiders/__init__.py
@@ -53,7 +66,10 @@ def get_sqs_metrics(purge=False):
             waiting_task += int(q.count())
         except Exception as e:
             print e
-    total_tasks_quantity = int(executed_task) + waiting_task
+    previous_day_tasks_at_sqs = get_key_value(keys_names[2], bucket,
+        next_value=waiting_task)
+    total_tasks_quantity = int(executed_task) + waiting_task -\
+        previous_day_tasks_at_sqs
     result['total_tasks_quantity'] = total_tasks_quantity
     return json.dumps(result)
 
