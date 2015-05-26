0e80771bbdd991613e61afc6f75e75b0a4289de2
lifelonglearner127 <life.long.learner127@outlook.com>
2015-05-26 11:47:33 +0300

    Amazon.fr: fix model, add bestseller keywords, add testing

--- product-ranking/product_ranking/amazon_bestsellers.py
+++ product-ranking/product_ranking/amazon_bestsellers.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #
 # See http://bugzilla.contentanalyticsinc.com/show_bug.cgi?id=285
 #
@@ -36,6 +37,57 @@ DEPARTMENTS_CATEGORIES = [
     'Apps & Games',
     'Kindle Accessories',
     'Gift Cards',
+    #amazon.fr
+    'Toutes nos boutiques', 
+    'Animalerie', 
+    'Applis & Jeux', 
+    'Auto et Moto', 
+    'Bagages', 
+    'Beauté et Parfum', 
+    'Bijoux', 
+    'Boutique chèques-cadeaux', 
+    'Boutique Kindle', 
+    'Bricolage', 
+    'Bébés & Puériculture', 
+    'Chaussures et Sacs', 
+    'Cuisine & Maison', 
+    'DVD & Blu-ray', 
+    'Fournitures de bureau', 
+    'Gros électroménager', 
+    'High-tech', 
+    'Informatique', 
+    'Instruments de musique & Sono', 
+    'Jardin', 
+    'Jeux et Jouets', 
+    'Jeux vidéo', 
+    'Livres anglais et étrangers', 
+    'Livres en français', 
+    'Logiciels', 
+    'Luminaires et Eclairage', 
+    'Montres', 
+    'Musique : CD & Vinyles', 
+    'Musique classique', 
+    'Santé et Soins du corps', 
+    'Sports et Loisirs', 
+    'Téléchargement de musique', 
+    'Vêtements et accessoires',
+    'Téléchargement de musique', 
+    'Amazon Cloud Drive', 
+    'Liseuses Kindle & ebooks', 
+    'Tablettes Fire', 
+    'App-Shop pour Android', 
+    'Jeux et Logiciels Digitaux', 
+    'Livres', 
+    'Musique, DVD et Blu-ray', 
+    'Jeux vidéo et Consoles', 
+    'High-Tech et Informatique', 
+    'Jouets, Enfants et Bébés', 
+    'Maison, Bricolage, Animalerie', 
+    'Beauté, Santé, Alimentation', 
+    'Vêtements et Chaussures', 
+    'Montres et Bijoux', 
+    'Sports et Loisirs', 
+    'Auto et Moto',
 ]
 
 
@@ -44,7 +96,12 @@ def amazon_parse_department(cats):
         cat = r.get('category')
         rank = r.get('rank')
         # first, try to match the full category
-        if cat in DEPARTMENTS_CATEGORIES:
+        print('+'*50)
+        print(cat)
+        print(cat.lower() in [x.lower() for x in DEPARTMENTS_CATEGORIES])
+        print('+'*50)
+        if cat in DEPARTMENTS_CATEGORIES \
+            or cat.lower() in [x.lower() for x in DEPARTMENTS_CATEGORIES]:
             return {cat: rank}
         cats = [_c.strip() for _c in cat.split('>')]
         if cats:
