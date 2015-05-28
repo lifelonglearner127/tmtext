ad73d385ba7e1c60d23956afa727430164fc022e
lifelonglearner127 <life.long.learner127@outlook.com>
2015-05-28 20:56:32 +0100

    asda

--- special_crawler/extract_asda_data.py
+++ special_crawler/extract_asda_data.py
@@ -4,6 +4,9 @@ import re
 import lxml
 import lxml.html
 import requests
+import urllib
+import json
+import ast
 
 from itertools import groupby
 
@@ -18,6 +21,12 @@ class AsdaScraper(Scraper):
 
     INVALID_URL_MESSAGE = "Expected URL format is http://www.ulta.com/ulta/browse/productDetail.jsp?productId=<product-id>"
 
+    def __init__(self, **kwargs):# **kwargs are presumably (url, bot)
+        Scraper.__init__(self, **kwargs)
+
+        # product json
+        self.product_json = None
+
     def check_url_format(self):
         """Checks product URL format for this scraper instance is valid.
         Returns:
@@ -25,7 +34,7 @@ class AsdaScraper(Scraper):
         """
 
         m = re.match(r"^http://groceries\.asda\.com/asda-webstore/landing/home\.shtml\?cmpid=ahc-_-ghs-d1-_"
-                     r"-asdacom-dsk-_-hp/search/.+$", self.product_page_url)
+                     r"-asdacom-dsk-_-hp/search/.+#/product/.+$", self.product_page_url)
 
         return not not m
 
@@ -39,9 +48,7 @@ class AsdaScraper(Scraper):
         """
         self._product_json()
 
-        itemtype = self.tree_html.xpath('//div[@id="pd-cms-container"]')
-
-        if not itemtype:
+        if not self.product_json:
             return True
 
         return False
@@ -53,46 +60,69 @@ class AsdaScraper(Scraper):
         return self.product_page_url
 
     def _product_json(self):
-        product_id_start_index = self.product_page_url.rfind("/", 0)
-        product_id = self.product_page_url[product_id_start_index:]
+        item_id_start_index = self.product_page_url.rfind("/", 0)
+        item_id = self.product_page_url[item_id_start_index + 1:]
+        product_json_url = "http://groceries.asda.com/api/items/view?itemid=" + item_id + \
+                           "&responsegroup=extended&cacheable=true&storeid=4565&shipdate=currentDate&requestorigin=gi"
 
-        product_json_url = "http://groceries.asda.com/api/items/view?itemid=910001528811&responsegroup=extended&cacheable=true&storeid=4565&shipdate=currentDate&requestorigin=gi"
+        contents = urllib.urlopen(product_json_url).read()
+
+        try:
+            self.product_json = json.loads(contents)
+        except:
+            self.product_json = None
 
     def _product_id(self):
-        product_id = self.tree_html.xpath("//div[@id='pd-wrapper']//div[@class='prod-code']/h3/span/text()")[0].strip()
+        if not self.product_json:
+            self._product_json()
 
-        return product_id
+        return self._product_json["items"][0]["cin"]
+
+    def _item_id(self):
+        item_id_start_index = self.product_page_url.rfind("/", 0)
+        item_id = self.product_page_url[item_id_start_index + 1:]
+
+        return item_id
 
     ##########################################
     ############### CONTAINER : PRODUCT_INFO
     ##########################################
     def _product_name(self):
-        return self.tree_html.xpath("//div[@id='pd-wrapper']//div[@id='itemDetails']/h1[class='prod-title']/text()")[0].strip()
+        if not self.product_json:
+            self._product_json()
+
+        return self.product_json["items"][0]["name"]
 
     def _product_title(self):
-        return self.tree_html.xpath("//div[@id='pd-wrapper']//div[@id='itemDetails']/h1[class='prod-title']/text()")[0].strip()
+        if not self.product_json:
+            self._product_json()
+
+        return self.product_json["items"][0]["name"]
 
     def _title_seo(self):
-        return self.tree_html.xpath("//div[@id='pd-wrapper']//div[@id='itemDetails']/h1[class='prod-title']/text()")[0].strip()
+        if not self.product_json:
+            self._product_json()
+
+        return self.product_json["items"][0]["name"]
 
     def _model(self):
         return None
 
     def _features(self):
-        nutritional_values_table = self.tree_html.xpath("//div[@id='pd-wrapper']//"
-                                                        "div[@class='nv-table']/table/tbody/tr")
+        if not self.product_json:
+            self._product_json()
 
-        if not nutritional_values_table:
+        if "nutritionalValues" not in self.product_json["items"][0]["productDetails"] or \
+                len(self.product_json["items"][0]["productDetails"]["nutritionalValues"]) == 0:
             return None
 
-        features = []
+        features_json = self.product_json["items"][0]["productDetails"]["nutritionalValues"]["values"]
+        features_string_list = []
 
-        for nutritional_value in nutritional_values_table:
-            feature = ""
-            feature = " " . join(nutritional_value.xpath("./td/text()"))
-            features.append(feature)
+        for feature in features_json:
+            features_string_list.append(feature["value1"] + " " + feature["value2"] + " " + feature["value3"])
 
-        return features
+        return features_string_list
 
     def _feature_count(self):
         features = self._features()
@@ -103,37 +133,38 @@ class AsdaScraper(Scraper):
         return len(features)
 
     def _description(self):
+        if not self.product_json:
+            self._product_json()
 
-        description_element = self.tree_html.xpath('//p[@id="prodDescription"]')
-
-        if not description_element:
-            return None
-
-        return lxml.html.tostring(description_element)
+        return self.product_json["items"][0]["description"]
 
     # extract product long description from its product product page tree
     # ! may throw exception if not found
     # TODO:
     #      - keep line endings maybe? (it sometimes looks sort of like a table and removing them makes things confusing)
     def _long_description(self):
-        description_elements = self.tree_html.xpath('//div[@class="product-catalog"]')
-        return None
+        long_description_url = "http://groceries.asda.com/asda-webstore/pages/product_details/view1.shtml?A521198.RWD"
+        contents = urllib.urlopen(long_description_url).read()
+        start_index = contents.find('<h4 class="sect-title">Product Information</h4><p class="p-text">')
+        start_index += len('<h4 class="sect-title">Product Information</h4><p class="p-text">')
+        end_index = contents.find("</p><% if(typeof ACCELERATOR.CONFIG.enableProductRatingReview")
+
+        return contents[start_index:end_index]
 
     def _ingredients(self):
-        product_catalog_list = self.tree_html.xpath('//div[@class="product-catalog"]')
+        if not self.product_json:
+            self._product_json()
 
-        for product_catalog in product_catalog_list:
-            head_text = product_catalog.xpath("div[@class='product-catalog-head']/text()")
+        ingredients = None
 
-            if head_text:
-                head_text = head_text[0].strip()
+        if len(self.product_json["items"][0]["productDetails"]["ingredients"]) == 0:
+            return None
 
-                if "Ingredients" in head_text:
-                    ingredients = product_catalog.xpath("div[@class='product-catalog-content']/text()")[0].strip()
+        ingredients = self.product_json["items"][0]["productDetails"]["ingredients"][1:-1]
 
-                    return ingredients.split(', ')
+        ingredients = ingredients.split(",")
 
-        return None
+        return ingredients
 
     def _ingredients_count(self):
         if not self._ingredients():
@@ -148,17 +179,13 @@ class AsdaScraper(Scraper):
         pass
         
     def _image_urls(self):
-        main_image_url = self.tree_html.xpath('//meta[@property="og:image"]/@content')[0]
-        thumb_image_urls = [main_image_url]
-        thumb_image_urls.extend(self.tree_html.xpath('//div[@class="product-detail-thumbnail-image"]//img/@alt'))
+        if not self.product_json:
+            self._product_json()
 
-        for idx, item in enumerate(thumb_image_urls):
-            if "http:" in item:
-                thumb_image_urls[idx] = item.strip()
-            else:
-                thumb_image_urls[idx] = "http:" + item.strip()
+        image_urls = []
+        image_urls.append(self.product_json["items"][0]["images"]["largeImage"])
 
-        return thumb_image_urls
+        return image_urls
 
     def _image_count(self):
         if not self._image_urls():
@@ -167,25 +194,10 @@ class AsdaScraper(Scraper):
         return len(self._image_urls())
 
     def _video_urls(self):
-        video_urls = self.tree_html.xpath('//iframe/@src')
-        video_urls = [url for url in video_urls if "www.youtube.com" in url]
-
-        for idx, item in enumerate(video_urls):
-            if "http:" in item:
-                video_urls[idx] = item.strip()
-            else:
-                video_urls[idx] = "http:" + item.strip()
-
-        if not video_urls:
-            return None
-
-        return video_urls
+        return None
 
     def _video_count(self):
-        if not self._video_urls():
-            return 0
-
-        return len(self._video_urls())
+        return 0
 
     # return dictionary with one element containing the PDF
     def _pdf_urls(self):
@@ -198,85 +210,52 @@ class AsdaScraper(Scraper):
         return 0
 
     def _htags(self):
-        htags_dict = {}
-        htags_dict["h1"] = map(lambda t: self._clean_text(t), self.tree_html.xpath("//h1//text()[normalize-space()!='']"))
-        htags_dict["h2"] = map(lambda t: self._clean_text(t), self.tree_html.xpath("//h2//text()[normalize-space()!='']"))
-
-        return htags_dict
+        return None
 
     def _keywords(self):
-        return self.tree_html.xpath('//meta[@name="keywords"]/@content')[0].strip()
+        return None
 
     ##########################################
     ############### CONTAINER : REVIEWS
     ##########################################
 
     def _average_review(self):
-        if not self.tree_html.xpath('//div[@id="product-review-container"]/a[@id="reviews"]'):
-            return None
-
-        return float(self.tree_html.xpath('//span[@class="pr-rating pr-rounded average"]/text()')[0])
+        return None
 
     def _review_count(self):
-        if not self.tree_html.xpath('//div[@id="product-review-container"]/a[@id="reviews"]'):
-            return int(0)
+        if not self.product_json:
+            self._product_json()
 
-        return int(self.tree_html.xpath('//p[@class="pr-snapshot-average-based-on-text"]/span[@class="count"]/text()')[0])
+        return int(self.product_json["items"][0]["totalReviewCount"])
 
     def _max_review(self):
-        '''
-        if not self._reviews():
-            return 0.0
-
-        return max(self._reviews())
-        '''
         return None
 
     def _min_review(self):
-        '''
-        if not self._reviews():
-            return 0.0
-
-        return min(self._reviews())
-        '''
         return None
 
     def _reviews(self):
-        '''
-        if not self.tree_html.xpath('//div[@id="product-review-container"]/a[@id="reviews"]'):
-            return None
-
-        reviews = self.tree_html.xpath('//span[@class="pr-rating pr-rounded"]/text()')
-
-        for idx, item in enumerate(reviews):
-            reviews[idx] = float(item)
-
-        reviews.sort()
-        review_count = [len(list(group)) for key, group in groupby(reviews)]
-        reviews = list(set(reviews))
-
-        reviews = [list(a) for a in zip(reviews, review_count)]
-
-        return reviews
-        '''
         return None
 
     ##########################################
     ############### CONTAINER : SELLERS
     ##########################################
     def _price(self):
-        currency = ""
+        if not self.product_json:
+            self._product_json()
 
-        if self._price_currency() == "USD":
-            currency = "$"
-
-        return currency + "{0:.2f}".format(self._price_amount())
+        return self.product_json["items"][0]["price"]
 
     def _price_amount(self):
-        return float(self.tree_html.xpath("//meta[@property='product:price:amount']/@content")[0])
+        if not self.product_json:
+            self._product_json()
+
+        return float(self.product_json["items"][0]["price"][1:])
 
     def _price_currency(self):
-        return self.tree_html.xpath("//meta[@property='product:price:currency']/@content")[0]
+        currency = "GBP"
+
+        return currency
 
     def _owned(self):
         return 0
@@ -285,58 +264,15 @@ class AsdaScraper(Scraper):
         return 0
 
     def _site_online(self):
-        if self._in_stores_only() == 1:
-            return 0
-
-        '''
-        if self._site_online_only() == 1:
-            return 1
-        '''
-
         return 1
 
     def _site_online_only(self):
-
-        skuDisplayName = self.tree_html.xpath('//h6[@id="skuDisplayName"]/text()')
-
-        if skuDisplayName:
-            skuDisplayName = skuDisplayName[0]
-
-            if "Online Only" in skuDisplayName:
-                return 1
-
-        contents = requests.get(self.product_page_url).text
-        tree = html.fromstring(contents)
-
-        if tree.xpath('//div[@id="productBadge"]/img'):
-            productBadge = " " . join(tree.xpath('//div[@id="productBadge"]/img/@src'))
-
-            if "http://images.ulta.com/is/image/Ulta/badge-online-only" in productBadge:
-                return 1
-
         return 0
 
     def _in_stores(self):
-        if self._site_online_only() == 1:
-            return 0
-
-        '''
-        if self._in_stores_only() == 1:
-            return 1
-        '''
-
         return 1
 
     def _in_stores_only(self):
-        contents = requests.get(self.product_page_url).text
-        tree = html.fromstring(contents)
-
-        if tree.xpath('//div[@id="productBadge"]/img'):
-            productBadge = " " . join(tree.xpath('//div[@id="productBadge"]/img/@src'))
-
-            if "http://images.ulta.com/is/image/Ulta/badge-instore" in productBadge:
-                return 1
-
         return 0
 
     def _site_online_out_of_stock(self):
@@ -355,15 +291,16 @@ class AsdaScraper(Scraper):
     ############### CONTAINER : CLASSIFICATION
     ##########################################    
     def _categories(self):
-        categories_text = self.tree_html.xpath('//div[@class="makeup-breadcrumb"]/ul/li/a/text()')
-
-        return categories_text[1:]
+        return None
 
     def _category_name(self):
-        return self._categories()[-1]
+        return None
 
     def _brand(self):
-        return self.tree_html.xpath('//input[@id="pinBrand"]/@value')[0].strip()
+        if not self.product_json:
+            self._product_json()
+
+        return self.product_json["items"][0]["brandName"]
 
     ##########################################
     ################ HELPER FUNCTIONS
