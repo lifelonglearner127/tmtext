479644f1265fa599e5f6b698c91ecaf33159cb2e
lifelonglearner127 <life.long.learner127@outlook.com>
2015-06-03 00:32:49 +0300

    regression

--- special_crawler/test_scraper_service.py
+++ special_crawler/test_scraper_service.py
@@ -642,6 +642,7 @@ class ServiceScraperTest(unittest.TestCase):
         for row in self.cur:
             urls.extend(row[0].splitlines())
 
+        urls = [url.replace('%3A',':') for url in urls]
         urls = list(set(urls))
         urls = filter(lambda x: website + ".com" in x, urls)
 #        urls = [urls[random.randrange(len(urls))] for item in range(test_url_number)]
