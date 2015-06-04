99784811b77335873b5b14e29587aed266060e75
lifelonglearner127 <life.long.learner127@outlook.com>
2015-06-04 12:08:06 +0300

    fix: added walmart new field - last_buyer_review_date

--- product-ranking/product_ranking/items.py
+++ product-ranking/product_ranking/items.py
@@ -184,3 +184,7 @@ class SiteProductItem(Item):
     date_of_last_question = Field()  # now for Walmart only
     recent_questions = Field()  # now for Walmart only; may change in the future
     special_pricing = Field() # 1/0 for TPC, Rollback; target, walmart
+
+    #  now for Walmart and Amazon;
+    #  may change in the future
+    last_buyer_review_date = Field()
