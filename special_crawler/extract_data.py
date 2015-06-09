10f4417824a23e0af2c592ee6839ceeda4fb8755
lifelonglearner127 <life.long.learner127@outlook.com>
2015-06-09 23:58:59 +0700

    Fix: method call

--- special_crawler/extract_data.py
+++ special_crawler/extract_data.py
@@ -451,9 +451,10 @@ class Scraper():
             try:
                 if isinstance(self.ALL_DATA_TYPES[info], (str, unicode)):
                     _method_to_call = getattr(self, self.ALL_DATA_TYPES[info])
+                    results = _method_to_call()
                 else:  # callable?
                     _method_to_call = self.ALL_DATA_TYPES[info]
-                results = _method_to_call(self)
+                    results = _method_to_call(self)
             except IndexError, e:
                 sys.stderr.write("ERROR: No " + info + " for " + self.product_page_url.encode("utf-8") + ":\n" + str(e) + "\n")
                 results = None
