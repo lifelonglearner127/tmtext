09b69580bb49a42c8c676767c3f1ad50469a3e71
lifelonglearner127 <life.long.learner127@outlook.com>
2015-06-13 14:21:36 +0100

    variantion

--- spiders_shared_code/amazon_variants.py
+++ spiders_shared_code/amazon_variants.py
@@ -1,7 +1,8 @@
 import json
 
 import lxml.html
-
+import itertools
+import re
 
 class AmazonVariants(object):
 
@@ -186,49 +187,68 @@ class AmazonVariants(object):
             return None
 
     def _stockstatus_for_variants(self):
-        color_list = self._color()
-        size_list = self._size()
-        style_list = self._style()
-        flavor_list = self._flavor()
-
-        if not color_list and not size_list and not style_list and not flavor_list:
-            return None
-
         try:
             page_raw_text = lxml.html.tostring(self.tree_html)
-            startIndex = page_raw_text.find('"dimensionValuesDisplayData":') + len('"dimensionValuesDisplayData":')
+            variation_key_values = json.loads(re.search('"variation_values":(.+?),"deviceType', page_raw_text).group(1))
+            variation_key_list = list(variation_key_values)
 
-            if startIndex == -1:
-                return None
+            for index, variation_key in enumerate(variation_key_list):
+                variation_key_list[index] = variation_key[:variation_key.find("_")]
 
-            endIndex = page_raw_text.find("}", startIndex) + 1
-            json_text = page_raw_text[startIndex:endIndex]
-            json_body =json.loads(json_text)
+            variation_values_list = []
+
+            for variation_key in variation_key_values:
+                variation_values_list.append(variation_key_values[variation_key])
+
+            variation_combinations_values = list(itertools.product(*variation_values_list))
+
+            variation_combinations_values = map(list, variation_combinations_values)
+            instock_variation_combinations_values = json.loads(re.search('"dimensionValuesDisplayData":(.+?),"hidePopover":', page_raw_text).group(1))
+            instock_variation_combinations_values = [instock_variation_combinations_values[key] for key in instock_variation_combinations_values]
+
+            outofstock_variation_combinations_values = [variation_combination for variation_combination in variation_combinations_values if variation_combination not in instock_variation_combinations_values]
+
+            selected_variation_combination = json.loads(re.search('"selected_variations":(.+?),"jqupgrade"', page_raw_text).group(1))
+
+            selected_variation_combination = [selected_variation_combination[key] for key in selected_variation_combination]
 
             stockstatus_for_variants_list = []
 
-            for asin in json_body:
+            for variation_combination in outofstock_variation_combinations_values:
                 stockstatus_for_variants = {}
+                properties = {}
+                isSelected = True
 
-                if style_list and not color_list and not size_list and not flavor_list:
-                    stockstatus_for_variants["style"] = json_body[asin][0]
-                    stockstatus_for_variants["stockstatus"] = 1
-                elif not style_list and color_list and size_list:
-                    stockstatus_for_variants["size"] = json_body[asin][0]
-                    stockstatus_for_variants["color"] = json_body[asin][1]
-                    stockstatus_for_variants["stockstatus"] = 1
-                elif not style_list and not color_list and size_list:
-                    stockstatus_for_variants["size"] = json_body[asin][0]
-                    stockstatus_for_variants["stockstatus"] = 1
-                elif not style_list and color_list and not size_list:
-                    stockstatus_for_variants["color"] = json_body[asin][0]
-                    stockstatus_for_variants["stockstatus"] = 1
-                elif flavor_list:
-                    stockstatus_for_variants["flavor"] = json_body[asin][0]
-                    stockstatus_for_variants["stockstatus"] = 1
+                for index, variation_key in enumerate(variation_key_list):
+                    properties[variation_key] = variation_combination[index]
 
-                if stockstatus_for_variants:
-                    stockstatus_for_variants_list.append(stockstatus_for_variants)
+                    if variation_combination[index] != selected_variation_combination[index]:
+                        isSelected = False
+
+                stockstatus_for_variants["properties"] = properties
+                stockstatus_for_variants["in_stock"] = False
+                stockstatus_for_variants["selected"] = isSelected
+                stockstatus_for_variants["price"] = None
+
+                stockstatus_for_variants_list.append(stockstatus_for_variants)
+
+            for variation_combination in instock_variation_combinations_values:
+                stockstatus_for_variants = {}
+                properties = {}
+                isSelected = True
+
+                for index, variation_key in enumerate(variation_key_list):
+                    properties[variation_key] = variation_combination[index]
+
+                    if variation_combination[index] != selected_variation_combination[index]:
+                        isSelected = False
+
+                stockstatus_for_variants["properties"] = properties
+                stockstatus_for_variants["in_stock"] = True
+                stockstatus_for_variants["selected"] = isSelected
+                stockstatus_for_variants["price"] = None
+
+                stockstatus_for_variants_list.append(stockstatus_for_variants)
 
             if not stockstatus_for_variants_list:
                 return None
