dd759e47ecee8e6a5e42a20a30ca402bfbb8189a
lifelonglearner127 <life.long.learner127@outlook.com>
2015-06-15 13:56:35 +0000

    fixed bug in script for running matching crawler in bestsellers mode

--- aws_config/run_all_crawlers_bs.sh
+++ aws_config/run_all_crawlers_bs.sh
@@ -8,5 +8,5 @@ MAX_RANGE=686
 
 for node in "$@"
 do
-    vagrant ssh  node$node -c "screen -dm /bin/bash /home/ubuntu/shared_sshfs/run_crawler_bs.sh $node $INPUT $SITE $NAME $MAXRANGE; sleep 5";
+    vagrant ssh  node$node -c "screen -dm /bin/bash /home/ubuntu/shared_sshfs/run_crawler_bs.sh $node $INPUT $SITE $NAME $MAX_RANGE; sleep 5";
 done
