b54fe38289d1df119852c4936813048a75a0a98b
lifelonglearner127 <life.long.learner127@outlook.com>
2015-06-15 18:05:09 +0100

    fix amazon marketsellers

--- special_crawler/extract_amazonde_data.py
+++ special_crawler/extract_amazonde_data.py
@@ -768,7 +768,13 @@ class AmazonDEScraper(Scraper):
                 if seller_name is not None:
                     mps.append(seller_name)
                     mpp.append(seller_price)
-            urls = tree.xpath(".//ul[contains(@class,'a-pagination')]//li[contains(@class,'a-last')]//a/@href")
+
+                    if len(mps) > 20:
+                        break
+            else:
+                urls = tree.xpath(".//ul[contains(@class,'a-pagination')]//li[contains(@class,'a-last')]//a/@href")
+                continue
+            break
 
         if len(mps)>0:
             if fl == 1:
