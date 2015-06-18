ee2c32a430c886595cbc55cd02788367bb925f2a
lifelonglearner127 <life.long.learner127@outlook.com>
2015-06-18 22:06:06 +0300

    Wiggle scraper

---
+++ special_crawler/extract_wiggle_data.py
@@ -0,0 +1,378 @@
+#!/usr/bin/python
+
+import re
+import lxml
+import lxml.html
+import requests
+import json
+
+from itertools import groupby
+
+from lxml import html, etree
+from extract_data import Scraper
+from spiders_shared_code.jcpenney_variants import JcpenneyVariants
+
+
+class WiggleScraper(Scraper):
+    ##########################################
+    ############### PREP
+    ##########################################
+
+    INVALID_URL_MESSAGE = "Expected URL format is http://www\.jcpenney\.com/.*/prod\.jump\?ppId=.+$"
+
+    def __init__(self, **kwargs):# **kwargs are presumably (url, bot)
+        Scraper.__init__(self, **kwargs)
+
+        # whether product has any webcollage media
+        self.review_json = None
+        self.price_json = None
+        self.jv = JcpenneyVariants()
+
+    def check_url_format(self):
+        """Checks product URL format for this scraper instance is valid.
+        Returns:
+            True if valid, False otherwise
+        """
+
+        m = re.match(r"^http://www\.jcpenney\.com/.*/prod\.jump\?ppId=.+$", self.product_page_url)
+
+        return not not m
+
+    def not_a_product(self):
+        """Checks if current page is not a valid product page
+        (an unavailable product page or other type of method)
+        Overwrites dummy base class method.
+        Returns:
+            True if it's an unavailable product page
+            False otherwise
+        """
+        self.jv.setupCH(self.tree_html)
+
+        try:
+            itemtype = self.tree_html.xpath('//div[@class="pdp_details"]')[0]
+
+            if not itemtype:
+                raise Exception()
+
+        except Exception:
+            return True
+
+        return False
+
+    ##########################################
+    ############### CONTAINER : NONE
+    ##########################################
+
+    def _canonical_link(self):
+        canonical_link = self.tree_html.xpath("//link[@rel='canonical']/@href")[0]
+
+        return canonical_link
+
+    def _url(self):
+        return self.product_page_url
+
+    def _product_id(self):
+        return re.search('prod\.jump\?ppId=(.+?)$', self.product_page_url).group(1)
+
+    ##########################################
+    ############### CONTAINER : PRODUCT_INFO
+    ##########################################
+    def _product_name(self):
+        return self.tree_html.xpath('//meta[@property="og:title"]/@content')[0].strip()
+
+    def _product_title(self):
+        return self.tree_html.xpath('//meta[@property="og:title"]/@content')[0].strip()
+
+    def _title_seo(self):
+        return self.tree_html.xpath('//meta[@property="og:title"]/@content')[0].strip()
+
+    def _model(self):
+        return None
+
+    def _features(self):
+        return None
+
+    def _feature_count(self):
+        return 0
+
+    def _description(self):
+        return html.tostring(self.tree_html.xpath("//div[@id='longCopyCont']//p[1]")[0])
+
+    # extract product long description from its product product page tree
+    # ! may throw exception if not found
+    # TODO:
+    #      - keep line endings maybe? (it sometimes looks sort of like a table and removing them makes things confusing)
+    def _long_description(self):
+        description_block = self.tree_html.xpath("//div[@id='longCopyCont']")[0]
+        long_description = ""
+        long_description_start = False
+
+        for element in description_block:
+            if element.tag == "p" and not long_description:
+                long_description_start = True
+                continue
+
+            if long_description_start:
+                long_description = long_description + html.tostring(element)
+
+        if not long_description.strip():
+            return None
+        else:
+            return long_description
+
+    def _ingredients(self):
+        return None
+
+    def _ingredients_count(self):
+        return 0
+
+    def _variants(self):
+        return self.jv._variants()
+
+    ##########################################
+    ############### CONTAINER : PAGE_ATTRIBUTES
+    ##########################################
+    def _mobile_image_same(self):
+        pass
+        
+    def _image_urls(self):
+        image_ids = re.search('var imageName = "(.+?)";', html.tostring(self.tree_html)).group(1)
+        image_ids = image_ids.split(",")
+
+        image_urls = ["http://s7d2.scene7.com/is/image/JCPenney/%s?wid=35&hei=35&fmt=jpg&op_usm=.4,.8,0,0&resmode=sharp2" % id for id in image_ids]
+
+        if not image_urls:
+            return None
+
+        if len(image_urls) > 1:
+            return image_urls[1:]
+        else:
+            return image_urls
+
+    def _image_count(self):
+        if self._image_urls():
+            return len(self._image_urls())
+
+        return 0
+
+    def _video_urls(self):
+        return None
+
+    def _video_count(self):
+        return 0
+
+    # return dictionary with one element containing the PDF
+    def _pdf_urls(self):
+        return None
+
+    def _pdf_count(self):
+        return 0
+
+    def _webcollage(self):
+        return 0
+
+    def _htags(self):
+        htags_dict = {}
+        htags_dict["h1"] = map(lambda t: self._clean_text(t), self.tree_html.xpath("//h1//text()[normalize-space()!='']"))
+        htags_dict["h2"] = map(lambda t: self._clean_text(t), self.tree_html.xpath("//h2//text()[normalize-space()!='']"))
+
+        return htags_dict
+
+    def _keywords(self):
+        return self.tree_html.xpath('//meta[@name="keywords"]/@content')[0].strip()
+
+    ##########################################
+    ############### CONTAINER : REVIEWS
+    ##########################################
+
+    def _extract_review_json(self):
+        try:
+            review_id = re.search('reviewId:"(.+?)",', html.tostring(self.tree_html)).group(1)
+
+            h = {"User-Agent" : "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36"}
+
+            contents = requests.get("https://jcpenney.ugc.bazaarvoice.com/1573-en_us/%s/reviews.djs?format=embeddedhtml" % review_id, headers=h).text
+            review_json = re.search('webAnalyticsConfig:(.+?),\nwidgetInitializers:initializers,', contents).group(1)
+            self.review_json = json.loads(review_json)
+        except:
+            self.review_json = None
+
+    def _extract_price_json(self):
+        try:
+            price_json= re.search('var jcpPPJSON = (.+?);\njcpDLjcp\.productPresentation = jcpPPJSON;', html.tostring(self.tree_html)).group(1)
+            self.price_json = json.loads(price_json)
+        except:
+            self.price_json = None
+
+    def _average_review(self):
+        if self._review_count() == 0:
+            return None
+
+        average_review = round(float(self.review_json["jsonData"]["attributes"]["avgRating"]), 1)
+
+        if str(average_review).split('.')[1] == '0':
+            return int(average_review)
+        else:
+            return float(average_review)
+
+    def _review_count(self):
+        if not self.review_json:
+            self._extract_review_json()
+
+        return int(self.review_json["jsonData"]["attributes"]["numReviews"])
+
+    def _max_review(self):
+        return None
+
+    def _min_review(self):
+        return None
+
+    def _reviews(self):
+        return None
+
+    ##########################################
+    ############### CONTAINER : SELLERS
+    ##########################################
+    def _price(self):
+        self._extract_price_json()
+
+        if not self.price_json:
+            return None
+
+        price = self.price_json["price"]
+
+        if float(price).is_integer():
+            price = int(price)
+
+        return "$" + str(price)
+
+    def _price_amount(self):
+        self._extract_price_json()
+
+        if not self.price_json:
+            return None
+
+        price = self.price_json["price"]
+
+        if float(price).is_integer():
+            price = int(price)
+
+        return price
+
+    def _price_currency(self):
+        return "USD"
+
+    def _owned(self):
+        return 0
+
+    def _marketplace(self):
+        return 0
+
+    def _site_online(self):
+        return 1
+
+    def _in_stores(self):
+        try:
+            if self.tree_html.xpath("//div[contains(@id, 'channelAvailabilitypp')]/text()")[0] == "online":
+                return 0
+        except:
+            return 1
+
+    def _site_online_out_of_stock(self):
+        return 0
+
+    def _marketplace_sellers(self):
+        return None
+
+    def _marketplace_out_of_stock(self):
+        return 0
+
+    ##########################################
+    ############### CONTAINER : CLASSIFICATION
+    ##########################################    
+    def _categories(self):
+        return self.tree_html.xpath("//div[@id='breadcrumb']/ul/li/a/text()")[1:-1]
+
+    def _category_name(self):
+        return self.tree_html.xpath("//div[@id='breadcrumb']/ul/li/a/text()")[-2]
+
+    def _brand(self):
+        self._extract_price_json()
+
+        if not self.price_json:
+            return None
+
+        return self.price_json["products"][0]["lots"][0]["brandName"]
+
+    ##########################################
+    ################ HELPER FUNCTIONS
+    ##########################################
+    def _clean_text(self, text):
+        return re.sub("&nbsp;", " ", text).strip()
+
+    ##########################################
+    ################ RETURN TYPES
+    ##########################################
+
+    # dictionaries mapping type of info to be extracted to the method that does it
+    # also used to define types of data that can be requested to the REST service
+
+    DATA_TYPES = { \
+        # CONTAINER : NONE
+        "url" : _url, \
+        "product_id" : _product_id, \
+
+        # CONTAINER : PRODUCT_INFO
+        "product_name" : _product_name, \
+        "product_title" : _product_title, \
+        "title_seo" : _title_seo, \
+        "model" : _model, \
+        "features" : _features, \
+        "feature_count" : _feature_count, \
+        "description" : _description, \
+        "long_description" : _long_description, \
+        "ingredients": _ingredients, \
+        "ingredient_count": _ingredients_count,
+        "variants": _variants,
+
+        # CONTAINER : PAGE_ATTRIBUTES
+        "image_count" : _image_count,\
+        "image_urls" : _image_urls, \
+        "video_count" : _video_count, \
+        "video_urls" : _video_urls, \
+        "pdf_count" : _pdf_count, \
+        "pdf_urls" : _pdf_urls, \
+        "webcollage" : _webcollage, \
+        "htags" : _htags, \
+        "keywords" : _keywords, \
+        "canonical_link": _canonical_link,
+
+        # CONTAINER : REVIEWS
+        "review_count" : _review_count, \
+        "average_review" : _average_review, \
+        "max_review" : _max_review, \
+        "min_review" : _min_review, \
+        "reviews" : _reviews, \
+        # CONTAINER : SELLERS
+        "price" : _price, \
+        "price_amount" : _price_amount, \
+        "price_currency" : _price_currency, \
+        "owned" : _owned, \
+        "marketplace" : _marketplace, \
+        "site_online": _site_online, \
+        "site_online_out_of_stock": _site_online_out_of_stock, \
+        "in_stores" : _in_stores, \
+        "marketplace_sellers" : _marketplace_sellers, \
+        "marketplace_out_of_stock": _marketplace_out_of_stock, \
+
+        # CONTAINER : CLASSIFICATION
+        "categories" : _categories, \
+        "category_name" : _category_name, \
+        "brand" : _brand, \
+        }
+
+    # special data that can't be extracted from the product page
+    # associated methods return already built dictionary containing the data
+    DATA_TYPES_SPECIAL = { \
+        "mobile_image_same" : _mobile_image_same, \
+    }
