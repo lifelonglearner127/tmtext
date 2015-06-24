f4f6a5036f69322b4350bdcd8284045ef1045bbb
lifelonglearner127 <life.long.learner127@outlook.com>
2015-06-24 17:58:50 +0100

    duplicate image

--- image_duplication_check/image_duplication_check/settings.py
+++ image_duplication_check/image_duplication_check/settings.py
@@ -86,4 +86,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_ROOT = '/home/ubuntu/tmtext/image_duplication_check/image_duplication_check/static'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+
+
+
