c80f7ddf62071cfd586cb5242a114d71c79ecf05
lifelonglearner127 <life.long.learner127@outlook.com>
2015-07-02 18:56:04 -0400

    csv attach

--- special_crawler/jenkins/regression_service_email_notification.py
+++ special_crawler/jenkins/regression_service_email_notification.py
@@ -138,7 +138,7 @@ msg = MIMEMultipart(
 
 csv_file = MIMEApplication(open(csv_file_name, "rb").read())
 csv_file.add_header('Content-Disposition', 'attachment', filename=csv_file_name)
-msg.add_header('Content-Disposition', 'attachment; filename="Test.csv"')
+msg.add_header('Content-Disposition', 'attachment', filename="Test.csv")
 msg.attach(csv_file)
 msg.attach(MIMEText(email_content))
 
