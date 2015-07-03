7a88e03475b24a61d25a6232bdc64b62d4882591
lifelonglearner127 <life.long.learner127@outlook.com>
2015-07-03 17:47:06 +0300

    fixing bug when trying to find element in walmart product page (for nutrition facts crawler)

--- nutrition_info_images/generate_nutrition_from_text.py
+++ nutrition_info_images/generate_nutrition_from_text.py
@@ -3,7 +3,7 @@
 # if it's found as text, generating a nutrition image
 import selenium.webdriver as webdriver
 from PIL import Image
-
+import sys
 import contextlib
 
 @contextlib.contextmanager
@@ -11,14 +11,48 @@ def quitting(thing):
     yield thing
     thing.quit()
 
-def screenshot_element(urls, element_xpath, image_name="nutrition"):
+def screenshot_element(urls, element_xpaths, image_name="nutrition"):
     with quitting(webdriver.Firefox()) as driver:
         for url in urls:
             driver.implicitly_wait(5)
             driver.get(url)
-            driver.save_screenshot('/tmp/' + image_name + '_full.png')
+            # need to scroll down to nutrition section to be able to load element
+            driver.execute_script("scroll(0,1500)")
+            driver.wait(5)
+            
+            ok = False
             try:
-                nutrition_element = driver.find_element_by_xpath(element_xpath)
+                nutrition_element = driver.find_element_by_xpath(element_xpaths[0])
+                ok = True
+                print "found"
+            except Exception, e:
+                print "not found", e
+                try:
+                    driver.execute_script("scroll(0,1500)")
+                    driver.wait(5)
+                    nutrition_element = driver.find_element_by_xpath(element_xpaths[0])
+                    ok = True
+                    print "found"
+                except Exception, e:
+                    print "not found", e
+                    ok = False
+                    try:
+                        nutrition_element = driver.find_element_by_xpath(element_xpaths[1])
+                        ok = True
+                    except Exception, e:
+                        print "not found", e
+                        ok = False
+                        try:
+                            driver.execute_script("scroll(0,1500)")
+                            driver.wait(5)
+                            nutrition_element = driver.find_element_by_xpath(element_xpaths[1])
+                            ok = True
+                        except Exception, e:
+                            print "not found", e
+                            ok = False
+            if ok:
+                driver.save_screenshot('/tmp/' + image_name + '_full.png')
+
                 (x,y) = nutrition_element.location.values()
                 (h,w) = nutrition_element.size.values()
 
@@ -28,5 +62,6 @@ def screenshot_element(urls, element_xpath, image_name="nutrition"):
                 from random import random
                 idx = int(random() * 1000)
                 cropped.save('/tmp/' + image_name + '_cropped%d.png' % idx)
-            except Exception:
-                pass
+
+if __name__=='__main__':
+    screenshot_element([sys.argv[1]], ("//div[@class='nutrition-section']", "//div[@class='NutFactsSIPT']"))
