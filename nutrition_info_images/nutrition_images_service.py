f75151d7eb3c55917a45de04840fe84c99806058
lifelonglearner127 <life.long.learner127@outlook.com>
2015-07-08 18:50:01 +0300

    fixed but in nutrition image web interface

--- nutrition_info_images/nutrition_images_service.py
+++ nutrition_info_images/nutrition_images_service.py
@@ -33,7 +33,7 @@ class CustomError(Exception):
         return rv
 
 
-@app.route('/results_', methods=['GET'])
+@app.route('/nutrition_image_results', methods=['GET'])
 def results():
     # example request: 
     # http://127.0.0.1:5000/is_nutrition_image?image=http://i5.walmartimages.com/dfw/dce07b8c-bf9c/k2-_f6770975-97a9-474c-a3c8-8edc3a4a14e1.v1.jpg&image=http://i5.walmartimages.com/dfw/dce07b8c-9417/k2-_6275bca7-da12-4925-9afd-048eea86da73.v1.jpg
