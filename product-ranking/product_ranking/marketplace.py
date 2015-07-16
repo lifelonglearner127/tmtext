7719896f965b226eb274d732c19ffe412d657d0b
lifelonglearner127 <life.long.learner127@outlook.com>
2015-07-16 14:58:29 +0300

    marketplace.py: additional fixings

--- product-ranking/product_ranking/marketplace.py
+++ product-ranking/product_ranking/marketplace.py
@@ -248,10 +248,12 @@ class Amazon_marketplace(object):
         if not is_already_have:
             if "amazon" in self.get_image_text(file_path):
                 cr["name"] = self.set_seller_amazon()
-                # os.rename(
-                #     path_to + self.NEW_IMG_FOLDER + file_name,
-                #     path_to + self.IMG_FOLDER + file_name
-                # )
+                import pdb
+                pdb.set_trace()
+                os.rename(
+                    self.NEW_IMG_FOLDER + file_name,
+                    self.IMG_FOLDER + file_name
+                )
 
         # if isfile(path_to + self.NEW_IMG_FOLDER + file_name):
         #     os.remove(path_to + self.NEW_IMG_FOLDER + file_name)
