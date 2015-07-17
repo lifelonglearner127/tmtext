e4c03350d75119625931056a8e2520315f66d5f4
lifelonglearner127 <life.long.learner127@outlook.com>
2015-07-17 14:46:56 +0100

    merge rest apis

--- rest_apis_content_analytics/nutrition_info_images/serializers.py
+++ rest_apis_content_analytics/nutrition_info_images/serializers.py
@@ -4,5 +4,6 @@ from rest_framework import serializers
 class StringListField(serializers.ListField):
     child = serializers.CharField()
 
+
 class ImageUrlSerializer(serializers.Serializer):
     urls = StringListField()
