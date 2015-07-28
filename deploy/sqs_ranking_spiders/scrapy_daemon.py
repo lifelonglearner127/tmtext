bc997e26a8d6e7f068e10d8e9571d164a6d0e315
lifelonglearner127 <life.long.learner127@outlook.com>
2015-07-28 22:49:02 +0300

    Rollback changes

--- deploy/sqs_ranking_spiders/scrapy_daemon.py
+++ deploy/sqs_ranking_spiders/scrapy_daemon.py
@@ -734,14 +734,8 @@ class ScrapyTask(object):
                 output_path+'_bs.jl')
         return cmd
 
-    def _check_if_scrapy_process_is_running(self):
-        # TODO: refactor for bestsellers?
-        return os.path.exists(self.get_output_path()+'.log')
-
     def _start_scrapy_process(self):
         cmd = self._parse_task_and_get_cmd()
-        if self._check_if_scrapy_process_is_running():
-            return  # scrapy is already running
         self.process = Popen(cmd, shell=True, stdout=PIPE,
                              stderr=PIPE, preexec_fn=os.setsid)
         if self.task_data.get('with_best_seller_ranking', False):
