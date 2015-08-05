aaeae47ddd1755a83ca750b4acfd753783ba89df
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-05 08:16:16 -0700

    fix amazon variants

--- spiders_shared_code/amazon_variants.py
+++ spiders_shared_code/amazon_variants.py
@@ -18,17 +18,22 @@ class AmazonVariants(object):
 
     def _variants(self):
         try:
+            original_product_canonical_link = self.tree_html.xpath("//link[@rel='canonical']/@href")[0]
             page_raw_text = lxml.html.tostring(self.tree_html)
             variation_key_values = json.loads(re.search('"variation_values":(.+?),"deviceType', page_raw_text).group(1))
             variation_key_list = list(variation_key_values)
+
+            # reversing variant key list if the site is amazon.in
+            if "amazon.in" in original_product_canonical_link:
+                variation_key_list = list(reversed(variation_key_list))
 
             for index, variation_key in enumerate(variation_key_list):
                 variation_key_list[index] = variation_key[:variation_key.find("_")]
 
             variation_values_list = []
 
-            for variation_key in variation_key_values:
-                variation_values_list.append(variation_key_values[variation_key])
+            for variation_key in variation_key_list:
+                variation_values_list.append(variation_key_values[variation_key + "_name"])
 
             variation_combinations_values = list(itertools.product(*variation_values_list))
 
@@ -42,6 +47,10 @@ class AmazonVariants(object):
 
             selected_variation_combination = [selected_variation_combination[key] for key in selected_variation_combination]
 
+            # reversing selected variant key list if the site is amazon.in
+            if "amazon.in" in original_product_canonical_link:
+                selected_variation_combination = list(reversed(selected_variation_combination))
+
             asin_variation_values = json.loads(re.search('"asin_variation_values":(.+?),"contextMetaData":', page_raw_text).group(1))
 
             variation_asin_values = {}
@@ -52,13 +61,11 @@ class AmazonVariants(object):
                 property_index = 0
                 variation_combination_key = ""
 
-                for property in asin_variation_values[asin_variation]:
-                    variation_combination_key += variation_key_values[variation_key_list[property_index] + "_name"][int(asin_variation_values[asin_variation][property])]
+                for key in variation_key_list:
+                    variation_combination_key += variation_key_values[key + "_name"][int(asin_variation_values[asin_variation][key+"_name"])]
                     property_index = property_index + 1
 
                 variation_asin_values[variation_combination_key] = asin_variation
-
-            original_product_canonical_link = self.tree_html.xpath("//link[@rel='canonical']/@href")[0]
 
             stockstatus_for_variants_list = []
 
@@ -84,16 +91,11 @@ class AmazonVariants(object):
                 stockstatus_for_variants = {}
                 properties = {}
                 isSelected = True
-                variation_combination_key = variation_combination_reversed_key = ""
+                variation_combination_key = ""
 
                 for index, variation_key in enumerate(variation_key_list):
-                    if variation_combination_key in variation_asin_values:
-                        properties[variation_key] = variation_combination[index]
-                    else:
-                        properties[variation_key] = variation_combination[len(variation_key_list) - 1 - index]
-
+                    properties[variation_key] = variation_combination[index]
                     variation_combination_key += variation_combination[index]
-                    variation_combination_reversed_key = variation_combination[index] + variation_combination_reversed_key
                     if variation_combination[index] != selected_variation_combination[index]:
                         isSelected = False
 
@@ -101,11 +103,7 @@ class AmazonVariants(object):
                 stockstatus_for_variants["in_stock"] = True
                 stockstatus_for_variants["selected"] = isSelected
                 stockstatus_for_variants["price"] = None
-
-                if variation_combination_key in variation_asin_values:
-                    stockstatus_for_variants["url"] = original_product_canonical_link[:original_product_canonical_link.rfind("/") + 1] + variation_asin_values[variation_combination_key]
-                else:
-                    stockstatus_for_variants["url"] = original_product_canonical_link[:original_product_canonical_link.rfind("/") + 1] + variation_asin_values[variation_combination_reversed_key]
+                stockstatus_for_variants["url"] = original_product_canonical_link[:original_product_canonical_link.rfind("/") + 1] + variation_asin_values[variation_combination_key]
 
                 stockstatus_for_variants_list.append(stockstatus_for_variants)
 
