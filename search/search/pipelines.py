8328921bb3ad8f873c3042f9a5963851cff0c2c7
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-13 13:32:07 +0300

    minor fix in matching pipeline

--- search/search/pipelines.py
+++ search/search/pipelines.py
@@ -97,7 +97,11 @@ class URLsPipeline(object):
                 fields = [item['origin_upc'][0], json.dumps(item['origin_name'])]
             else:
                 if option == 5:
-                    fields = [json.dumps(item['origin_name']), item['origin_url'], str(item['product_target_price'])]
+                    if 'product_target_price' in item:
+                        price = ""
+                    else:
+                        price = item['product_target_price']
+                    fields = [json.dumps(item['origin_name']), item['origin_url'], str(price)]
                 else:
                     fields = [item['origin_url']]
 
