e06b90293cc3caba5c0fd8362d6455fb7d883f42
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-13 06:18:17 +0300

    added price to matching output type 5

--- search/search/pipelines.py
+++ search/search/pipelines.py
@@ -43,6 +43,7 @@ class URLsPipeline(object):
             if int(spider.output) == 5:
                 titles.append("Product_Name")
                 titles.append("Original_URL")
+                titles.append("Target_Price")
 
             # TODO: uncomment.
             # if int(spider.output) == 3:
@@ -96,7 +97,7 @@ class URLsPipeline(object):
                 fields = [item['origin_upc'][0], json.dumps(item['origin_name'])]
             else:
                 if option == 5:
-                    fields = [json.dumps(item['origin_name']), item['origin_url']]
+                    fields = [json.dumps(item['origin_name']), item['origin_url'], item['product_price']]
                 else:
                     fields = [item['origin_url']]
 
