2d603b0bbfdffd0929069c2403abfabf71f0f86a
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-14 14:08:52 +0300

    amazon_base_class: fix _scrape_total_matches

--- product-ranking/product_ranking/amazon_base_class.py
+++ product-ranking/product_ranking/amazon_base_class.py
@@ -1,6 +1,19 @@
 # ~~coding=utf-8~~
 
-from product_ranking.spiders import BaseProductsSpider, FLOATING_POINT_RGEX
+
+import re
+import urlparse
+
+from scrapy.http import Request
+from scrapy.http.request.form import FormRequest
+from scrapy.log import msg, ERROR, WARNING, INFO, DEBUG
+
+from product_ranking.items import SiteProductItem, Price, BuyerReviews
+from product_ranking.spiders import BaseProductsSpider, cond_set, \
+    cond_set_value, FLOATING_POINT_RGEX
+from product_ranking.amazon_tests import AmazonTests
+
+from product_ranking.amazon_bestsellers import amazon_parse_department
 
 
 is_empty = lambda x, y=None: x[0] if x else y
@@ -10,12 +23,13 @@ class AmazonBaseClass(BaseProductsSpider):
 
     # String from html body that means there's no results ( "no results.", for example)
     total_matches_str = 'did not match any products.'
-    # Regexp for total matches to parse a number from html body ( "of ([\d,]+)", for example)
-    total_matches_re = 'of ([\d,]+)'
+    # For regexp for total matches to parse a number from html body ( "sur", for example for French)
+    total_matches_re = 'of'
 
     def _scrape_total_matches(self, response):
         """
-        Overrides BaseProductsSpider method to scrape total result matches
+        Overrides BaseProductsSpider method to scrape total result matches. total_matches_str
+        and total_matches_re need to be set for every concrete amazon spider.
         :param response:
         :return: Number of total matches (int)
         """
@@ -23,12 +37,15 @@ class AmazonBaseClass(BaseProductsSpider):
         if unicode(self.total_matches_str) in response.body_as_unicode():
             total_matches = 0
         else:
-            count_matches = response.xpath(
-                '//h2[@id="s-result-count"]/text()').re(self.total_matches_re)
-
-            if count_matches and count_matches[-1]:
-                total_matches = int(count_matches[-1].replace(
-                    ' ', '').replace(u'\xa0', '').replace(',', ''))
+            count_matches = is_empty(
+                response.xpath(
+                    '//h2[@id="s-result-count"]/text()').re(
+                    u'{0}\s?([\d,.\s?]+)'.format(unicode(self.total_matches_re))
+                )
+            )
+            if count_matches:
+                total_matches = int(count_matches.replace(
+                    ' ', '').replace(u'\xa0', '').replace(',', '').replace('.', ''))
             else:
                 total_matches = None
 
