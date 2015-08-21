c1eda3359131a362113c7b0df9670145b2c13784
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-21 17:31:25 +0300

    wayfair.com: implemented base methods

---
+++ product-ranking/product_ranking/spiders/wayfair.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-#
+
+import json
+import re
+
+from scrapy.http import FormRequest, Request
+from scrapy.log import ERROR, INFO, WARNING
+
+from product_ranking.items import SiteProductItem, RelatedProduct, Price, \
+    BuyerReviews
+from product_ranking.spiders import BaseProductsSpider, FormatterWithDefaults, \
+    cond_set_value
+
+is_empty = lambda x, y=None: x[0] if x else y
+
+
+class WayfairProductSpider(BaseProductsSpider):
+
+    name = 'wayfair_products'
+    allowed_domains = ["wayfair.com"]
+
+    SEARCH_URL = "http://www.wayfair.com/keyword.php?keyword={search_term}"
+
+    def __init__(self, *args, **kwargs):
+        super(WayfairProductSpider, self).__init__(
+            site_name=self.allowed_domains[0], *args, **kwargs)
+
+    def parse_product(self, response):
+        reqs = []
+        meta = response.meta.copy()
+        product = meta['product']
+
+        if reqs:
+            return self.send_next_request(reqs, response)
+
+        return product
+
+    def send_next_request(self, reqs, response):
+        """
+        Helps to handle several requests
+        """
+
+        req = reqs.pop(0)
+        new_meta = response.meta.copy()
+        if reqs:
+            new_meta["reqs"] = reqs
+        return req.replace(meta=new_meta)
+
+    def _parse_single_product(self, response):
+        return self.parse_product(response)
+
+    def _scrape_total_matches(self, response):
+        """
+        Scraping number of resulted product links
+        """
+        total_matches = is_empty(
+            response.xpath("//span[@class='viewingItems']/text()").extract(),
+        )
+
+        if total_matches:
+            total_matches = total_matches.replace(',', '')
+            return int(total_matches)
+        else:
+            self.log(
+                "Failed to get total matches", WARNING
+            )
+            return 0
+
+    def _scrape_results_per_page(self, response):
+        """
+        Number of results on page
+        """
+        num = is_empty(
+            response.xpath("//span[@class='js-product-range-end']/text()").extract()
+        )
+
+        if not num:
+            self.log(
+                "Failed to get number of results", WARNING
+            )
+            return 0
+
+        return int(num)
+
+    def _scrape_product_links(self, response):
+        """
+        Scraping product links from search page
+        """
+
+        items = response.xpath(
+            '//div[@id="sbprodgrid"]/.//a'
+        )
+
+        if items:
+            for item in items:
+                link = is_empty(
+                    item.xpath('.//@href').extract()
+                )
+                res_item = SiteProductItem()
+                yield link, res_item
+        else:
+            self.log("Found no product links in {url}".format(response.url), INFO)
+
+    def _scrape_next_results_page_link(self, response):
+        url = response.xpath(
+            '//a[contains(@class, "pages_prev_next")]/@href'
+        ).extract()
+
+        print('*'*50)
+        print(url[2:])
+        print('*'*50)
+
+        if len(url) == 1:
+            return url[0]
+        elif len(url) > 1:
+            self.log("Found more than one 'next page' link.", ERROR)
+            return url[1]
