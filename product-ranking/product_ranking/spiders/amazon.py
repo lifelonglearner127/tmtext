41d9edc964e31c9cab2b4f6d60b19c8f809d0a53
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-26 21:45:03 +0300

    adjust auto-test pattern

--- product-ranking/product_ranking/spiders/amazon.py
+++ product-ranking/product_ranking/spiders/amazon.py
@@ -64,7 +64,7 @@ class AmazonValidatorSettings(object):  # do NOT set BaseValidatorSettings as pa
         'car navigator garmin maps 44LM': [1, 20],
         'yamaha drums midi': [50, 300],
         'black men shoes size 8  red stripes': [50, 300],
-        'car audio equalizer pioneer mp3': [40, 150]
+        'car audio equalizer pioneer mp3': [20, 150]
     }
 
 
