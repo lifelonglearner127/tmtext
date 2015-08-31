4085dea336d360b80e49f279209636c88a79b4f3
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-31 16:40:54 +0300

    clarks.co.uk: base implement

---
+++ product-ranking/product_ranking/spiders/clarkscouk.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-#
+
+import json
+import re
+import hjson
+
+from scrapy.http import FormRequest, Request
+from scrapy.log import ERROR, INFO, WARNING
+
+from product_ranking.items import SiteProductItem, RelatedProduct, Price, \
+    BuyerReviews
+from product_ranking.spiders import BaseProductsSpider, FormatterWithDefaults, \
+    cond_set_value
+
+is_empty = lambda x, y=None: x[0] if x else y
+
+
+class ClarksProductSpider(BaseProductsSpider):
+
+    name = 'clarkscouk_products'
+    allowed_domains = ["www.clarks.co.uk"]
+
+    SEARCH_URL = "http://www.clarks.co.uk/s/{search_term}#pagesize-is-40{sort}"
+
+    _SORT_MODES = {
+        "DEFAULT": "",
+        "PRICE_ASC": "-and-Sort-is-price-by-ascending",
+        "PRICE_DESC": "-and-Sort-is-price-by-descending"
+    }
+
+    ITEMS_PER_PAGE = 40
+
+    def __init__(self, search_sort='DEFAULT', *args, **kwargs):
+        self.start_pos = 0
+        super(ClarksProductSpider, self).__init__(
+            site_name=self.allowed_domains[0],
+            url_formatter=FormatterWithDefaults(
+                search_sort=self._SORT_MODES[search_sort]
+            ),
+            *args, **kwargs)
+
+    def parse_product(self, response):
+        reqs = []
+        meta = response.meta.copy()
+        product = meta['product']
+
+        print('Hello')
+
+        if reqs:
+            return self.send_next_request(reqs, response)
+
+        return product
+
+    def send_next_request(self, reqs, response):
+        """
+        Helps to handle several requests
+        """
+
+        req = reqs.pop(0)
+        new_meta = response.meta.copy()
+        if reqs:
+            new_meta["reqs"] = reqs
+        return req.replace(meta=new_meta)
+
+    def _parse_single_product(self, response):
+        return self.parse_product(response)
+
+    def _scrape_total_matches(self, response):
+        """
+        Scraping number of resulted product links
+        """
+        total_matches = is_empty(
+            response.xpath('//div[@id="product-list-paging-top"]/./'
+                           '/span[class="page-size-container"]/text()').extract()
+        )
+
+        try:
+            total_matches = re.findall(
+                r'Found (\d+) styles',
+                total_matches
+            )
+            return int(
+                is_empty(total_matches, '0')
+            )
+        except (KeyError, ValueError) as exc:
+            total_matches = None
+            self.log(
+                "Failed to extract total matches from {url}: {exc}".format(
+                    response.url, exc
+                ), ERROR
+            )
+
+        return total_matches
+
+    def _scrape_results_per_page(self, response):
+        """
+        Number of results on page
+        """
+
+        return self.ITEMS_PER_PAGE
+
+    def _scrape_product_links(self, response):
+        """
+        Scraping product links from search page
+        """
+
+        items = response.xpath(
+            '//ul[@id="prod-list"]/li[@class="product-list-item"]'
+        )
+
+        if items:
+            for item in items:
+                link = is_empty(
+                    item.xpath('./span[@class="product-name-header"]/'
+                               'a/@href').extract()
+                )
+                res_item = SiteProductItem()
+                yield link, res_item
+        else:
+            self.log("Found no product links in {url}".format(response.url), INFO)
+
+    def _scrape_next_results_page_link(self, response):
+        url = self.NEXT_PAGE_URL.format(start_pos=self.start_pos)
+        self.start_pos += self.items_per_page
+        return url
