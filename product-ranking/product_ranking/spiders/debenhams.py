3cff18eb041d928ea958ad102d47c3f6f71fb672
lifelonglearner127 <life.long.learner127@outlook.com>
2015-09-03 13:21:40 +0300

    debenhams.com: implemented base methods

---
+++ product-ranking/product_ranking/spiders/debenhams.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-#
+
+import json
+import re
+import string
+import itertools
+import urllib
+
+from scrapy.http import FormRequest, Request
+from scrapy.log import ERROR, INFO, WARNING
+
+from product_ranking.items import SiteProductItem, RelatedProduct, Price, \
+    BuyerReviews
+from product_ranking.spiders import BaseProductsSpider, FormatterWithDefaults, \
+    cond_set_value
+
+is_empty = lambda x, y=None: x[0] if x else y
+
+
+class DebenhamsProductSpider(BaseProductsSpider):
+
+    name = 'debenhams_products'
+    allowed_domains = ["debenhams.com"]
+
+    SEARCH_URL = "http://www.debenhams.com/webapp/wcs/stores/servlet/" \
+                 "Navigate?langId=-1&storeId=10701&catalogId=10001&txt={search_term}"
+
+    items_per_page = 60
+
+    def parse_product(self, response):
+        reqs = []
+        meta = response.meta.copy()
+        product = meta['product']
+
+        if reqs:
+            return self.send_next_request(reqs, response)
+
+        return product
+
+    def send_next_request(self, reqs, response):
+        """
+        Helps to handle several requests
+        """
+
+        req = reqs.pop(0)
+        new_meta = response.meta.copy()
+        if reqs:
+            new_meta["reqs"] = reqs
+        return req.replace(meta=new_meta)
+
+    def _parse_single_product(self, response):
+        return self.parse_product(response)
+
+    def _scrape_total_matches(self, response):
+        """
+        Scraping number of resulted product links
+        """
+        total_matches = is_empty(
+            response.xpath(
+                '//*[@id="products_found"]/span/text()'
+            ).extract(), 0
+        )
+        total_matches = is_empty(
+            re.findall(
+                r'(\d+) products found',
+                total_matches
+            )
+        )
+
+        if total_matches:
+            return int(total_matches)
+        else:
+            return 0
+
+    def _scrape_results_per_page(self, response):
+        """
+        Number of results on page
+        """
+        return self.items_per_page
+
+    def _scrape_product_links(self, response):
+        """
+        Scraping product links from search page
+        """
+
+        items = response.xpath(
+            '//div[@id="productDisplay"]/./'
+            '/tr[@class="item_container"]'
+            '/td[contains(@class, "item")]'
+        )
+
+        if items:
+            for item in items:
+                link = is_empty(
+                    item.xpath('././/input[@id="productTileImageUrl"]'
+                               '/@value').extract()
+                )
+                res_item = SiteProductItem()
+                yield link, res_item
+        else:
+            self.log("Found no product links in {url}".format(url=response.url), INFO)
+
+    def _scrape_next_results_page_link(self, response):
+        url = is_empty(
+            response.xpath(
+                '//a[text()="Next"]/@href'
+            ).extract()
+        )
+
+        if url:
+            return url
+        else:
+            self.log("Found no 'next page' links", WARNING)
+            return None
