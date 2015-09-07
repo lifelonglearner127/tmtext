ebddbe9d6127658117e5c99480551bde21ae6e0a
lifelonglearner127 <life.long.learner127@outlook.com>
2015-09-07 15:47:49 +0300

    Parse IDs only (optional)

--- upc_to_url/walmart.py
+++ upc_to_url/walmart.py
@@ -10,6 +10,8 @@ import lxml.html
 
 urls = [l.strip() for l in open(sys.argv[1], 'r').readlines() if l.strip()]
 
+ids_only = 'ids_only' in sys.argv
+
 for u in urls:
     walmart_url = 'http://www.walmart.com/search/?query=%s' % u
     walmart_content = requests.get(walmart_url).text
@@ -22,5 +24,8 @@ for u in urls:
     href = href[0]
     if href.startswith('/'):
         href = 'http://www.walmart.com' + href
+    if ids_only:
+        if '/' in href:
+            href = href.rsplit('/', 1)[1].strip()
     print href
     sys.stdout.flush()
