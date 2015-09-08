14b1fb4989511421c755fd404c2af2faa0dfac65
lifelonglearner127 <life.long.learner127@outlook.com>
2015-09-08 15:17:06 +0300

    babymonitorsdirect.co.uk: parse special pricing

--- product-ranking/product_ranking/spiders/babymonitorsdirectcouk.py
+++ product-ranking/product_ranking/spiders/babymonitorsdirectcouk.py
@@ -1,10 +1,9 @@
 from __future__ import division, absolute_import, unicode_literals
 
 import re
-import urllib
 import string
 
-from scrapy.http import Request
+from scrapy.log import ERROR, INFO, WARNING
 
 from product_ranking.items import SiteProductItem, RelatedProduct, \
     Price, BuyerReviews
@@ -27,7 +26,6 @@ class BabymonitorsdirectProductsSpider(BaseProductsSpider):
     """
     name = 'babymonitorsdirectcouk_products'
     allowed_domains = ["babymonitorsdirect.co.uk"]
-    start_urls = []
     SEARCH_URL = "http://www.babymonitorsdirect.co.uk/catalogsearch/" \
                  "result/index/?dir={sort}&order={order}&q={search_term}"
 
@@ -54,6 +52,9 @@ class BabymonitorsdirectProductsSpider(BaseProductsSpider):
     def parse_product(self, response):
         product = response.meta['product']
 
+        # Set locale
+        cond_set_value(product, 'locale', 'en_GB')
+
         # Parse title
         title = self._parse_title(response)
         cond_set_value(product, 'title', title, conv=string.strip)
@@ -65,6 +66,10 @@ class BabymonitorsdirectProductsSpider(BaseProductsSpider):
         # Parse price
         price = self._parse_price(response)
         cond_set_value(product, 'price', price)
+
+        # Parse special pricing
+        special_pricing = self._parse_special_pricing(response)
+        cond_set_value(product, 'special_pricing', special_pricing, conv=bool)
 
         # Parse stock status
         is_out_of_stock = self._parse_stock_status(response)
@@ -78,58 +83,15 @@ class BabymonitorsdirectProductsSpider(BaseProductsSpider):
         description = self._parse_description(response)
         cond_set_value(product, 'description', description)
 
-        rp = []
-        for prod in response.xpath('//ul[@class="ProductList"]/li'):
-            prod_all = prod.xpath('div[@class="ProductDetails"]/strong/a')
-            title = prod_all.xpath('text()').extract()
-            url = prod_all.xpath('@href').extract()
-            if title:
-                title = title[0]
-            if url:
-                url = url[0]
-            rp.append(RelatedProduct(title, url))
-        cond_set_value(product, 'related_products', rp)
+        # Parse related products
+        # related_products = self._parse_related_products(response)
+        # cond_set_value(product, 'related_products', related_products)
 
-        cond_set_value(product, 'locale', 'en_GB')
+        # Parse buyer reviews
+        # buyer_reviews = self._parse_buyer_reviews(response)
+        # cond_set_value(product, 'buyer_reviews', buyer_reviews)
 
-        product["url"] = response.url
-
-        # Reviews
-        num_of_reviews = response.xpath(
-            '//div[@class="DetailRow"]/div[contains(text(), "Rating:")]'
-            '/../div[@class="Value"]/span/a/text()'
-        ).extract()
-        if num_of_reviews:
-            num_of_reviews = re.findall("\d+", num_of_reviews[0])
-            if num_of_reviews:
-                num_of_reviews = int(num_of_reviews[0])
-            if not num_of_reviews:
-                num_of_reviews = None
-        average_rating = response.xpath(
-            '//div[@class="DetailRow"]/div[contains(text(), "Rating:")]'
-            '/../div[@class="Value"]/img/@src'
-        ).extract()
-        if average_rating:
-            average_rating = int(re.findall("IcoRating(\d)",
-                                 average_rating[0])[0])
-        if not average_rating:
-            average_rating = 0
-            num_of_reviews = 0
-        buyer_reviews = BuyerReviews(num_of_reviews=int(num_of_reviews),
-                                     average_rating=float(average_rating),
-                                     rating_by_star={1: 0, 2: 0, 3: 0,
-                                                     4: 0, 5: 0})
-        if average_rating or num_of_reviews:
-            cond_set_value(product, 'buyer_reviews', buyer_reviews)
-            new_meta = response.meta.copy()
-            new_meta['product'] = product
-            return Request(url=response.url,
-                           meta=new_meta,
-                           callback=self._extract_reviews,
-                           dont_filter=True)
-        else:
-            cond_set_value(product, 'buyer_reviews', ZERO_REVIEWS_VALUE)
-            return product
+        return product
 
     def _parse_title(self, response):
         title = is_empty(
@@ -174,6 +136,16 @@ class BabymonitorsdirectProductsSpider(BaseProductsSpider):
             priceCurrency='GBP'
         )
 
+    def _parse_special_pricing(self, response):
+        special_pricing = is_empty(
+            response.xpath(
+                '//*[@class="old-price"]/span[@class="price"]'
+                '/text()'
+            ), False
+        )
+
+        return special_pricing
+
     def _parse_stock_status(self, response):
         is_out_of_stock = is_empty(
             response.xpath(
@@ -206,33 +178,89 @@ class BabymonitorsdirectProductsSpider(BaseProductsSpider):
 
         return description
 
-    def _extract_reviews(self, response):
-        product = response.meta['product']
-        num, avg, by_star = product.get('buyer_reviews')
+    # def _parse_related_products(self, response):
+    #     related_products = []
+    #
+    #     for prod in response.xpath('//ul[@class="ProductList"]/li'):
+    #         prod_all = prod.xpath('div[@class="ProductDetails"]/strong/a')
+    #         title = prod_all.xpath('text()').extract()
+    #         url = prod_all.xpath('@href').extract()
+    #         if title:
+    #             title = title[0]
+    #         if url:
+    #             url = url[0]
+    #         related_products.append(RelatedProduct(title, url))
+    #
+    #     return related_products
 
-        stars = response.xpath('//ol[@class="ProductReviewList"]/'
-                               'li/h4/img/@src').re('IcoRating(\d)')
-
-        for i in stars:
-            by_star[int(i)] += 1
-
-        buyer_reviews = BuyerReviews(num_of_reviews=num,
-                                     average_rating=avg,
-                                     rating_by_star=by_star)
-
-        cond_set_value(product, 'buyer_reviews', buyer_reviews)
-        next_page = response.xpath('//p[@class="ProductReviewPaging"]/span'
-                                   '/a[contains(text(),"Next")]/@href')\
-            .extract()
-
-        if next_page:
-            new_meta = response.meta.copy()
-            new_meta['product'] = product
-            return Request(url=next_page[1], meta=new_meta,
-                           callback=self._extract_reviews,
-                           dont_filter=True)
-        else:
-            return product
+    # def _parse_buyer_reviews(self, response):
+    #     num_of_reviews = response.xpath(
+    #         '//div[@class="DetailRow"]/div[contains(text(), "Rating:")]'
+    #         '/../div[@class="Value"]/span/a/text()'
+    #     ).extract()
+    #
+    #     if num_of_reviews:
+    #         num_of_reviews = re.findall("\d+", num_of_reviews[0])
+    #         if num_of_reviews:
+    #             num_of_reviews = int(num_of_reviews[0])
+    #         if not num_of_reviews:
+    #             num_of_reviews = None
+    #
+    #     average_rating = response.xpath(
+    #         '//div[@class="DetailRow"]/div[contains(text(), "Rating:")]'
+    #         '/../div[@class="Value"]/img/@src'
+    #     ).extract()
+    #     if average_rating:
+    #         average_rating = int(re.findall("IcoRating(\d)",
+    #                              average_rating[0])[0])
+    #     if not average_rating:
+    #         average_rating = 0
+    #         num_of_reviews = 0
+    #
+    #     buyer_reviews = BuyerReviews(num_of_reviews=int(num_of_reviews),
+    #                                  average_rating=float(average_rating),
+    #                                  rating_by_star={1: 0, 2: 0, 3: 0,
+    #                                                  4: 0, 5: 0})
+    #     if average_rating or num_of_reviews:
+    #         cond_set_value(product, 'buyer_reviews', buyer_reviews)
+    #         new_meta = response.meta.copy()
+    #         new_meta['product'] = product
+    #         return Request(url=response.url,
+    #                        meta=new_meta,
+    #                        callback=self._extract_reviews,
+    #                        dont_filter=True)
+    #     else:
+    #         buyer_reviews = ZERO_REVIEWS_VALUE
+    #
+    #     return buyer_reviews
+    #
+    # def _extract_reviews(self, response):
+    #     product = response.meta['product']
+    #     num, avg, by_star = product.get('buyer_reviews')
+    #
+    #     stars = response.xpath('//ol[@class="ProductReviewList"]/'
+    #                            'li/h4/img/@src').re('IcoRating(\d)')
+    #
+    #     for i in stars:
+    #         by_star[int(i)] += 1
+    #
+    #     buyer_reviews = BuyerReviews(num_of_reviews=num,
+    #                                  average_rating=avg,
+    #                                  rating_by_star=by_star)
+    #
+    #     cond_set_value(product, 'buyer_reviews', buyer_reviews)
+    #     next_page = response.xpath('//p[@class="ProductReviewPaging"]/span'
+    #                                '/a[contains(text(),"Next")]/@href')\
+    #         .extract()
+    #
+    #     if next_page:
+    #         new_meta = response.meta.copy()
+    #         new_meta['product'] = product
+    #         return Request(url=next_page[1], meta=new_meta,
+    #                        callback=self._extract_reviews,
+    #                        dont_filter=True)
+    #     else:
+    #         return product
 
     def _scrape_total_matches(self, response):
         if 'No products found' in \
@@ -272,13 +300,13 @@ class BabymonitorsdirectProductsSpider(BaseProductsSpider):
             yield link, SiteProductItem()
 
     def _scrape_next_results_page_link(self, response):
-        links = response.xpath(
-            '//div[@class="CategoryPagination"]'
-            '/div[@class="FloatRight"]/a/@href'
+        link = is_empty(
+            response.xpath('//div[@class="pages"]/./'
+                           '/li[@class="next"]/a/@href').extract()
         )
-        if links:
-            ajax_link = links.extract()[0].strip().rstrip('#results')
-            ajax_link += '&ajax=1'
-            full_link = "http://www.babymonitorsdirect.co.uk/" + ajax_link
-            return full_link
-        return None
+
+        if link:
+            return link
+        else:
+            self.log('Unable to find next page link', ERROR)
+            return None
