111c24b6a70d83716d1c41768d23c7ef769fc951
lifelonglearner127 <life.long.learner127@outlook.com>
2015-09-22 01:47:09 +0300

    Working LB version (upscale only for now)

--- deploy/sqs_ranking_spiders/sqs_instances_load_balancer.py
+++ deploy/sqs_ranking_spiders/sqs_instances_load_balancer.py
@@ -57,7 +57,7 @@ def get_sqs_tasks_count():
     tasks_count = 0
     try:
         conn = boto.sqs.connect_to_region('us-east-1')
-        queues = conn.get_all_queues(prefx=q_prefix)
+        queues = conn.get_all_queues(prefix=q_prefix)
         tasks_count = sum(q.count() for q in queues)
     except Exception as e:
         print e
@@ -66,7 +66,7 @@ def get_sqs_tasks_count():
 
 def scale_instances(tasks_per_instance):
     conn = AutoScaleConnection()
-    group = conn.get_all_groups('SCCluster1')[0]
+    group = conn.get_all_groups(names=['SCCluster1'])[0]
 
     if group.desired_capacity == group.max_size:
         print 'Maximum number of instances reached'
@@ -74,6 +74,8 @@ def scale_instances(tasks_per_instance):
     tasks_count = get_sqs_tasks_count()
     if not tasks_count:
         print 'No tasks left in queues'
+        return
+    print 'Num of tasks in queues', tasks_count
     tasks_per_instance *= 1.0  # convert to float
     additional_instances_count = ceil(tasks_count/tasks_per_instance)
     updated_instances_count = \
@@ -84,6 +86,7 @@ def scale_instances(tasks_per_instance):
     print 'Updating group from %s to %s instances' % (
         group.desired_capacity, updated_instances_count)
     group.set_capacity(updated_instances_count)
+    group.desired_capacity = updated_instances_count
     group.update()
     print 'Done'
 
