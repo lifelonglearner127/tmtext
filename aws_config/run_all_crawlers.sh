b025e2cd2b95123bab43e45bc538f96dbf67b75e
lifelonglearner127 <life.long.learner127@outlook.com>
2015-09-23 22:21:51 +0000

    modified script to run matching to make more general (exclude parameters from code)

--- aws_config/run_all_crawlers.sh
+++ aws_config/run_all_crawlers.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-INPUT="hanes_amazon_manual"
-SITE="kohls"
-
+INPUT=$1
+SITE=$2
+# now argument list starts from argument 3
+shift 2
+NODES="$@"
 # separate argument list one on each line
-echo $@ | sed -e 's/[ ]/\n/g' | \
+echo $NODES | sed -e 's/[ ]/\n/g' | \
 	xargs -P10 -I% vagrant ssh node% -c "screen -dm /bin/bash /home/ubuntu/shared_sshfs/run_crawler.sh % $INPUT $SITE; sleep 5"
