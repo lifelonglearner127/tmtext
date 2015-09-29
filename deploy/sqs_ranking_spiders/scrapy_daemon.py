7ec805132bdcb75811025531c29e5fa2ae96ac4b
lifelonglearner127 <life.long.learner127@outlook.com>
2015-09-29 15:54:46 +0300

    sqs fixed issue with too long name and statistics error

--- deploy/sqs_ranking_spiders/scrapy_daemon.py
+++ deploy/sqs_ranking_spiders/scrapy_daemon.py
@@ -578,8 +578,8 @@ class ScrapyTask(object):
             # maybe should be changed to product_url
             additional_part = 'single-product-url-request'
         job_name += '____' + additional_part + '____' + site
-        # job_name += '____' + site + '____' + get_random_hash()
-        return job_name
+        # truncate resulting string as file name limitation is 256 characters
+        return job_name[:200]
 
     def _parse_signal_settings(self, signal_settings):
         """
