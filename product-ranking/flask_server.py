2a6d1e21f2ca5344cb4bb6bb1f427fd69fdf0f6b
lifelonglearner127 <life.long.learner127@outlook.com>
2015-10-05 15:50:48 +0300

    New data sent

--- product-ranking/flask_server.py
+++ product-ranking/flask_server.py
@@ -6,10 +6,17 @@
 import os
 import sys
 import json
+import urlparse
+import cgi
+import subprocess
+import tempfile
+import time
 
 from flask import Flask, jsonify, abort, request
 app = Flask(__name__)
 
+CWD = os.path.dirname(os.path.abspath(__file__))
+from monitoring.deploy_to_monitoring_host import find_spiders
 
 ajax_template = """
 <html>
@@ -185,6 +192,7 @@ ajax_template = """
 </style>
 </head>
 <body>
+  <h4>Spider: <span id="spider">{{ spider }}</span> (use "spider" GET param to change)</h4>
   <h4>URL: <span id="url">{{ url }}</span></h4>
   <p>
     <pre id="result" class="jjson">
@@ -195,7 +203,7 @@ ajax_template = """
     $.ajax({
       url: "/get_data_ajax",
       method: "GET",
-      data: {url: $('#url').text()},
+      data: {url: $('#url').text(), spider: $('#spider').text()},
       success: function(result){
         $("#result").html(result);
         //alert( JSON.parse($("#result").html()) );
@@ -208,23 +216,106 @@ ajax_template = """
 """
 
 
+def _find_spider_by_url(url):
+    given_domain = urlparse.urlparse(url).netloc.replace('www.', '')
+    all_spiders = find_spiders(os.path.join(CWD, 'product_ranking', 'spiders'))
+    all_spiders = [[s[0], s[1]] for s in all_spiders if len(s) == 2]
+    for spider_name, spider_domains in all_spiders:
+        for s_domain in spider_domains.split(','):
+            s_domain = s_domain.strip().replace('www.', '')
+            if s_domain == given_domain:
+                return spider_name
+
+
+def _get_all_spidernames():
+    all_spiders = find_spiders(os.path.join(CWD, 'product_ranking', 'spiders'))
+    all_spiders = [[s[0], s[1]] for s in all_spiders if len(s) == 2]
+    return [s[0] for s in all_spiders]
+
+
+def run(command, shell=None):
+    """ Run the given command and return its output
+    """
+    out_stream = subprocess.PIPE
+    err_stream = subprocess.PIPE
+
+    if shell is not None:
+        p = subprocess.Popen(command, shell=True, stdout=out_stream,
+                             stderr=err_stream, executable=shell)
+    else:
+        p = subprocess.Popen(command, shell=True, stdout=out_stream,
+                             stderr=err_stream)
+    (stdout, stderr) = p.communicate()
+
+    return stdout, stderr
+
+
+def check_running_instances(marker):
+    """ Check how many processes with such marker are running already"""
+    processes = 0
+    output = run('ps aux')
+    output = ' '.join(output)
+    for line in output.split('\n'):
+        line = line.strip()
+        line = line.decode('utf-8')
+        if marker in line and not '/bin/sh' in line:
+            processes += 1
+    return processes
+
+
+def _start_spider_and_wait_for_finish(spider, url, max_wait=60*2):
+    """ Returns output data file name """
+    output_file = tempfile.NamedTemporaryFile(delete=False, suffix='.jl')
+    try:
+        output_file.close()
+    except:
+        pass
+    run(
+        "scrapy crawl {spider} -a product_url={url} -o {output}".format(
+            spider=spider, url=url, output=output_file.name
+        )
+    )
+    _total_slept = 0
+    while 1:
+        _total_slept += 1
+        time.sleep(1)
+        if os.path.exists(output_file.name) and check_running_instances('-o '+output_file.name) == 0:
+            # spider created file and stopped; successfully finished?
+            return output_file.name
+        if _total_slept > max_wait:
+            return
+
+
 @app.route('/get_data_ajax')
 def get_data_ajax():
     url = request.args.get('url', '').strip()
-    print 'URL', url
-    import time
-    time.sleep(1)
-    # TODO: process URL
-    spider_result = {'abc': 1, 'list': [1,2,3]}
-    return json.dumps(spider_result, sort_keys=True)
+    spider = request.args['spider'].strip()
+    print 'SPIDER', spider
+    print 'PROCESSING URL', url
+    output_fname = _start_spider_and_wait_for_finish(spider, url)
+    if output_fname:
+        content = [json.loads(l.strip()) for l in open(output_fname, 'r').readlines()
+                   if l.strip()]
+        #content = {'URL': url, 'SPIDER': spider, 'DATA': content}
+    else:
+        content = {'ERROR': 'spider execution timeout (the spider has been running for too long)'}
+    return cgi.escape(json.dumps(content))
 
 
 @app.route('/get_data', methods=['GET'])
 def get_data():
     url = request.args.get('url', '').strip()
+    spider = request.args.get('spider', '').strip()
+    if not spider:
+        spider = _find_spider_by_url(url)
+    if isinstance(spider, (str, unicode)):
+        if not '_products' in spider:
+            spider = spider + '_products'
+    if spider not in _get_all_spidernames():
+        return 'Invalid spider name. Allowed: <br><br> %s' % '<br>'.join(sorted(_get_all_spidernames()))
     if not url:
-        return 'Invalid URL param given'
-    return ajax_template.replace('{{ url }}', url)  # django-like templates =)
+        return 'Invalid URL param given (use "url" GET param)'
+    return ajax_template.replace('{{ url }}', url).replace('{{ spider }}', spider)  # django-like templates =)
 
 
 if __name__ == "__main__":
