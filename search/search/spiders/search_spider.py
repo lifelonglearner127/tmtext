1abb17d192a56120a138426a5b8c8494a683e15c
lifelonglearner127 <life.long.learner127@outlook.com>
2015-10-20 03:01:45 +0300

    updated matching crawler version

--- search/search/spiders/search_spider.py
+++ search/search/spiders/search_spider.py
@@ -76,7 +76,7 @@ class SearchSpider(BaseSpider):
         # call specific init for each derived class
         self.init_sub()
 
-        self.version = "edead362dff971c1202f1b98f11ff1ac6f800519"
+        self.version = "a4d38cc4ab510d799c29b8b195a18e2f59c9ac8e"
 
         self.product_url = product_url
         self.products_file = products_file
