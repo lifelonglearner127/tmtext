45995600b8925425c5c06f5e3a12a0cb91155b5e
lifelonglearner127 <life.long.learner127@outlook.com>
2015-10-21 17:36:37 +0300

    fixed bug in refactored spiders: removed old code that was overriding parent class code

--- search/search/spiders/bestbuy_spider.py
+++ search/search/spiders/bestbuy_spider.py
@@ -24,7 +24,7 @@ class BestbuySpider(SearchResultsSpider):
         self.target_site = "bestbuy"
         self.start_urls = [ "http://www.bestbuy.com" ]
 
-    def extract_results_products(self, response):
+    def extract_result_products(self, response):
 
         hxs = HtmlXPathSelector(response)
 
@@ -65,63 +65,3 @@ class BestbuySpider(SearchResultsSpider):
             items.append(item)
 
         return items
-
-
-# OUTDATED, used for inheritance directly from search_spider:
-
-
-    def parseResults(self, response):
-
-        hxs = HtmlXPathSelector(response)
-
-        #site = response.meta['origin_site']
-        origin_name = response.meta['origin_name']
-        origin_model = response.meta['origin_model']
-        origin_url = response.meta['origin_url']
-
-        # if this comes from a previous request, get last request's items and add to them the results
-
-        if 'items' in response.meta:
-            items = response.meta['items']
-        else:
-            items = set()
-
-
-        results = hxs.select("//div[@class='list-item-info']/div[@class='sku-title']/h4/a")
-
-        for result in results:
-            item = SearchItem()
-            #item['origin_site'] = site
-            product_name_holder = result.select("text()").extract()
-            if product_name_holder:
-                item['product_name'] = product_name_holder[0].strip()
-            else:
-                self.log("Error: No product name: " + str(response.url) + " from product: " + origin_url, level=log.ERROR)
-
-            item['product_url'] = Utils.clean_url(Utils.add_domain(result.select("@href").extract()[0], "http://www.bestbuy.com"))
-
-            if 'origin_url' in response.meta:
-                item['origin_url'] = response.meta['origin_url']
-
-            if 'origin_name' in response.meta:
-                item['origin_name'] = response.meta['origin_name']
-
-            if 'origin_model' in response.meta:
-                item['origin_model'] = response.meta['origin_model']                
-
-            model_holder = result.select("../../../div[@class='sku-model']/ul/li[@class='model-number']/span[@id='model-value']/text()").extract()
-            if model_holder:
-                item['product_model'] = model_holder[0]
-
-            price_holder = result.select("../../../../div[@class='list-item-price']//div[@class='price-block']//div[@class='medium-item-price']/text()[normalize-space()]").extract()
-            if price_holder:
-                price = price_holder[0].strip()
-                price = re.sub(",", "", price)
-                price = float(price)
-                item['product_target_price'] = price
-
-            items.add(item)
-
-        response.meta['items'] = items
-        response.meta['parsed'] = items
-        return self.reduceResults(response)
