7637f32a391191da3377df4c84379adca62e727d
lifelonglearner127 <life.long.learner127@outlook.com>
2015-10-23 17:19:40 +0300

    work on

--- product-ranking/product_ranking/spiders/jcpenney_shelf_pages.py
+++ product-ranking/product_ranking/spiders/jcpenney_shelf_pages.py
@@ -7,15 +7,28 @@ import scrapy
 from scrapy.http import Request
 
 from product_ranking.items import SiteProductItem
+from .jcpenney import JcpenneyProductsSpider
 
 is_empty = lambda x: x[0] if x else None
 
 
-class JCPenneyShelfPagesSpider(scrapy.Spider):
+class JCPenneyShelfPagesSpider(JcpenneyProductsSpider):
     name = 'jcpenney_shelf_urls_products'
     allowed_domains = ['jcpenney.com', 'www.jcpenney.com']
 
     current_page = 1
+
+    def _setup_class_compatibility(self):
+        """ Needed to maintain compatibility with the SC spiders baseclass """
+        self.quantity = 99999
+        self.site_name = self.allowed_domains[0]
+        self.user_agent_key = None
+        self.zipcode = '12345'
+        self.current_page = 1
+
+    def _setup_meta_compatibility(self):
+        """ Needed to prepare first request.meta vars to use """
+        return {'remaining': 99999, 'search_term': ''}.copy()
 
     def __init__(self, *args, **kwargs):
         self.product_url = kwargs['product_url']
@@ -30,7 +43,8 @@ class JCPenneyShelfPagesSpider(scrapy.Spider):
             " Chrome/37.0.2062.120 Safari/537.36"
 
     def start_requests(self):
-        yield Request(url=self.valid_url(self.product_url))
+        yield Request(url=self.valid_url(self.product_url),
+                      meta=self._setup_meta_compatibility())
 
     def parse(self, response):
         yield self.get_urls(response)
