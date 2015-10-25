622aad231616d32702cefa09790cc01fa25478db
lifelonglearner127 <life.long.learner127@outlook.com>
2015-10-25 14:52:23 +0300

    Detect redirects (not_found product)

--- product-ranking/product_ranking/spiders/jet.py
+++ product-ranking/product_ranking/spiders/jet.py
@@ -173,10 +173,25 @@ class JetProductsSpider(BaseProductsSpider):
             for req in super(JetProductsSpider, self).start_requests():
                 yield req
 
+    def redirected_from_product_to_main_page(self, response):
+        """ Returns True if the spider was redirected from a product page
+             to the main website page (//jet.com).
+            Means "not_found" product. """
+        history = response.meta.get('redirect_urls', None)
+        if history:
+            current_url = response.url.replace('/', '').replace('https:', '')\
+                .replace('http:', '').replace('www.', '')
+            if current_url == 'jet.com':
+                return True
+
     def parse_product(self, response):
         meta = response.meta.copy()
         product = meta['product']
         reqs = []
+
+        if self.redirected_from_product_to_main_page(response):
+            product['not_found'] = True
+            return product
 
         cond_set(
             product, "title", response.xpath(
