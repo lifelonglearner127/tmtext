8f8d3db015f0c3fb8d80c1525021e1c7bd67c0f4
lifelonglearner127 <life.long.learner127@outlook.com>
2015-10-26 15:27:28 +0200

    work on jcpenney shelf

--- product-ranking/product_ranking/spiders/jcpenney_shelf_pages.py
+++ product-ranking/product_ranking/spiders/jcpenney_shelf_pages.py
@@ -1,5 +1,3 @@
-# TODO: check "product_per_page" fields, may be wrong
-
 import re
 import urlparse
 
@@ -31,6 +29,7 @@ class JCPenneyShelfPagesSpider(JcpenneyProductsSpider):
         return {'remaining': 99999, 'search_term': ''}.copy()
 
     def __init__(self, *args, **kwargs):
+        self._setup_class_compatibility()
         self.product_url = kwargs['product_url']
 
         if "num_pages" in kwargs:
@@ -42,57 +41,42 @@ class JCPenneyShelfPagesSpider(JcpenneyProductsSpider):
             " AppleWebKit/537.36 (KHTML, like Gecko)" \
             " Chrome/37.0.2062.120 Safari/537.36"
 
+    @staticmethod
+    def valid_url(url):
+        if not re.findall("http(s){0,1}\:\/\/", url):
+            url = "http://" + url
+        return url
+
     def start_requests(self):
         yield Request(url=self.valid_url(self.product_url),
                       meta=self._setup_meta_compatibility())
 
-    def parse(self, response):
-        yield self.get_urls(response)
-        request = self.next_pagination_link(response)
-        if request is not None:
-            yield request
-
-    def get_urls(self, response):
-        item = SiteProductItem()
+    def _scrape_product_links(self, response):
         urls = response.xpath(
             '//div[contains(@class, "product_description")]'
             '//img[contains(@id, "ThumbnailImage")]/../../../a/@href'
         ).extract()
         urls = [urlparse.urljoin(response.url, x) if x.startswith('/') else x
                 for x in urls]
-        #print "-"*50
-        #print len(urls)
-        #print "-"*50
-        assortment_url = {response.url: urls}
-        item["assortment_url"] = assortment_url
-        item['results_per_page'] = self._scrape_results_per_page(response)
-        item['scraped_results_per_page'] = len(urls)
-        return item
 
-    def _scrape_results_per_page(self, response):
-        num = None
-        try:
-            num = response.xpath(
-                '//*[contains(text(), "items per page")]/..//strong//span/text()'
-            ).re('\d+')[0]
-        except IndexError:
-            self.log('Failed to get num of results')
-        if isinstance(num, (str, unicode)) and num.isdigit():
-            return int(num)
+        sample = response.xpath('//div[@id="breadcrumb"]/ul/li//text()').extract()
+        shelf_categories = [i.strip() for i in sample if i.strip() and i != '>']
+        shelf_category = shelf_categories[-1] if shelf_categories else None
 
-    def next_pagination_link(self, response):
+        for url in urls:
+            item = SiteProductItem()
+            if shelf_categories:
+                item['shelf_name'] = shelf_categories
+            if shelf_category:
+                item['shelf_path'] = shelf_category
+            yield 'http://www.jcpenney.com' + url, item
+
+    def _scrape_next_results_page_link(self, response):
         if self.current_page >= self.num_pages:
             return
         self.current_page += 1
+        return super(JCPenneyShelfPagesSpider, self)._scrape_next_results_page_link(response)
 
-        next_link = is_empty(
-            response.xpath('//a[contains(@title, "next page")]/@href').extract())
-
-        if next_link:
-            url = urlparse.urljoin(response.url, next_link)
-            return Request(url=url)
-
-    def valid_url(self, url):
-        if not re.findall("http(s){0,1}\:\/\/", url):
-            url = "http://" + url
-        return url
+    def parse_product(self, response):
+        product = response.meta['product']
+        return super(JCPenneyShelfPagesSpider, self).parse_product(response)
