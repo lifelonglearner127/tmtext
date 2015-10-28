21da7f123cef34672c604e0bf8cd1ecc71de6245
lifelonglearner127 <life.long.learner127@outlook.com>
2015-10-28 10:34:23 +0200

    macys works

--- product-ranking/product_ranking/spiders/macys_shelf_pages.py
+++ product-ranking/product_ranking/spiders/macys_shelf_pages.py
@@ -8,18 +8,31 @@ from scrapy.http import Request
 from scrapy import Selector
 
 from product_ranking.items import SiteProductItem
+from .macys import MacysProductsSpider
 
 is_empty = lambda x: x[0] if x else None
 
 
-class MacysShelfPagesSpider(scrapy.Spider):
+class MacysShelfPagesSpider(MacysProductsSpider):
     name = 'macys_shelf_urls_products'
     allowed_domains = ['macys.com', 'www1.macys.com', 'www.macys.com']
 
-    current_page = 1
+    def _setup_class_compatibility(self):
+        """ Needed to maintain compatibility with the SC spiders baseclass """
+        self.quantity = 99999
+        self.site_name = self.allowed_domains[0]
+        self.user_agent_key = None
+        self.zipcode = '12345'
+        self.current_page = 1
+
+    def _setup_meta_compatibility(self):
+        """ Needed to prepare first request.meta vars to use """
+        return {'remaining': 99999, 'search_term': ''}.copy()
 
     def __init__(self, *args, **kwargs):
+        super(MacysShelfPagesSpider, self).__init__(*args, **kwargs)
         self.product_url = kwargs['product_url']
+        self._setup_class_compatibility()
 
         if "num_pages" in kwargs:
             self.num_pages = int(kwargs['num_pages'])
@@ -31,52 +44,42 @@ class MacysShelfPagesSpider(scrapy.Spider):
             " Chrome/37.0.2062.120 Safari/537.36"
 
     def start_requests(self):
-        yield Request(url=self.valid_url(self.product_url))
+        yield Request(url=self.valid_url(self.product_url),
+                      meta=self._setup_meta_compatibility())
 
-    def parse(self, response):
-        yield self.get_urls(response)
-        request = self.next_pagination_link(response)
-        if request is not None:
-            yield request
+    @staticmethod
+    def valid_url(url):
+        if not re.findall("http(s){0,1}\:\/\/", url):
+            url = "http://" + url
+        return url
 
-    def get_urls(self, response):
-        item = SiteProductItem()
+    def _scrape_product_links(self, response):
         urls = response.xpath(
             '//span[contains(@id, "main_images_holder")]/../../a/@href'
         ).extract()
 
-        urls = [urlparse.urljoin(response.url, x) if x.startswith('/') else x
-                for x in urls]
-        assortment_url = {response.url: urls}
-        item["assortment_url"] = assortment_url
-        item['results_per_page'] = self._scrape_results_per_page(response)
-        item['scraped_results_per_page'] = len(urls)
-        return item
+        urls = ['http://www1.macys.com' + i for i in urls]
 
-    def _scrape_results_per_page(self, response):
-        num = ''.join(
-            response.xpath(
-                '//*[contains(@id, "productCount")]/text()').extract()
-        ).strip()
-        if num:
-            num = num.strip()
-            if num.isdigit():
-                return int(num)
+        sample = response.xpath('//div[@id="featureNav"]/ul/li//text()').extract()
+        categories = [i.strip() for i in reversed(sample) if i.strip()]
 
-    def next_pagination_link(self, response):
+        shelf_categories = categories[1:]
+        shelf_category = shelf_categories[-1] if shelf_categories else None
+
+        for url in urls:
+            item = SiteProductItem()
+            if shelf_category:
+                item['shelf_name'] = shelf_category
+            if shelf_categories:
+                item['shelf_path'] = shelf_categories
+            yield url, item
+
+    def _scrape_next_results_page_link(self, response):
         if self.current_page >= self.num_pages:
             return
         self.current_page += 1
+        return super(MacysShelfPagesSpider,
+                     self)._scrape_next_results_page_link(response)
 
-        next_link = is_empty(
-            response.xpath('//a[contains(@class, "arrowRight")]/@href').extract()
-        )
-
-        if next_link:
-            url = urlparse.urljoin(response.url, next_link)
-            return Request(url=url)
-
-    def valid_url(self, url):
-        if not re.findall("http(s){0,1}\:\/\/", url):
-            url = "http://" + url
-        return url
+    def parse_product(self, response):
+        return super(MacysShelfPagesSpider, self).parse_product(response)
