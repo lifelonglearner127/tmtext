51050868bc8c90a1fb23e5aac5dbd108f91704d5
lifelonglearner127 <life.long.learner127@outlook.com>
2015-10-29 12:04:39 +0200

    multiple fixes to coupon scrappers: jcpenney, kohls, macys

--- product-ranking/product_ranking/spiders/jcpenney_coupons.py
+++ product-ranking/product_ranking/spiders/jcpenney_coupons.py
@@ -17,50 +17,56 @@ is_empty = lambda x: x[0] if x else None
 class JCPenneyCouponsSpider(scrapy.Spider):
     name = 'jcpenney_coupons_products'
     allowed_domains = ['jcpenney.com', 'www.jcpenney.com']
+    DEFAULT_URL = 'http://www.jcpenney.com/jsp/browse/marketing/promotion.jsp?pageId=pg40027800029#'
 
     def __init__(self, *args, **kwargs):
-        self.product_url = kwargs['product_url']
-
+        super(JCPenneyCouponsSpider, self).__init__(**kwargs)
+        self.product_url = kwargs.get('product_url', self.DEFAULT_URL)
         self.user_agent = "Mozilla/5.0 (X11; Linux i686 (x86_64))" \
-            " AppleWebKit/537.36 (KHTML, like Gecko)" \
-            " Chrome/37.0.2062.120 Safari/537.36"
+                          " AppleWebKit/537.36 (KHTML, like Gecko)" \
+                          " Chrome/37.0.2062.120 Safari/537.36"
+        self.start_urls = [self._valid_url(self.product_url)]
 
-    def start_requests(self):
-        yield Request(url=self.valid_url(self.product_url))
+    def _valid_url(self, url):
+        if not re.match("https?://", url):
+            url = '%s%s' % ("http://", url)
+        return url
+
+    def _parse_coupons(self, response):
+        return response.css('.coupon > .offer')
+
+    def _parse_description(self, coupon):
+        return is_empty(coupon.css('::text').extract())
+
+    def _parse_category(self, coupon):
+        return is_empty(
+            coupon.xpath('preceding-sibling::div[contains(@class,"grey12")]'
+                         '/strong/text()').extract()
+        )
 
     def _parse_end_date(self, coupon):
-        element = coupon.xpath('.//*[contains(text(), "Valid THROUGH")]/text()').extract()
-        if element:
-            element = element[0].lower().replace('valid through', '').strip()
-            return parse_date(element)
+        e = coupon.xpath('following-sibling::div[contains(@class,"grey12")]'
+                         '/text()').extract()
+        if e:
+            e = re.search('valid through (.+)', e[0], re.IGNORECASE)
+            if e:
+                return parse_date(e.group(1))
 
-    def _parse_discount(self, description):
-        if description:
-            description = description.lower()
-            _match = re.search(r' (\d+)%.*off', description)
-            if _match:
-                return _match.group(1) + '%'
+    def _parse_discount(self, coupon):
+        return coupon.css('::text').re('\d+\%')
 
     def _parse_conditions(self, coupon):
         return is_empty(
-            coupon.xpath('.//*[contains(@class, "txt_caps")]/following-sibling'
-                         '::div[contains(@class, "grey14")]/text()').extract()
+            coupon.xpath('following-sibling::div[contains(@class,"grey14")]'
+                         '/text()').extract()
         )
 
     def parse(self, response):
-        for coupon in response.xpath('//li[contains(@class, "flt_lft")]'):
+        for coupon in self._parse_coupons(response):
             item = DiscountCoupon()
-            item['category'] = is_empty(
-                coupon.xpath('../div[contains(@class, "section-bar")]'
-                             '/p/text()').extract()
-            )
-            item['description'] = is_empty(coupon.css('.offer ::text').extract())
+            item['category'] = self._parse_category(coupon)
+            item['description'] = self._parse_description(coupon)
             item['end_date'] = self._parse_end_date(coupon)
-            item['discount'] = self._parse_discount(item.get('description', None))
+            item['discount'] = self._parse_discount(coupon)
             item['conditions'] = self._parse_conditions(coupon)
             yield item
-
-    def valid_url(self, url):
-        if not re.findall("http(s){0,1}\:\/\/", url):
-            url = "http://" + url
-        return url
