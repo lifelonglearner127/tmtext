fdda5847e93c244727edc8613a6e8ce047aa3ce5
lifelonglearner127 <life.long.learner127@outlook.com>
2015-10-31 01:40:12 +0300

    Uncomment all servers

--- deploy/parallel_deploy.sh
+++ deploy/parallel_deploy.sh
@@ -15,32 +15,32 @@ promptyn () {
 
 deploy () {
     echo "DEPLOYING...";
-    #fab -H keywords.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords2.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords3.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords4.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords5.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords2.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords3.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords4.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords5.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
 
-    #fab -H keywords6.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords7.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords8.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords9.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords10.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords11.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords6.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords7.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords8.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords9.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords10.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords11.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
 
-    #fab -H keywords12.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords13.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords14.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords15.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords16.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords17-pg.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords12.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords13.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords14.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords15.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords16.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords17-pg.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
 
-    #fab -H keywords18.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords19.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords20.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords21-clorox.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords22.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
-    #fab -H keywords23.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords18.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords19.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords20.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords21-clorox.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords22.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    fab -H keywords23.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
 
     fab -H keywords24.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
     fab -H keywords25.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
@@ -49,7 +49,7 @@ deploy () {
     fab -H keywords28-maplin.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
     fab -H keywords29-pg.contentanalyticsinc.com set_production deploy:branch=sc_production,restart_scrapyd=True -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
 
-    fab -H sc-tests.contentanalyticsinc.com set_production deploy_sc_test_server:branch=sc_production -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
+    #fab -H sc-tests.contentanalyticsinc.com set_production deploy_sc_test_server:branch=sc_production -u ubuntu -i ../../../ssh_certificates//ubuntu_id_rsa &
 }
 
 if promptyn "Do you really want to deploy the code to all the servers?"; then
