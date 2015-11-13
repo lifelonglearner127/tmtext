d7c6d43a943053244c8591d430eef4f8678c3f77
lifelonglearner127 <life.long.learner127@outlook.com>
2015-11-13 21:10:18 +0300

    Speed up script (use queues instead of listing the file)

--- product-ranking/sqs_tests_gui/gui/management/commands/update_jobs.py
+++ product-ranking/sqs_tests_gui/gui/management/commands/update_jobs.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import datetime
+import json
 import zipfile
 import subprocess
 import tempfile
@@ -10,6 +11,8 @@ import tempfile
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Q
 from django.utils.timezone import now
+import boto
+from dateutil.parser import parse as parse_date
 
 CWD = os.path.dirname(os.path.abspath(__file__))
 #sys.path.append(os.path.join(CWD, '..', '..', '..', '..'))
@@ -59,36 +62,6 @@ def num_of_running_instances(file_path):
     return processes
 
 
-def list_amazon_bucket(bucket=AMAZON_BUCKET_NAME,
-                       local_fname=LOCAL_AMAZON_LIST_CACHE):
-    filez = list_files_in_bucket(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, bucket)
-    # dump to a temporary file and replace the original one then
-    tmp_file = tempfile.NamedTemporaryFile(mode='rb', delete=False)
-    tmp_file.close()
-
-    with open(tmp_file.name, 'w') as fh:
-        for f in filez:
-            fh.write(str(f)+'\n')
-    if os.path.exists(local_fname):
-        os.unlink(local_fname)
-    os.rename(tmp_file.name, local_fname)
-
-
-def get_filenames_for_task_id(task_id, server_name,
-                              local_fname=LOCAL_AMAZON_LIST_CACHE):
-    slug_server_name = scrapy_daemon.slugify(server_name)
-    with open(local_fname, 'r') as fh:
-        for line in fh:
-            line = line.strip()
-            if not line:
-                continue
-            if '____%s--%s____' % (slug_server_name, task_id) in line:
-                if ',' in line:
-                    line = line.split(',')[-1]
-                line = line.replace('<', '').replace('>', '')
-                yield line.strip()
-
-
 def rename_first_file_with_extension(base_dir, new_fname, extension='.csv'):
     for filename in os.listdir(base_dir):
         if filename.lower().endswith(extension):
@@ -110,111 +83,194 @@ def _unzip_local_file(fname, new_fname, ext):
         )
 
 
+def _get_output_queue_name_for_job(job):
+    return job.server_name + scrapy_daemon.OUTPUT_QUEUE_NAME
+
+
+def _get_progress_queue_name_for_job(job):
+    return job.server_name + scrapy_daemon.PROGRESS_QUEUE_NAME
+
+
+def _get_server_name_from_queue(queue_name):
+    return queue_name.replace(scrapy_daemon.OUTPUT_QUEUE_NAME, '')\
+        .replace(scrapy_daemon.PROGRESS_QUEUE_NAME, '')
+
+
+def get_output_queues_for_jobs(jobs):
+    """ Returns unique SQS queues names for the given jobs """
+    queues = []
+    for j in jobs:
+        q_name = _get_output_queue_name_for_job(j)
+        if not q_name in queues:
+            queues.append(q_name)
+    return list(set(queues))
+
+
+def _get_queue(queue_name, region="us-east-1"):
+    conn = boto.sqs.connect_to_region(region)
+    return conn.get_queue(queue_name)
+
+
+def read_messages_from_queue(queue_name, region="us-east-1", timeout=5, num_messages=999):
+    q = _get_queue(queue_name, region=region)
+    num_iterations = num_messages / 10 + 1
+    result = []
+    for i in range(0, num_iterations):
+        for m in q.get_messages(num_messages=10, visibility_timeout=timeout):
+            result.append(m)
+    return result
+
+
+def _delete_queue_message(queue_or_name, msg, region="us-east-1"):
+    if isinstance(queue_or_name, (str, unicode)):
+        queue_or_name = _get_queue(queue_or_name, region=region)
+    queue_or_name.delete_message(msg)
+
+
+def get_progress_queues_for_jobs(jobs):
+    """ Returns unique SQS queues names for the given jobs """
+    queues = []
+    for j in jobs:
+        q_name = _get_progress_queue_name_for_job(j)
+        if not q_name in queues:
+            queues.append(q_name)
+    return list(set(queues))
+
+
 class Command(BaseCommand):
-    help = 'Updates 10 random jobs, downloading their files if ready'
+    help = 'Updates 50 random jobs taken from the output queues,' \
+           ' downloading their files if ready'
 
     def handle(self, *args, **options):
         if num_of_running_instances('update_jobs') > 1:
             print 'an instance of the script is already running...'
             sys.exit()
+
+        # get random jobs
         jobs = Job.objects.filter(
             Q(status='pushed into sqs') | Q(status='in progress')
-        ).order_by('?').distinct()[0:10]
-        if jobs:
-            list_amazon_bucket()  # get list of files from S3
-        for job in jobs:
-            # try to find the appropriate S3 file by task ID
-            amazon_fnames = get_filenames_for_task_id(job.task_id,
-                                                      job.server_name)
-            if not isinstance(amazon_fnames, (list, tuple)):  # generator?
-                amazon_fnames = list(amazon_fnames)
-            amazon_data_file = [f for f in amazon_fnames if '.csv' in f]
-            amazon_json_data_file = [f for f in amazon_fnames if '.jl' in f]
-            amazon_log_file = [f for f in amazon_fnames if '.log' in f]
-            amazon_progress_file = [f for f in amazon_fnames
-                                    if '.progress' in f]
+        ).order_by('?').distinct()[0:50]
 
-            amazon_data_file = amazon_data_file[0] if amazon_data_file else []
-            amazon_json_data_file = amazon_json_data_file[0]\
-                if amazon_json_data_file else []
-            amazon_log_file = amazon_log_file[0] if amazon_log_file else []
-            amazon_progress_file = amazon_progress_file[0]\
-                if amazon_progress_file else []
+        # get output & progress queue names
+        output_queues = get_output_queues_for_jobs(jobs)
+        progress_queues = get_progress_queues_for_jobs(jobs)
 
-            print
-            print '=' * 79
-            print 'Processing JOB', job.__dict__
-            print 'Data file', amazon_data_file
-            print 'Log file', amazon_log_file
-            print 'Progress file', amazon_progress_file
-
-            if amazon_progress_file:
-                job.status = 'in progress'
-                job.save()
-
-            if amazon_progress_file:
-                full_local_prog_path = MEDIA_ROOT + get_progress_filename(job)
-                if not os.path.exists(os.path.dirname(full_local_prog_path)):
-                    os.makedirs(os.path.dirname(full_local_prog_path))
-                download_s3_file(AMAZON_BUCKET_NAME, amazon_progress_file,
-                                 full_local_prog_path)
-                _unzip_local_file(full_local_prog_path, 'progress.progress',
-                                  '.progress')
-
-            if amazon_log_file:
-                full_local_log_path = MEDIA_ROOT + get_log_filename(job)
-                if not os.path.exists(os.path.dirname(full_local_log_path)):
-                    os.makedirs(os.path.dirname(full_local_log_path))
-                download_s3_file(AMAZON_BUCKET_NAME, amazon_log_file,
-                                 full_local_log_path)
-                _unzip_local_file(full_local_log_path, 'log.log', 'log')
-                with open(full_local_log_path, 'r') as fh:
-                    cont = fh.read()
-                    if not "'finish_reason': 'finished'" in cont:
-                        job.status = 'failed'
-                        job.save()
+        # scan output & progress queues (read the messages)
+        for output_queue in output_queues:
+            output_messages = read_messages_from_queue(output_queue)
+            for m in output_messages:
+                m_body = m.get_body()
+                if isinstance(m_body, (str, unicode)):
+                    m_body = json.loads(m_body)
+                task_id = m_body.get('task_id', m_body.get('_msg_id', None))
+                # remove messages that are way too old
+                utc_datetime = m_body.get('utc_datetime', None)
+                if utc_datetime:
+                    utc_datetime = parse_date(utc_datetime)
+                    if utc_datetime < datetime.datetime.now() - datetime.timedelta(days=3):
+                        _delete_queue_message(output_queue, m)
+                        print('Deleted old message: %s' % str(m_body))
                         continue
+                if task_id is None:
+                    continue
+                server_name = _get_server_name_from_queue(output_queue)
+                # get the appropriate DB job
+                db_job = Job.objects.filter(
+                    Q(status='pushed into sqs') | Q(status='in progress'),
+                    task_id=task_id, server_name=server_name)
+                if db_job.count() > 1:
+                    print 'Too many jobs found for message %s' % str(m_body)
+                    print db_job
+                    continue
+                if not db_job:
+                    continue  # not found in DB?
+                db_job = db_job[0]
 
-            if amazon_data_file:
-                print 'For job with task ID %s we found amazon fname [%s]' % (
-                    job.task_id, amazon_data_file)
-                full_local_data_path = MEDIA_ROOT + get_data_filename(job)
-                if not os.path.exists(os.path.dirname(full_local_data_path)):
-                    os.makedirs(os.path.dirname(full_local_data_path))
-                download_s3_file(AMAZON_BUCKET_NAME, amazon_data_file,
-                                 full_local_data_path)
-                _unzip_local_file(full_local_data_path, 'data_file.csv', '.csv')
-                if zipfile.is_zipfile(full_local_data_path):
-                    unzip_file(full_local_data_path,
-                               unzip_path=full_local_data_path)
-                    os.remove(full_local_data_path)
-                    rename_first_file_with_extension(
-                        os.path.dirname(full_local_data_path),
-                        'data_file.csv',
-                        '.csv'
-                    )
+                # ok now we've got a message in the queue and its appropriate DB job
+                # now - save files
+                amazon_data_file = m_body.get('csv_data_key', None)
+                amazon_json_data_file = m_body.get('s3_key_data', None)
+                amazon_log_file = m_body.get('s3_key_logs', None)
 
-            if not amazon_data_file and amazon_json_data_file:  # CSV conversion failed?
-                print 'For job with task ID %s we found amazon fname [%s]' % (
-                    job.task_id, amazon_json_data_file)
-                full_local_data_path = MEDIA_ROOT + get_data_filename(job)
-                if not os.path.exists(os.path.dirname(full_local_data_path)):
-                    os.makedirs(os.path.dirname(full_local_data_path))
-                download_s3_file(AMAZON_BUCKET_NAME, amazon_json_data_file,
-                                 full_local_data_path)
-                _unzip_local_file(full_local_data_path, 'data_file.csv', '.csv')
-                if zipfile.is_zipfile(full_local_data_path):
-                    unzip_file(full_local_data_path,
-                               unzip_path=full_local_data_path)
-                    os.remove(full_local_data_path)
-                    rename_first_file_with_extension(
-                        os.path.dirname(full_local_data_path),
-                        'data_file.csv',
-                        '.csv'
-                    )
+                print
+                print '=' * 79
+                print 'Processing JOB', db_job.__dict__
+                print 'Data file', amazon_data_file
+                print 'Log file', amazon_log_file
 
-            if amazon_data_file or amazon_json_data_file:
-                if amazon_log_file:  # log file should exist for successful jobs
-                    job.status = 'finished'
-                    job.finished = now()
+                if amazon_log_file:
+                    full_local_log_path = MEDIA_ROOT + get_log_filename(db_job)
+                    if not os.path.exists(os.path.dirname(full_local_log_path)):
+                        os.makedirs(os.path.dirname(full_local_log_path))
+                    download_s3_file(AMAZON_BUCKET_NAME, amazon_log_file,
+                                     full_local_log_path)
+                    _unzip_local_file(full_local_log_path, 'log.log', 'log')
+                    with open(full_local_log_path, 'r') as fh:
+                        cont = fh.read()
+                        if not "'finish_reason': 'finished'" in cont:
+                            db_job.status = 'failed'
+                            db_job.save()
+                            continue
+
+                if amazon_data_file:
+                    print 'For job with task ID %s we found amazon fname [%s]' % (
+                        db_job.task_id, amazon_data_file)
+                    full_local_data_path = MEDIA_ROOT + get_data_filename(db_job)
+                    if not os.path.exists(os.path.dirname(full_local_data_path)):
+                        os.makedirs(os.path.dirname(full_local_data_path))
+                    download_s3_file(AMAZON_BUCKET_NAME, amazon_data_file,
+                                     full_local_data_path)
+                    _unzip_local_file(full_local_data_path, 'data_file.csv', '.csv')
+                    if zipfile.is_zipfile(full_local_data_path):
+                        unzip_file(full_local_data_path,
+                                   unzip_path=full_local_data_path)
+                        os.remove(full_local_data_path)
+                        rename_first_file_with_extension(
+                            os.path.dirname(full_local_data_path),
+                            'data_file.csv',
+                            '.csv'
+                        )
+
+                if not amazon_data_file and amazon_json_data_file:  # CSV conversion failed?
+                    print 'For job with task ID %s we found amazon fname [%s]' % (
+                        db_job.task_id, amazon_json_data_file)
+                    full_local_data_path = MEDIA_ROOT + get_data_filename(db_job)
+                    if not os.path.exists(os.path.dirname(full_local_data_path)):
+                        os.makedirs(os.path.dirname(full_local_data_path))
+                    download_s3_file(AMAZON_BUCKET_NAME, amazon_json_data_file,
+                                     full_local_data_path)
+                    _unzip_local_file(full_local_data_path, 'data_file.csv', '.csv')
+                    if zipfile.is_zipfile(full_local_data_path):
+                        unzip_file(full_local_data_path,
+                                   unzip_path=full_local_data_path)
+                        os.remove(full_local_data_path)
+                        rename_first_file_with_extension(
+                            os.path.dirname(full_local_data_path),
+                            'data_file.csv',
+                            '.csv'
+                        )
+
+                if amazon_data_file or amazon_json_data_file:
+                    if amazon_log_file:  # log file should exist for successful jobs
+                        db_job.status = 'finished'
+                        db_job.finished = now()
+                        db_job.save()
+                        _delete_queue_message(output_queue, m)
+                        print('Deleted old message: %s' % str(m_body))
+
+                # TODO: handle progress messages
+                """
+                if amazon_progress_file:
+                    job.status = 'in progress'
                     job.save()
+
+                if amazon_progress_file:
+                    full_local_prog_path = MEDIA_ROOT + get_progress_filename(job)
+                    if not os.path.exists(os.path.dirname(full_local_prog_path)):
+                        os.makedirs(os.path.dirname(full_local_prog_path))
+                    download_s3_file(AMAZON_BUCKET_NAME, amazon_progress_file,
+                                     full_local_prog_path)
+                    _unzip_local_file(full_local_prog_path, 'progress.progress',
+                                      '.progress')
+                """
+
