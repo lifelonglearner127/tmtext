93d5a9fed7ecff21ccb966c9eb5486aabdd90967
lifelonglearner127 <life.long.learner127@outlook.com>
2015-11-19 12:03:44 +0200

    homedepot_validator

--- product-ranking/product_ranking/validators/homedepot_validator.py
+++ product-ranking/product_ranking/validators/homedepot_validator.py
@@ -23,12 +23,12 @@ class HomedepotValidatorSettings(object):  # do NOT set BaseValidatorSettings as
 
     test_urls = {
         'http://www.homedepot.com/p/Kraftware-Fishnet-24-oz-Walnut-Insulated-Drinkware-Set-of-4-38024/203171306',
-        'http://www.homedepot.com/p/Unbranded-Global-Goodwill-Jazz-23-oz-4-3-4-in-Mug-in-Pearl-1-Piece-849851027541/204615618',
+        'http://www.homedepot.com/p/Rachael-Ray-Dinnerware-Gold-Scroll-4-Piece-Mug-Set-in-Assorted-57634/205450504',
         'http://www.homedepot.com/p/Carlisle-32-oz-SAN-Plastic-Stackable-Tumbler-in-Ruby-Case-of-48-553210/204658635',
         'http://www.homedepot.com/p/BonJour-Coffee-2-Piece-Insulated-Glass-Cappuccino-Cup-Set-51285/205450204',
         'http://www.homedepot.com/p/ORE-International-5-gal-Water-Bottle-in-Clear-WS50GH-48/206022555?MERCH=REC-_-NavPLPHorizontal1_rr-_-NA-_-206022555-_-N',
         'http://www.homedepot.com/p/The-Folding-Table-Cloth-6-ft-Table-Cloth-Made-for-Folding-Tables-Natural-3072NAT/203335976',
         'http://www.homedepot.com/p/Kraftware-Fishnet-Wedge-Placemat-in-Tan-Set-of-12-34139/203171295',
         'http://www.homedepot.com/p/Old-Dutch-13-in-Antique-Embossed-Victoria-Charger-Plates-Set-of-6-OS421/203757467',
-        'http://www.homedepot.com/p/Unbranded-Kingston-and-Grace-13-in-x-18-in-Frame-Placemat-in-Brown-Set-of-12-QPR00961/206529322',
+        'http://www.homedepot.com/p/Good-Life-Gear-24-oz-Stainless-Steel-Water-Bottle-in-Pink-SF6013SS-PNK/100627255',
     }
