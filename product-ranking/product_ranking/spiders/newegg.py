c8d61cd4b89cb66b1ca1d484215db7350a82ed28
lifelonglearner127 <life.long.learner127@outlook.com>
2015-12-01 12:55:18 +0200

    parse title, brand, model,image_url,description, in_stock, review, locale

--- product-ranking/product_ranking/spiders/newegg.py
+++ product-ranking/product_ranking/spiders/newegg.py
@@ -57,18 +57,22 @@ class NeweggProductSpider(BaseProductsSpider):
        #      '//script[contains(text(), "productId")]/text()').re(
        #      r"productId: '(\d+)'"))
        #  meta['product_id'] = product_id
-       #
-       #  # Set locale
-       #  product['locale'] = 'en_US'
-       #
-       #  # Parse title
-       #  title = self.parse_title(response)
-       #  cond_set_value(product, 'title', title)
-       #
-       #  # Parse brand
-       #  brand = self.parse_brand(response)
-       #  cond_set_value(product, 'brand', brand)
-       #
+
+        # Set locale
+        product['locale'] = 'en_US'
+
+        # Parse title
+        title = self.parse_title(response)
+        cond_set_value(product, 'title', title)
+
+        # Parse brand
+        brand = self.parse_brand(response)
+        cond_set_value(product, 'brand', brand)
+
+        # Parse model
+        model = self.parse_model(response)
+        cond_set_value(product, 'model', model)
+
        # # Parse price
        #  price = self.parse_price(response)
        #  cond_set_value(product, 'price', price)
@@ -76,41 +80,114 @@ class NeweggProductSpider(BaseProductsSpider):
        #  # # Parse special pricing
        #  # special_pricing = self._parse_special_pricing(response)
        #  # cond_set_value(product, 'special_pricing', special_pricing, conv=bool)
-       #
-       #  # Parse image url
-       #  image_url = self.parse_image_url(response)
-       #  cond_set_value(product, 'image_url', image_url, conv=string.strip)
-       #
-       #  # Parse description
-       #  description = self.parse_description(response)
-       #  cond_set_value(product, 'description', description, conv=string.strip)
-       #
-       #  # # Parse stock status
-       #  # is_out_of_stock = self._parse_stock_status(response)
-       #  # cond_set_value(product, 'is_out_of_stock', is_out_of_stock)
-       #  #
-       #  # # Parse upc
-       #  # upc = self._parse_upc(response)
-       #  # cond_set_value(product, 'upc', upc)
-       #
+
+        # Parse image url
+        image_url = self.parse_image_url(response)
+        cond_set_value(product, 'image_url', image_url)
+
+        # Parse description
+        description = self.parse_description(response)
+        cond_set_value(product, 'description', description)
+
+        # Parse stock status
+        is_out_of_stock = self.parse_stock_status(response)
+        cond_set_value(product, 'is_out_of_stock', is_out_of_stock)
+
        #  # Parse variants
        #  variants = self.parse_variant(response)
        #  cond_set_value(product, 'variants', variants)
-       #
-       #  # Parse buyer reviews
-       #  reqs.append(
-       #      Request(
-       #          url=self.REVIEWS_URL.format(product_id=product_id),
-       #          dont_filter=True,
-       #          callback=self.parse_buyer_reviews,
-       #          meta=meta
-       #      )
-       #  )
-       #
-       #  if reqs:
-       #      return self.send_next_request(reqs, response)
+
+        # Parse review
+        review = self.parse_buyer_review(response)
+        cond_set_value(product, 'buyer_reviews', review)
+
+        # # Parse buyer reviews
+        # reqs.append(
+        #     Request(
+        #         url=self.REVIEWS_URL.format(product_id=product_id),
+        #         dont_filter=True,
+        #         callback=self.parse_buyer_reviews,
+        #         meta=meta
+        #     )
+        # )
+
+        if reqs:
+            return self.send_next_request(reqs, response)
 
         return product
+
+    def parse_buyer_review(self, response):
+        num_of_review = is_empty(response.xpath('//*[@id="linkSumRangeAll"]/span//text()').re(r'\d+'))
+        res = []
+        rating_by_star = {}
+        for i in range(1, 6):
+            rvm = response.xpath('//span[@id="reviewNumber%s"]//text()' % i).extract()
+            if len(rvm) > 0 and (rvm[0]) > 0:
+                res.append([i, (rvm[0])])
+        if len(res) > 0:
+            for item in res:
+                rating_by_star[item[0]] = item[1]
+        if rating_by_star:
+            s = n =0
+            for k, v in rating_by_star.iteritems():
+                s += int(k) * int(v)
+                n += int(v)
+            if n > 0:
+                average_rating = round(s/float(n),2)
+
+        buyer_reviws = {'num_of_reviews': int(num_of_review),
+                        'average_rating': float(average_rating),
+                        'rating_by_star': rating_by_star}
+
+        return buyer_reviws
+
+        return None
+
+    def parse_stock_status(self, response):
+        stock_status = response.xpath('//script[contains(text(), "product_instock")]/text()').re(r"product_instock:\['(\d+)']")
+
+        if stock_status[0] == '1':
+            in_stock = True
+        elif stock_status[0] == '0':
+            in_stock = False
+
+        return in_stock
+
+
+    def parse_description(self, response):
+        description = response.xpath('//div[@class="grpBullet"]').extract()
+        description = [i.strip() for i in description]
+        if description:
+            return description
+
+    def parse_title(self, response):
+        title = is_empty(response.xpath('//h1/span[@itemprop="name"]/text()').extract())
+
+        if title:
+            return title
+
+    def parse_brand(self, response):
+        brand = is_empty(response.xpath('//dl/dt[contains(text(), "Brand")]/following-sibling::*/text()').extract())
+
+        if brand:
+            return brand
+
+    def parse_model(self, response):
+        model = is_empty(response.xpath('//dl/dt[contains(text(), "Model")]/following-sibling::*/text()').extract())
+
+        if model:
+            return model
+
+    def parse_image_url(self, response):
+        image = is_empty(response.xpath('//div[@class="objImages"]/'
+                                        'a[@name="gallery"]/'
+                                        'span[@class="mainSlide"]'
+                                        '/img/@src').extract())
+
+        if image:
+            return image
+
+        return None
 
 
     def send_next_request(self, reqs, response):
