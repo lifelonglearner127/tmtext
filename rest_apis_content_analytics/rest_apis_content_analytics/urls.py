b5bc84d72b366d24a78bcf1d80edfa275d758af4
lifelonglearner127 <life.long.learner127@outlook.com>
2015-12-21 10:01:33 +0000

    walmart api

--- rest_apis_content_analytics/rest_apis_content_analytics/urls.py
+++ rest_apis_content_analytics/rest_apis_content_analytics/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from image_duplication.views import CompareTwoImageViewSet, ClassifyImagesBySimilarity, FindSimilarityInImageList
 from walmart_developer_accounts.views import WalmartAccountViewSet
-from walmart_api.views import InvokeWalmartApiViewSet
+from walmart_api.views import InvokeWalmartApiViewSet, ItemsUpdateByWalmartApiViewSet, CheckFeedStatusByWalmartApiViewSet
 from nutrition_info_images.views import ClassifyTextImagesByNutritionInfoViewSet
 
 router = routers.SimpleRouter()
@@ -12,6 +12,8 @@ router.register(r'findsimilarityinimagelist', FindSimilarityInImageList, 'findsi
 router.register(r'walmartaccounts', WalmartAccountViewSet, 'walmartaccounts')
 router.register(r'classifytextimagesbynutritioninfo', ClassifyTextImagesByNutritionInfoViewSet, 'classifytextimagesbynutritioninfo')
 router.register(r'invokewalmartapi', InvokeWalmartApiViewSet, 'invokewalmartapi')
+router.register(r'items_update_by_walmart_api', ItemsUpdateByWalmartApiViewSet, 'items_update_by_walmart_api')
+router.register(r'check_feed_status_by_walmart_api', CheckFeedStatusByWalmartApiViewSet, 'check_feed_status_by_walmart_api')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- rest_apis_content_analytics/walmart_api/serializers.py
+++ rest_apis_content_analytics/walmart_api/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 
 
-class WalmartApiRequestJsonSerializer(serializers.Serializer):
+class WalmartApiItemsRequestJsonSerializer(serializers.Serializer):
     request_url = serializers.ChoiceField(
         choices=["https://marketplace.walmartapis.com/v2/feeds?feedType=item"])
     request_method = serializers.ChoiceField(
         choices=["POST"])
-#    xml_file_content = serializers.CharField(style={'base_template': 'textarea.html'})
     xml_file_to_upload = serializers.FileField()
+
+class WalmartApiFeedRequestJsonSerializer(serializers.Serializer):
+    request_url = serializers.ChoiceField(
+        choices=["https://marketplace.walmartapis.com/v2/feeds/{feedId}?includeDetails=true"])
+    feed_id = serializers.CharField()
