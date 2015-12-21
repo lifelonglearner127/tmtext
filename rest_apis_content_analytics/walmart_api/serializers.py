febbc706be7abe722f3e76cfe860f4589a955131
lifelonglearner127 <life.long.learner127@outlook.com>
2015-12-21 09:31:21 +0000

    walmart api

--- rest_apis_content_analytics/walmart_api/serializers.py
+++ rest_apis_content_analytics/walmart_api/serializers.py
@@ -2,14 +2,9 @@ from rest_framework import serializers
 
 
 class WalmartApiRequestJsonSerializer(serializers.Serializer):
-#    consumer_id = serializers.CharField(default="a8bab1e0-c18c-4be7-b47a-0411153b7514")
-#    private_key = serializers.CharField(default="MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBALBpb58mZiP4VHwx6R7fbPd/u7T22eE7vxECjsS6QHulIN/DExLSFIUrKzHgM11hm7ElRh35cKLcBR/XXZw6u/FTzFQbCiRmuDnJyz53PZi/YjhGTD2GY7jIqVILBz30J/3HRnPx9V0nBnWEEeKBeqb3rYJqsr8k1r71Sy45xey9AgMBAAECgYBFDA+PWCU0QPc4YQSge8yXlpwueUvQF2VyT/D3WPryKjCSxDSL8kPr13ihneIc055vmGo4QzBt3fX3f4D5LBfw9YFH0u29At2p9AH4FiyejhEeQ2tWNR4+zOUiMFVxDyM03zlKAsoJcRz1USklr0J1NtRnPBY7RslXU+wnps4RVQJBAPgzIV5Uo8uT4WYrbxc+Yu4Yd8imFqvGuhZpZdeS1EsRObseZc++v360k5Dx/rJzzJqd4JmeQjMJ2Y76V62jcQsCQQC19L4WYn0EYrPuGWMMoswKmOGHlU8eg3JVooZ/ufrvb6YNjVTDHMFLhU5Netw1s2eMo927giLQXF5+7ANg5MZXAkAqBrZau6g0e2jKHQalf+nOeRQnRIBIO9EcpGIbO4B46YTF+2Kv55OTR85I18ERxGvbrmnueQ6qh7tv61HXU/p7AkAqxePBg1l8JG/DsvgTyllIzHOH2dOFisTf2Jrhf6i7jHVujiC01RejVyz3DcCiZxAagZLoN0lTzcLw9y48Ist1AkEA28Opbyzq6BZOZMvXAQ/HEOGW4CVZZ9rHOwp6JByAIHxQcvwQ++TU/118qdA1HriZTZxGE1dZwnDEa2I79ICfrg==")
-    name = serializers.CharField()
-    env = serializers.CharField()
-    version = serializers.CharField()
-    correlation_id = serializers.CharField()
-    signature = serializers.CharField()
-    timestamp = serializers.IntegerField()
-    upload_file_url = serializers.CharField()
-
-
+    request_url = serializers.ChoiceField(
+        choices=["https://marketplace.walmartapis.com/v2/feeds?feedType=item"])
+    request_method = serializers.ChoiceField(
+        choices=["POST"])
+#    xml_file_content = serializers.CharField(style={'base_template': 'textarea.html'})
+    xml_file_to_upload = serializers.FileField()
