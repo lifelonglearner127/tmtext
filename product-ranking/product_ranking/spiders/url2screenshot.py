fd11669c46479469f2d8710aec9e84d87d8bfc83
lifelonglearner127 <life.long.learner127@outlook.com>
2015-12-22 21:53:43 +0300

    Import inside scrapy method does not work - fix

--- product-ranking/product_ranking/spiders/url2screenshot.py
+++ product-ranking/product_ranking/spiders/url2screenshot.py
@@ -13,6 +13,10 @@ from scrapy.conf import settings
 from scrapy.http import Request, FormRequest
 from scrapy.log import INFO, WARNING, ERROR, DEBUG
 import lxml.html
+try:
+    from pyvirtualdisplay import Display
+except ImportError:
+    print('pyvirtualdisplay not installed')
 
 try:
     import requesocks as requests
@@ -90,7 +94,6 @@ class URL2ScreenshotSpider(scrapy.Spider):
 
         from selenium import webdriver
         from selenium.webdriver import FirefoxProfile
-        from pyvirtualdisplay import Display
 
         # temporary file for the output image
         t_file = tempfile.NamedTemporaryFile(delete=False, suffix='.png')
