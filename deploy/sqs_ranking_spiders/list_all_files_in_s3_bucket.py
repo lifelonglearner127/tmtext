0ed0e72a38abe915cf59596dfc1c68227e258c7c
lifelonglearner127 <life.long.learner127@outlook.com>
2015-12-29 17:25:48 +0200

    Calculate total bucket size

--- deploy/sqs_ranking_spiders/list_all_files_in_s3_bucket.py
+++ deploy/sqs_ranking_spiders/list_all_files_in_s3_bucket.py
@@ -28,8 +28,24 @@ def list_files_in_bucket(access_key, secret_key, bucket_name):
     return bucket.list()
 
 
+def get_bucket_size(access_key, secret_key, bucket_name):
+    conn = S3Connection(access_key, secret_key)
+    bucket = conn.get_bucket(bucket_name)
+    total_bytes = 0
+    for key in bucket.list():
+        total_bytes += key.size
+    return total_bytes
+
+
 if __name__ == '__main__':
-    for f in list_files_in_bucket(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_BUCKET_NAME):
-        print f
+    if 'size' in sys.argv:
+        print 'Calculating bucket size, wait...'
+        print 'Total bucket size (in bytes): %s' % get_bucket_size(
+            AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_BUCKET_NAME)
+    elif 'list' in sys.argv:
+        for f in list_files_in_bucket(
+                AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_BUCKET_NAME):
+            print f
 #        if 'job_output' in f.name: 
 #            f.get_contents_to_filename(os.path.basename(f.name))
+
