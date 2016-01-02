00f4540023ae04b94a843aedab85f9ed7fc6a5cf
lifelonglearner127 <life.long.learner127@outlook.com>
2016-01-02 19:42:29 +0200

    Remove old DB records

---
+++ product-ranking/sqs_tests_gui/gui/management/commands/remove_old_raw_cache_db_records.py
@@ -0,0 +1,34 @@
+import os
+import time
+import datetime
+import sys
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Q
+from django.utils.timezone import now
+
+CWD = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(CWD, '..', '..', '..', '..', 'product_ranking'))
+
+from cache_models import session, Run
+
+
+def remove_db_records(max_days=14):
+    """ Get cache data, spider -> date -> searchterm
+    supports strings for spider (as name) and for terms(as term)
+    :return: dict
+    """
+    global session
+    query = session.query(Run)
+    query = query.filter(Run.date <= datetime.datetime.now()-datetime.timedelta(days=max_days))
+    for run in query:
+        print('Removing Run[%s]' % run.id)
+        session.query(Run).filter_by(id=run.id).delete()
+    session.commit()
+
+
+class Command(BaseCommand):
+    help = 'Removes old Raw Cache DB records (older than 14 days)'
+
+    def handle(self, *args, **options):
+        print remove_db_records()
