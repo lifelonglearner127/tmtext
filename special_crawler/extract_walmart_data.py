05a4554354c9b6f968803642ce30ce89aff55752
lifelonglearner127 <life.long.learner127@outlook.com>
2016-01-19 03:08:53 +0100

    Fix image urls

--- special_crawler/extract_walmart_data.py
+++ special_crawler/extract_walmart_data.py
@@ -1970,7 +1970,7 @@ class WalmartScraper(Scraper):
         self.extracted_image_urls = True
 
         if self._version() == "Walmart v1":
-            self.image_urls = list(set(self._image_urls_old()))
+            self.image_urls = self.remove_duplication_keeping_order_in_list(self._image_urls_old())
             return self.image_urls
 
         if self._version() == "Walmart v2":
