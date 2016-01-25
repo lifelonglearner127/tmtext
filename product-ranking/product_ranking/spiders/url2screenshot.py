2fc2020994772e092a23f37fad3ed7d7d135e0db
lifelonglearner127 <life.long.learner127@outlook.com>
2016-01-25 20:04:45 +0200

    Fix clicks on class

--- product-ranking/product_ranking/spiders/url2screenshot.py
+++ product-ranking/product_ranking/spiders/url2screenshot.py
@@ -113,8 +113,12 @@ class URL2ScreenshotSpider(scrapy.Spider):
         try:
             driver.execute_script(script)
         except Exception as e:
+            self.log('Error on clicking (JS) element with class %s: %s' % (cls, str(e)))
+        try:
+            for element in driver.find_elements_by_class_name(cls):
+                element.click()
+        except Exception as e:
             self.log('Error on clicking element with class %s: %s' % (cls, str(e)))
-
 
     def _click_on_element_with_id(self, driver, _id):
         script = """
@@ -226,7 +230,7 @@ class URL2ScreenshotSpider(scrapy.Spider):
 
         # check if the page returns code != 200
         if self.code_200_required and str(self.code_200_required).lower() not in ('0', 'false', 'off'):
-            page_code = r_session.get(self.product_url).status_code
+            page_code = r_session.get(self.product_url, verify=False).status_code
             if page_code != 200:
                 self.log('Page returned code %s at %s' % (page_code, self.product_url), ERROR)
                 yield ScreenshotItem()  # return empty item
