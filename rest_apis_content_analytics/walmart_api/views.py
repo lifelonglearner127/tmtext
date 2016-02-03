c29e0aa29f5b2ad633a6d913d345513e497cc38e
lifelonglearner127 <life.long.learner127@outlook.com>
2016-02-03 06:52:17 +0000

    Bug 6025 - Duplicate content detection
    deploy dpulicate content detection api

--- rest_apis_content_analytics/walmart_api/views.py
+++ rest_apis_content_analytics/walmart_api/views.py
@@ -587,8 +587,9 @@ class DetectDuplicateContentViewset(viewsets.ViewSet):
                             seller_block = left_block
                             break
 
-                    seller_name_list = seller_block.xpath(".//li[@class='sr__item']//a/text()")
-                    seller_name_list = [seller for seller in seller_name_list if seller.lower() != "walmart"]
+                    if seller_block:
+                        seller_name_list = seller_block.xpath(".//li[@class='sr__item']//a/text()")
+                        seller_name_list = [seller for seller in seller_name_list if seller.lower() != "walmart"]
 
                     '''
                     if not seller_block or len((" ".join(seller_block.xpath(".//li[@class='sr__item']/a/text()"))).
