ad539cecaa9a46099571723787359eab3930c158
lifelonglearner127 <life.long.learner127@outlook.com>
2016-02-08 02:37:31 +0100

    fix amazon page extract

--- special_crawler/extract_nike_data.py
+++ special_crawler/extract_nike_data.py
@@ -75,6 +75,7 @@ class NikeScraper(Scraper):
             # replace NULL characters
             contents = self._clean_null(contents)
 
+        print contents
         self.page_raw_text = contents
         self.tree_html = html.fromstring(contents)
 
