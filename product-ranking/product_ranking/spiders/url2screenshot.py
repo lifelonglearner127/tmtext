9a02088bfd20ebd71529ff96b11bc56c2783cdb2
lifelonglearner127 <life.long.learner127@outlook.com>
2016-02-18 00:06:42 +0200

    Fix popups for carters.com

--- product-ranking/product_ranking/spiders/url2screenshot.py
+++ product-ranking/product_ranking/spiders/url2screenshot.py
@@ -255,6 +255,7 @@ class URL2ScreenshotSpider(scrapy.Spider):
             self._click_on_element_with_id(driver, 'closeBtn')  # 2 times - first time doesn't work sometimes
             self._click_on_elements_with_class(driver, 'brdialog-close')  # for madewell.com
             self._click_on_element_with_id(driver, 'skipSignup')  # for madewell.com
+            self._click_on_element_with_xpath(driver, '//*[contains(@class, "padiPopupContent")]//*[contains(@class, "padiClose")]')  # for carters.com
             self._click_on_element_with_id(driver, 'oo_no_thanks')  # for levi.com
             self._click_on_element_with_xpath(driver, '//*[contains(@id, "cookiebar")]//button')  # for HM.com
             self._remove_element_with_xpath(driver, '//*[contains(@id, "emailAcqPopupContainer")]')  # for http://bananarepublic.gap.com
