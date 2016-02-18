1e2fada41417f8ca4e8b2b67d53342ef7c6445de
lifelonglearner127 <life.long.learner127@outlook.com>
2016-02-18 19:43:04 +0200

    Login form

--- rest_apis_content_analytics/rest_apis_content_analytics/urls.py
+++ rest_apis_content_analytics/rest_apis_content_analytics/urls.py
@@ -63,5 +63,6 @@ urlpatterns += [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('accounts.urls')),
 ]
 
