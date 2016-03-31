8d27dc0758b288cb6b9a830d907393075217062b
lifelonglearner127 <life.long.learner127@outlook.com>
2016-03-31 16:44:12 +0200

    Fix: description field

--- product-ranking/product_ranking/spiders/costco.py
+++ product-ranking/product_ranking/spiders/costco.py
@@ -111,11 +111,14 @@ class CostcoProductsSpider(BaseProductsSpider):
 
         des = response.xpath('//div[@id="product-tab1"]//text()').extract()
         des = ' '.join(i.strip() for i in des)
-        if des.strip() == '[ProductDetailsESpot_Tab1]':
-            des = response.xpath('//div[@id="product-tab1"]/..//text()').extract()
+        if '[ProductDetailsESpot_Tab1]' in des.strip():
+            des = response.xpath("//div[@id='product-tab1']/*[position()>1]//text()").extract()
             des = ' '.join(i.strip() for i in des)
             if des.strip():
                 prod['description'] = des.strip()
+
+        elif des:
+            prod['description'] = des.strip()
 
         img_url = response.xpath('//img[@itemprop="image"]/@src').extract()
         cond_set(prod, 'image_url', img_url)
