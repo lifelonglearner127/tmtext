4b9756815900f06fc4c4185a592847148b087928
lifelonglearner127 <life.long.learner127@outlook.com>
2016-04-07 01:27:31 +0200

    Fix args

--- product-ranking/sqs_tests_gui/gui/management/commands/create_test_jobs_from_file.py
+++ product-ranking/sqs_tests_gui/gui/management/commands/create_test_jobs_from_file.py
@@ -16,29 +16,32 @@ from gui.models import Job
 def parse_arguments():
     parser = argparse.ArgumentParser(
         description='Load jobs from file.')
-    parser.add_argument('file', help="a file to load jobs from (1 URL per line)")
-    parser.add_argument('spider', help="the name of the spider")
-    parser.add_argument('branch', help="the name of the branch to run the scrapers at")
+
     return parser.parse_args()
 
 
 class Command(BaseCommand):
     help = 'Load jobs from file'
 
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('file', help="a file to load jobs from (1 URL per line)")
+        parser.add_argument('spider', help="the name of the spider")
+        parser.add_argument('branch', help="the name of the branch to run the scrapers at")
+
     def handle(self, *args, **options):
-        cmd_args = parse_arguments()
-        for line in open(cmd_args.file, 'r'):
+        for line in open(options['file'], 'r'):
             line = line.strip()
             if not line:
                 continue
             Job.objects.create(
                 name='jobs from file - spider %s, created at %s' % (
-                    cmd_args.spider,
+                    options['spider'],
                     datetime.datetime.utcnow().strftime("%Y.%m.%d")),
-                spider=cmd_args.spider,
+                spider=options['spider'],
                 product_url=line,
                 quantity=99,
-                task_id=cmd_args.spider + random.randrange(100000, 900000),
+                task_id=options['spider'] + random.randrange(100000, 900000),
                 mode='no cache',
-                branch_name=cmd_args.branch
+                branch_name=options['branch']
             )
