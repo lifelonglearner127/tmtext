0c8ac81565abb5cb7b6201e483fe02907ee6067d
lifelonglearner127 <life.long.learner127@outlook.com>
2016-05-12 16:02:30 -0700

    Revert "remove threading"
    This reverts commit 6f6f959d79f1321f847ac0f2aa383cdafed44f1b.

--- special_crawler/extract_amazon_data.py
+++ special_crawler/extract_amazon_data.py
@@ -20,7 +20,6 @@ sys.path.append(os.path.abspath('../search'))
 import captcha_solver
 import compare_images
 from socket import timeout
-import socket
 import random
 from spiders_shared_code.amazon_variants import AmazonVariants
 import datetime
@@ -86,14 +85,12 @@ class AmazonScraper(Scraper):
 
         for i in range(retries):
             try:
-                socket.setdefaulttimeout(90)
                 self.browser.open(self.store_url, timeout=90)
                 contents = self.browser.open(self.product_page_url, timeout=90).read()
                 break
             except timeout:
                 self.is_timeout = True
                 self.ERROR_RESPONSE["failure_type"] = "Timeout"
-                self.ERROR_RESPONSE['error'] = 'Timeout'
                 return
 
         try:
