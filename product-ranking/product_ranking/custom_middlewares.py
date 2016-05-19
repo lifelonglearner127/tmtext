845381d6439a1a5f736ae195f5a78844f44627b9
lifelonglearner127 <life.long.learner127@outlook.com>
2016-05-19 17:32:16 +0200

    Fix: missing img_url, variants, uniformed price

--- product-ranking/product_ranking/custom_middlewares.py
+++ product-ranking/product_ranking/custom_middlewares.py
@@ -10,6 +10,7 @@ from urlparse import urljoin
 class VerizonMetaRefreshMiddleware(MetaRefreshMiddleware):
     def process_response(self, request, response, spider):
         request.meta['dont_filter'] = True
+        print request.meta.get('redirect_times')
         if 'dont_redirect' in request.meta or request.method == 'HEAD' or \
                 not isinstance(response, HtmlResponse) or request.meta.get('redirect_times') >= 1:
 
@@ -19,7 +20,6 @@ class VerizonMetaRefreshMiddleware(MetaRefreshMiddleware):
         if isinstance(response, HtmlResponse):
             interval, url = get_meta_refresh(response)
             if url and interval < self._maxdelay:
-                print "BBBBBBBBBBBBBBBBBBb"
                 redirected = self._redirect_request_using_get(request, url)
                 redirected.dont_filter = True
                 return self._redirect(redirected, request, spider, 'meta refresh')
