9b0ac23c91f67af9aeb919b7142eaddc14b25da5
lifelonglearner127 <life.long.learner127@outlook.com>
2016-06-18 14:37:58 +0300

    Fixed price and categories extraction

--- product-ranking/product_ranking/spiders/samsclub.py
+++ product-ranking/product_ranking/spiders/samsclub.py
@@ -174,6 +174,11 @@ class SamsclubProductsSpider(BaseProductsSpider):
             if old_price:
                 cond_set_value(product, 'price', Price(price=old_price,
                                                        priceCurrency='USD'))
+                if not price:
+                    price = response.xpath(
+                        ".//div[contains(@class, 'pricingInfo')]//*[@class='lgFont']/li/span[@class='price' or "\
+                        "@class='superscript' and position()>1]/text()").extract()
+                    price = ['.'.join(price)]
                 cond_set_value(product, 'price_with_discount', Price(price=price[0],
                                                                      priceCurrency='USD'))
 
@@ -278,10 +283,11 @@ class SamsclubProductsSpider(BaseProductsSpider):
         # Categories
         categorie_filters = [u'sam\u2019s club']
         # Clean and filter categories names from breadcrumb
+        bc = response.xpath('//*[@id="breadcrumb"]//a/text()').extract()
+        if not bc:
+            bc = response.xpath('//*[@id="breadcrumb"]//a//*[@itemprop="title"]/text()').extract()
         categories = list(filter((lambda x: x.lower() not in categorie_filters),
-                          map((lambda x: x.strip()),
-                              response.xpath(
-                              '//*[@id="breadcrumb"]//a/text()').extract())))
+                          map((lambda x: x.strip()), bc)))
         category = categories[-1] if categories else None
         cond_set_value(product, 'categories', categories)
         cond_set_value(product, 'category', category)
