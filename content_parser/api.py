0ce4cb8ee47cddf16865163e2be5405e35a2c75a
lifelonglearner127 <life.long.learner127@outlook.com>
2016-07-06 23:26:30 +0000

    Added debugging info

--- content_parser/api.py
+++ content_parser/api.py
@@ -9,6 +9,10 @@ MC_API = '/'
 @app.route('/parse', methods = ['GET'])
 def parse():
     url = request.args.get('url')
+
+    if not url:
+	return 'Please specify the url of an xml file to parse<br/><br/>e.g. http://matt-test.contentanalyticsinc.com:8888/parse?url=http://&lt;server&gt;/&lt;path-to-file&gt;/filename.xlsx'
+
     filename = url.split('/')[-1]
 
     content = requests.get(url).content
@@ -19,4 +23,4 @@ def parse():
     return flask.jsonify(response)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', threaded=True, port=8888)
+    app.run('0.0.0.0', debug=True, port=8888)
