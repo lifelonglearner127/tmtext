2f1cd6a605272a5a622fdaff807215028d6b6a0a
lifelonglearner127 <life.long.learner127@outlook.com>
2016-07-20 19:26:48 +0000

    Added json messages

--- content_parser/api.py
+++ content_parser/api.py
@@ -8,12 +8,15 @@ MC_API = '/'
 
 ERR_MESSAGE = 'Please specify the url of an %s file to parse<br/><br/>e.g. http://matt-test.con    tentanalyticsinc.com:8888/parse?url=http://&lt;server&gt;/&lt;path-to-file&gt;/filename.%s'
 
+def _wrap(message):
+    return jsonify({'message' : message})
+
 @app.route('/parse', methods = ['GET'])
 def parse():
     url = request.args.get('url')
 
     if not url:
-        return ERR_MESSAGE, 400
+        return _wrap(ERR_MESSAGE), 400
 
     filename = url.split('/')[-1]
 
@@ -21,15 +24,18 @@ def parse():
 
     if template == 'kwikee':
         if not re.search('\.xlsx?$', filename):
-            return ERR_MESSAGE % (('xls(x)',)*2), 400
+            return _wrap(ERR_MESSAGE % (('xls(x)',)*2)), 400
     else:
         if not re.search('\.xml$', filename):
-            return ERR_MESSAGE % (('xml',)*2), 400
+            return _wrap(ERR_MESSAGE % (('xml',)*2)), 400
 
-    r = requests.get(url)
+    try:
+        r = requests.get(url)
+    except:
+        return _wrap('There was an error connecting to ' + url), 400
 
     if not r.status_code == 200:
-        return 'not found: ' + url, 404
+        return _wrap('Not found: ' + url), 404
 
     content = r.content
 
@@ -41,12 +47,12 @@ def parse():
     try:
         parser.setup_parse(content, MC_API)
     except (ValueError, xml.etree.ElementTree.ParseError) as e:
-        return 'error parsing %s with %s template parser: %s' % (filename, template, e.message), 400
+        return _wrap('Error parsing %s with %s template parser: %s' % (filename, template, e.message)), 400
     except Exception as e:
         print e
-        return 'an error occurred', 400
+        return _wrap('An error occurred'), 400
 
-    return filename + ' is being parsed and the result will be sent to ' + MC_API
+    return _wrap(filename + ' is being parsed and the result will be sent to ' + MC_API)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True, port=8888)
