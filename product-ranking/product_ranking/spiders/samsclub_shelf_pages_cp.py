6e4023643d31c9d9e9fd955a71eb354b5a8ecbc1
lifelonglearner127 <life.long.learner127@outlook.com>
2016-07-23 21:35:43 +0300

    Fix: shelf_name matching

--- product-ranking/product_ranking/spiders/samsclub_shelf_pages_cp.py
+++ product-ranking/product_ranking/spiders/samsclub_shelf_pages_cp.py
@@ -15,6 +15,31 @@ is_empty = lambda x: x[0] if x else None
 class SamsclubCpPagesSpider(SamsclubShelfPagesSpider):
     name = 'samsclub_cp_urls_products'
 
+    def _get_shelf_path_from_firstpage(self, response):
+        shelf_categories = [c.strip() for c in response.xpath('.//ol[@id="breadCrumbs"]/li//a/text()').extract()
+                            if len(c.strip()) > 1]
+
+        shelf_category = response.xpath('//h1[contains(@class, "catLeftTitle")]/text()').extract()
+        shelf_category = shelf_category[0] if shelf_category else None
+        total_matches = self._scrape_total_matches(response)
+        if total_matches:
+            try:
+                # determining final amount of pages to scrape
+                self.num_pages = min(ceil(int(total_matches)/float(self.prods_per_page)),self.num_pages)
+            except BaseException:
+                pass
+        return Request(self._NEXT_SHELF_URL.format(
+            category_id=self._get_category_id(response),
+            offset=0,
+            prods_per_page=self.prods_per_page), meta={
+            'shelf_path': shelf_categories,
+            'shelf_name': shelf_category,
+            'total_matches':total_matches,
+            'search_term': '',
+            'remaining': self.quantity,
+            'club': 4},
+            dont_filter=True)
+
     def _scrape_product_links(self, response):
         if response.url.find('ajaxSearch') > 0:
             shelf_category = response.meta.get('shelf_name')
