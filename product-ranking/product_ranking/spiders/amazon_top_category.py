bfcac4ab0d7ee73d4b9136e84d890ec94faf44ae
lifelonglearner127 <life.long.learner127@outlook.com>
2016-09-13 17:42:08 +0300

    Implemented optional flags for walmart and target matching, fixed asin to upc conversion

--- product-ranking/product_ranking/spiders/amazon_top_category.py
+++ product-ranking/product_ranking/spiders/amazon_top_category.py
@@ -22,12 +22,7 @@ class AmazonBestSellersProductsSpider(AmazonTests, AmazonBaseClass):
     allowed_domains = ["amazon.com", "asintoupc.com", "walmart.com", "target.com", 'http://psp-gps.info']
 
     settings = AmazoncaValidatorSettings
-    ASIN_UPC_URL = "http://asintoupc.com/?__EVENTVALIDATION=%2FwEdAAMFqOzoH1a8XwCofuUjrlmPM0K3g6Ucd7mv%2FKHQcK9QwZ%" \
-                  "2BI%2FHPZ86NEZcbXwR9jxA4x0nggqK%2FxAM5I8ZALxidoKUw7uSVwqO3jL8TFRtYRzQ%3D%3D&__VIEWSTATE=" \
-                  "%2FwEPDwULLTExNDk1NTA3MzUPZBYCZg9kFgICAw9kFgICAw9kFgQCBQ8PFgIeB1Zpc2libGVoZBY" \
-                  "IAgEPDxYCHgRUZXh0ZWRkAgMPDxYCHwFlZGQCBw8PFgIfAWVkZAIJDw8WAh4ISW1hZ2VVcmxlZGQCBw8" \
-                  "PFgIfAWVkZGRL0SMg3%2BJmPP6c%2FkOeHOL0SszKS1HpppYAmGE%2FAgS8rA%3D%3D&__VIEWSTATEGENERATOR" \
-                  "=CA0B0334&ctl00%24MainContent%24btnSearch=Search&ctl00%24MainContent%24txtASIN={}"
+    ASIN_UPC_URL = "http://asintoupc.com"
 
     ASIN_UPC_URL_A = "http://psp-gps.info/index.php?i={}"
 
@@ -35,6 +30,18 @@ class AmazonBestSellersProductsSpider(AmazonTests, AmazonBaseClass):
 
     def __init__(self, *args, **kwargs):
         super(AmazonBestSellersProductsSpider, self).__init__(*args, **kwargs)
+        # Optional flags to match target and walmart respectively, turned off by default
+        self.match_walmart = kwargs.get('match_walmart', None)
+        if self.match_walmart in ('1', True, 'true', 'True'):
+            self.match_walmart = True
+        else:
+            self.match_walmart = False
+
+        self.match_target = kwargs.get('match_target', None)
+        if self.match_target in ('1', True, 'true', 'True'):
+            self.match_target = True
+        else:
+            self.match_target = False
 
         # String from html body that means there's no results ( "no results.", for example)
         self.total_match_not_found_re = 'did not match any products.'
@@ -58,17 +65,20 @@ class AmazonBestSellersProductsSpider(AmazonTests, AmazonBaseClass):
                 yield Request(url)
 
     def parse(self, response):
-        url = response.url + '&pg={}&ajax=1&isAboveTheFold={}'
+        url = response.url + '?pg={}&ajax=1&isAboveTheFold={}'
+        shelf_name = response.xpath('//h1/span[@class="category"]/text()').extract()
+        shelf_name = [s.strip() for s in shelf_name if s.strip()]
+        shelf_path = response.xpath(
+                    '//li[@class="zg_browseUp"]/a/text()').extract()[1:] + response.xpath(
+                    '//span[@class="zg_selected"]/text()').extract()
+        shelf_path = [s.strip() for s in shelf_path if s.strip()]
         for page in range(1, 6):
             for position in [1, 0]:
                 request = Request(url=url.format(page, position),
                                   callback=self._scrape_product_links,
                                   dont_filter=True)
-                request.meta['shelf_name'] = response.xpath(
-                    '//h1/span[@class="category"]/text()').extract()
-                request.meta['shelf_path'] = response.xpath(
-                    '//li[@class="zg_browseUp"]/a/text()').extract()[1:] + response.xpath(
-                    '//span[@class="zg_selected"]/text()').extract()
+                request.meta['shelf_name'] = shelf_name
+                request.meta['shelf_path'] = shelf_path
                 yield request
 
     def _scrape_product_links(self, response):
@@ -97,13 +107,16 @@ class AmazonBestSellersProductsSpider(AmazonTests, AmazonBaseClass):
         cond_set_value(product, 'url', response.url)
         cond_set_value(product, 'ranking', response.meta.get('ranking'))
 
-        req = Request(url='http://asintoupc.com', callback=self.get_payload, dont_filter=True)
+        if self.match_target or self.match_walmart:
+            req = Request(url='http://asintoupc.com', callback=self.get_payload, dont_filter=True)
+            req.meta['product'] = product
+            yield req
+        else:
+            yield product
+
         # req = Request(url=self.ASIN_UPC_URL.format(asin), callback=self.threadsafe_ASIN2UPC, dont_filter=True)
         # req = Request(url=self.ASIN_UPC_URL_A.format(product.get('asin')), callback=self.ASIN2UPC_alternative,
         #               dont_filter=True)
-
-        req.meta['product'] = product
-        yield req
 
     def ASIN2UPC_alternative(self, response):
         product = response.meta.get('product')
@@ -116,17 +129,24 @@ class AmazonBestSellersProductsSpider(AmazonTests, AmazonBaseClass):
             if upc:
                 self.log("Got UPC: {}".format(upc), level=INFO)
                 cond_set_value(product, 'upc', upc)
-                req = Request('http://www.walmart.com/search/?query={}'.format(upc),
-                              callback=self._match_walmart_threadsafe)
-                req.meta['product'] = product
-                yield req
+                if self.match_walmart:
+                    req = Request('http://www.walmart.com/search/?query={}'.format(upc),
+                                  callback=self._match_walmart_threadsafe)
+                    req.meta['product'] = product
+                    yield req
+                elif self.match_target:
+                    target_url = 'http://tws.target.com/searchservice/item/search_results/v2/by_keyword?search_term={}&alt=json&' \
+                                 'pageCount=24&response_group=Items&zone=mobile&offset=0'
+                    req = Request(target_url.format(upc), callback=self._match_target_threadsafe)
+                    req.meta['product'] = product
+                    yield req
             else:
                 self.log("No UPC for ASIN {} at {}".format(product.get('asin'), response.url), level=INFO)
                 yield product
 
     def get_payload(self, response):
         product = response.meta.get('product')
-        if response.status == 429:
+        if response.status == 429 or response.status == 500:
             req = Request(url=self.ASIN_UPC_URL_A.format(product.get('asin')),
                           callback=self.ASIN2UPC_alternative, dont_filter=True)
             req.meta['product'] = product
@@ -149,7 +169,7 @@ class AmazonBestSellersProductsSpider(AmazonTests, AmazonBaseClass):
 
     def threadsafe_ASIN2UPC(self, response):
         product = response.meta.get('product')
-        if response.status == 429:
+        if response.status == 429 or response.status == 500:
             req = Request(url=self.ASIN_UPC_URL_A.format(product.get('asin')),
                           callback=self.ASIN2UPC_alternative, dont_filter=True)
             req.meta['product'] = product
@@ -168,10 +188,17 @@ class AmazonBestSellersProductsSpider(AmazonTests, AmazonBaseClass):
                 if upc:
                     self.log("Got UPC: {}".format(upc), level=INFO)
                     cond_set_value(product, 'upc', upc)
-                    req = Request('http://www.walmart.com/search/?query={}'.format(upc),
-                                  callback=self._match_walmart_threadsafe)
-                    req.meta['product'] = product
-                    yield req
+                    if self.match_walmart:
+                        req = Request('http://www.walmart.com/search/?query={}'.format(upc),
+                                      callback=self._match_walmart_threadsafe)
+                        req.meta['product'] = product
+                        yield req
+                    elif self.match_target:
+                        target_url = 'http://tws.target.com/searchservice/item/search_results/v2/by_keyword?search_term={}&alt=json&' \
+                                     'pageCount=24&response_group=Items&zone=mobile&offset=0'
+                        req = Request(target_url.format(upc), callback=self._match_target_threadsafe)
+                        req.meta['product'] = product
+                        yield req
                 else:
                     self.log("No UPC for ASIN {} at {}".format(product.get('asin'), response.url), level=INFO)
                     yield product
@@ -189,12 +216,15 @@ class AmazonBestSellersProductsSpider(AmazonTests, AmazonBaseClass):
         cond_set_value(product, 'walmart_url', walmart_url)
         cond_set_value(product, 'walmart_category', walmart_category)
         cond_set_value(product, 'walmart_exists', walmart_exists)
-
-        target_url = 'http://tws.target.com/searchservice/item/search_results/v2/by_keyword?search_term={}&alt=json&' \
-                     'pageCount=24&response_group=Items&zone=mobile&offset=0'
-        req = Request(target_url.format(upc), callback=self._match_target_threadsafe)
-        req.meta['product'] = product
-        yield req
+        # This is for case when both flags are true
+        if self.match_target:
+            target_url = 'http://tws.target.com/searchservice/item/search_results/v2/by_keyword?search_term={}&alt=json&' \
+                         'pageCount=24&response_group=Items&zone=mobile&offset=0'
+            req = Request(target_url.format(upc), callback=self._match_target_threadsafe)
+            req.meta['product'] = product
+            yield req
+        else:
+            yield product
 
     def _match_target_threadsafe(self, response):
         product = response.meta.get('product')
