a76ec4a0c61100295cdb6322a8cb0585beea0a3c
lifelonglearner127 <life.long.learner127@outlook.com>
2016-09-21 04:30:15 +0300

    Fixed next reviews link issue

--- product-ranking/product_ranking/amazon_base_class.py
+++ product-ranking/product_ranking/amazon_base_class.py
@@ -1173,6 +1173,7 @@ class AmazonBaseClass(BaseProductsSpider):
                 ' or contains(text(), "See the customer review")' \
                 ' or contains(text(), "See both customer reviews")]/@href'
             ).extract())
+            buyer_rev_link = urlparse.urljoin(response.url, buyer_rev_link)
             # Amazon started to display broken (404) link - fix
             if buyer_rev_link:
                 buyer_rev_link = re.search(r'.*product-reviews/[a-zA-Z0-9]+/',
