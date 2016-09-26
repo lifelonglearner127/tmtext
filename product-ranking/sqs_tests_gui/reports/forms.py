cf842f57d599bf5ad0cbd496a16753c8fb131b94
lifelonglearner127 <life.long.learner127@outlook.com>
2016-09-26 23:38:34 +0200

    Add date picker

--- product-ranking/sqs_tests_gui/reports/forms.py
+++ product-ranking/sqs_tests_gui/reports/forms.py
@@ -4,4 +4,7 @@ from django import forms
 
 
 class ReportDateForm(forms.Form):
-    date = forms.DateField(initial=datetime.datetime.today)
+    date = forms.DateField(
+        initial=datetime.datetime.today,
+        widget=forms.TextInput(attrs={'class': 'datepicker'})
+    )
