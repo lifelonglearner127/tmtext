690f3254a6e73573e567ee544866e00a7f40ba26
lifelonglearner127 <life.long.learner127@outlook.com>
2016-10-15 17:26:56 +0300

    Fix: color matching on cart page

--- product-ranking/product_ranking/spiders/amazon_checkout.py
+++ product-ranking/product_ranking/spiders/amazon_checkout.py
@@ -21,31 +21,6 @@ class AmazonSpider(BaseCheckoutSpider):
     def start_requests(self):
         yield scrapy.Request('http://www.amazon.com/')
 
-    def _parse_item(self, product):
-        item = CheckoutProductItem()
-        name = self._get_item_name(product)
-        item['name'] = name.strip() if name else name
-        item['id'] = self._get_item_id(product)
-        price = self._get_subtotal()
-        item['price_on_page'] = self._get_item_price_on_page(product)
-        color = self.current_color
-        quantity = self._get_item_quantity(product)
-
-        if quantity and price:
-            quantity = int(quantity)
-            item['price'] = round(float(price) / quantity, 2)
-            item['quantity'] = quantity
-            item['requested_color'] = self.requested_color
-            item['requested_quantity_not_available'] = quantity != self.current_quantity
-
-        if color:
-            item['color'] = color
-
-        item['requested_color_not_available'] = (
-            color and self.requested_color and
-            (self.requested_color.lower() != color.lower()))
-        return item
-
     def _get_colors_names(self):
         time.sleep(4)
         pattern = re.compile("\"color_name\":\[(.+?)\]")
@@ -82,13 +57,6 @@ class AmazonSpider(BaseCheckoutSpider):
                                     'and contains(translate(text(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "{color}")]'.format(color=color.lower())
         else:
             default_attr_xpath = '//*[contains(@id, "color_name_") and @class="swatchSelect"]'
-            try:
-                self.current_color = self._find_by_xpath(
-                    '//*[contains(@id, "color_name_") and contains(@class, "swatchSelect")]//img')[0].get_attribute('alt')
-            except IndexError:
-                pass
-        if len(self.available_colors) == 1:
-            self.current_color = self.available_colors[0]
 
         self.select_attribute(default_attr_xpath,
                               avail_attr_xpath,
@@ -188,9 +156,11 @@ class AmazonSpider(BaseCheckoutSpider):
         ).re('\$(.*)')))
 
     def _get_item_color(self, item):
-        return is_empty(item.xpath(
-                        '*//span[@class="size" and contains(text(),"color:")]'
-                        '/strong/text()').extract())
+        name = self._get_item_name(item).lower()
+        for color in self.available_colors:
+            if color.lower() in name:
+                return color
+        return None
 
     def _get_item_quantity(self, item):
         return is_empty(item.xpath(
