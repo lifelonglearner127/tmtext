6bf1e8001aeaf7bfeb21b7268232c6710e25d454
lifelonglearner127 <life.long.learner127@outlook.com>
2016-11-09 22:27:01 +0200

    fix 12498: Amazon to Walmart Content Converter

--- master_category/api_convert/urls.py
+++ master_category/api_convert/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^export/google-manufacturer/$', views.GoogleManufacturerView.as_view(),
         name='google-manufacturer'),
+    url(r'^amazon-to-walmart/$', views.AmazonToWalmartView.as_view(),
+        name='amazone-to-walmart'),
 ]
--- master_category/api_convert/views.py
+++ master_category/api_convert/views.py
@@ -2,9 +2,19 @@ from django.views.generic import View, TemplateView
 from django.http import HttpResponse
 from django.template import loader
 
-import csv
 from django.conf import settings
+from django.utils.encoding import smart_str
+
 from lxml import html, etree
+
+import csv
+import xlrd
+from openpyxl import Workbook
+from openpyxl.compat import range
+from openpyxl.utils import get_column_letter
+
+import uuid
+
 
 class GoogleManufacturerView(TemplateView):
     template_name = 'GoogleManufacturer.html'
@@ -20,7 +30,9 @@ class GoogleManufacturerView(TemplateView):
             reader = csv.reader(csvfile)
             for item in reader:
                 tree_description = html.fromstring(item[9])
-                tree_bullets = tree_description.xpath("//*[contains(@id,'feature-bullets')]//ul/li[not(contains(@class,'hidden'))]")
+                tree_bullets = \
+                    tree_description.xpath("//*[contains(@id,'feature-bullets')]//ul/"
+                                           "li[not(contains(@class,'hidden'))]")
                 bullet_points = []
                 for bullet in tree_bullets:
                     bullet_points.append(bullet.text_content())
@@ -39,5 +51,94 @@ class GoogleManufacturerView(TemplateView):
 
         response = HttpResponse(template.render(context, request), content_type='text/xml')
         response['Content-Disposition'] = 'attachment; filename=google-manufacturer.xml'
+        return response
+
+
+class AmazonToWalmartView(View):
+    def get(self, request, *args, **kwargs):
+        template = loader.get_template('walmart_long_description.html')
+        def create_walmart_description(bullet1, bullet2, bullet3, bullet4, bullet5):
+            context = {
+                "bullet1": bullet1 if bullet1 != "" else None,
+                "bullet2": bullet2 if bullet2 != "" else None,
+                "bullet3": bullet3 if bullet3 != "" else None,
+                "bullet4": bullet4 if bullet4 != "" else None,
+                "bullet5": bullet5 if bullet5 != "" else None,
+            }
+            return template.render(context, request)
+
+        filename = settings.BASE_DIR + \
+                   settings.MEDIA_URL + \
+                   'amazon_to_walmart/Amazon-HPC-Template-Export-2016-10-25(22-26-50).xls'
+
+        wb = xlrd.open_workbook(filename = filename)
+        item_sheet = wb.sheet_by_name('Item_Sheet')
+
+        items = []
+        # Read data from amazone form
+        for idx, row in enumerate(item_sheet.get_rows()):
+            if idx > 3:
+                data = {
+                    "tool_id": row[4].value,
+                    "title": row[6].value,
+                    "short_desc": row[30].value,
+                    "long_desc": create_walmart_description(
+                        row[31].value, row[32].value, row[33].value, row[34].value, row[35].value),
+                    "ingredients": row[37].value,
+                    "directions": row[38].value,
+                    "warnings": row[38].value,
+                }
+                if row[16].value == "UPC":
+                    data["upc"] = row[17].value
+                    data["gstn"] = ""
+                elif row[16].value == "GSTN":
+                    data["upc"] = ""
+                    data["gstn"] = row[17].value
+                else:
+                    data["upc"] = ""
+                    data["gstn"] = ""
+
+                items.append(data)
+
+        wb = Workbook()
+        dest_filename = 'empty_book.xlsx'
+        ws1 = wb.active
+        ws1.title = "Sheet1"
+
+        # Create header
+        ws1.append(["GTIN-14",
+                    "UPC",
+                    "Tool ID",
+                    "Product Name",
+                    "Long Description",
+                    "Shelf Description (Optional)",
+                    "Short Description",
+                    "Usage Directions (optional)",
+                    "Ingredients (optional)",
+                    "Caution Warnings Allergens (optional)"])
+        # write product info
+        for item in items:
+            ws1.append([item["gstn"],
+                        item["upc"],
+                        item["tool_id"],
+                        item["title"],
+                        item["long_desc"],
+                        "",
+                        item["short_desc"],
+                        item["ingredients"],
+                        item["directions"],
+                        item["warnings"],])
+
+        temp_filename = filename = settings.BASE_DIR + \
+                   settings.MEDIA_URL + \
+                   "tmp/" + str(uuid.uuid4()) + ".xlsx"
+        wb.save(filename = temp_filename)
+
+        fp = open(temp_filename, 'rb')
+        response = HttpResponse(fp.read())
+        fp.close()
+        response['Content-Type'] = 'application/vnd.ms-excel'
+        # response['Content-Type'] = 'application/force-download'
+        response['Content-Disposition'] = 'attachment; filename=WalmartBulkContentUpload.xlsx'
         return response
 
