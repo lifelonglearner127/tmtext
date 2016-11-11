df8cfb297ae2cafd3aab98e2d75f3899897f8b5f
lifelonglearner127 <life.long.learner127@outlook.com>
2016-11-11 14:48:35 +0100

    New petsmart.com shelf scraper completed

--- product-ranking/product_ranking/spiders/petsmart_shelf_pages.py
+++ product-ranking/product_ranking/spiders/petsmart_shelf_pages.py
@@ -1,99 +1,71 @@
-import re
-import string
-import urllib
+from __future__ import division, absolute_import, unicode_literals
+from future_builtins import *
 
+import re
 from scrapy import Request
 
-from product_ranking.items import SiteProductItem
-from petsmart import PetsmartProductsSpider
+from product_ranking.br_bazaarvoice_api_script import BuyerReviewsBazaarApi
+from product_ranking.items import SiteProductItem, Price, BuyerReviews
+from product_ranking.spiders import BaseProductsSpider, cond_set, cond_set_value
+from product_ranking.spiders.petsmart import PetsmartProductsSpider
 
 
-
-class PetsmartShelfPagesSpider(PetsmartProductsSpider):
-    name = 'petsmart_shelf_urls_products'
-    allowed_domains = ['petsmart.com']
-
-
-    def _setup_class_compatibility(self):
-        """ Needed to maintain compatibility with the SC spiders baseclass """
-        self.quantity = 99999
-        self.site_name = self.allowed_domains[0]
-        self.user_agent_key = None
-        self.current_page = 1
-
-
-    def _setup_meta_compatibility(self):
-        """ Needed to prepare first request.meta vars to use """
-        return {'remaining': 99999, 'search_term': ''}.copy()
-
+class PetsmartShelfUrlsSpider(PetsmartProductsSpider):
+    name = "petsmart_shelf_urls_products"
+    ROOT_URL = 'www.petsmart.com'
 
     def __init__(self, *args, **kwargs):
-        super(PetsmartShelfPagesSpider, self).__init__(*args, **kwargs)
-        self._setup_class_compatibility()
-        self.product_url = kwargs['product_url']
+        super(PetsmartShelfUrlsSpider, self).__init__(*args, **kwargs)
         if "num_pages" in kwargs:
             self.num_pages = int(kwargs['num_pages'])
         else:
             self.num_pages = 1
-        self.user_agent = "Mozilla/5.0 (X11; Linux i686 (x86_64))" \
-                          " AppleWebKit/537.36 (KHTML, like Gecko)" \
-                          " Chrome/37.0.2062.120 Safari/537.36"
-        # variants are switched off by default, see Bugzilla 3982#c11
-        self.scrape_variants_with_extra_requests = False
-        if 'scrape_variants_with_extra_requests' in kwargs:
-            scrape_variants_with_extra_requests = kwargs['scrape_variants_with_extra_requests']
-            if scrape_variants_with_extra_requests in (1, '1', 'true', 'True', True):
-                self.scrape_variants_with_extra_requests = True
-
-
-    @staticmethod
-    def valid_url(url):
-        if not re.match(r'http(s)?:\/\/', url):
-            url = 'http://' + url
-        return url
-
+        self.current_page = 1
 
     def start_requests(self):
-        yield Request(url=self.valid_url(self.product_url),
-                      meta=self._setup_meta_compatibility())
-
+        yield Request(self.product_url, meta={'remaining': self.quantity, "search_term": ''})
 
     def _scrape_product_links(self, response):
-        categories = response.xpath(
-            '//li[contains(@class,"ws-breadcrumbs-list-item")]'
-        ).xpath('string()').extract()
-        shelf_categories = map(string.strip, categories[1:]) if categories else None
-        shelf_category = categories[-1] if categories else None
-
-        for link in response.xpath(self.XPATH['search']['prod_links']).extract():
-            url = link.split('?')[0].split(';')[0]
+        links = response.xpath(
+            '//ul[contains(@class,"search-result-items")]/li/a/@href'
+        ).extract()
+        if not links:
+            links = response.xpath('//a[@class="name-link"]/@href').extract()
+        if links:
+            for i in range(len(links)):
+                if self.ROOT_URL not in links[i]:
+                    links[i] = 'http://' + self.ROOT_URL + links[i]
+        cats = response.xpath('.//link[@rel="canonical"]/@href').extract()
+        shelf_categories = []
+        shelf_category = ''
+        if cats:
+            shelf_categories = [c.strip() for c in cats[0].split('/') if len(c.strip()) > 1]
+            shelf_category = shelf_categories[-1] if shelf_categories else None
+        for item_url in links:
             item = SiteProductItem()
-            if shelf_categories:
-                item['shelf_name'] = shelf_categories
             if shelf_category:
-                item['shelf_path'] = shelf_category
-            yield url, item
+                item['shelf_name'] = shelf_category
+            if shelf_categories:
+                item['shelf_path'] = shelf_categories
+            yield item_url, item
+
+    def _scrape_results_per_page(self, response):
+        count = response.xpath('//ul[@class="items-per-page-options"]/li[@class="selected"]/text()').extract()
+        count = int(count[0]) if count else None
+        return count
 
     def _scrape_next_results_page_link(self, response):
-        if self.current_page >= self.num_pages:
-            return
-        self.current_page += 1
-        return super(
-            PetsmartShelfPagesSpider, self
-        )._scrape_next_results_page_link(response)
-
-
-    def _total_matches_from_html(self, response):
-        total_matches = response.xpath(
-            '//a[contains(@class, "ws-product-listing-view-all-items")]/text()'
-        ).extract()
-        total_matches = map(
-            lambda _: re.search(r'(\d+)\s+item', _, re.I),
-            total_matches
-        )
-        if total_matches:
-            return int(total_matches[0].group(1))
+        if self.current_page >= int(self.num_pages):
+            return None
         else:
-            return 0
+            self.current_page += 1
+            links = response.xpath(
+                './/li[@class="current-page"]/following-sibling::li[1]/a/@href'
+            ).extract()
+            if links:
+                link = links[0]
+            else:
+                link = None
 
+            return link
 
