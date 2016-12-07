a25ba3ed74b046b104125ccba0cf34f545f1dddc
lifelonglearner127 <life.long.learner127@outlook.com>
2016-12-07 23:17:03 +0000

    Bug 13635 - zulily.com SC Scraper
    initial version

--- product-ranking/product_ranking/spiders/zulily.py
+++ product-ranking/product_ranking/spiders/zulily.py
@@ -5,6 +5,7 @@ import re
 import json
 import time
 from scrapy.utils.response import open_in_browser
+from scrapy.log import DEBUG
 from scrapy import FormRequest, Request, Spider
 from product_ranking.items import SiteProductItem, BuyerReviews, Price
 from product_ranking.spiders import BaseProductsSpider, cond_set, \
@@ -15,18 +16,19 @@ class KrogerProductsSpider(BaseProductsSpider):
     name = "zulily_products"
     allowed_domains = ["zulily.com"]
 
+    LOG_IN_URL = "https://www.zulily.com/auth"
+    SEARCH_URL = 'http://www.petsmart.com/search?SearchTerm={search_term}'
+    IMAGE_URL = 'http://s7d2.scene7.com/is/image/PetSmart/{sku}_Imageset'
     use_proxies = True
-    SEARCH_URL = "https://www.kroger.com/storecatalog/servlet/SearchDisplay?urlRequestType=Base&" \
-                 "storeId={store_id}&searchTerm={search_term}&pageTitle=Search%20results&pageNumber={page}&sort=popularity"
-    LOG_IN_URL = "https://www.kroger.com/user/authenticate"
 
     def __init__(self, login="", password="", *args, **kwargs):
-        super(KrogerProductsSpider, self).__init__(*args, **kwargs)
-        self.login = login
-        self.password = password
+        super(KrogerProductsSpider, self).__init__(
+            site_name=self.allowed_domains[0], *args, **kwargs)
 
     def start_requests(self):
-            body = '{"account":{"email":"arnoldmessi777@gmail.com","password":"apple123","rememberMe":false},"location":""}'
+        body = '{"login": {"username": "arnoldmessi777@gmail.com", "password": "apple123"}, ' \
+               '"redirectUrl": "https://www.zulily.com/"}'
+
         yield Request(self.LOG_IN_URL,
                       method='POST',
                       body=body,
@@ -34,17 +36,12 @@ class KrogerProductsSpider(BaseProductsSpider):
                       headers={'Content-Type': 'application/json;charset=UTF-8'})
 
     def _log_in(self, response):
-        url = "https://www.kroger.com/storecatalog/servlet/Logon?pickupStoreId=" \
-              "{pickup_id}&divisionSwitch=true&catalogId=13551&langId=-1&storeId=" \
-              "{store_id}&URL=https%3A%2F%2Fwww.kroger.com%2Fstorecatalog%2Fservlet%2FTopCategoriesDisplayView".format(store_id=self.store_id, pickup_id=self.pickup_id)
-        yield Request(url,
-                      callback=self._pick_store)
+        prod = SiteProductItem()
+        prod['is_single_result'] = True
+        prod['url'] = self.product_url
+        prod['search_term'] = ''
 
-    def _pick_store(self, response):
-        url = "https://www.kroger.com/storecatalog/servlet/CategoryDisplay?urlRequestType=Base&catalogId=13551&categoryId=104698" \
-              "&pageView=grid&urlLangId=-1&beginIndex=0&langId=-1&top_category=104501&storeId={store_id}".format(store_id=self.store_id)
-        yield Request(url,
-                      callback=self._start_requests)
+        yield Request(self.product_url, callback=self._start_requests, meta={'product': prod})
 
     def _scrape_next_results_page_link(self, response):
         # <a role="button" class="right_arrow " id = "WC_SearchBasedNavigationResults_pagination_link_right_categoryResults" href = 'javascript:dojo.publish("showResultsForPageNumber",[{pageNumber:"2",pageSize:"60", linkId:"WC_SearchBasedNavigationResults_pagination_link_right_categoryResults"}])' title="Show next page"></a>
@@ -55,6 +52,7 @@ class KrogerProductsSpider(BaseProductsSpider):
         else:
             return
         url = re.sub('pageNumber=\d+', 'pageNumber={}'.format(next_page), response.url)
+
     def _start_requests(self, response):
         """Generate Requests from the SEARCH_URL and the search terms."""
         for st in self.searchterms:
@@ -69,104 +67,171 @@ class KrogerProductsSpider(BaseProductsSpider):
             )
 
         if self.product_url:
-            prod = SiteProductItem()
-            prod['is_single_result'] = True
-            prod['url'] = self.product_url
-            prod['search_term'] = ''
-            upc = self._get_upc_from_url(self.product_url)
-            yield Request(self.product_url,
-                          self._get_product_id,
-                          meta={'product': prod,
-                                'upc': upc})
+            yield self.parse_product(response)
 
-    def _scrape_product_links(self, response):
-        links = response.xpath('//div[@class="product_name"]/a/@href').extract()
-        for no, link in enumerate(links):
-            yield link, SiteProductItem()
+    def extract_product_json(self, response):
+        product_json = {"id_json": {}, "event_data": {}, "style_data": {}}
 
-    def _scrape_total_matches(self, response):
-        matches = re.search('searchTabProdCount.innerHTML = (.+?);', response.body)
-        return int(matches.group(1)) if matches else 0
+        try:
+            id_json = response.xpath("//script[@type='application/ld+json']/text()").extract()[0].strip()
+            product_json["id_json"] = json.loads(id_json)
+        except Exception as e:
+            self.log("Parsing issue in id_json.", DEBUG)
 
+        try:
+            event_data = re.findall(r'window.eventData =(.+);\n\twindow.styleData =', response.body_as_unicode())[0]
+            product_json["event_data"] = json.loads(event_data)
+        except Exception as e:
+            self.log("Parsing issue in even_data.", DEBUG)
 
-    def _get_product_id(self, response):
-        # https://www.kroger.com/storecatalog/clicklistbeta/#/productdetails/0007480618639?categoryId=0101400172
-        upc = response.meta.get('upc')
-        url = "https://www.kroger.com/storecatalog/servlet/SearchDisplay?urlRequestType=Base&storeId=" \
-              "{store_id}&searchTerm={upc}".format(store_id=self.store_id, upc=upc)
-        yield Request(url,
-                      callback=self._get_product_page,
-                      meta=response.meta)
+        try:
+            style_data = re.findall(r'window.styleData =(.+);\n', response.body_as_unicode())[0]
+            product_json["style_data"] = json.loads(style_data)
+        except Exception as e:
+            self.log("Parsing issue in style_data.", DEBUG)
 
-    def _get_product_page(self, response):
-        # SearchJS.updateSearchTermHistoryCookieAndRedirect("0007480618639", "https://www.kroger.com/storecatalog/servlet/ProductDisplay?urlRequestType=Base&catalogId=13551&categoryId=&productId=136183&errorViewName=ProductDisplayErrorView&urlLangId=-1&langId=-1&top_category=&parent_category_rn=&storeId=13651")
-        url = re.search('SearchJS.updateSearchTermHistoryCookieAndRedirect\(\"\d+", "(.+?)"', response.body).group(1)
-
-        yield Request(url,
-                      callback=self._parse_single_product,
-                      meta=response.meta)
-
-    def _parse_single_product(self, response):
-        yield self.parse_product(response)
-
-    @staticmethod
-    def _get_upc_from_url(url):
-        pattern = 'productdetails\/(\d+)'
-        found = re.search(pattern, url)
-        return found.group(1) if found else None
-
-    @staticmethod
-    def _parse_title(response):
-        # <h1 role="heading" aria-level="1" class="main_header">Daily's Bahama Mama Pouch</h1>
-        title = response.xpath('//h1[@role="heading"]/text()').extract()
-        return title[0] if title else ''
-
-    @staticmethod
-    def _parse_price(response):
-        # <span id="offerPrice_136183" class="price offer_price" itemprop="price">
-        price = response.xpath('//*[@itemprop="price"]/text()').re(FLOATING_POINT_RGEX)
-        if not price:
-            return None
-        currency = 'USD'
-        return Price(price=price[0], priceCurrency=currency)
-
-    @staticmethod
-    def _parse_upc(response):
-        # <span id="product_SKU_136183" class="sku">SKU:0007480618639</span>
-        upc = response.xpath('//*[@class="sku"]/text()').re('\d+')
-        return upc[0] if upc else None
-
-    @staticmethod
-    def _parse_image_url(response):
-        # <input type="hidden" id="ProductInfoImage_136183" value="https://www.kroger.com/product/images/140494904/large/front/0007480618639"/>
-        image = response.xpath('//*[contains(@id, "ProductInfoImage_")]/@value').extract()
-        return image[0] if image else None
-
-    @staticmethod
-    def _parse_no_longer_available(product_json):
-        return not product_json.get('buyable')
+        return product_json
 
     def parse_product(self, response):
-
         product = response.meta['product']
 
-        image_url = self._parse_image_url(response)
-        cond_set_value(product, 'image_url', image_url)
+        # locale
+        product['locale'] = 'en_US'
 
-        # Parse title
-        title = self._parse_title(response)
-        cond_set_value(product, 'title', title, conv=string.strip)
+        product_json = self.extract_product_json(response)
 
-        # Parse price
-        price = self._parse_price(response)
+        # title
+        title = product_json["id_json"]["name"]
+        cond_set_value(product, 'title', title)
+
+        # categories
+        categories = [category_info["value"] for category_info in product_json["style_data"]["categories"]]
+
+        if categories:
+            cond_set_value(product, 'categories', categories)
+
+        if product.get('categories'):
+            product['category'] = product['categories'][-1]
+
+        # description
+        description = product_json["style_data"]["descriptionHtml"]
+        cond_set_value(product, 'description', description)
+
+        # price
+        price = product_json["style_data"]["price"]
         cond_set_value(product, 'price', price)
 
-        # Parse upc
-        upc = self._parse_upc(response)
-        cond_set_value(product, 'upc', upc)
-
-        # Parse url
-        url = "https://www.kroger.com/storecatalog/clicklistbeta/#/productdetails/{upc}".format(upc=upc)
-        product['url'] = url
+        # variants
 
         return product
+
+        # variants
+        variants = set(response.xpath(self.XPATH['product']['variants']).extract())
+
+        if len(variants) > 1:
+            product['variants'] = []
+            response.meta['product'] = product
+            variant_sku = variants.pop()
+            response.meta['variants'] = variants
+            return Request(
+                response.url.split('?')[0].split(';')[0] + '?var_id=' + variant_sku,
+                meta=response.meta,
+                callback=self._parse_variants,
+                # dont_filter=True
+            )
+        else:
+            product['variants'] = [self._parse_variant_data(response)]
+            return product
+
+    def _parse_variants(self, response):
+        response.meta['product']['variants'].append(
+            self._parse_variant_data(response)
+        )
+        if response.meta.get('variants'):
+            variant_sku = response.meta['variants'].pop()
+            return Request(
+                response.url.split('?')[0] + '?var_id=' + variant_sku,
+                meta=response.meta,
+                callback=self._parse_variants,
+                # dont_filter=True
+            )
+        else:
+            return response.meta['product']
+
+    def _parse_variant_data(self, response):
+        data = {}
+        # id
+        id = response.xpath(self.XPATH['product']['id']).extract()
+        _populate(data, 'id', id, first=True)
+
+        # sku
+        sku = response.xpath(self.XPATH['product']['sku']).extract()
+        _populate(data, 'sku', sku, first=True)
+
+        # image url
+        if data.get('sku'):
+            image_url = [self.IMAGE_URL.format(sku=data['sku'])]
+        else:
+            image_url = [""]
+        _populate(data, 'image_url', image_url, first=True)
+
+        # in stock?
+        stock = response.xpath(self.XPATH['product']['out_of_stock_button']).extract()
+        data['is_out_of_stock'] = True
+        if stock:
+            if re.search('in stock', stock[0], re.IGNORECASE):
+                data['is_out_of_stock'] = False
+            else:
+                data['is_out_of_stock'] = True
+
+        if data['is_out_of_stock']:
+            data['available_online'] = False
+            data['available_store'] = False
+        else:
+            available_online = re.search('this item is not available for in-store pickup', response.body_as_unicode(), re.IGNORECASE)
+            available_store = re.search('your items will be available', response.body_as_unicode(), re.IGNORECASE)
+            data['available_online'] = False
+            data['available_store'] = False
+            data['is_in_store_only'] = False
+            if available_store:
+                data['is_in_store_only'] = True
+                data['available_online'] = False
+                data['available_store'] = True
+            elif available_online:
+                data['available_online'] = True
+                data['available_store'] = False
+                data['is_in_store_only'] = False
+
+        # currency
+        currency = response.xpath(self.XPATH['product']['currency']).extract()
+        _populate(data, 'currency', currency, first=True)
+
+        # price
+        price = response.xpath(self.XPATH['product']['price']).extract()
+        if price:
+            price = price[0].strip(currency[0])
+            data['price'] = float(price)
+
+        # size
+        size = response.xpath(self.XPATH['product']['size']).extract()
+        _populate(data, 'size', size)
+
+        # color
+        color = response.xpath(self.XPATH['product']['color']).extract()
+        _populate(data, 'color', color)
+
+        return data
+
+    def _total_matches_from_html(self, response):
+        total_matches = response.xpath(self.XPATH['search']['total_matches']).re(r'\d+')
+        if total_matches:
+            return int(total_matches[0])
+
+    def _scrape_next_results_page_link(self, response):
+        next_page = response.xpath(self.XPATH['search']['next_page']).extract()
+        if next_page:
+            return next_page[0]
+
+    def _scrape_product_links(self, response):
+        for link in response.xpath(self.XPATH['search']['prod_links']).extract():
+            yield link.split('?')[0].split(';')[0], SiteProductItem()
