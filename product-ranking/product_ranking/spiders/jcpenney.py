d0707ed9caafb58c105c1344d0c507668667506b
lifelonglearner127 <life.long.learner127@outlook.com>
2016-12-14 23:16:15 +0300

    Remove huge loop

--- product-ranking/product_ranking/spiders/jcpenney.py
+++ product-ranking/product_ranking/spiders/jcpenney.py
@@ -467,50 +467,15 @@ class JcpenneyProductsSpider(BaseValidator, BaseProductsSpider):
         cond_set_value(prod, 'locale', 'en-US')
         self._populate_from_html(response, prod)
 
-        jp = JcpenneyVariants()
-        jp.setupSC(response)
-        prod['variants'] = jp._variants()
-        # perform blocking http request to scrape dynamic structure of variants,
-        # otherwise invalid variants get into the output file
-        processed_lots = []  # lots for which we collected dynamic variants
-        new_lot_structure = {}
-        p_l = 0
-        if prod.get('variants'):
-            for var_indx, variant in enumerate(prod['variants']):
-                if getattr(self, 'scrape_variants_with_extra_requests', None):
+        availability_json = self._extract_availability_json(response.body_as_unicode())
+        availability_dict = self._build_availability_dict(availability_json)
+        product_json = self._get_product_json(product_id)
+        properties_dict = self._build_properties_dict(product_json)
+        variants = self._build_variants(product_json, properties_dict, availability_dict, prod.get('price'))
+        if variants:
+            prod['variants'] = variants
 
-                    if variant.get('properties', {}).get('lot', '').lower() in processed_lots:
-                        continue
-                    _lot, _dynamic_structure = self._ajax_variant_request(
-                        product_id, response, prod['variants'], variant, var_indx,
-                        async=False, null_values=['size']
-                    )
-                    if variant.get('properties', {}).get('lot', '').lower():
-                        processed_lots.append(variant.get('properties', {}).get('lot', '').lower())
-                    else:
-                        processed_lots.append(variant.get('lot', '').lower())
 
-                    new_lot_structure[_lot] = _dynamic_structure
-                    if _lot:
-                        self.remove_old_static_variants_of_lot(prod['variants'], _lot)
-                        self.append_new_dynamic_variants(prod, prod['variants'], _lot, _dynamic_structure, self.log)
-            try:
-                processed_lots[1] = processed_lots[0]
-            except:
-                pass
-            for var_indx, variant in enumerate(prod['variants']):
-                if getattr(self, 'scrape_variants_with_extra_requests', None):
-
-                    if processed_lots:
-                        lot_id = processed_lots[p_l]
-                        p_l += 1
-                        if p_l >= len(processed_lots):
-                            p_l = 0
-                    else:
-                        lot_id = ''
-                    yield self._ajax_variant_request(
-                        product_id, response, prod['variants'], variant, var_indx,
-                        Lot=lot_id)
 
         new_meta = response.meta.copy()
         new_meta['product'] = prod
@@ -787,3 +752,72 @@ class JcpenneyProductsSpider(BaseValidator, BaseProductsSpider):
         if next_page:
             next_page = 'http://www.jcpenney.com'+next_page[-1]
             return next_page
+
+    @staticmethod
+    def _get_product_json(product_id):
+        url = "http://m.jcpenney.com/v4/products/{product_id}".format(product_id=product_id)
+        proxy_host = "content.crawlera.com"
+        proxy_port = "8010"
+        CRAWLERA_APIKEY = '0dc1db337be04e8fb52091b812070ccf'
+        proxy_auth = HTTPProxyAuth(CRAWLERA_APIKEY, "")
+        proxies = {"http": "http://{}:{}/".format(proxy_host, proxy_port), \
+                   "https": "https://{}:{}/".format(proxy_host, proxy_port)}
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+            'X-Crawlera-UA': 'pass'}
+        text = requests.get(url, proxies=proxies, timeout=300, auth=proxy_auth, headers=headers,
+                                      verify=False).text
+        return json.loads(text) if text else None
+
+    @staticmethod
+    def _build_properties_dict(product_json):
+        properties = product_json.get('dimensions')
+        properties_dict = {}
+        for property in properties:
+            options = property.get('options')
+            name = property.get('name')
+            for option in options:
+                option_id = option.get('id')
+                value = option.get('value')
+                properties_dict[option_id] = {'name': name, 'value': value}
+        return properties_dict
+
+    @staticmethod
+    def _extract_availability_json(body):
+        pattern = re.compile('var\s*jcpPPJSON\s*=\s*({.+?})\s*;', re.DOTALL)
+        availability_json = pattern.search(body)
+        return json.loads(availability_json.group(1)) if availability_json else None
+
+    @staticmethod
+    def _build_availability_dict(availability_json):
+        availability_dict = {}
+        products = availability_json.get('products')
+        lots = products[0].get('lots') if products else []
+        for lot in lots:
+            skus = lot.get('skus')
+            for sku in skus:
+                sku_id = sku.get('skuID')
+                sku_availability = sku.get('skuStatus') != 'NotAvailable'
+                availability_dict[sku_id] = sku_availability
+        return availability_dict
+
+    @staticmethod
+    def _build_variants(product_json, properties_dict, availability_dict, price):
+        lots = product_json.get('lots')
+        variants = []
+        for lot in lots:
+            items = lot.get('items')
+            for item in items:
+                sku_id = item.get('id')
+                variant = {}
+                variant['price'] = float(price.price)
+                variant['in_stock'] = availability_dict.get(sku_id)
+                variant['properties'] = {'sku': sku_id}
+                options = item.get('options')
+                for option_id in options:
+                    option_data = properties_dict.get(option_id)
+                    option_name = option_data.get('name')
+                    option_value = option_data.get('value')
+                    variant['properties'][option_name] = option_value
+                variants.append(variant)
+        return variants
